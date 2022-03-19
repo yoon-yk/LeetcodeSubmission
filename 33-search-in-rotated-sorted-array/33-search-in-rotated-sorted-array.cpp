e2942fdf43c4240de2675a581a0f1525919aba32
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-03-19 14:01:13 -0400

    Time: 0 ms (100.00%), Space: 11.1 MB (76.05%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -17,7 +17,8 @@ public:
 
         int start = left; // beginning of sorted array
         left = 0; right = nums.size()-1; 
-        if (target <= nums[right] && target >= nums[start]) left = start;
+        if (target == nums[start]) return start;
+        else if (target <= nums[right] && target >= nums[start]) left = start;
         else right = start-1;
         
         while (left <= right) { // left <= right!!!!!!!!
