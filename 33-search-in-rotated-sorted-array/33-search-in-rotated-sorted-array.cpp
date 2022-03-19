c0b0350658f08f62f5e23ebfba23939cd1c2b708
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-03-19 14:12:11 -0400

    Time: 7 ms (45.25%), Space: 11 MB (95.78%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -24,6 +24,8 @@ public:
         while (left <= right) { // left <= right!!!!!!!!
             mid = left + (right-left)/2;
             if (nums[mid] == target) return mid;
+            // else if (nums[mid] < target) left = mid+1;
+            // else right = mid-1;
             else if (nums[mid] < target) left = mid+1;
             else right = mid-1;
         }
