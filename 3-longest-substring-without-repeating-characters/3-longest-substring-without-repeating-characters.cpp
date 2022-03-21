8ab9d973193a2a6b938d02e5c83b102c962dda7a
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-03-20 20:40:29 -0400

    Time: 1114 ms (5.58%), Space: 150.2 MB (5.93%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,18 +3,19 @@ public:
     int lengthOfLongestSubstring(string s) {
         if (s.length()==0) return 0;
         if (s.length()==1) return 1;
-        int left = 0, right = 0, count, maxCount = INT_MIN;;
-
+        int left = 0, right = 0, count, maxCount = INT_MIN;
+        
+        unordered_map <char, int> hashT;
         while ((left <= right) && (right < s.length())) {
-            unordered_map <char, int> hashT;
-            hashT[s[left]] = 1; // 첫번째 글자 해쉬테이블에 넣기
             count = 1; 
+            hashT[s[left]] = 1; // 첫번째 글자 해쉬테이블에 넣기
             while ((right+1 < s.length()) 
                    && (s[right]!=s[right+1]) 
                    && (hashT.find(s[right+1])==hashT.end())) {
                 hashT[s[right+1]] += 1;
                 right++; count++; 
             }
+            hashT.clear();
             left ++;
             right = left;
             maxCount = max(count, maxCount);
