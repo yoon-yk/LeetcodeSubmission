c6cd84e7b84ad9768ca5ff8fd98bb46ea417cc59
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-03-22 11:37:45 -0400

    Time: 268 ms (5.03%), Space: 48.2 MB (6.19%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size()==1) return intervals;
+        
+        sort(intervals.begin(), intervals.end(), 
+             [](vector<int> a, vector<int> b)->bool{
+                 return a[0] < b[0];
+             });
+        
+        for (auto i:intervals)
+            printf("[%d, %d] ", i[0], i[1]);
+        printf("\n");
+        
+        // [1,3], [2,6], [5,9], [8,10]
+        vector<vector<int>> res;
+        int i=0, j=0, start=i, end=j;
+        while (i<=j && (j<intervals.size())) {
+            if ((intervals[j][0] <= intervals[end][1]) && 
+                   (intervals[j][0] >= intervals[end][0])) {
+                end = (intervals[end][1] > intervals[j][1]) ? end : j;
+                j++;
+            }
+            else {
+                res.push_back({intervals[start][0], intervals[end][1]});
+                i=j; start=i; end=start; j++;
+            }
+        }
+        res.push_back({intervals[start][0], intervals[end][1]});
+        return res;
+    }
+};
