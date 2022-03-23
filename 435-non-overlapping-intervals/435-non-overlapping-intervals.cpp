52516ba2a999f19312a96c4d1cc3a4ea5d2ce9ad
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-03-23 10:47:17 -0400

    Time: 416 ms (88.24%), Space: 89.9 MB (47.52%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -3,12 +3,8 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int ret, curEnd = 0, delN = 0;
         sort(intervals.begin(), intervals.end());
-        for (auto i:intervals)
-            printf("[%d,%d],", i[0], i[1]);
-        printf("\n");
         
         for (int i=1; i<intervals.size(); i++) {
-            printf("i : %d, curEnd : %d\n", i, curEnd);
             if (intervals[i][0] < intervals[curEnd][1]) { // e.g. [1,5] [4,7]
                 curEnd = (intervals[curEnd][1] < intervals[i][1])? curEnd : i; 
                 delN++;
