f7561d9035cfd86db473e3ab5aa704c51207bee4
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-04-09 17:18:44 -0400

    Time: 82 ms (76.64%), Space: 27.4 MB (96.33%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
-        int dir[5] = {0, 1, 0, -1, 0};
-        /*
-            r, c+1
-            r+1, c
-            r, c-1
-            r-1, c
-        */        
-        int rowSize = mat.size(), colSize = mat[0].size();
-        typedef pair<int, int> pos;
-        queue<pos> q;
-        
-        for (int row = 0; row < rowSize; row++) {
-            for (int col = 0; col < colSize; col++) {
-                if (mat[row][col] == 0) q.push({row, col});
-                else mat[row][col] = -1;
+        int rowSize = mat.size(); int colSize = mat[0].size();
+        int top, down, right, left;
+        int INF = rowSize + colSize;
+        // left and top
+        for (int r = 0; r < rowSize; r++) {
+            for (int c = 0; c < colSize; c++) {
+                if (mat[r][c] == 0) continue;
+                top = (r-1 >= 0)? mat[r-1][c] : INF;
+                left = (c-1 >= 0)? mat[r][c-1] : INF; 
+                mat[r][c] = min(top, left) + 1;
             }
         }
         
-        while(!q.empty()){
-            pos cur = q.front(); q.pop();
-            for (int i=0; i<4; i++) {
-                int curR = cur.first + dir[i];
-                int curC = cur.second + dir[i+1];
-                if (curR < 0 || curC < 0 || curR >= rowSize || curC >= colSize)
-                    continue;
-                if (mat[curR][curC] != -1)
-                    continue;
-                mat[curR][curC] = mat[cur.first][cur.second] + 1;
-                q.push({curR, curC});
+        // right and down
+        for (int r = rowSize-1; r >= 0; r--) {
+            for (int c = colSize-1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                down = (r+1 < rowSize)? mat[r+1][c] : INF;
+                right = (c+1 < colSize)? mat[r][c+1] : INF; 
+                mat[r][c] = min(mat[r][c], min(down, right)+1);
             }
         }
-        
         return mat;
     }
-    
 };
