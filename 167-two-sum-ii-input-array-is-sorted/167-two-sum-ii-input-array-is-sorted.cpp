d04c8fc8e02958c9d09b05fabcc107b14507f5fe
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-06-17 23:02:01 +0900

    Time: 40 ms (5.76%), Space: 15.9 MB (5.74%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int left = 0, right = numbers.size()-1;
-        while (left < right){
-            if (numbers[left]+numbers[right] > target) right--;
-            else if (numbers[left]+numbers[right] < target) left++;
-            else return {left+1, right+1};
+        unordered_map<int, int> hashM;
+        int cand;
+        
+        for (int i=0; i<numbers.size(); i++) 
+            hashM[numbers[i]] = i+1;
+        
+        for (int i=0; i<numbers.size(); i++) {
+            cand = target - numbers[i];
+            if(hashM.find(cand)!=hashM.end()) 
+                return {i+1, hashM[cand]};
         }
+        
         return {};
     }
 };
