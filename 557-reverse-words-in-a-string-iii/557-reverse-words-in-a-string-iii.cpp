370d7859d7b6cb0964995e0f30f70da11155c98e
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-06-17 23:48:35 +0900

    Time: 34 ms (15.78%), Space: 9.7 MB (75.93%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string res = "";
-        string delimiter = " ";
-        size_t pos = 0;
-        
-        string token;
-        while ((pos = s.find(delimiter)) != string::npos) {
-            token = s.substr(0, pos);
-            reverse(token.begin(), token.begin() + pos);
-            res = res + token + " ";
-            s.erase(0, pos + delimiter.length());
+        int start = 0, end;
+        for (int i=0; i<s.size(); i++){
+            if (s[i]==' ') {
+                end = i;
+                reverse(s.begin()+start, s.begin()+end);
+                start = i+1;
+            }
         }
         
-        reverse(s.begin(), s.end());
-        res = res + s;
-        return res;
+        reverse(s.begin()+start, s.end());
+        
+        return s;
     }
 };
