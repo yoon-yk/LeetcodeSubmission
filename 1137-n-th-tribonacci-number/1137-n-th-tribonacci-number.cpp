0a4ab1ffe506f162f4c48b6d3568ec63a766f056
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-06-21 11:18:29 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (83.15%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,7 +4,7 @@ public:
     int tribonacci(int n) {
         trib[0] = 0, trib[1] = trib[2] = 1;
         for (int i=3; i<=n; ++i) {
-            trib[i%3] = trib[0] + trib[1] + trib[2];
+            trib[i%3] = trib[(i-3)%3] + trib[(i-2)%3] + trib[(i-1)%3];
         }
         return trib[n % 3];
     }
