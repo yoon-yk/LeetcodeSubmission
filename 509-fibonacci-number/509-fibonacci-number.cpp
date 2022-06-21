4530c0cf3a301ef6e3564468fcbe5d8b483376fb
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-06-21 11:23:41 +0900

    Time: 4 ms (52.86%), Space: 6 MB (41.34%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n < 2) return n;
-        return fib(n-1) + fib(n-2);
+        int fibArr[] = {0, 1};
+        for (int i=2; i<=n; i++)
+            fibArr[i%2] = fibArr[0] + fibArr[1];
+        return fibArr[n%2];
     }
 };
