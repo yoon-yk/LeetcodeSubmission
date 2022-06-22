cb353720284678a6e502b08343fd0e8d35f39f30
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-06-22 13:36:50 +0900

    Time: 16 ms (38.76%), Space: 6.8 MB (62.87%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -3,13 +3,17 @@ public:
     string multiply(string num1, string num2) {
         
         if (num1 == "0" || num2 == "0") return "0";
-        vector<int> res(num1.size() + num2.size(), 0);
+        int size = num1.size() + num2.size();
+        vector<int> res(size, 0);
         
-        for (int i=num1.size()-1; i>=0; i--) {
-            for (int j=num2.size()-1; j>=0; j--) {
-                res[i+j+1] += (num1[i]-'0') * (num2[j]-'0');
-                res[i+j] += res[i+j+1] / 10;
-                res[i+j+1] %= 10;
+        int idx;
+        for (int i=num1.size()-1; i>=0; i--){ // first num
+            idx = i + num2.size();
+            for (int j=num2.size()-1; j>=0; j--){ // second num
+                res[idx] += (num1[i]-'0')* (num2[j]-'0');
+                res[idx-1] += res[idx]/10;
+                res[idx] %= 10;
+                idx--;
             }
         }
         
