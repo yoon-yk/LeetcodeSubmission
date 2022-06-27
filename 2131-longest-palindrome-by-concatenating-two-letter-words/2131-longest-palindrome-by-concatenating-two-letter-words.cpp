195e9afd2dafd3eae2a02fce6ad16eed93a71f26
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-06-27 13:47:32 +0900

    Time: 849 ms (19.19%), Space: 168 MB (61.89%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        
+        unordered_map<string, int> M;
+        unordered_map<string, int> pM;
+        
+        string lstSgW = "";
+        int maxLstSgW = 0;
+            
+        // 1. 모두 집어넣기
+        for (auto w : words) {
+            if (w[0]==w[1]){
+                if (pM.find(w)!=pM.end())
+                    pM[w]++;
+                else pM[w] = 1;
+            }
+            else {
+                if (M.find(w)!=M.end())
+                    M[w]++;
+                else M[w] = 1;
+            }
+        }
+        
+        // 2. 짝 찾기
+        int maxLen = 0, curLen = 0, 
+        commMax = 0, maxEven = 0;
+        bool center = false;
+        
+        for (auto w: pM) {
+            if (!center && w.second%2==1) {
+                center = true;
+                maxLen += w.second * 2;
+            } else {
+                curLen = w.second;
+                curLen = (curLen%2==0) ? curLen : curLen-1;
+                maxLen += curLen*2;
+            }
+        }
+        
+        for (auto w : M) {
+            string target = w.first;
+            swap(target[0], target[1]);
+            
+            if (M.find(target) != M.end() 
+                     && M[target] > 0) {
+                
+                commMax = min(M[target], w.second);
+                cout << commMax << endl;
+                maxLen += commMax*4;
+                cout << "maxLen : " << maxLen << endl;
+                M[w.first] = 0;
+                M[target] = 0;
+                // hashM.erase(w.first);
+                // hashM.erase(target);
+            }
+        }
+        
+        return maxLen;
+    }
+};
