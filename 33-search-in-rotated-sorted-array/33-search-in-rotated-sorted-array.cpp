ab59f8eb358aab3b74650afda93c8274c7ec4c0a
Yoon <63157606+suejjangxx@users.noreply.github.com>
2022-06-29 21:29:16 +0900

    Time: 6 ms (55.39%), Space: 11.1 MB (74.85%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.size() == 0 ) return -1;
-        if (nums.size() == 1 ) {
-            if (nums[0] == target) return 0;
-            else return -1;
-        }
-        
-        int mid, left = 0, right = nums.size()-1;
-        while (left < right) {
-            mid = left + (right-left)/2; //*****
-            if (nums[mid] > nums[right])
-                left = mid+1;
-            else right = mid;
-        }
+        int size = nums.size();
+        if (size == 0) return -1;
 
-        int start = left; // beginning of sorted array
-        left = 0; right = nums.size()-1; 
-        if (target == nums[start]) return start;
-        else if (target <= nums[right] && target >= nums[start]) left = start;
-        else right = start-1;
+        // 1. Find the index of the smallest value
+        int start=0, end=size-1, mid;
         
-        while (left <= right) { // left <= right!!!!!!!!
-            mid = left + (right-left)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid+1;
-            else right = mid-1;
+        while (start<end){
+            mid=(start+end)/2;
+            if (nums[mid] > nums[end]) start = mid+1;
+            else end = mid;
+        }
+        
+
+        // 2. 
+        if (nums[start] == target) return start;
+        int rot = start;
+        
+        // 3. 
+        start = 0, end = size-1;
+        int realM; 
+        
+        while (start<=end) {
+            mid = (start+end)/2;
+            realM = (mid+rot)%size;
+            if (nums[realM] == target) return realM;
+            if (nums[realM] < target) start = mid+1;
+            else end = mid-1;
         }
         
         return -1;
