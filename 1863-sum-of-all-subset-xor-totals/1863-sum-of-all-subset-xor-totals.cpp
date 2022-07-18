22cb6f3f74433dcb519546f94b6adb80cd7072a3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-18 20:39:09 +0900

    Time: 127 ms (12.30%), Space: 23.7 MB (19.14%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> subsets;
+        
+    int subsetXORSum(vector<int>& nums) {
+        
+        vector<int> idxV;
+        for (int i=0; i<nums.size(); i++) idxV.push_back(i);
+        
+        backtrack(0, idxV);
+
+        int total = 0;
+    
+        // for (vector<int> next : subsets) {
+        //     int sum = 0;
+        //     for (int n : next)
+        //         cout << n << " ";
+        //     cout << endl;
+        // }
+        
+        for (vector<int> next : subsets) {
+            int sum = 0;
+            for (int n : next)
+                sum ^= nums[n];
+            total += sum;
+        }
+
+        return total;
+    }
+    
+    void backtrack(int idx, vector<int>& nums){
+    
+        if (nums.size() == 0) return;
+        
+        subsets.push_back(nums);
+
+        for (int i=idx; i<nums.size(); i++) {
+            int tmp = nums[i];
+            nums.erase(nums.begin()+i);
+            backtrack(i, nums);
+            nums.insert(nums.begin()+i, tmp);
+        }
+    }
+};
+
