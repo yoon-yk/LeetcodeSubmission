2fcd49d0a7fbd7d2c47134952edac11ef35043bc
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-19 20:24:04 +0900

    Time: 15 ms (7.13%), Space: 13.2 MB (48.96%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -15,27 +15,22 @@ public:
     vector<string> binaryTreePaths(TreeNode* root) {
         
         if (!root) return ans;
+        
         string rootStr = to_string(root->val);
-        if (!root->left && !root->right) {
-            ans.push_back(rootStr);
-        }
-        dfs(root->left, rootStr);
-        dfs(root->right, rootStr);
+
+        dfs(root, rootStr);
         
         return ans;
     }
     
     void dfs(TreeNode* root, string currPath) {
         if (!root) return;
-    
-        currPath += "->" + to_string(root->val);
-        
         if (!root->left && !root->right) {
             ans.push_back(currPath);
         }
         else {
-            dfs(root->left, currPath);  
-            dfs(root->right, currPath);            
+            if(root->left)dfs(root->left, currPath+ "->" + to_string(root->left->val));  
+            if(root->right)dfs(root->right, currPath+ "->" + to_string(root->right->val));            
         }
         
     }
