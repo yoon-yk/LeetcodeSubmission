a21a288dfa33d881d53e9a5f00727a74e7d03aaa
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-20 11:40:04 +0900

    Time: 34 ms (85.51%), Space: 23.3 MB (63.67%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -8,39 +8,14 @@
  * };
  */
 
-
-/*
-  6
- 2 8
-[2,8] ==> 6 
-[2,6] ==> 6
-[6,8] ==> 6
-
-    6
-  2   8
- 0 4 7 9
- [2,9] ==> 6
- [0,8] ==> 6
- [4,7] ==> 6
-
-*/
-
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL) return NULL;
-        
-        if (root->val == p->val || root->val == q->val) return root;
-        if (p->val < root->val && q->val < root->val) {
-            // 왼쪽 root 탐색하기 
+        if (!root) return nullptr;
+        if (p->val < root->val && q->val < root->val) 
             return lowestCommonAncestor(root->left, p, q);
-        }
-        if (p->val > root->val && q->val > root->val) {
-            // 오른쪽 root 탐색하기
+        if (p->val > root->val && q->val > root->val) 
             return lowestCommonAncestor(root->right, p, q);
-        }
-        //if (p->val < root->val && q->val < root->val) {
-            return root;
+        return root;
     }
-    
 };
