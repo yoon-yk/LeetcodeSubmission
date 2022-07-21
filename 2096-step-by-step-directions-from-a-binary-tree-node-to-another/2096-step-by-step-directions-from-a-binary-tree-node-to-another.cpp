856de6a59266433f6b6bfb98e6c11e1c0114d53b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-21 15:49:18 +0900

    Time: 399 ms (21.73%), Space: 113.9 MB (74.15%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -12,52 +12,35 @@
 class Solution {
 public:
     string getDirections(TreeNode* root, int startValue, int destValue) {
-
-        TreeNode* lcaRoot = LCA(root, startValue, destValue);
         
-        string finalSPath = "", finalDPath = "";
-        findPath(lcaRoot, startValue, true, finalSPath);
-        findPath(lcaRoot, destValue, false, finalDPath);
-
-        return finalSPath+finalDPath;
+        string s_path, d_path;
+        findPath(root, startValue, s_path);
+        findPath(root, destValue, d_path);
+        
+        while(!s_path.empty()&&!d_path.empty()
+            &&(s_path.back()==d_path.back())){
+            s_path.pop_back(); d_path.pop_back();
+        }
+        
+        return string(s_path.size(), 'U') + string(rbegin(d_path), rend(d_path)); 
     }
     
-    bool findPath (TreeNode* root, int& target, bool isStart, string& path) {
-        if (!root) return false;
+    bool findPath(TreeNode* root, int& target, string& path) {
         
-        if (root->val == target) return true;
+        bool isFound = false;
         
-        if (isStart){
-            path+="U";
-            if ((root->left && findPath(root->left, target, isStart, path)) ||
-                (root->right && findPath(root->right, target, isStart, path))) return true;
-            path.pop_back();
+        if (!root) return isFound;
+        
+        if (root->val == target) 
+            return isFound = true;
+        
+        if (root->left && (isFound = findPath(root->left, target, path))) {
+            path.push_back('L');
         }
-        else {
-            path+="L";
-            if (root->left && findPath(root->left, target, isStart, path)) return true;
-            path.pop_back();
-            
-            path+="R";
-            if (root->right && findPath(root->right, target, isStart, path)) return true;
-            path.pop_back();
+        else if (root->right && (isFound = findPath(root->right, target, path))) {
+            path.push_back('R');
         }
-        
-        return false;
-
+        return isFound;
     }
-        
-    TreeNode* LCA(TreeNode* root, int& startValue, int& destValue) {
-        if (!root) return root;
-        if (root->val == startValue) return root;
-        if (root->val == destValue) return root;
-        
-        TreeNode* left = LCA(root->left, startValue, destValue);
-        TreeNode* right = LCA(root->right, startValue, destValue);
-
-        if (!left) return right;
-        if (!right) return left;
-        
-        return root;
-    }
+    
 };
