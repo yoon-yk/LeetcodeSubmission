0fd6fe37341fe635dac7ebb5a4251263dc76e378
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-21 20:41:35 +0900

    Time: 22 ms (80.72%), Space: 17.9 MB (63.93%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
-    
-    /*
-    0 1 2 3 4 5 
-    0 1 3 4 5
-    
-    // edge cases
-    1. very first elem
-    2. very end elem
-    3. rest
-    4. single input
-    */
-    
     int missingNumber(vector<int>& nums) {
-        int start = 0, end = nums.size()-1, mid;
-        sort(nums.begin(), nums.end());
         
-        // if(nums[end] == end) return end+1;
-        while (start < end) {
-            mid = (start + end + 1) >> 1;
-            if (mid < nums[mid])
-                end = mid - 1;
-            else start = mid;
+        int sum = nums.size();
+        for (int i=0; i<nums.size(); i++) {
+            sum ^= nums[i];
+            sum ^= i;
         }
-        return (start==nums[start])? start+1 : start;
-        
+        return sum;
     }
 };
