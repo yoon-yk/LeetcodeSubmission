41c1e5c8ca6dc0a19d841a38a732c3bb342b64ce
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-24 20:18:03 +0900

    Time: 5 ms (76.21%), Space: 11.7 MB (61.69%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
+        
+        vector<int> ans;
         sort(nums.begin(), nums.end());
         
-        // edge case : # == 1
-        if (nums.size()==0) return {};
-        if (nums.size()==1 && target==nums[0]) return {0};
-
-        vector<int> ans;
-        int start = 0, end = nums.size()-1, mid;
-        
+        // LB 
+        int start = 0, end = nums.size()-1, leftMid, rightMid;
         while (start < end) {
-            mid = start + ((end-start+1)>>1);
-            if (nums[mid] == target) {
-                while (nums[start]!=target) start++;
-                while (nums[end]!=target) end--;
-                for (int i=start; i<=end; i++) ans.push_back(i);
-                return ans;
-            }
-            if (target < nums[mid]) 
-                end = mid-1;
-            else start = mid;
-        }
-        
-        if (start == end && nums[start] == target) {
-            ans.push_back(start);
+            leftMid = start + ((end-start)>>1);
+            if (nums[leftMid] < target)
+                start = leftMid + 1;
+            else 
+                end = leftMid;
+            
         }
                 
-        return ans;
+        if (nums[start]!=target) return ans;
         
+        // RB
+        leftMid = start;
+        end = nums.size()-1;
+        while (start < end) {
+            rightMid = start + ((end-start+1)>>1);
+            if (target < nums[rightMid])
+                end = rightMid - 1;
+            else start = rightMid;
+        }
+        
+        rightMid = start;
+        
+        for(int i=leftMid; i<=rightMid; i++)
+            ans.push_back(i);
+        
+        return ans;
     }
 };
