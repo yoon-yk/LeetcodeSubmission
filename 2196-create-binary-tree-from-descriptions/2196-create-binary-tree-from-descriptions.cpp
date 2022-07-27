01e4910c95704ff3bab9932cf12c0552111c0675
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-27 13:08:53 +0900

    Time: 1545 ms (59.80%), Space: 277.7 MB (73.23%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -12,40 +12,29 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        
-        if (descriptions.size() == 0) return nullptr;
-        
-        unordered_map<int, int> leftChild;
-        unordered_map<int, int> rightChild;
-        unordered_map<int, int> parent; 
-        
-        for (vector<int>& des : descriptions) {
-            if(des[2])
-                leftChild[des[0]] = des[1];
-            else
-                rightChild[des[0]] = des[1];
-            parent[des[1]] = des[0];
+        unordered_map<int, TreeNode*> getNode;
+        unordered_map<int, bool> isChild;
+        for (auto &v: descriptions) {
+            if (getNode. count(v[0])==0) { // create parent node if not exist
+                TreeNode* par = new TreeNode(v[0]);
+                getNode[v[0]] = par;
+            }
+            if (getNode.count(v[1])==0) { // create child node if not exist
+                TreeNode* child = new TreeNode(v[1]);
+                getNode[v[1]] = child;
+            }
+            if(v[2]) getNode[v[0]]->left = getNode[v[1]];
+            else getNode[v[0]]->right = getNode[v[1]];
+            isChild[v[1]] = true;
         }
         
-        int root = descriptions[0][0]; // any leaf node can do 
-        while (parent.find(root)!=parent.end()){
-            root = parent[root];
+        TreeNode* ans = NULL;
+        for (auto &v : descriptions) {
+            if (!isChild[v[0]]) { // 그 누구의 child도 아닌 경우 
+                ans = getNode[v[0]]; // tree의 root! 
+                break; 
+            }
         }
-        
-        return constructTree(root, leftChild, rightChild);            
-        
-    }
-    
-    TreeNode* constructTree(int val, unordered_map<int, int>& leftChild, unordered_map<int, int>& rightChild) {
-        
-        TreeNode* root = new TreeNode(val);
-        
-        if (leftChild.find(val)!=leftChild.end())
-            root->left = constructTree(leftChild[val], leftChild, rightChild);
-        
-        if (rightChild.find(val)!=rightChild.end())
-            root->right = constructTree(rightChild[val], leftChild, rightChild);
-        
-        return root;
+        return ans;
     }
 };
