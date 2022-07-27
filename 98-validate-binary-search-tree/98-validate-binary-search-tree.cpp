fe00c7187a8d5020640bbc482eae6f05879120e4
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-28 01:43:20 +0900

    Time: 23 ms (34.13%), Space: 21.7 MB (70.14%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -8,23 +8,23 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
-    5
-  3   8
- 1 4 7 10     
-
-*/
-
+ */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        if (root == nullptr) return true;
-        return isValidBST(root, NULL, NULL);
+        return helper(root, LLONG_MIN, LLONG_MAX);
     }
     
-    bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
-        if (root == nullptr) return true;
-        if ((minNode && minNode->val >= root->val) || (maxNode && maxNode->val <= root->val))
-            return false;
-        return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+    bool helper(TreeNode* root, long long lowerbound, long long upperbound){
+        if (!root) return true;
+        
+        bool left = !(root->left) || 
+                        (root->left && root->left->val < root->val 
+                        && helper(root->left, lowerbound, root->val));
+        bool right = !(root->right) || 
+                        (root->right && root->val < root->right->val 
+                        && helper(root->right, root->val, upperbound));
+        
+        return lowerbound < root->val && root->val < upperbound && left && right;
     }
 };
