450798226e9a35409619c6799d10be7adcc3334b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-27 13:20:53 +0900

    Time: 1334 ms (78.91%), Space: 277.6 MB (86.49%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -12,29 +12,30 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, TreeNode*> getNode;
-        unordered_map<int, bool> isChild;
-        for (auto &v: descriptions) {
-            if (getNode. count(v[0])==0) { // create parent node if not exist
-                TreeNode* par = new TreeNode(v[0]);
-                getNode[v[0]] = par;
-            }
-            if (getNode.count(v[1])==0) { // create child node if not exist
-                TreeNode* child = new TreeNode(v[1]);
-                getNode[v[1]] = child;
-            }
-            if(v[2]) getNode[v[0]]->left = getNode[v[1]];
-            else getNode[v[0]]->right = getNode[v[1]];
-            isChild[v[1]] = true;
+        
+        if (descriptions.size() == 0) return nullptr;
+        
+        unordered_map<int, TreeNode*> nodeMap;
+        unordered_map<int, int> parentMap; 
+        
+        for (vector<int>& v : descriptions) {
+            
+            TreeNode* parent = (nodeMap.count(v[0]) > 0)? 
+                nodeMap[v[0]] : (nodeMap[v[0]] = new TreeNode(v[0]));
+            TreeNode* child = (nodeMap.count(v[1]) > 0)? 
+                nodeMap[v[1]] : (nodeMap[v[1]] = new TreeNode(v[1]));
+            
+            if(v[2]) parent->left = child;
+            else parent->right = child;
+            
+            parentMap[v[1]] = v[0];
         }
         
-        TreeNode* ans = NULL;
-        for (auto &v : descriptions) {
-            if (!isChild[v[0]]) { // 그 누구의 child도 아닌 경우 
-                ans = getNode[v[0]]; // tree의 root! 
-                break; 
-            }
+        int root = descriptions[0][0]; // any leaf node can do 
+        while (parentMap.count(root) > 0){
+            root = parentMap[root];
         }
-        return ans;
+        
+        return nodeMap[root];   
     }
 };
