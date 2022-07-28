e75e2891e582d39de5fdeabb623e1e31c0f6bd17
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-28 18:38:53 +0900

    Time: 7 ms (79.41%), Space: 8.6 MB (10.52%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,17 +11,11 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (!head) return NULL;
+        if (!head || !head->next) return head;
         
-        ListNode* prev = NULL, *curr = head, *fast = head->next;
-        
-        while (curr) {
-            curr->next = prev;
-            prev = curr;
-            curr = fast;
-            if (fast) fast = fast->next;
-        }
-        
-        return prev;
+        ListNode* reversedHead = reverseList(head->next);
+        head->next->next=head;
+        head->next = NULL;
+        return reversedHead; 
     }
 };
