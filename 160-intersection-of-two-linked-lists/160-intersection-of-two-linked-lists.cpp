417447ea7909a11ee147e9ee3b37ccc2206512fd
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-30 00:01:08 +0900

    Time: 113 ms (13.12%), Space: 17.9 MB (8.84%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -11,16 +11,35 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
         if(!headA || !headB) return NULL;
-                
+        
+        unordered_map<ListNode*, ListNode*> hashMap;
+        
         ListNode* ptrA = headA;
         ListNode* ptrB = headB;
-
-        while (ptrA!=ptrB) {
-            ptrA = (ptrA)? ptrA->next : headB;
-            ptrB = (ptrB)? ptrB->next : headA;
+        
+        while (ptrA) {
+            ListNode* temp = ptrA;
+            hashMap[temp] = temp;
+            ptrA = ptrA->next;
+        }
+        // cout << "hh" << endl;
+        // for (auto i:hashMap)
+        //     cout << i.first << endl;
+        
+        while (headB) {
+            if (hashMap.count(headB)) {
+                ListNode *ptr1 = hashMap[headB];
+                ListNode *ptr2 = headB;
+                while (ptr1 && ptr2 && ptr1 == ptr2){
+                    ptr1=ptr1->next;
+                    ptr2=ptr2->next;
+                }
+                if (!ptr1 && !ptr2) return headB;
+            }
+            headB = headB->next;
         }
         
-        return ptrA;
+        return NULL;
     
     }
 };
