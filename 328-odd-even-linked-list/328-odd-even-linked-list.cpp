419194fecdf254e0d6c3aeaf8c7cc5c104ca5518
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-31 01:23:27 +0900

    Time: 10 ms (95.07%), Space: 10.5 MB (29.31%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -12,37 +12,14 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         if (!head) return NULL;
-        
-        // 1. create an even Head 
-        ListNode* evenHead = new ListNode();
-        ListNode* evenPtr = evenHead;
-        
-        // 2. iterate over the list and take out the even indiced node and link to the new list
-        ListNode *ptr = head, *prev = head;
-        bool isEven = false;
-        
-        while (ptr) {
-            if (isEven) {
-                prev->next = ptr->next;
-                evenPtr->next = ptr;
-                ptr = ptr->next;
-
-                evenPtr = evenPtr->next;
-                evenPtr->next = NULL;
-            }
-            else {
-                prev = ptr;
-                ptr = ptr->next;
-            }
-            
-            isEven = !isEven;
+        ListNode* odd = head, *even = head->next, *evenHead = even;
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
         }
-
-        
-        // 3. link the odd head to the last node of the list when the iteration is over.
-        prev->next = evenHead->next;
-        
-        // return NULL;
+        odd->next = evenHead;
         return head;
     }
     
