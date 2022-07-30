f20331d1a539fc022c94cc8952be95df1ecd48c8
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-30 21:43:00 +0900

    Time: 53 ms (59.08%), Space: 71.5 MB (50.81%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        
+        int currSum = 0, rest;
+        bool prev_carry = false, curr_carry = false;
+    
+        ListNode *dummyHead = new ListNode();
+        ListNode *ptr = dummyHead;
+        
+        while (l1 && l2) {
+            prev_carry = curr_carry;
+            currSum = l1->val + l2->val + prev_carry;
+            curr_carry = (currSum >= 10);
+            rest = currSum %= 10;
+            
+            ListNode *curr = new ListNode(rest);
+            ptr->next = curr;
+            ptr = ptr->next;
+            
+            l1 = l1->next; l2 = l2->next;
+        }
+        
+        ListNode *restPtr;
+        if (!l1) restPtr = l2;
+        if (!l2) restPtr = l1;
+                
+        while (restPtr) {
+            prev_carry = curr_carry;
+            currSum = restPtr->val + prev_carry;
+            curr_carry = (currSum >= 10);
+            rest = currSum %= 10;
+            
+            ListNode *curr = new ListNode(rest);
+            ptr->next = curr;
+            ptr = ptr->next;    
+            restPtr = restPtr->next;
+        }
+        
+        if (curr_carry) {
+            ptr->next = new ListNode(1);
+        }
+        
+        return dummyHead->next;
+    }
+};
