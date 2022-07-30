8df824dd77a8b466c420fe0cb33a9794f5522d4b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-30 21:49:32 +0900

    Time: 85 ms (10.05%), Space: 71.4 MB (50.81%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -14,15 +14,19 @@ public:
         if(!l1) return l2;
         if(!l2) return l1;
         
-        int currSum = 0, digit;
+        int currSum = 0, digit, l1val, l2val;
         bool prev_carry = false, curr_carry = false;
     
         ListNode *dummyHead = new ListNode();
         ListNode *ptr = dummyHead;
         
-        while (l1 && l2) { 
+        while (l1 || l2 || curr_carry) { 
             prev_carry = curr_carry;
-            currSum = l1->val + l2->val + prev_carry;
+            
+            l1val = (l1)? l1->val : 0;
+            l2val = (l2)? l2->val : 0;
+            
+            currSum = l1val + l2val + prev_carry;
             curr_carry = (currSum >= 10);
             digit = currSum %= 10;
             
@@ -31,28 +35,10 @@ public:
             ptr->next = curr;
             ptr = ptr->next;
             
-            l1 = l1->next; l2 = l2->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
         }
-        
-        ListNode *restPtr;
-        if (!l1) restPtr = l2;
-        if (!l2) restPtr = l1;
-                
-        while (restPtr) {
-            prev_carry = curr_carry;
-            currSum = restPtr->val + prev_carry;
-            curr_carry = (currSum >= 10);
-            digit = currSum %= 10;
-            
-            ListNode *curr = new ListNode(digit);
-            
-            ptr->next = curr;
-            ptr = ptr->next;    
-            restPtr = restPtr->next;
-        }
-        
-        if (curr_carry) 
-            ptr->next = new ListNode(1);
+
         
         return dummyHead->next;
     }
