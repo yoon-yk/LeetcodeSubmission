17df8a03cd29aa23ee87c974ffa8020de4d746f3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-07-30 16:38:17 +0900

    Time: 3 ms (74.09%), Space: 7.4 MB (93.26%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head) return NULL;
+        
+        int cnt = 1;
+        ListNode* ptr = head, *prev = NULL, *rest = NULL, *start = NULL;
+        
+        while (ptr && cnt < left) {
+            prev = ptr;
+            ptr = ptr->next;
+            cnt ++;
+        }
+        start = ptr; // Save the starting pointer to modify the next link at line 32.
+
+        if (prev) prev->next = helper(ptr, rest, cnt, right);
+        else head = helper(ptr, rest, cnt, right);
+        
+        start->next = rest; // Link the rest nodes;
+
+        return head;
+    }
+    
+    
+    ListNode* helper(ListNode* ptr, ListNode*& rest, int &cnt, int right) {
+        if (!ptr) return NULL;
+        if (cnt == right) {
+            rest = ptr->next;
+            return ptr;
+        }
+        cnt ++;
+        ListNode* node = helper(ptr->next, rest, cnt, right);
+        ptr->next->next = ptr;
+        ptr->next = NULL;
+        return node;
+    }
+};
