c118dfd7edcdbb873fc317134e1d16dfa3dcf44a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-01 22:37:20 +0900

    Time: 0 ms (100.00%), Space: 8.9 MB (35.49%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -4,11 +4,7 @@ public:
         int left = 0, ptr = 0;
         while (ptr < nums.size()) {
             // keep valid elems in the left side 
-            if (nums[ptr] == val){
-                // if ptr encounters a target, set its value to -1
-                nums[ptr] = -1;
-            }
-            else {
+            if (nums[ptr] != val){
                 // if ptr encounters not a target value, swap it with the left val
                 swap(nums[left], nums[ptr]);
                 left++;
