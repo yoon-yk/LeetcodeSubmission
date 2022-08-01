8eb5b3bf7fc11dd7ce1a5ef6d909872ba1b19ac6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-01 22:10:42 +0900

    Time: 30 ms (74.88%), Space: 19.1 MB (98.07%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeroIdx = 0, ptr = 0;
-        while (ptr < nums.size()) {
-            if (nums[zeroIdx] != 0) { // Increase zeroIdx until encountering zero cell 
-                ptr = ++zeroIdx;
-            }
-            else { // If zeroIdx points to zero
-                if (nums[ptr] == 0) { // Adjust ptr to the non-zero cell
-                    ++ptr;
-                }
-                else { // Swap elems if ptr points to non-zero and zeroIdx points to zero
-                    swap(nums[zeroIdx], nums[ptr]);
-                    ++zeroIdx; ++ptr;
-                }
-            }
+        int nonZeroIdx = 0, zeroIdx = 0;
+        for (; zeroIdx<nums.size(); zeroIdx++){
+            if (nums[zeroIdx]!=0)
+                swap(nums[zeroIdx],nums[nonZeroIdx++]);
         }
     }
 };
