fe4c350318d9ae99645c6c1d723790f07ddd1ade
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-02 15:58:20 +0900

    Time: 104 ms (9.86%), Space: 13 MB (92.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,21 +1,44 @@
+class Element {
+    public :
+        int value;
+        int column;
+        int row;
+    Element(int v, int r, int c) {
+        value = v;
+        column = c;
+        row = r;
+    }
+};
+
+class Comparator {
+    public:
+    bool operator() (const Element& a, const Element& b) {
+        return a.value > b.value;
+    }
+};
+
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        // max heap 만들기
-        priority_queue<int> pq;
+        // min heap 만들어주기
+        priority_queue<Element, vector<Element>, Comparator> pq;
         
-        // max heap k크기로 유지해주기
         int m_row = matrix.size(), m_col = matrix[0].size();
-        for (int r=0; r<m_row; ++r) {
-            for (int c=0; c<m_col; ++c) {
-                pq.push(matrix[r][c]);
-                
-                if (pq.size() > k) {
-                    pq.pop();    
-                }
-            }
+
+        // 첫번째 row 다 넣어주기
+        for (int c=0; c < min(m_col, k); ++c) {
+            pq.push(Element(matrix[0][c], 0, c));
+        } 
+        
+        // k만큼 min heap에서 하나씩 빼면서 top아래 애도 넣어주기
+        
+        int r, c, val;
+        for (int i=0; i < k; i++) {
+            Element curr = pq.top(); pq.pop();
+            val = curr.value, r = curr.row, c = curr.column;
+            if (r+1 < m_row) pq.push(Element(matrix[r+1][c], r+1, c));
         }
         
-        return pq.top();
+        return val;
     }
 };
