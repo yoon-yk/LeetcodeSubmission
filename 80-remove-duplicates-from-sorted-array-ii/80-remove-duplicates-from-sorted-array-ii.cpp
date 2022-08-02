4d7883731b6f67f727933c3391c4a8a60693e1d6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-02 17:11:55 +0900

    Time: 3 ms (95.48%), Space: 10.8 MB (93.05%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,16 +4,16 @@ public:
         int left = 0, right = 0, occur = 1;
         nums[left++] = nums[right++];
         while (right < nums.size()) {
-            if (nums[right-1] == nums[right]) {
+            if (nums[right-1] == nums[right]) { // Duplicate
                 if (++occur <= 2) 
                     nums[left++] = nums[right++];
-                else while (right < nums.size() && nums[right-1] == nums[right]) right++;
+                // else 
+                    while (right < nums.size() && nums[right-1] == nums[right]) right++;
             }
-            else {
+            else { // Changing point
                 occur = 1;
                 nums[left++] = nums[right++];
             }
-            // right++;
         }
         return left;
     }
