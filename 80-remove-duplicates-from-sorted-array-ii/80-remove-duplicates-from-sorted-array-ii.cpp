9e3d696b98d94090151c06e5717c78d8d0ec54f2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-02 17:28:36 +0900

    Time: 12 ms (33.55%), Space: 10.9 MB (75.43%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int left = 0, right = 0;
-        nums[left++] = nums[right++];
+        
         while (right < nums.size()) {
-            if (nums[right-1] == nums[right]) { // Duplicate
+            nums[left++] = nums[right++];
+            if (right < nums.size() && nums[right-1] == nums[right]) { // Duplicate
                 nums[left++] = nums[right++];
                 while (right < nums.size() && nums[right-1] == nums[right]) right++;
-            }
-            else { // Changing point
-                nums[left++] = nums[right++];
             }
         }
         return left;
