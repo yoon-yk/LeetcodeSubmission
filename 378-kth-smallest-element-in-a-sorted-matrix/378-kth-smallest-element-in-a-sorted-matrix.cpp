056187271962bf4b41bad1a60f6239ccc84c22ec
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-02 15:46:53 +0900

    Time: 54 ms (50.35%), Space: 14.4 MB (33.33%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,37 +1,21 @@
-class Element {
-    public:
-        int value;
-        int row;
-        int col;
-    Element(int val, int r, int c) {
-        value = val; row = r; col = c;
-    }
-};
-
-class Comparator {
-    public :
-        bool operator() (const Element& A, const Element& B) {
-            return A.value > B.value;
-        }
-};
-
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        // max heap 만들기
+        priority_queue<int> pq;
         
-        priority_queue<Element, vector<Element>, Comparator> pq;
+        // max heap k크기로 유지해주기
         int m_row = matrix.size(), m_col = matrix[0].size();
-        
-        for (int i=0; i<m_row; i++) {
-            for (int j=0; j<m_col; j++) {
-                pq.push(Element(matrix[i][j],i,j));
+        for (int r=0; r<m_row; ++r) {
+            for (int c=0; c<m_col; ++c) {
+                pq.push(matrix[r][c]);
+                
+                if (pq.size() > k) {
+                    pq.pop();    
+                }
             }
         }
         
-        for (int i=0; i<k-1; i++) {
-            pq.pop();
-        }
-        
-        return pq.top().value;
+        return pq.top();
     }
 };
