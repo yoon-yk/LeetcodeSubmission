c22d50fa66a06094d43e4c59bc97b4a94d2b9b0e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-03 17:06:54 +0900

    Time: 31 ms (55.33%), Space: 8.6 MB (26.79%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -22,7 +22,7 @@ public:
             }
             
             if (hashM.find(s[right])!=hashM.end()) {
-                q.push(right);
+                if(left!=right) q.push(right);
                 --hashM[s[right]];
                 
                 if (hashM[s[right]] == 0)
@@ -32,7 +32,7 @@ public:
                 
            if (tCnt == 0) {
                
-               while (!q.empty() && q.front() <= left) q.pop();
+               // while (!q.empty() && q.front() <= left) q.pop();
 
                            
                while (!q.empty() && hashM[s[left]] < 0) { 
@@ -47,7 +47,6 @@ public:
                     minLeft = left;
                     minRight = right;
                     minSubLen = right-left+1;
-
                }
 
 
