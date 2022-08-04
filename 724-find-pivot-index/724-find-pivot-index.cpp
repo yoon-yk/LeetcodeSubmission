e486db4cd888dc74f029644ca28bcaf2299c6655
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-04 20:03:24 +0900

    Time: 28 ms (83.90%), Space: 31.2 MB (53.59%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        vector<int> leftPrefix (nums.size()+1, 0);
-        vector<int> rightPrefix (nums.size()+1, 0);
         
-        // left
-        for (int i=0; i<nums.size(); i++)
-            leftPrefix[i+1] = leftPrefix[i]+nums[i];
+        int leftSum = 0; 
+        int rightSum = 0;
         
-        // right
-        for (int i=nums.size()-1; i >= 0; i--)
-            rightPrefix[i] = rightPrefix[i+1]+nums[i];
-
+        for (int n:nums)
+            rightSum += n;
+        
         int ans = -1;
-        for (int i=0; i<nums.size(); i++)
-            if (leftPrefix[i] == rightPrefix[i+1]){
+        for (int i=0; i<nums.size(); i++) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum){
                 ans = i;
                 break;
             }
+            leftSum += nums[i];
+        }
+        
+
         
         return ans;
         
