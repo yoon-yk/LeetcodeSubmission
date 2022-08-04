ee839f5f6f576ba07e5958f6ca1f982084613fb2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-04 15:28:52 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        int m = q, n = p;
+        while (m % 2 == 0 && n % 2 == 0){
+            m /= 2;
+            n /= 2;
+        }
+        if (n % 2 == 0) return 2;
+        else if (m % 2 == 0) return 0;
+        else return 1;
+    }
+};
