0c4e799344391a97cb555dd439af2120acfddbf1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-08 13:07:47 +0900

    Time: 396 ms (52.12%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
-        if (nums.size() == 1) return 1;
-        int LIS[nums.size()], globalMax = INT_MIN, curMax, tmp;
-        LIS[nums.size()-1] = 1;
+        /*
+         dp
+         
+         10 |  9 |  2 |  5 |  3 |  7 | 101 |  18 
+          1    1    1    2    2    3     4     4
+          
+        dp[i] = max(dp[c] + 1, 1) (c < i, dp[c] < dp[i])
+          
+        */
         
-        for (int i = nums.size()-2; i >= 0; i--) {
-            LIS[i] = 1;
-            for (int j = i+1; j < nums.size(); j++) {
-                tmp = (nums[j] > nums[i])? LIS[j] + 1 : 1; 
-                LIS[i] = max(LIS[i], tmp);
+        int size = nums.size();
+        vector<int> dp(size, 1);
+        int maxCnt = 1;
+        
+        for (int i=0; i<size; i++) {
+            for (int j=i+1; j<size; j++) {
+                if (nums[j] > nums[i]) {
+                    dp[j] = max(dp[j], dp[i]+1);
+                    maxCnt = max(maxCnt, dp[j]);
+                }
             }
-            globalMax = max(LIS[i], globalMax);
         }
-        return globalMax;
+        
+        return maxCnt;
+        
     }
 };
