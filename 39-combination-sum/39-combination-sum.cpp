721b75da1cbf97dfd451361fe5106735102e2377
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-08 22:32:56 +0900

    Time: 8 ms (81.64%), Space: 11.1 MB (75.19%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,43 +1,31 @@
-/*
-[2, 3, 6, 7]
-target = 7
-
-[2] -> (7-2) = 5 -> (5-2) = 3 -> 3
-[3] -> (7-3) = 4 -> (4-2) = 2 -> 2
-[6] -> (7-6) = 1
-[7] -> (7-7) = 0
-
-
-
-                [2]                []
-        [2,2]           [2]     [3]   []
-    [2,2,2] [2,2,3]  []
-
-
-*/
-
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        dfs(0, target, 0, candidates, {}, res);
-        return res;
+        
+        vector<int> cur;
+        vector<vector<int>> ans;
+        
+        backtrack(candidates, 0, target, cur, ans);
+        
+        return ans;
     }
     
-    void dfs(int index, int target, int sum, vector<int> candidates, vector<int> comb, vector<vector<int>>& res) {
-        if (sum > target || index >= candidates.size()) return;
-        if (sum  == target) {
-            res.push_back(comb); 
+    void backtrack(vector<int>& candidates, int idx, int curSum, vector<int>& cur, vector<vector<int>>& ans) {
+        
+        if (idx > candidates.size() || curSum < 0)
+            return;
+        
+        if (curSum == 0) {
+            ans.push_back(cur);
             return;
         }
         
-        // exclude current index
-        dfs(index+1, target, sum, candidates, comb, res);  
+        for (int i=idx; i<candidates.size(); i++) {
+            cur.push_back(candidates[i]);
+            backtrack(candidates, i, curSum-candidates[i], cur, ans);
+            cur.pop_back();
+        }
         
-        // include current index
-        comb.push_back(candidates[index]);
-        sum += candidates[index];
-        dfs(index, target, sum, candidates, comb, res);
     }
-        
+    
 };
