c11f27c665ff4f486b4c2645d936ac280342e41c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-09 01:22:04 +0900

    Time: 3 ms (82.69%), Space: 7.7 MB (54.56%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> cur;
+        vector<vector<int>> ans;
+        
+        backtrack(0, nums, ans, cur);
+        
+        return ans;
+    }
+    
+    void backtrack(int idx, vector<int>& nums, vector<vector<int>>& ans, vector<int>& cur) {
+        
+        cout << idx << endl;
+        
+        if (cur.size() == nums.size()) {
+            ans.push_back(cur);
+            return;
+        }
+        else {
+            for (int i=0; i < nums.size(); i++) {
+                if (find(cur.begin(), cur.end(), nums[i])!=cur.end()) continue; // element already exists, skip
+                cur.push_back(nums[i]);
+                backtrack(i+1, nums, ans, cur);
+                cur.pop_back();
+            }
+        }
+
+    }
+};
