0e8aee0a8e30604a5bf22d13cac80f93cb0f3998
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-10 15:21:32 +0900

    Time: 211 ms (18.77%), Space: 9.2 MB (81.85%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,63 +1,27 @@
 class Solution {
 public:
     int MOD = 1e9+7;
+    
     int numFactoredBinaryTrees(vector<int>& arr) {
         
-        sort(arr.begin(), arr.end());
-        unordered_map<int, long> dp;
         long ans = 0;
+        sort(arr.begin(), arr.end());
+        unordered_map<int, long> hashM;        
+        for (int n : arr)
+            hashM[n]++;
         
-        for (int i=0; i<arr.size(); ++i) {
-            dp[arr[i]] = 1;
-            for (int j=0; j<i; ++j) {
-                // arr[i] : target
-                // arr[j] : left 
-                // arr[i]/arr[j] : right
-                
-                if (arr[i] % arr[j] == 0) {
-                    int right = arr[i]/arr[j];
-                    dp[arr[i]] = (dp[arr[i]] + dp[arr[j]] * dp[right]) % MOD;
-
+        for (int rootI=0; rootI<arr.size(); rootI++) {
+            for (int leftI=0; leftI<rootI; leftI++) {
+                int right = arr[rootI] / arr[leftI]; // right는 root보다 작은 값
+                if (arr[rootI] % arr[leftI] == 0 && hashM.count(right)) { // 완전히 나누어 떨어지는 값이어야 함. 
+                    hashM[arr[rootI]] += (hashM[arr[leftI]] * hashM[right]);
+                    hashM[arr[rootI]] %= MOD;
                 }
             }
-            ans = (ans + dp[arr[i]]) % MOD;
+            ans += hashM[arr[rootI]];
+            ans %= MOD;
         }
         
         return ans;
     }
 };
-
-
-
-/*
-[2,4,5,10,15,50, 100]
-
-[ 2 ] 
-[ 4 ] [ 2, 2]
-[ 5 ] 
-[ 10] [ 2, 5] [ 5, 2] [10, 10]
-[ 50] [10, 5] [5, 10]
-[100] [2, 50] [50, 2] 
-
-[2] [4] [5] [10] [15] [50] [100]
- 1   2   1    3   1    7     7
- 
-[2,2,4] [2,5,10] [2,50,100]
-1*1 = 1. 1*1 = 1. 1*7 = 7
-[5,2,10] [5, 10, 50] [10,5,50] [50,2,100]
-1*1 = 1.  1*3 = 3.    3*1 = 3.  7*1 = 7
-
-
-
-    50
- 10     5
-5 2    
-
-    50
- 5     10
-      5  2    
-
-
-
-
-*/
