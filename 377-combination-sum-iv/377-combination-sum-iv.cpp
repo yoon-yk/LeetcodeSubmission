fa8adf3aa502208527081971c4849223a226c561
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-11 20:47:01 +0900

    Time: 2 ms (67.26%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     
-    vector<int> dp;
-    
-    Solution() {
-        dp.resize(1001);
-        fill(dp.begin(), dp.end(), -1);
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, -1);
+        return helper(nums, dp, target);
     }
     
-    int combinationSum4(vector<int>& nums, int target, int currSum=0) {
-        if(currSum > target) return 0;
-        if(currSum == target) return 1;
-        if(dp[currSum] != -1) return dp[currSum];
-        int ways = 0;
-        for(int i=0; i<nums.size(); i++) {
-            if(currSum + nums[i] <= target)
-                ways += combinationSum4(nums, target, currSum + nums[i]);
+    int helper(vector<int>& nums, vector<int>& dp, int target) {
+        if (target == 0)
+            return 1;
+        if (target < 0)
+            return 0;
+        if (dp[target] != -1)
+            return dp[target];
+        
+        int ans = 0;
+        for (int i=0; i<nums.size(); i++) {
+            if (target-nums[i] < 0) continue;
+            ans += helper(nums, dp, target-nums[i]);
         }
-        return dp[currSum] = ways;
+        return dp[target] = ans;
+        
     }
 };
