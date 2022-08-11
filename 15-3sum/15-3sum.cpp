b9add1addd1591ba20227f18f6fad3ecdecc8f60
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-11 17:55:43 +0900

    Time: 79 ms (94.96%), Space: 20 MB (77.68%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+     
         vector<vector<int>> ans;
-        if (nums.size() < 3) return ans;
         
-        unordered_map <int, int> m;
-        for (int n : nums)
-            m[n]++;
+        //1. sort
+        sort(nums.begin(), nums.end());
         
-        for (auto n:m) {
-            int target = n.first; 
-            m[target]--;
+        //2. two pointers (skip the duplicates)
+        // a + b + c = 0
+        // --> 'a' should be a negavtive num; at least one elem should be negative
+        // --> c should be starting from the bigger num; 
+        
+        int aIdx = 0;
+        for (; aIdx<nums.size()-2;) {
+            if (nums[aIdx] > 0) break;
             
-            for (auto l:m) {
-                int left = l.first;
-                if (left > target || m[left] == 0) continue; 
+            int bIdx = aIdx+1, cIdx = nums.size()-1;
+            
+            while (bIdx < cIdx) {
                 
-                int right = -1 * (target+left);
-                if (right > left) continue;
-                
-                m[left]--;
-                if (m.count(right) && m[right] > 0)
-                    ans.push_back({target, left, right});
-                m[left]++;
+                int sum = nums[aIdx]+nums[bIdx]+nums[cIdx];
+
+                if (sum==0) {
+                    ans.push_back({nums[aIdx], nums[bIdx], nums[cIdx]});
+                    bIdx++;
+                    while (bIdx < nums.size() && nums[bIdx-1] == nums[bIdx]) bIdx++;
+                    while (cIdx > 0 && nums[cIdx-1] == nums[cIdx]) cIdx--;
+                }
+                else if (sum < 0) {
+                    bIdx++;
+                }
+                else {
+                    cIdx--;
+                }   
             }
-            m[target]++;
+            
+            aIdx++;
+            while (aIdx > 0 && aIdx < nums.size() && nums[aIdx-1] == nums[aIdx]) aIdx++;
+
         }
         
         return ans;
+
     }
 };
