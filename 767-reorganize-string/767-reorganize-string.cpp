57c82e6f5d8ad4ba0ec7a67def4cc8c9107be7d0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-13 01:49:36 +0900

    Time: 8 ms (15.10%), Space: 6.1 MB (98.17%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -2,47 +2,45 @@ class Solution {
 public:
     string reorganizeString(string s) {
         
-        auto compare = [] (pair<char, int> a, pair<char, int> b) {
+        auto compare = [] (pair<int, int> a, pair<int, int> b) {
             return a.second < b.second;
         };
         
         string ans = "";
         int maxCnt = INT_MIN;
-        unordered_map<char, int> hashM; 
+        vector<int> chars(26, 0);
         
         for (const char& c:s) {
-            hashM[c]++;
-            maxCnt = max(hashM[c], maxCnt);
+            chars[c-'a']++;
+            maxCnt = max(chars[c-'a'], maxCnt);
         }
         
         if (s.size() - maxCnt < maxCnt - 1) return ans;
         
-        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(compare)> pq(compare);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(compare)> pq(compare);
 
-        for (auto h : hashM) {
-            pq.push({h.first, h.second});
+        for (int i=0; i<26; i++) {
+            if (chars[i] > 0)
+                pq.push({i, chars[i]});
         }        
         
         while (pq.size() > 1) {
             auto firstCh = pq.top(); pq.pop();
             auto secondCh = pq.top(); pq.pop();
             
-            ans += firstCh.first;
-            ans += secondCh.first;
-            
-            firstCh.second--;
-            secondCh.second--;
-            
-            if (firstCh.second > 0) 
+            ans += firstCh.first + 'a';
+            ans += secondCh.first + 'a';
+
+            if (--firstCh.second > 0) 
                 pq.push({firstCh.first, firstCh.second});
-            if (secondCh.second > 0) 
+            if (--secondCh.second > 0) 
                 pq.push({secondCh.first, secondCh.second});
         }
         
         if (!pq.empty()) {
             auto lastCh = pq.top(); pq.pop();
             // if (lastCh.second > 1) return "";
-            ans += lastCh.first;
+            ans += lastCh.first + 'a';
         }
         
         return ans;
