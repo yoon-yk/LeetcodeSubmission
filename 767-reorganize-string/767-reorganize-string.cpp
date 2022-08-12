758149e57394dfcd5138adda1a570d551b8449b3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-13 01:32:02 +0900

    Time: 3 ms (67.97%), Space: 6.2 MB (67.06%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        string ans = "";
+        
+        unordered_map<char, int> hashM; 
+        
+        auto compare = [] (pair<char, int> a, pair<char, int> b) {
+            return a.second < b.second;
+        };
+        
+        
+        int maxCnt = INT_MIN;
+        for (const char& c:s) {
+            hashM[c]++;
+            maxCnt = max(hashM[c], maxCnt);
+        }
+        
+        if (s.size() - maxCnt < maxCnt - 1) return ans;
+        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(compare)> pq(compare);
+
+        for (auto h : hashM) {
+            pq.push({h.first, h.second});
+        }        
+        
+        char firstCh, secondCh;
+        int firstCnt = 0, secondCnt = 0;
+        
+        while (pq.size() > 1) {
+            auto firstCh = pq.top(); pq.pop();
+            auto secondCh = pq.top(); pq.pop();
+            
+            ans += firstCh.first;
+            ans += secondCh.first;
+            
+            firstCh.second--;
+            secondCh.second--;
+            
+            if (firstCh.second > 0) 
+                pq.push({firstCh.first, firstCh.second});
+            if (secondCh.second > 0) 
+                pq.push({secondCh.first, secondCh.second});
+        }
+        
+        if (!pq.empty()) {
+            auto lastCh = pq.top(); pq.pop();
+            ans += lastCh.first;
+        }
+        
+        
+        return ans;
+    }
+};
