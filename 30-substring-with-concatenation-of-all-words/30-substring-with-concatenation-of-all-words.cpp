2af2bbaaa6d789e2a6cbb5cb57bb9abb4b7812cd
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-13 21:30:44 +0900

    Time: 247 ms (73.99%), Space: 22 MB (79.28%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    
-    void slidingWindow(string s, int startIdx, vector<string>& words, unordered_map<string, int>& counts, vector<int> &ans) {
-        int j = 0;
-        int len = words[0].length();
-        int num = words.size();
-        unordered_map<string, int> seen;
-        for (; j < num; j++) {
-            string word = s.substr(startIdx + j * len, len);
-            if (counts.count(word)) {
-                seen[word]++;
-                if (seen[word] > counts[word])
-                    break;
-            }
-            else break;
-        }
-        if (j == num) ans.push_back(startIdx);
-    }
-    
     vector<int> findSubstring(string s, vector<string>& words) {
         unordered_map<string, int> counts;
+        unordered_map<string, int> seen;
+
         for (string word : words)
             counts[word]++;
         
-        int n = s.length(), num = words.size(), len = words[0].length();
+        int n = s.length(), size = words.size(), len = words[0].length();
         vector<int> ans;
         
-        for (int i = 0; i < n - num * len + 1; i++) {
-            slidingWindow(s, i, words, counts, ans);
+        for (int startIdx = 0; startIdx < n - size * len + 1; startIdx++) {
+            seen.clear();
+            int j = 0;
+            for (; j < size; j++) {
+                string word = s.substr(startIdx + j * len, len);
+                if (counts.count(word)) {
+                    seen[word]++;
+                    if (seen[word] > counts[word])
+                        break;
+                }
+                else break;
+            }
+            if (j == size) ans.push_back(startIdx);
         }
         return ans;        
     }
