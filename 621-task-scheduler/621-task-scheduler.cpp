5386f6fc5f3c09a49af6ddde4c06d1aacdb1f053
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-14 03:28:12 +0900

    Time: 124 ms (58.26%), Space: 34.5 MB (45.77%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -3,18 +3,18 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
         
         vector<int> chars(26, 0);
-        int maxCnt = INT_MIN;
+        int maxCnt = INT_MIN, maxChNum;
         for (const char& ch : tasks) {
             chars[ch-'A']++;
-            maxCnt = max(maxCnt, chars[ch-'A']);
+            if (maxCnt == chars[ch-'A']) {
+                maxChNum++;
+            }
+            else if (maxCnt < chars[ch-'A']) {
+                maxChNum = 1;
+                maxCnt = chars[ch-'A'];
+            }
         }
-        
-        int maxChNum = 0;
-        for (int i=0; i<26; i++) {
-            if (chars[i] == maxCnt)
-                maxChNum ++;
-        }
-        
+
         int minRequired = maxCnt + (n * (maxCnt-1)) + (maxChNum-1);
         
         return max((int) tasks.size(), minRequired);
