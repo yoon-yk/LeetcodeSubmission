154d09cad27207ead8a332b407ab83314e27f9c0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-16 01:05:52 +0900

    Time: 31 ms (23.09%), Space: 7.8 MB (43.03%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -15,29 +15,27 @@ M             1000
 
     int romanToInt(string s) {
 
-      unordered_map <char,int> m;
-        m['I'] = 1;
-        m['V'] = 5;
-        m['X'] = 10;
-        m['L'] = 50;
-        m['C'] = 100;
-        m['D'] = 500;
-        m['M'] = 1000;
+      unordered_map <char,int> m = {
+          {'I', 1},
+          {'V', 5},
+          {'X', 10},
+          {'L', 50},
+          {'C', 100},
+          {'D', 500},
+          {'M', 1000}
+      };
 
       // acceptable 
       int ans = 0;
       // char
       int i;
-      for (i = 0; i < s.length() - 1; i++) {
-        if (m[s[i + 1]] > m[s[i]]) {
+      for (i = 0; i < s.length(); i++) {
+        if (i+1 < s.length() && m[s[i + 1]] > m[s[i]]) {
           ans += m[s[i + 1]] - m[s[i]];
           i ++;
-        } else {
+        } else 
           ans += m[s[i]];
-        }
       }
-      if (i == s.length() - 1)
-        ans += m[s[i]];
 
       return ans;
     }
