f1d463101b2e1ad82dad7f45c29ed0f1cc02d70d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-16 14:31:52 +0900

    Time: 289 ms (65.53%), Space: 68.2 MB (39.94%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -6,17 +6,10 @@ public:
         vector<int> v(n+2, 0);
         
         for (const auto &e : edges) {
-            v[e[0]]++, v[e[1]]++;
+            if (v[e[0]]++ > 0) return e[0];
+            if (v[e[1]]++ > 0) return e[1];
         }
         
-        int ans = -1;
-        for (int i=1; i<=n+1; i++) {
-            if (v[i] == n) {
-                ans = i;
-                break;
-            }
-        }
-        
-        return ans;
+        return 0;
     }
 };
