dea3ef84477ab75df64c4089bbe4e9ceab7016a3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-18 13:16:36 +0900

    Time: 251 ms (81.89%), Space: 78.3 MB (79.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         
-        auto compare = [] (pair<int, int>& a, pair<int, int>& b){
-            return a.second < b.second;
-        };
-        
         // put into the hashMap
         unordered_map<int, int> hashM;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(compare)> maxHeap(compare);
+        priority_queue<int> maxHeap;
         for (int i : arr)
             hashM[i] ++;
         
         // put into the priority queue
         for (auto i : hashM)
-            maxHeap.push({i.first, i.second});
+            maxHeap.push(i.second);
         
         int ans = 0;
         int targetSize = arr.size() >> 1, curSize = arr.size();
         while (curSize > targetSize) {
-           auto cur = maxHeap.top(); maxHeap.pop();
-            curSize -= cur.second;
+           int cur = maxHeap.top(); maxHeap.pop();
+            curSize -= cur;
             ans++;
         }
         
