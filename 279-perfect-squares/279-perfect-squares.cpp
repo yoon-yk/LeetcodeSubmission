242a4ec507b774ad62785f0887f5ffce88a2a4e5
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-18 23:49:03 +0900

    Time: 266 ms (57.02%), Space: 9.2 MB (45.49%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -3,7 +3,7 @@ public:
     int numSquares(int n) {
         
         vector<int> dp(n+1, 10001); 
-        dp[0] = 0, dp[1] = 1;
+        dp[0] = 0;
         
         for (int i=1; i<=n; i++) 
             for (int j=1; j<=floor(sqrt(i)); j++) 
