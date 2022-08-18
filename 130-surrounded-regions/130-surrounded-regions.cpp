2381f76fcac57a527c8368476e27185121181dd7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-18 20:13:52 +0900

    Time: 31 ms (18.91%), Space: 10.3 MB (27.89%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -6,44 +6,43 @@ public:
         
         int rowSize = board.size(), colSize = board[0].size();
         
-        vector<vector<int>> bluePrint(rowSize, vector<int>(colSize, 0));
+        vector<vector<bool>> bluePrint(rowSize, vector<bool>(colSize, true));
         
         for (int r=0; r<rowSize; r++) {
-            for (int c=0; c<colSize; c++) {
-                if (r == 0 || r == rowSize-1 || c == 0 || c == colSize-1 || board[r][c] == 'X')
-                    bluePrint[r][c] = -1;
-            }
+            if (board[r][0] == 'O') 
+                dfs(r, 0, board);
+            if (board[r][colSize-1] == 'O')
+                dfs(r, colSize-1, board);
         }
-        
-        
-        for (int r=0; r<rowSize; r++) {
-            for (int c=0; c<colSize; c++) {
-                if (bluePrint[r][c] == -1 && board[r][c] == 'O') {
-                    dfs(r, c, board, bluePrint);
 
-                }
-            }
+        
+        for (int c=1; c<colSize-1; c++) {
+            if (board[0][c] == 'O') 
+                dfs(0, c, board);
+            if (board[rowSize-1][c] == 'O') 
+                dfs(rowSize-1, c, board);
         }
-        
-        
-        for (int r=0; r<rowSize; r++) {
+ 
+        for (int r=0; r<rowSize; r++) { // O (r*c)
             for (int c=0; c<colSize; c++) {
-                if (bluePrint[r][c] == 0)
+                if (board[r][c] == 'C')
+                    board[r][c] = 'O';
+                else if (board[r][c] == 'O')
                     board[r][c] = 'X';
             }
-        }   
-                
+        }
+            
     }
     
-    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>& bluePrint) {
+    void dfs(int r, int c, vector<vector<char>>& board){
         
-        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || (bluePrint[r][c] == -2 ) || (bluePrint[r][c] == -1 && board[r][c] == 'X'))
+        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] != 'O')
             return;
         
-        bluePrint[r][c] = -2;
+        board[r][c] = 'C';
         
         for (vector<int>& d : dir)
-            dfs(r+d[0], c+d[1], board, bluePrint);
+            dfs(r+d[0], c+d[1], board);
     }
     
 };
