4461d102a0d897d025d9ec1edc5746c63d197dd1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-21 03:04:58 +0900

    Time: 45 ms (13.50%), Space: 19.6 MB (13.58%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> hashM;
+        int targetFreq = nums.size() >> 1;
         int maxN = -1, freqN = -1;
         for (int n:nums){
-            if (++hashM[n] > maxN) {
-                maxN = hashM[n];
+            if (++hashM[n] > targetFreq) {
                 freqN = n;
+                break;
             }
         }
         return freqN;
