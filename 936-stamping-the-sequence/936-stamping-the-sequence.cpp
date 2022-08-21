03f61b03933bd875df1abcd64783ac1841f5589c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-21 21:49:26 +0900

    Time: 10 ms (88.52%), Space: 7.7 MB (58.20%) - LeetHub

--- 936-stamping-the-sequence/936-stamping-the-sequence.cpp
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int can_replace(string &s, string &t , int pos){
+    bool can_replace(string &s, string &t , int pos){
         for (int i=0; i<s.size(); i++)
             if (t[i+pos]!='?' && t[i+pos]!=s[i]) // ?가 아닌데도 같지 않다면 
-                return 0;    
-        return 1;
+                return false;    
+        return true;
     }
     
     int replace(string &s, string &t , int pos){
-        int replace=0;
+        int replace = 0;
         for (int i=0;i<s.size();i++)
             if (t[i+pos]!='?')
                 t[i+pos]='?', replace++;
@@ -20,20 +20,20 @@ public:
         vector<bool> vis(t.size(), false);
         int cnt=0;
         
-        while (cnt!=t.size()){
-            bool changed = false;
+        while (cnt < t.size()){
+            bool replaceable = false;
             for (int i=0; i<=t.size()-s.size(); i++)
                 if (!vis[i] && can_replace(s,t,i)){
                     cnt += replace(s,t,i);
                     vis[i]=true;
-                    changed=true;
+                    replaceable=true;
                     res.push_back(i);
                 }
             
-            if (!changed)
+            if (!replaceable)
                 return {};
         }
-        reverse(res.begin(),res.end());
+        reverse(res.begin(), res.end());
         return res;
     }
 };
