19617e968771c8d2d9fc66cc8f1d7e36e5a8c042
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-21 14:41:30 +0900

    Time: 36 ms (35.08%), Space: 19.5 MB (92.21%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        nth_element(nums.begin(), nums.begin() + (nums.size() >> 1), nums.end());
-        return nums[nums.size() >> 1];
+        return majority(nums, 0, nums.size() - 1);
     }
-};
+    
+private:
+    int majority(vector<int>& nums, int l, int r) {
+        if (l == r) {
+            return nums[l];
+        }
+        int m = l + ((r - l) >> 1);
+        int lm = majority(nums, l, m), rm = majority(nums, m + 1, r);
+        if (lm == rm) {
+            return lm;
+        }
+        return count(nums.begin() + l, nums.begin() + r + 1, lm) > count(nums.begin() + l, nums.begin() + r + 1, rm) ? lm : rm;
+    }
+}; 
