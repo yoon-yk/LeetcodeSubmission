f2e4f1a17d31cc1eaa72bdb305d1dac7df633a1b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-22 00:24:20 +0900

    Time: 96 ms (22.31%), Space: 16.1 MB (80.69%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -10,16 +10,21 @@ public:
         //When I don't use any fuelStop, I can travel as much as I had startFuel in the beginning
         t[0] = startFuel;
         
+        int curStLoc, curStFuel;
+        
+        //traverse on all fuel stops
         for(int i = 1; i<n+1; i++) {
-            //traverse on all fuel stops
+            
+            curStLoc = stations[i-1][0];
+            curStFuel = stations[i-1][1];
             
             //I can use all i fuelStops, or (i-1), or (i-2) ... till 0 to find which gave me best result
             //So. j goes from j = i to j = 0
             for(int j = i; j > 0; j--) {
                 //But first, I need to check If I can even reach curr station or not to use it
-                if(t[j-1] >= stations[i-1][0]) {
+                if(curStLoc <= t[j-1]) {
                     //So, we can use this fuelStop and now we are using (j+1) fuelStops
-                    t[j] = max(t[j], t[j-1] + stations[i-1][1]);
+                    t[j] = max(t[j], t[j-1] + curStFuel);
                 }
             }
         }
@@ -27,10 +32,9 @@ public:
         //After you end up with all stations, check which was best for reaching target
         //Best means, minimum j used by you to reach target
         
-        for(int j = 0; j<n+1; j++) {
+        for(int j = 0; j<n+1; j++) 
             if(t[j] >= target)
                 return j;
-        }
         
         return -1;
     }
