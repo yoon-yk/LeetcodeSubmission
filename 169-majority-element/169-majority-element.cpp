7a0e83cf82a3f26e6e8af4f39d8da86cf6aa1a28
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-21 15:38:40 +0900

    Time: 35 ms (38.51%), Space: 19.4 MB (99.60%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cnt = 0, cand = -1;
-        for (int n:nums) {
-            if (cnt == 0)
-                cand = n;
-            if (cand == n) cnt ++;
-            else cnt--;
+        int majority = 0;
+        for (unsigned int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
+            int bits = 0;
+            for (int num : nums) {
+                if (num & mask) {
+                    bits++;
+                }
+            }
+            if (bits > nums.size() / 2) {
+                majority |= mask;
+            }
         }
-        return cand;
+        return majority;
     }
 };
