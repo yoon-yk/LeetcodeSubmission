e14828d0d01120a32ef42de8239d6859871f5a62
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-22 00:10:15 +0900

    Time: 134 ms (16.25%), Space: 16.3 MB (51.25%) - LeetHub

---
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,37 @@
+//Approach-2 : O(n^2)dp
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        int n = stations.size();
+        
+        vector<long long> t(n+1, 0);
+        //t[j] = max distance that I can reach using j fuelStops
+        
+        //When I don't use any fuelStop, I can travel as much as I had startFuel in the beginning
+        t[0] = startFuel;
+        
+        for(int i = 1; i<n+1; i++) {
+            //traverse on all fuel stops
+            
+            //I can use all i fuelStops, or (i-1), or (i-2) ... till 0 to find which gave me best result
+            //So. j goes from j = i to j = 0
+            for(int j = i-1; j>=0; j--) {
+                //But first, I need to check If I can even reach curr station or not to use it
+                if(t[j] >= stations[i-1][0]) {
+                    //So, we can use this fuelStop and now we are using (j+1) fuelStops
+                    t[j+1] = max(t[j+1], t[j] + stations[i-1][1]);
+                }
+            }
+        }
+        
+        //After you end up with all stations, check which was best for reaching target
+        //Best means, minimum j used by you to reach target
+        
+        for(int j = 0; j<n+1; j++) {
+            if(t[j] >= target)
+                return j;
+        }
+        
+        return -1;
+    }
+};
