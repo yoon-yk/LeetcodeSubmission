19dbe1f72a0b98b0ab21b8fee58453994c77b056
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-22 14:21:16 +0900

    Time: 18 ms (23.19%), Space: 13.1 MB (79.16%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -3,14 +3,12 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int freshNum = 0;
         queue<pair<int, int>> Q;
-        for (int i=0; i<grid.size(); i++) {
+        for (int i=0; i<grid.size(); i++)
             for (int j=0; j<grid[0].size(); j++) {
                 freshNum += (grid[i][j] == 1);
-                if (grid[i][j] == 2) {
+                if (grid[i][j] == 2) 
                     Q.push({i, j});
-                }
             }
-        }
         
         int dir[] = {-1, 0, 1, 0, -1};
         int minutes = 0;
@@ -22,13 +20,11 @@ public:
                 auto top = Q.front(); Q.pop();
                 i = top.first, j = top.second;                
                 for (int d = 0; d < 4; d++) {
-                    if (i+dir[d] < 0 || i+dir[d] >= grid.size() || j+dir[d+1] < 0 || j+dir[d+1] >= grid[0].size())
+                    if (i+dir[d] < 0 || i+dir[d] >= grid.size() || j+dir[d+1] < 0 || j+dir[d+1] >= grid[0].size() || grid[i+dir[d]][j+dir[d+1]] != 1)
                         continue;
-                    if (grid[i+dir[d]][j+dir[d+1]] == 1) {
-                        freshNum--;
-                        grid[i+dir[d]][j+dir[d+1]] = 2;
-                        Q.push({i+dir[d], j+dir[d+1]});
-                    }
+                    freshNum--;
+                    grid[i+dir[d]][j+dir[d+1]] = 2;
+                    Q.push({i+dir[d], j+dir[d+1]});
                 }
             }
             if (Q.empty()) break;
@@ -37,6 +33,5 @@ public:
         
         if (freshNum == 0) return minutes;
         return -1;
-        
     }
 };
