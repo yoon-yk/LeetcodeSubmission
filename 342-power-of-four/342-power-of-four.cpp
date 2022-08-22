525a7f930ee956f9be36bef708ddc6827d608a6e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-23 00:14:07 +0900

    Time: 13 ms (8.64%), Space: 6.9 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -2,7 +2,13 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if (n <= 0) return false;
-        while (n%4 == 0) n >>= 2;
-        return (n==1);
+        unordered_map<int, int> hashM;
+        
+        for (int i=1; i < 1000000000; i*=4) 
+            hashM[i]++;
+        
+        while (n%4 == 0 && !hashM.count(n)) n >>= 2;
+        
+        return (hashM.count(n));
     }
 };
