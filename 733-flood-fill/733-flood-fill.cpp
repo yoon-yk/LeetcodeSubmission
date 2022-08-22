56ab7d8db9f978e2b034c7be91a6b44485e0e0cf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-22 13:35:09 +0900

    Time: 23 ms (16.72%), Space: 14 MB (78.54%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
-        int m = image.size(), n = image[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        DFS(image, visited, sr, sc, image[sr][sc], color);
-        
+        dfs(image, sr, sc, image[sr][sc], color);
         return image;
     }
     
-    void DFS(vector<vector<int>>& image, vector<vector<bool>>& visited, int sr, int sc, int origcolor, int color) {
+    void dfs(vector<vector<int>>& image, int sr, int sc, int origColor, int color) {
+        if (sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc]!=origColor || image[sr][sc] == color) {
+            return;
+        }
         
-        if (sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || visited[sr][sc] || image[sr][sc]!=origcolor) return;
-        
-        visited[sr][sc] = true;
         image[sr][sc] = color;
-        DFS(image, visited, sr+1, sc, origcolor, color);
-        DFS(image, visited, sr-1, sc, origcolor, color);
-        DFS(image, visited, sr, sc+1, origcolor, color);
-        DFS(image, visited, sr, sc-1, origcolor, color);
+        
+        dfs(image, sr+1, sc, origColor, color);
+        dfs(image, sr-1, sc, origColor, color);
+        dfs(image, sr, sc+1, origColor, color);
+        dfs(image, sr, sc-1, origColor, color);
     }
 };
