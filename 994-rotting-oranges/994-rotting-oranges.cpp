347c468ba84ab405583f30edfe2b4f91334482b7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-22 14:15:23 +0900

    Time: 3 ms (98.13%), Space: 13.1 MB (47.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
-    int dir[5] = {0, -1, 0, 1, 0};
     int orangesRotting(vector<vector<int>>& grid) {
-        typedef struct pair<int, pair<int, int>> pos;
-        queue<pos> q;
-        int rowSize = grid.size(), colSize = grid[0].size();
-        int freshN = 0;
-        for (int i=0; i<rowSize; i++) {
-            for (int j=0; j<colSize; j++) {
+        int freshNum = 0;
+        queue<pair<int, int>> Q;
+        for (int i=0; i<grid.size(); i++) {
+            for (int j=0; j<grid[0].size(); j++) {
+                freshNum += (grid[i][j] == 1);
                 if (grid[i][j] == 2) {
-                    q.push({0, {i, j}});
-                }else if (grid[i][j] == 1) {
-                    freshN++;
+                    Q.push({i, j});
                 }
             }
         }
-        if (freshN == 0) return 0;
         
-        int currR, currC, currStage, finalStage = -1;
-        while(!q.empty()){
-            pos curr = q.front(); q.pop();
-            for (int i=0; i<4; i++) {
-                currR = curr.second.first+dir[i];
-                currC = curr.second.second+dir[i+1];
-                currStage = curr.first + 1;
-                if (currR < 0 || currC < 0 || currR >= rowSize || currC >= colSize)
-                    continue;
-                if (grid[currR][currC] != 1)
-                    continue;
-                grid[currR][currC] = 2;
-                freshN--;
-                finalStage = max(currStage, finalStage);
-                q.push({currStage, {currR, currC}});
+        int dir[] = {-1, 0, 1, 0, -1};
+        int minutes = 0;
+        int i, j;
+        
+        while (!Q.empty()) {
+            int size = Q.size();
+            while (size-- > 0) {
+                auto top = Q.front(); Q.pop();
+                i = top.first, j = top.second;                
+                for (int d = 0; d < 4; d++) {
+                    if (i+dir[d] < 0 || i+dir[d] >= grid.size() || j+dir[d+1] < 0 || j+dir[d+1] >= grid[0].size())
+                        continue;
+                    if (grid[i+dir[d]][j+dir[d+1]] == 1) {
+                        freshNum--;
+                        grid[i+dir[d]][j+dir[d+1]] = 2;
+                        Q.push({i+dir[d], j+dir[d+1]});
+                    }
+                }
             }
+            if (Q.empty()) break;
+            minutes++;
         }
         
-        for (int i=0; i<grid.size(); i++){
-            for (int j=0; j<grid[0].size(); j++) {
-                cout << grid[i][j] << ' ';
-            }
-            cout << endl;
-        }
+        if (freshNum == 0) return minutes;
+        return -1;
         
-        if (freshN > 0) return -1;
-        else return finalStage;
     }
 };
