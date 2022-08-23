5f76b82eaf5abaab79b4b99920cafc003d4dfedf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-23 17:26:01 +0900

    Time: 22 ms (91.74%), Space: 10.5 MB (24.12%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,101 +1,112 @@
-// BFS gives TLE if we store path while traversing because whenever we find a better visit time for a word, we have to clear/make a new path vector everytime. 
-// The idea is to first use BFS to search from beginWord to endWord and generate the word-to-children mapping at the same time. 
-// Then, use DFS (backtracking) to generate the transformation sequences according to the mapping. 
-// The reverse DFS allows us to only make the shortest paths, never having to clear a whole sequence when we encounter better result in BFS
-// No string operations are done, by dealing with indices instead.
-
-
-
-class Solution {
-public:
-bool able(string s,string t){
-    int c=0;
-    for(int i=0;i<s.length();i++)
-        c+=(s[i]!=t[i]);
-    return c==1;
-}
-void bfs(vector<vector<int>> &g,vector<int> parent[],int n,int start,int end){
-    vector <int> dist(n,1005);
-    queue <int> q;
-    q.push(start);
-    parent[start]={-1};
-    dist[start]=0;
-    while(!q.empty()){
-        int x=q.front();
-        q.pop();
-        for(int u:g[x]){
-            if(dist[u]>dist[x]+1){
-                dist[u]=dist[x]+1;
-                q.push(u);
-                parent[u].clear();
-                parent[u].push_back(x);
-            }
-            else if(dist[u]==dist[x]+1)
-                parent[u].push_back(x);
+class Solution 
+{
+    vector<vector<string>> result;
+    
+    void printPath(const vector<vector<int>> &parents, const vector<string>& wordList, int currNode, vector<string> &path, int pathIndex)
+    {
+        if (parents[currNode].empty())
+        {
+            result.push_back(path);
+            return;
+        }
+        
+        for(auto &e: parents[currNode])
+        {
+            path[pathIndex] = wordList[e];
+            printPath(parents, wordList, e, path, pathIndex - 1);
         }
     }
-}
-void shortestPaths(vector<vector<int>> &Paths, vector<int> &path, vector<int> parent[],int node){
-    if(node==-1){
-        // as parent of start was -1, we've completed the backtrack
-        Paths.push_back(path);
-        return ;
-    }
-    for(auto u:parent[node]){
-        path.push_back(u);
-        shortestPaths(Paths,path,parent,u);
-        path.pop_back();
-    }
-}
-vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-    // start and end are indices of beginWord and endWord
-    int n=wordList.size(),start=-1,end=-1;
-    vector<vector<string>> ANS;
-    for(int i=0;i<n;i++){
-        if(wordList[i]==beginWord)
-            start=i;
-        if(wordList[i]==endWord)
-            end=i;
-    }
-    
-    // if endWord doesn't exist, return empty list
-    if(end==-1)
-        return ANS;
-    
-    // if beginWord doesn't exist, add it in start of WordList
-    if(start==-1){
-        wordList.emplace(wordList.begin(),beginWord);
-        start=0;
-        end++;
-        n++;
-    }
-    // for each word, we're making adjency list of neighbour words (words that can be made with one letter change)
-    // Paths will store all the shortest paths (formed later by backtracking)
-    vector<vector<int>> g(n,vector<int>()),Paths;
-    
-    // storing possible parents for each word (to backtrack later), path is the current sequence (while backtracking)
-    vector<int> parent[n],path;
-    
-    // creating adjency list for each pair of words in the wordList (including beginword)
-    for(int i=0;i<n-1;i++)
-        for(int j=i+1;j<n;j++)
-            if(able(wordList[i],wordList[j])){
-                g[i].push_back(j);
-                g[j].push_back(i);
+     
+public:
+    inline vector<vector<string>> findLadders(const string &beginWord, const string &endWord, const vector<string>& wordList) 
+    {
+        srand(time(NULL) ^ rand());
+        unordered_map<string, vector<int>> mp;
+        
+        string prefix = "";
+        bool endWordExists = false;
+        for(int i = 0; i < wordList.size(); ++i)
+        {
+            if(wordList[i] == endWord) endWordExists = true;
+            
+            prefix = "";
+            for(int j = 0; j < wordList[i].size(); ++j)
+            {
+                mp[prefix + "*" + wordList[i].substr(j+1)].push_back(i);
+                prefix += wordList[i][j];
             }
-    
-    bfs(g,parent,n,start,end); 
-    
-    // backtracking to make shortestpaths
-    shortestPaths(Paths,path,parent,end);
-    for(auto u:Paths){
-        vector <string> now;
-        for(int i=0;i<u.size()-1;i++)
-            now.push_back(wordList[u[i]]);
-        reverse(now.begin(),now.end());
-        now.push_back(wordList[end]);
-        ANS.push_back(now);
+        }
+        
+        if(!endWordExists) return vector<vector<string>>();
+        
+        queue<int> Q;
+        prefix = "";
+        vector<int> distance(wordList.size(), -1);
+        for(int i = 0; i < beginWord.size(); ++i)
+        {
+            if(mp.count(prefix + "*" + beginWord.substr(i+1)))
+            {
+                for(const auto &j: mp[prefix + "*" + beginWord.substr(i+1)])
+                {
+                    Q.push(j);
+                    distance[j] = 1;
+                }
+            }
+            prefix += beginWord[i];
+        }
+        
+        if (Q.empty()) return vector<vector<string>>();
+        
+        vector<int> endNodes;
+        vector<vector<int>> parents(wordList.size());
+        while(Q.size() && endNodes.empty())
+        {
+            int sz = Q.size() + 1;
+            while(--sz)
+            {
+                auto i = Q.front();
+                Q.pop();
+
+                if(wordList[i] == endWord)
+                {
+                    endNodes.push_back(i);
+                    continue;
+                }
+
+                prefix = "";
+                for(int j = 0; j < wordList[i].size(); ++j)
+                {
+                    if(mp.count(prefix + "*" + wordList[i].substr(j+1)))
+                    {
+                        for(const auto &k: mp[prefix + "*" + wordList[i].substr(j+1)])
+                        {
+                            if(distance[k] == -1)
+                            {
+                                Q.push(k);
+                                parents[k].push_back(i);
+                                distance[k] = distance[i] + 1;
+                            }
+                            else if (distance[k] == distance[i] + 1)
+                            {
+                                parents[k].push_back(i);
+                            }
+                        }
+                    }
+                    prefix += wordList[i][j];
+                }
+            }
+        }
+        
+        if (endNodes.empty()) return vector<vector<string>>();
+        
+        vector<string> path(distance[endNodes.front()] + 1);
+        path[0] = beginWord;
+        for(auto &e: endNodes)
+        {
+            path[distance[endNodes.front()]] = wordList[e];
+            printPath(parents, wordList, e, path, distance[endNodes.front()] - 1);
+        }
+        
+        return result;
     }
-    return ANS;
-}
-}; 
+};
