527d8de738847e642c1b2f2c826013a847119357
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-24 20:14:26 +0900

    Time: 38 ms (44.49%), Space: 14.6 MB (26.35%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<bool> visited(numCourses, false);
+        vector<int> indegrees(numCourses, 0);
+        vector<vector<int>> adjList(numCourses);
+        queue<int> Q;
+        
+        for (auto pre : prerequisites) {
+            adjList[pre[1]].push_back(pre[0]);
+            adjList[pre[0]].push_back(pre[1]);
+            indegrees[pre[1]]++;
+        }
+        
+        for (int i=0; i<indegrees.size(); i++){
+            if (indegrees[i] == 0) {
+                Q.push(i);
+                visited[i] = true;
+            }
+        }
+        
+        int cur;
+        int cnt = numCourses;
+        while (!Q.empty()) {
+            cur = Q.front(); Q.pop();
+            numCourses--;
+            
+            for (int i : adjList[cur]) {
+                if (!visited[i] && --indegrees[i] == 0) {
+                    visited[i] = true;
+                    Q.push(i);
+                }
+                
+            }
+        }
+            
+        return (numCourses == 0);
+    }
+};
