737b8f788f9b5f2552dbd521c112f3a2d07844d4
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-24 23:48:01 +0900

    Time: 19 ms (96.02%), Space: 14.2 MB (46.73%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
-        vector<vector<int>> adjList(numCourses);
-        vector<bool> visited(numCourses, false);
-        vector<int> indegree(numCourses);
-        queue<int> Q;
         
+        vector<vector<int>> adjList(numCourses);
+        vector<int> visited(numCourses);
         vector<int> ans;
         
         for (auto &p : pre) {
             adjList[p[1]].push_back(p[0]);
-            indegree[p[0]]++;
         }
         
-        int cnt = 0;
-        bool isCycle = true;
-        for (int i=0; i<numCourses; i++) {
-            if (indegree[i] == 0) {
-                isCycle = false;
-                Q.push(i);
-                cnt++;
-                visited[i] = true;
-            }
-        }
-        if (isCycle) return {};
+        for (int i=0; i<numCourses; i++) 
+            dfs(i, adjList, visited, ans);
         
-        int cur;
-        while (!Q.empty()) {
-            cur = Q.front(); Q.pop();
-            ans.push_back(cur);
-            
-            for (int &nei : adjList[cur]) {
-                indegree[nei]--;
-                if (!visited[nei] && indegree[nei] == 0) {
-                    Q.push(nei);
-                    cnt++;
-                    visited[nei] = true;
-                }
-            }
-        }
+        if (ans.size()!= numCourses)
+            return {};
         
-        if (cnt < numCourses) return {};
+        reverse(ans.begin(), ans.end());
         return ans;
+        
+    }
+    
+    void dfs(int cur, vector<vector<int>>& adjList, vector<int>& visited, vector<int>& ans) {
+        
+        if (visited[cur] == 2)
+            return;
+        
+        if (visited[cur] == 1) {
+            ans.push_back(-1);
+            return;
+        }    
+        
+        visited[cur] = 1;
+        
+        for (int v : adjList[cur]) {
+            dfs(v, adjList, visited, ans);
+        }
+        
+        visited[cur] = 2;
+        ans.push_back(cur);
     }
 };
