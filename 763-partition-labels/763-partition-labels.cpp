604276aafce38031391487c0eb9edd7a39900199
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-26 01:38:59 +0900

    Time: 8 ms (41.99%), Space: 6.9 MB (23.61%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
+        vector<int> begin(26, -1);
         vector<int> end(26, -1);
         
-        for (int i=0; i<s.length(); i++)
-            end[s[i]-'a'] = i;
-
-        vector<int> ans;
-        int curIdx, startIdx = -1, endIdx = -1;
         for (int i=0; i<s.length(); i++) {
-            curIdx = s[i]-'a';
-            if (startIdx == -1) {
-                startIdx = i;
-                endIdx = end[curIdx];
-            }
-            
-            else if (i < endIdx && end[curIdx] > endIdx)
-                endIdx = end[curIdx];
-            
-            if (i == endIdx){
-                ans.push_back(endIdx-startIdx+1);
-                startIdx = -1;
-                endIdx = -1;
-            }
-            
+            if (begin[s[i]-'a'] < 0)
+                begin[s[i]-'a'] = i;
+            end[s[i]-'a'] = i;
         }
+        
+        vector<pair<int, int>> intervals;
+
+        for (int i=0; i<26; i++) {
+            if (begin[i] < 0) continue;
+            // cout << (char)(i +'a') << ": " << begin[i] << "|" << end[i] << endl;
+            intervals.push_back({begin[i], end[i]});
+        }
+    
+        sort(intervals.begin(), intervals.end());
+        
+        
+        vector<int> ans;
+        int startIdx = -1, endIdx = -1;
+        for (auto i : intervals) {
+            cout << i.first << "/" << i.second << endl;
+            if (startIdx == -1) {
+                cout << "case1" << endl;
+                startIdx = i.first;
+                endIdx = i.second;
+            }
+            else if (i.first > endIdx) {
+                cout << "case2" << endl;
+                ans.push_back(endIdx-startIdx+1);
+                startIdx = i.first;
+                endIdx = i.second;
+            }            
+            else if (i.second > endIdx) {
+                cout << "case3" << endl;
+                endIdx = i.second;
+            }
+        }
+        ans.push_back(endIdx-startIdx+1);
         
         return ans;
         
