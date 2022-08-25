c67712a28a1fe5b21a9a6ab15107153307626760
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-25 20:15:16 +0900

    Time: 314 ms (78.89%), Space: 61.3 MB (74.30%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& pre, vector<vector<int>>& queries) {
+        
+        vector<vector<int>> adjList(numCourses);
+        vector<vector<bool>> isReachable(numCourses, vector<bool>(numCourses, false));
+        
+        queue<int> Q;
+        
+        for (auto &p : pre) {
+            adjList[p[0]].push_back(p[1]);
+        }
+        
+        for (int i=0; i<numCourses; i++) {
+            Q.push(i);
+            
+            int cur;
+            while(!Q.empty()) {
+                cur = Q.front(); Q.pop();
+                
+                for (int c : adjList[cur]) {
+                    if (isReachable[i][c]) continue;
+                    isReachable[i][c] = true;
+                    Q.push(c);
+                }
+            }
+        }
+
+        vector<bool> ans;
+        for (auto &q : queries) 
+            ans.push_back(isReachable[q[0]][q[1]]);
+        
+        return ans;
+    }
+};
