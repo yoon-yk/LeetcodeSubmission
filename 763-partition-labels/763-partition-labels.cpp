c802a55b81ca2f4dab9ef64504dbdbccd668f650
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-26 00:20:04 +0900

    Time: 4 ms (74.78%), Space: 6.5 MB (89.73%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> begin(26, -1);
         vector<int> end(26, -1);
         
-        for (int i=0; i<s.length(); i++) {
-            // if (begin[s[i]-'a'] < 0)
-            //     begin[s[i]-'a'] = i;
+        for (int i=0; i<s.length(); i++)
             end[s[i]-'a'] = i;
-        }
 
         vector<int> ans;
         int curIdx, startIdx = -1, endIdx = -1;
@@ -18,13 +14,11 @@ public:
                 startIdx = i;
                 endIdx = end[curIdx];
             }
-            else if (i < endIdx && end[curIdx] > endIdx){
+            
+            else if (i < endIdx && end[curIdx] > endIdx)
                 endIdx = end[curIdx];
-            }
-            // cout << s[i] << "::" << startIdx << "::" << endIdx << endl;
             
             if (i == endIdx){
-                // cout << "case" << i << "/" << endIdx << endl;
                 ans.push_back(endIdx-startIdx+1);
                 startIdx = -1;
                 endIdx = -1;
