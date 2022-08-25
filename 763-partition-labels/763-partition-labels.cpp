d726d0c45b477e34d71623b6905339dffa753b8b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-26 01:53:50 +0900

    Time: 3 ms (88.12%), Space: 6.6 MB (68.81%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> begin(26, -1);
         vector<int> end(26, -1);
         
-        for (int i=0; i<s.length(); i++) {
-            if (begin[s[i]-'a'] < 0)
-                begin[s[i]-'a'] = i;
+        for (int i=0; i<s.length(); i++)
             end[s[i]-'a'] = i;
-        }
-        
-        set<pair<int, int>> intervals;
 
-        for (int i=0; i<26; i++) {
-            if (begin[i] < 0) continue;
-            intervals.insert({begin[i], end[i]});
-        }
-    
-        // sort(intervals.begin(), intervals.end());
-    
         vector<int> ans;
-        int startIdx = -1, endIdx = -1;
-        for (auto &i : intervals) {
-            if (startIdx == -1) {
-                startIdx = i.first;
-                endIdx = i.second;
-            }
-            else if (i.first > endIdx) {
+        int startIdx = 0, endIdx = 0;
+        for (int i=0; i<s.length(); i++) {
+            endIdx = max(endIdx, end[s[i]-'a']);
+            if (i == endIdx){
                 ans.push_back(endIdx-startIdx+1);
-                startIdx = i.first;
-                endIdx = i.second;
-            }            
-            else if (i.second > endIdx) {
-                endIdx = i.second;
+                startIdx = i+1;
             }
         }
-        ans.push_back(endIdx-startIdx+1);
         
         return ans;
         
