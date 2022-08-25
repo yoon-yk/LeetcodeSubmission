8b4a485a7f6985774cae838b2714d7dea00cf73f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-26 00:19:29 +0900

    Time: 0 ms (100.00%), Space: 6.6 MB (68.81%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -5,23 +5,17 @@ public:
         vector<int> end(26, -1);
         
         for (int i=0; i<s.length(); i++) {
-            if (begin[s[i]-'a'] < 0)
-                begin[s[i]-'a'] = i;
+            // if (begin[s[i]-'a'] < 0)
+            //     begin[s[i]-'a'] = i;
             end[s[i]-'a'] = i;
         }
-        
-        // for (int i=0; i<26; i++) {
-        //     if (begin[i] < 0) continue;
-        //     cout << (char)(i+'a') << "| start- : " << begin[i] << "::" << end[i] << endl;
-        // }
-        
-        
+
         vector<int> ans;
         int curIdx, startIdx = -1, endIdx = -1;
         for (int i=0; i<s.length(); i++) {
             curIdx = s[i]-'a';
             if (startIdx == -1) {
-                startIdx = begin[curIdx];
+                startIdx = i;
                 endIdx = end[curIdx];
             }
             else if (i < endIdx && end[curIdx] > endIdx){
