ee76301c7e12a25cbad781bc0b936b52a6ce5a18
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-25 21:12:11 +0900

    Time: 354 ms (69.34%), Space: 60.9 MB (75.41%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& pre, vector<vector<int>>& queries) {
         
+        vector<int> indegree(numCourses, 0);
         vector<vector<int>> adjList(numCourses);
         vector<vector<bool>> isReachable(numCourses, vector<bool>(numCourses, false));
         
@@ -9,22 +10,31 @@ public:
         
         for (auto &p : pre) {
             adjList[p[0]].push_back(p[1]);
+            // isReachable[p[0]][p[1]] = true;
+            indegree[p[1]]++;
         }
         
-        for (int i=0; i<numCourses; i++) {
-            Q.push(i);
-            
-            int cur;
-            while(!Q.empty()) {
-                cur = Q.front(); Q.pop();
+        for (int i=0; i<numCourses; i++)
+            if (indegree[i] == 0)
+                Q.push(i);
+        
+        int cur;
+        while (!Q.empty()) {
+            cur = Q.front(); Q.pop();
                 
-                for (int& c : adjList[cur]) {
-                    if (isReachable[i][c]) continue;
-                    isReachable[i][c] = true;
-                    Q.push(c);
+            for (int& c : adjList[cur]) {
+                isReachable[cur][c] = true;
+                for (int i=0; i<numCourses; i++) {
+                    if (isReachable[i][cur])
+                        isReachable[i][c] = true;
                 }
+                
+                indegree[c]--;
+                if (indegree[c] == 0) 
+                    Q.push(c);
             }
         }
+
 
         vector<bool> ans;
         for (auto &q : queries) 
