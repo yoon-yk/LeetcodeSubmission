eebc15f748cd41f5b22272d8ee36e043c19b1f1d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-25 11:01:20 +0900

    Time: 16 ms (80.72%), Space: 8.9 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> dict(26, 0);
+        for (char& c:magazine)
+            dict[c-'a']++;
+        
+        for (char& c : ransomNote)
+            if (--dict[c-'a'] < 0)
+                return false;
+        
+        return true;
+    }
+};
