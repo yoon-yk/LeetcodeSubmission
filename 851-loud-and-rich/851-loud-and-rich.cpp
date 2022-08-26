550dc24d9da102c2c9ab9d5a4b3bd4888a80e84b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-26 18:21:11 +0900

    Time: 110 ms (95.85%), Space: 42.5 MB (82.76%) - LeetHub

---
+++ 851-loud-and-rich/851-loud-and-rich.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        int n=quiet.size();
+        vector<int> ans(n);
+        vector<vector<int>> adjList(n);
+        vector<int> indeg(n, 0);
+        queue<int> Q;
+        
+        for (int i=0; i<n; i++)
+            ans[i] = i;
+        
+        for (auto &r : richer){
+            adjList[r[1]].push_back(r[0]);
+            indeg[r[0]]++;
+        }
+        
+        for (int i=0; i<n; i++) {
+            if (indeg[i] == 0)
+                Q.push(i);
+        }
+        
+        int cur;
+        while (!Q.empty()) {
+            cur = Q.front(); Q.pop();
+            dfs(cur, adjList, quiet, ans);
+        }
+        
+        return ans;
+        
+    }
+    
+    int dfs (int cur, vector<vector<int>>& adjList, vector<int>& quiet, vector<int>& ans) {
+        
+        int minQ = ans[cur];
+        
+        int neiQ;
+        for (int &nei : adjList[cur]) {
+            if (ans[nei] != nei) neiQ = ans[nei];
+            else neiQ = dfs(nei, adjList, quiet, ans);
+            
+            if (quiet[neiQ] < quiet[minQ])
+                minQ = neiQ;
+        }
+        
+        return ans[cur] = minQ; 
+    }
+};
