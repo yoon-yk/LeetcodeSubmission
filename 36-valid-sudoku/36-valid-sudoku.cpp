2e56182883c2c03cac2d1a4f25d140e2967faf50
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-29 18:34:17 +0900

    Time: 39 ms (46.43%), Space: 18.6 MB (60.20%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i=0; i<9; i++){
+            
+            // row
+            vector<int> check(10, 1);
+            for (int j=0; j<9; j++) {
+                if (board[i][j] == '.') continue;
+                else if (--check[board[i][j]-'0'] < 0) return false;
+            }
+            
+            
+            // col
+            vector<int> check2(10, 1);
+            for (int j=0; j<9; j++) {
+                if (board[j][i] == '.') continue;
+                else if (--check2[board[j][i]-'0'] < 0) return false;
+            }
+        }
+        
+        int curi, curj;
+        for (int i=0; i<9; i++) {
+            vector<int> check(10, 1);
+            for (int j=0; j<9; j++) {
+                curi = ((i/3)*3 + j/3);
+                curj = ((i%3)*3) + j%3;
+                if (board[curi][curj] == '.') continue;
+                else if (--check[board[curi][curj]-'0'] < 0) return false;
+            }
+        }
+        
+        return true;
+    }
+};
