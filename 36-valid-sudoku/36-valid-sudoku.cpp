35b793adef41941b3e3e5db18151cd5ca6ac0d23
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-29 18:43:23 +0900

    Time: 19 ms (95.33%), Space: 18.5 MB (62.44%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,25 +3,28 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         
         for (int i=0; i<9; i++){
-            vector<int> check(10, 1);
+            vector<bool> check(10, false);
             for (int j=0; j<9; j++) {
                 if (board[i][j] == '.') continue;
-                else if (--check[board[i][j]-'0'] < 0) return false;
+                else if (check[board[i][j]-'0']) return false;
+                check[board[i][j]-'0'] = true;
             }
 
-            vector<int> check2(10, 1);
+            vector<bool> check2(10, false);
             for (int j=0; j<9; j++) {
                 if (board[j][i] == '.') continue;
-                else if (--check2[board[j][i]-'0'] < 0) return false;
+                else if (check2[board[j][i]-'0']) return false;
+                check2[board[j][i]-'0'] = true;
             }
         
             int curi, curj;
-            vector<int> check3(10, 1);
+            vector<bool> check3(10, false);
             curi = (i/3)*3;
             curj = ((i%3)*3);
             for (int j=0; j<9; j++) {
                 if (board[curi+j/3][curj+j%3] == '.') continue;
-                else if (--check3[board[curi+j/3][curj+j%3]-'0'] < 0) return false;
+                else if (check3[board[curi+j/3][curj+j%3]-'0']) return false;
+                check3[board[curi+j/3][curj+j%3]-'0'] = true;
             }   
         }
         
