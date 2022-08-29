03671be051d7e959ec355a38239a7c26a711bbf2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-29 13:48:13 +0900

    Time: 227 ms (84.85%), Space: 59.6 MB (96.82%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -2,45 +2,24 @@ class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         
-        queue<int> Q;
-        vector<int> indegree(n);
+        vector<vector<int>> adjList(n);
         vector<vector<int>> ans(n);
 
-        vector<vector<int>> adjList(n);
-        vector<vector<bool>> isAncestor(n, vector<bool>(n, false)); // i is an ancestor of j
-
-        for (auto& e : edges) {
+        for (auto& e : edges) 
             adjList[e[0]].push_back(e[1]);
-            indegree[e[1]]++;
-        }
         
         for (int i=0; i<n; i++) 
-            if (indegree[i] == 0)
-                Q.push(i);
-        
-        int size, cur;
-        while (!Q.empty()) {
-            cur = Q.front(); Q.pop();
-            
-            for (int k=0; k<n; k++) 
-                if (isAncestor[k][cur]) 
-                    for (int& nei : adjList[cur])
-                        isAncestor[k][nei] = true; 
-            
-            for (int& nei : adjList[cur]) {
-                indegree[nei]--;
-                if (indegree[nei] == 0)
-                    Q.push(nei);
-                isAncestor[cur][nei] = true;
-            }
-        }
-        
-        for (int cur=0; cur<n; cur++) 
-            for (int j=0; j<n; j++) 
-                if (isAncestor[j][cur])
-                    ans[cur].push_back(j);
+            dfs(i, i, ans, adjList);
         
         return ans;
-        
+    }
+    
+    void dfs(int root, int curr, vector<vector<int>>& ans, vector<vector<int>>& adjList) {
+        for (auto& nei : adjList[curr]) {
+            if (ans[nei].empty() || ans[nei].back()!=root) {
+                ans[nei].push_back(root);
+                dfs(root, nei, ans, adjList);
+            }
+        }
     }
 };
