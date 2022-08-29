7c6df512bdced73bb9530a247edb9c459d5d7c00
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-29 18:35:37 +0900

    Time: 42 ms (38.95%), Space: 18.8 MB (58.66%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         for (int i=0; i<9; i++){
-            
-            // row
             vector<int> check(10, 1);
             for (int j=0; j<9; j++) {
                 if (board[i][j] == '.') continue;
                 else if (--check[board[i][j]-'0'] < 0) return false;
             }
-            
-            
-            // col
+        }
+        
+        for (int i=0; i<9; i++) {
             vector<int> check2(10, 1);
             for (int j=0; j<9; j++) {
                 if (board[j][i] == '.') continue;
                 else if (--check2[board[j][i]-'0'] < 0) return false;
             }
         }
+        
         
         int curi, curj;
         for (int i=0; i<9; i++) {
