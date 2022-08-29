682e1bbdbc54faff0992e247a3419b4ec7938a41
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-08-29 13:19:43 +0900

    Time: 43 ms (79.06%), Space: 12.5 MB (44.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    
-    vector<int> dir = {-1, 0, 1, 0, -1};
-    
+    int dir[5] = {-1, 0, 1, 0, -1};
     int numIslands(vector<vector<char>>& grid) {
         
-        int cnt = 0;
+        int cnt = 0, r, c;
+        int rlim = grid.size(), clim = grid[0].size();
+        stack<pair<int, int>> st;
         
-        for (int i=0; i<grid.size(); i++) {
-            for (int j=0; j<grid[0].size(); j++) {
-                if (grid[i][j] == '1') {
-                    cnt++;
-                    dfs(i, j, grid);
+        for (int i=0; i<rlim; i++) {
+            for (int j=0; j<clim; j++) {
+                if (grid[i][j] != '1') continue;
+                
+                cnt ++;
+                r = i, c = j;
+                st.push({r, c});
+                
+                while (!st.empty()) {
+                    auto cur = st.top(); st.pop();
+                    grid[cur.first][cur.second] = '0';
+                    
+                    for (int d = 0; d < 4; d++)
+                        if ((cur.first+dir[d] >= 0) && (cur.first+dir[d] < rlim) && 
+                            (cur.second+dir[d+1] >= 0) && (cur.second+dir[d+1] < clim) && grid[cur.first+dir[d]][cur.second+dir[d+1]] == '1')
+                            st.push({cur.first+dir[d], cur.second+dir[d+1]});
                 }
+                
             }
         }
         
         return cnt;
-    }
-    
-    void dfs(int r, int c, vector<vector<char>>& grid) {
-        if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] == '0')
-            return;
-        
-        grid[r][c] = '0';
-        
-        for (int i=0; i<4; i++) {
-            dfs(r+dir[i], c+dir[i+1], grid);
-        }
     }
 };
