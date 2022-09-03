3b0504227e646ac20830c650fbabbf35cfff4022
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-03 13:04:44 +0900

    Time: 50 ms (85.70%), Space: 23.6 MB (21.27%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
 
     vector<int> dir = {-1, 0, 1, 0, -1};
-    vector<vector<int>> ans;
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         
         int rlim = heights.size(), clim = heights[0].size();
+        vector<vector<int>> ans;
         vector<vector<short>> vis(rlim, vector<short>(clim));
         
         for (int i=0; i<rlim; i++){
-            dfs(i, 0, -1, heights, vis, 1);
-            dfs(i, clim-1, -1, heights, vis, 2);
+            dfs(i, 0, -1, heights, vis, 1, ans);
+            dfs(i, clim-1, -1, heights, vis, 2, ans);
         }
         
         
         for (int i=0; i<clim; i++){
-            dfs(0, i, -1, heights, vis, 1);
-            dfs(rlim-1, i, -1, heights, vis, 2);
+            dfs(0, i, -1, heights, vis, 1, ans);
+            dfs(rlim-1, i, -1, heights, vis, 2, ans);
         }        
         
         return ans;
     }
     
-    void dfs(int r, int c, int prev, vector<vector<int>>& heights, vector<vector<short>>& vis, int mark) {
+    void dfs(int r, int c, int prev, vector<vector<int>>& heights, vector<vector<short>>& vis, int mark, vector<vector<int>>& ans) {
         
         if (r < 0 || r >= heights.size() || c < 0 || c >= heights[0].size() || (vis[r][c] & mark) == mark || heights[r][c] < prev)
             return;
@@ -33,7 +33,7 @@ public:
             ans.push_back({r, c});
         
         for (int d = 0; d < 4; d++) {
-            dfs(r+dir[d], c+dir[d+1], heights[r][c], heights, vis, mark);
+            dfs(r+dir[d], c+dir[d+1], heights[r][c], heights, vis, mark, ans);
         }
     }
 };
