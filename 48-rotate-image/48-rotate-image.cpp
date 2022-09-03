c892056e0a6fd7de6fae8bd72fb8fd89b0ec9875
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-03 19:13:54 +0900

    Time: 16 ms (5.36%), Space: 7.5 MB (33.45%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> ans(n, vector<int>(n));
+        unordered_map<string, int> hashM;
+        
         for (int r=0; r<n; r++) {
             for (int c=0; c<n; c++) {
-                ans[c][n-r-1] = matrix[r][c]; 
+                hashM[to_string(c)+"/"+to_string(n-r-1)] = matrix[r][c]; 
             }
         }
         
         for (int r=0; r<n; r++) {
             for (int c=0; c<n; c++) {
-                matrix[r][c] = ans[r][c]; 
+                matrix[r][c] = hashM[to_string(r)+"/"+to_string(c)];
             }
         }
     }
