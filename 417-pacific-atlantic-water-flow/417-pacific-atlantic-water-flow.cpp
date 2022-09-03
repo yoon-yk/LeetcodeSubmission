8cdbf571e4b8d0d82ecf0816c494fc98c9deb279
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-03 13:02:09 +0900

    Time: 98 ms (32.21%), Space: 24.6 MB (12.50%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -7,34 +7,33 @@ public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         
         int rlim = heights.size(), clim = heights[0].size();
-        vector<vector<bool>> pacificVis(rlim, vector<bool>(clim));
-        vector<vector<bool>> atlanticVis(rlim, vector<bool>(clim));
+        vector<vector<int>> vis(rlim, vector<int>(clim));
         
         for (int i=0; i<rlim; i++){
-            dfs(i, 0, -1, heights, pacificVis, atlanticVis);
-            dfs(i, clim-1, -1, heights, atlanticVis, pacificVis);
+            dfs(i, 0, -1, heights, vis, 1);
+            dfs(i, clim-1, -1, heights, vis, 2);
         }
         
         
         for (int i=0; i<clim; i++){
-            dfs(0, i, -1, heights, pacificVis, atlanticVis);
-            dfs(rlim-1, i, -1, heights, atlanticVis, pacificVis);
+            dfs(0, i, -1, heights, vis, 1);
+            dfs(rlim-1, i, -1, heights, vis, 2);
         }        
         
         return ans;
     }
     
-    void dfs(int r, int c, int prev, vector<vector<int>>& heights, vector<vector<bool>>& mvis, vector<vector<bool>>& cvis) {
+    void dfs(int r, int c, int prev, vector<vector<int>>& heights, vector<vector<int>>& vis, int mark) {
         
-        if (r < 0 || r >= heights.size() || c < 0 || c >= heights[0].size() || mvis[r][c] || heights[r][c] < prev)
+        if (r < 0 || r >= heights.size() || c < 0 || c >= heights[0].size() || (vis[r][c] & mark) == mark || heights[r][c] < prev)
             return;
         
-        mvis[r][c] = true;
-        if (cvis[r][c])
+        vis[r][c] |= mark;
+        if (vis[r][c] == 3)
             ans.push_back({r, c});
         
         for (int d = 0; d < 4; d++) {
-            dfs(r+dir[d], c+dir[d+1], heights[r][c], heights, mvis, cvis);
+            dfs(r+dir[d], c+dir[d+1], heights[r][c], heights, vis, mark);
         }
     }
 };
