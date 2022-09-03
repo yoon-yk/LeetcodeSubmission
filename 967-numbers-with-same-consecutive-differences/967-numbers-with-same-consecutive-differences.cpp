ff54343cd211e2609ff021f48e2c79c8229ce07f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-03 15:09:02 +0900

    Time: 24 ms (5.89%), Space: 9.8 MB (10.75%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -2,40 +2,46 @@ class Solution {
 public:
     vector<int> numsSameConsecDiff(int n, int k) {
 
-        string curS;
         vector<int> ans;
+        queue<string> Q;
         unordered_map<string, int> vis;
         
         for (int i=1; i<=9; i++) {
-            curS = to_string(i);
-            mutation (n, k, vis, curS, ans);
+            Q.push(to_string(i));
+        }
+        
+        string curS;
+        while (!Q.empty()) {
+            
+            curS = Q.front(); Q.pop();
+            
+            if (curS.length() == n) {
+                ans.push_back(stoi(curS));
+                continue;
+            }
+        
+            int prev = curS.back() - '0';
+            if (prev + k < 10) {
+                curS += (prev + k) + '0';
+                if (vis[curS] == 0) {
+                    Q.push(curS);
+                    vis[curS]++;
+                }
+                curS.pop_back();
+            }
+
+            if (prev - k >= 0) {
+                curS += (prev - k) + '0';
+                if (vis[curS] == 0) {
+                    Q.push(curS);
+                    vis[curS]++;
+                }
+                curS.pop_back();        
+            }
+            
         }
         
         return ans;
     }
     
-    void mutation (int n, int k, unordered_map<string, int>& vis, string& curS, vector<int>& ans) {
-        
-        if (curS.length() == n) {
-            if (!vis.count(curS)) {
-                vis[curS]++;
-                ans.push_back(stoi(curS));
-            }
-            return;
-        }
-        
-        int prev = curS.back() - '0';
-        if (prev + k < 10) {
-            curS += (prev + k) + '0';
-            mutation(n, k, vis, curS, ans);
-            curS.pop_back();
-        }
-
-        if (prev - k >= 0) {
-            curS += (prev - k) + '0';
-            mutation(n, k, vis, curS, ans);
-            curS.pop_back();        
-        }
-        
-    }
 };
