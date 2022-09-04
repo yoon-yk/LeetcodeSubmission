4a3face536fc19a84357ff09f4a027f5af8f1fcf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-04 23:41:15 +0900

    Time: 15 ms (16.40%), Space: 12 MB (93.87%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,21 +13,25 @@ class Solution {
 public:
     
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        unordered_map<int, vector<int>> m;
-        queue<pair<int, TreeNode*>> Q;
         
+        unordered_map<int, vector<int>> m; // <# of vertical order, vector of node values>
+        queue<pair<int, TreeNode*>> Q; // Make a pair of <vertical order, TreeNode pointer>  
+       
         auto compare = [] (pair<int, TreeNode*> A, pair<int, TreeNode*> B) {
-            return (A.second->val > B.second->val);
+            return (A.second->val > B.second->val); 
         };
         
-        priority_queue<pair<int, TreeNode*>, vector<pair<int, TreeNode*>>, decltype(compare)> pq(compare);
-        Q.push({0, root});
+        priority_queue<pair<int, TreeNode*>, vector<pair<int, TreeNode*>>, decltype(compare)> pq(compare); // Declare min heap to sort the nodes by values in every level  
         
-        int left = 0, right = 0, size;
+        int left = 0, right = 0; // Keep the most lowest vertical level and highest vertical level so we can access the level in order later when inserting to answer vector.
+        int size;
+        
+        Q.push({0, root});
+
         while (!Q.empty()) {
             size = Q.size();
             
-            while (size--) {
+            while (size--) { // To access by (horizontal) level 
                 auto cur = Q.front(); Q.pop();
                 m[cur.first].push_back(cur.second->val);
                 left = min(left, cur.first);
@@ -40,7 +44,7 @@ public:
                     pq.push({cur.first+1, cur.second->right});
             }
             
-            while (!pq.empty()) {
+            while (!pq.empty()) { // Push into the queues in order of node value
                 auto cur = pq.top(); pq.pop();
                 Q.push(cur);
             }
