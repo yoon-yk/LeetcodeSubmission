2601b8dcfbf7efa0aa97cdc6ac3e66e51e66c584
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-04 16:12:14 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (33.40%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        // complement of matrix 
         int n = matrix.size();
-        int tmp, ilim = (n+1)>>1, jlim = n>>1; 
-        for (int i=0; i<ilim; i++) {
-            for (int j=0; j<jlim; j++) {
-                tmp = matrix[n-1-j][i];
-                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
-                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
-                matrix[j][n-1-i] = matrix[i][j];
-                matrix[i][j] = tmp;
+        for(int i=0; i<n; ++i) 
+            for(int j=i; j<n; ++j) 
+                swap(matrix[i][j], matrix[j][i]);
+            
+
+        for (int i=0; i<n; ++i) {
+        // 2 Pointer approach :  just like we do in 1D array we take left and right pointers
+        // and swap the values and then make those pointers intersect at some point.
+            int left = 0, right = n-1;
+            while (left < right) {
+                swap(matrix[i][left], matrix[i][right]);
+                ++left;
+                --right;
             }
         }
-        
     }
 };
