cbd738bfb2365188411e7053e2b91d4ccea6d584
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-05 19:22:23 +0900

    Time: 223 ms (40.30%), Space: 255.8 MB (33.83%) - LeetHub

---
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* findRoot(vector<Node*> tree) {
+        unordered_map<Node*, Node*> parent;
+        
+        Node* cur;
+        for (auto& nd : tree) {
+            if (!nd) continue;
+            cur = nd;
+            for (auto& ch : nd->children) {
+                parent[ch] = nd;
+            }
+        }
+        
+        
+        while (parent.count(cur)){
+            cur = parent[cur];
+        }
+        
+        return cur;
+    }
+};
