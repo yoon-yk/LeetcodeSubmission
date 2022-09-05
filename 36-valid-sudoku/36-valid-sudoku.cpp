dcc395adbffa597124e4b739cbfd8ae0c5931b62
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-06 02:04:35 +0900

    Time: 51 ms (21.85%), Space: 19.4 MB (47.16%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,13 +3,14 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int n = board.size();
         unordered_map<int, int> visit;
+        int curN, curR,curC, curB;
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 if (board[i][j] == '.') continue;
-                int curN = board[i][j] - '0';
-                int curR = i*1000 + curN*100;
-                int curC = j*100 + curN*10;
-                int curB = ((i/3)*3+j/3)*10 + curN;
+                curN = board[i][j] - '0';
+                curR = i*1000 + curN*100;
+                curC = j*100 + curN*10;
+                curB = ((i/3)*3+j/3)*10 + curN;
                 if (visit.count(curR) || visit.count(curC) || visit.count(curB))
                     return false;
                 visit[curR]++, visit[curC]++, visit[curB]++;
