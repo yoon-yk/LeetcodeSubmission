c2f62f5d62e6968a61f7911ed43df8634de599b9
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-06 02:02:07 +0900

    Time: 71 ms (5.37%), Space: 19.4 MB (47.16%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        
-        for (int i=0; i<9; i++){
-            vector<bool> check(10, false);
-            for (int j=0; j<9; j++) {
+        int n = board.size();
+        unordered_map<int, int> visit;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
                 if (board[i][j] == '.') continue;
-                else if (check[board[i][j]-'0']) return false;
-                check[board[i][j]-'0'] = true;
+                int curN = board[i][j] - '0';
+                
+                if (visit.count(i*1000 + curN*100) || visit.count(j*100 + curN*10) || visit.count(((i/3)*3+j/3)*10 + curN))
+                    return false;
+                visit[i*1000 + curN*100]++, visit[j*100 + curN*10]++, visit[((i/3)*3+j/3)*10 + curN]++;
             }
-
-            vector<bool> check2(10, false);
-            for (int j=0; j<9; j++) {
-                if (board[j][i] == '.') continue;
-                else if (check2[board[j][i]-'0']) return false;
-                check2[board[j][i]-'0'] = true;
-            }
-        
-            int basei, basej, curi, curj;
-            vector<bool> check3(10, false);
-            basei = (i/3)*3;
-            basej = ((i%3)*3);
-            for (int j=0; j<9; j++) {
-                curi = basei+j/3, curj = basej+j%3;
-                if (board[curi][curj] == '.') continue;
-                else if (check3[board[curi][curj]-'0']) return false;
-                check3[board[curi][curj]-'0'] = true;
-            }   
         }
-        
         return true;
     }
 };
