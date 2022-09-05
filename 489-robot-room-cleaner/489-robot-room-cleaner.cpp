780454b7610bef2db16496a848a14029d7af89d7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-05 17:18:11 +0900

    Time: 8 ms (96.88%), Space: 8.3 MB (92.96%) - LeetHub

--- 489-robot-room-cleaner/489-robot-room-cleaner.cpp
+++ 489-robot-room-cleaner/489-robot-room-cleaner.cpp
@@ -23,7 +23,7 @@ public:
     vector<int> direc = {-1, 0, 1, 0, -1};
     
     void cleanRoom(Robot& robot) {
-        unordered_map<string, int> m;
+        unordered_map<int, int> m;
             
         int curR = 0, curC = 0;
         dfs(robot, curR, curC, 0, m);
@@ -37,20 +37,20 @@ public:
         robot.turnRight();
     }
     
-    void dfs(Robot& robot, int curR, int curC, int dir, unordered_map<string, int>& m) {
+    void dfs(Robot& robot, int curR, int curC, int dir, unordered_map<int, int>& m) {
         
         robot.clean();
-        string curLoc = to_string(curR) +'/'+ to_string(curC);
+        int curLoc = curR*10000 + curC;
         m[curLoc]++;
         
         int newDir, newR, newC;
-        string newLoc;
+        int newLoc;
         
         for (int i=0; i<4; i++) {
             newDir = (dir+i) % 4;
             newR = curR+direc[newDir];
             newC = curC+direc[newDir+1];
-            newLoc = to_string(newR) +'/'+ to_string(newC);
+            newLoc = newR*10000 + newC;
             
             if (!m.count(newLoc) && robot.move()) {
                 dfs(robot, newR, newC, newDir, m);
