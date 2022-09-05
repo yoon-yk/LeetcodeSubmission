a6478d9c2e2464376e1b704a9818f929f2a69b98
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-05 17:16:49 +0900

    Time: 26 ms (40.64%), Space: 9.4 MB (24.35%) - LeetHub

---
+++ 489-robot-room-cleaner/489-robot-room-cleaner.cpp
@@ -0,0 +1,63 @@
+/**
+ * // This is the robot's control interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Robot {
+ *   public:
+ *     // Returns true if the cell in front is open and robot moves into the cell.
+ *     // Returns false if the cell in front is blocked and robot stays in the current cell.
+ *     bool move();
+ *
+ *     // Robot will stay in the same cell after calling turnLeft/turnRight.
+ *     // Each turn will be 90 degrees.
+ *     void turnLeft();
+ *     void turnRight();
+ *
+ *     // Clean the current cell.
+ *     void clean();
+ * };
+ */
+
+class Solution {
+public:
+    
+    vector<int> direc = {-1, 0, 1, 0, -1};
+    
+    void cleanRoom(Robot& robot) {
+        unordered_map<string, int> m;
+            
+        int curR = 0, curC = 0;
+        dfs(robot, curR, curC, 0, m);
+    }
+    
+    void goBack(Robot& robot){
+        robot.turnRight();
+        robot.turnRight();
+        robot.move();
+        robot.turnRight();
+        robot.turnRight();
+    }
+    
+    void dfs(Robot& robot, int curR, int curC, int dir, unordered_map<string, int>& m) {
+        
+        robot.clean();
+        string curLoc = to_string(curR) +'/'+ to_string(curC);
+        m[curLoc]++;
+        
+        int newDir, newR, newC;
+        string newLoc;
+        
+        for (int i=0; i<4; i++) {
+            newDir = (dir+i) % 4;
+            newR = curR+direc[newDir];
+            newC = curC+direc[newDir+1];
+            newLoc = to_string(newR) +'/'+ to_string(newC);
+            
+            if (!m.count(newLoc) && robot.move()) {
+                dfs(robot, newR, newC, newDir, m);
+                goBack(robot);
+            }
+            
+            robot.turnRight();
+        }
+    }
+};
