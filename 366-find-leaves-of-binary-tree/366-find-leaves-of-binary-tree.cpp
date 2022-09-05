abfd96a477037f02ec35e4244fc0a1747d15e075
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-05 15:01:59 +0900

    Time: 0 ms (100.00%), Space: 8.9 MB (43.62%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -3,8 +3,6 @@ public:
     vector<vector<int>> findLeaves(TreeNode* root) {
     
         vector<vector<int>> ans;
-        
-        int maxHeight = -1;
         dfs(root, ans);
         
         return ans;
@@ -13,19 +11,18 @@ public:
     int dfs(TreeNode* root, vector<vector<int>>& ans) {
         
         int height, left = 0, right = 0; 
-        if (!root->left && !root->right) {
-            height = 0;
-        } else {
-            if (root->left)
-                left = dfs(root->left, ans)+1;
-            if (root->right)
-                right = dfs(root->right, ans)+1;
-            height = max(left, right);
-        }
 
-        // maxHeight = max(maxHeight, height);
-        if ((int) ans.size() < height+1) ans.push_back(vector<int>());
+        if (root->left)
+            left = dfs(root->left, ans)+1;
+        if (root->right)
+            right = dfs(root->right, ans)+1;
+        height = max(left, right);
+
+
+        if ((int) ans.size() < height+1) 
+            ans.push_back(vector<int>());
         ans[height].push_back(root->val);
+        
         return height;
     }    
 };
