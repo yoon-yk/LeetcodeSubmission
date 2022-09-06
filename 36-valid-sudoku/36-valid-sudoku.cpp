2b21b4a8ccdc2f474d1f26e2a6a43c0dc1a9ff21
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-06 16:45:00 +0900

    Time: 31 ms (68.83%), Space: 19.8 MB (43.25%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    
     bool isValidSudoku(vector<vector<char>>& board) {
         int n = board.size();
-        unordered_map<int, unordered_set<int>> seenRow;
-        unordered_map<int, unordered_set<int>> seenCol;
-        unordered_map<int, unordered_set<int>> seenBlock;
+        vector<vector<bool>> seenRow(10, vector<bool>(10, false));
+        vector<vector<bool>> seenCol(10, vector<bool>(10, false));
+        vector<vector<bool>> seenBlock(10, vector<bool>(10, false));
 
         int curN, curR,curC, curB;
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 if (board[i][j] == '.') continue;
                 curN = board[i][j] - '0';
-                curB = ((i/3)*3+j/3)*10;
-                if (seenRow[i].count(curN) || seenCol[j].count(curN) || seenBlock[curB].count(curN))
+                curB = ((i/3)*3+j/3);
+                if (seenRow[i][curN] || seenCol[j][curN] || seenBlock[curB][curN])
                     return false;
-                seenRow[i].insert(curN), seenCol[j].insert(curN), seenBlock[curB].insert(curN);
+                seenRow[i][curN] = true, seenCol[j][curN] = true, seenBlock[curB][curN] = true;
             }
         }
         return true;
