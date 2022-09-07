80b53092eb0004d38a11edb52d8ebd4e33170f0d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-07 11:30:35 +0900

    Time: 31 ms (64.91%), Space: 53.7 MB (30.71%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -12,15 +12,17 @@
 class Solution {
 public:
     string tree2str(TreeNode* root) {
-        string ans;
-        if (!root) return "";
-        
+        string ans;        
         ans += to_string(root->val);
         
-        if (root->left || (!root->left && root->right))
+        if (root->left)
             ans += "(" + tree2str(root->left) + ")";
-        if (root->right) 
+        if (root->right) {
+            if (!root->left)
+                ans += "()";
             ans += "(" + tree2str(root->right) + ")";
+        }
+            
         
         return ans;
     }
