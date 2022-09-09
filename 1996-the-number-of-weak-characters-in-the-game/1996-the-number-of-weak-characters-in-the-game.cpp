efd7331b52d177131364fd746f8f039060b191bb
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-10 01:20:47 +0900

    Time: 456 ms (99.29%), Space: 144.9 MB (14.93%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
 
-        int maxAttack = -1;
+        int maxAttack = -1, minAttack = INT_MAX;
         vector<int> maxDefense (100002,0);
         // Store the maximum defense for an attack value
+        
         for (auto & p : properties) {            
             maxDefense[p[0]] = max(maxDefense[p[0]], p[1]);
             maxAttack = max(maxAttack, p[0]);
+            minAttack = min(minAttack, p[0]);
         }
 
         // Store the maximum defense for attack greater than or equal to a value
-        for (int i = maxAttack - 1; i >= 0; i--) {
+        for (int i = maxAttack - 1; i >= minAttack; i--) {
             maxDefense[i] = max(maxDefense[i], maxDefense[i + 1]);
         }
         
