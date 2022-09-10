0333e451b327389b797a90924dfd6e2ec20fcb9e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-10 19:45:22 +0900

    Time: 24 ms (45.58%), Space: 11.3 MB (68.64%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -8,15 +8,20 @@ public:
         return ans;    
     }
     
-    void dfs (int cur, int n, vector<int>& ans) {
+    void dfs (int &cur, int n, vector<int>& ans) {
         
         if (cur > n)
             return;
         
         ans.push_back(cur);
 
+        int orig;
         for (int i=0; i<=9; i++) {
-            dfs(cur*10 + i, n, ans);
+            orig = cur;
+            cur*=10;
+            cur+=i;
+            dfs(cur, n, ans);
+            cur = orig;
         }
     }
 };
