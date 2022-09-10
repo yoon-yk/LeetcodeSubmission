7bcef433a8e697fda536379db55c078b5a803be6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-10 19:44:25 +0900

    Time: 20 ms (54.15%), Space: 11.1 MB (82.39%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -3,15 +3,24 @@ public:
     vector<int> lexicalOrder(int n) {
         
         vector<int> ans;
-        for (int i=1; i<=n; i++)
-            ans.push_back(i);
-        
-       auto compare = [](int& a, int& b) {
-           return to_string(a) < to_string(b);
-       };
-        
-        sort (ans.begin(), ans.end(), compare);
-        
+        for (int i=1; i<=9; i++)
+            dfs(i, n, ans);
         return ans;    
+    }
+    
+    void dfs (int &cur, int n, vector<int>& ans) {
+        
+        if (cur > n)
+            return;
+        
+        ans.push_back(cur);
+
+        for (int i=0; i<=9; i++) {
+            cur*=10;
+            cur+=i;
+            dfs(cur, n, ans);
+            cur-=i;
+            cur/=10;
+        }
     }
 };
