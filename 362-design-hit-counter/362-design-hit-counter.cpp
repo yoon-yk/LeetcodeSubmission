6ee4149a668a431bf27865aaf2a4fb26ac9c206c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-10 13:28:02 +0900

    Time: 10 ms (6.75%), Space: 7.3 MB (46.41%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.cpp
@@ -0,0 +1,34 @@
+/*
+
+1 2 3 4 300 301
+
+
+*/
+
+class HitCounter {
+public:
+    
+    deque<int> q;
+    
+    HitCounter() {
+        
+    }
+    
+    void hit(int timestamp) {
+        q.push_back(timestamp);
+    }
+    
+    int getHits(int timestamp) {
+        while (!q.empty() && timestamp- q.front() >= 300)
+            q.pop_front();
+        
+        return q.size();
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
