aa8209527e8792c558d063ac7bac101e9380fc05
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-11 21:28:07 +0900

    Time: 259 ms (11.44%), Space: 36.8 MB (74.47%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+        
         vector<pair<int, int>> ens;
-        for(int i=0; i<speed.size(); i++) ens.emplace_back(efficiency[i], speed[i]);
+        for(int i=0; i<speed.size(); i++) ens.push_back({efficiency[i], speed[i]});
         sort(ens.begin(), ens.end(), greater<pair<int, int>>());
-        long res = 0;
-        long sum = 0;
+        
+        long res = 0, sum = 0;
         priority_queue<int, vector<int>, greater<int>> q;
         for(int i=0; i<ens.size(); i++) {
             q.push(ens[i].second);
