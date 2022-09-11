a7b6657ea59ab31368cfad4ab586cd94ee6d3262
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-11 21:03:51 +0900

    Time: 189 ms (20.95%), Space: 35.3 MB (95.60%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,66 +1,22 @@
 class Solution {
 public:
     
-    struct Node {
-        int speed;
-        int efficiency;
-        Node(int _speed, int _efficiency) :
-            speed(_speed), efficiency(_efficiency) {};
-    };
-    
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        
-    int MOD = 1e9 + 7;
-    
-    auto compareSpeed = [] (Node*& a, Node*& b) {
-        return a->speed < b->speed;
-    };
-        
-    auto minEfficiency = [] (Node*& a, Node*& b) {
-        return a->efficiency > b->efficiency;
-    };
-        
-        priority_queue<Node*, vector<Node*>, decltype(compareSpeed)> speedMaxQ(compareSpeed);
-        priority_queue<Node*, vector<Node*>, decltype(minEfficiency)> effMinQ(minEfficiency);
-        
-        for (int i=0; i<n; i++) 
-            speedMaxQ.push(new Node(speed[i], efficiency[i]));
-        
-        int minEff;
-        long long maxPerformance = 0;
-        long long curSpeedSum = 0;
-        
-        for (int i=0; i<k; i++) {
-            auto cur = speedMaxQ.top(); speedMaxQ.pop();
-            effMinQ.push(cur);
-            curSpeedSum += cur->speed;
-            minEff = effMinQ.top()->efficiency;
-            maxPerformance = max(maxPerformance, (curSpeedSum * minEff));
+        vector<pair<int, int>> ess(n);
+        for (int i = 0; i < n; ++i)
+            ess[i] = {efficiency[i], speed[i]};
+        sort(rbegin(ess), rend(ess));
+        long sumS = 0, res = 0;
+        priority_queue <int, vector<int>, greater<int>> pq; //min heap
+        for(auto& [e, s]: ess){
+            pq.emplace(s);
+            sumS += s;
+            if (pq.size() > k) {
+                sumS -= pq.top();
+                pq.pop();
+            }
+            res = max(res, sumS * e);
         }
-        
-        while (!speedMaxQ.empty()) {
-            curSpeedSum -= effMinQ.top()->speed;
-            effMinQ.pop();
-
-            auto newMember = speedMaxQ.top(); speedMaxQ.pop();
-            curSpeedSum += newMember->speed;
-            effMinQ.push(newMember); 
-            
-            minEff = effMinQ.top()->efficiency;
-            maxPerformance = max(maxPerformance, (curSpeedSum * minEff));
-
-        }
-        
-        while (!effMinQ.empty()) {
-            curSpeedSum -= effMinQ.top()->speed;
-            effMinQ.pop();
-
-            minEff = effMinQ.top()->efficiency;
-            maxPerformance = max(maxPerformance, (curSpeedSum * minEff));
-        }
-        
-        return maxPerformance % MOD;
-        
-        
+        return res % (int)(1e9+7);
     }
 };
