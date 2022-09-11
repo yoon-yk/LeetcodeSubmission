b33a8765c5e81df633cd78d70e2ca51afa3aa2b2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-11 21:00:27 +0900

    Time: 358 ms (5.10%), Space: 42 MB (19.37%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -4,7 +4,7 @@ public:
     struct Node {
         int speed;
         int efficiency;
-        Node(long long _speed, long long _efficiency) :
+        Node(int _speed, int _efficiency) :
             speed(_speed), efficiency(_efficiency) {};
     };
     
@@ -13,64 +13,50 @@ public:
     int MOD = 1e9 + 7;
     
     auto compareSpeed = [] (Node*& a, Node*& b) {
-      // if (a->speed == b->speed) ㅎㅇㅎㅇ 윤여훈 천재
-      //     return a->efficiency < b->efficiency;
         return a->speed < b->speed;
     };
         
     auto minEfficiency = [] (Node*& a, Node*& b) {
-      // if (a->efficiency == b->efficiency) 
-      //     return a->speed > b->speed;
         return a->efficiency > b->efficiency;
     };
         
         priority_queue<Node*, vector<Node*>, decltype(compareSpeed)> speedMaxQ(compareSpeed);
         priority_queue<Node*, vector<Node*>, decltype(minEfficiency)> effMinQ(minEfficiency);
         
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<n; i++) 
             speedMaxQ.push(new Node(speed[i], efficiency[i]));
-        }
         
-        long long minEff;
+        int minEff;
         long long maxPerformance = 0;
         long long curSpeedSum = 0;
         
         for (int i=0; i<k; i++) {
             auto cur = speedMaxQ.top(); speedMaxQ.pop();
-            // cout << cur->speed << "||" << cur->efficiency << endl;
             effMinQ.push(cur);
-            minEff = effMinQ.top()->efficiency;
             curSpeedSum += cur->speed;
+            minEff = effMinQ.top()->efficiency;
             maxPerformance = max(maxPerformance, (curSpeedSum * minEff));
-            // cout << "(curSpeedSum) : " << curSpeedSum << " | (minEff) : " << minEff << " | (currPf) : " << (curSpeedSum * minEff) % MOD <<" | (maxPf) : " << maxPerformance << endl;
-
         }
         
         while (!speedMaxQ.empty()) {
             curSpeedSum -= effMinQ.top()->speed;
-            // cout << "(pop)" << effMinQ.top()->speed << "||" << effMinQ.top()->efficiency << endl;
             effMinQ.pop();
 
             auto newMember = speedMaxQ.top(); speedMaxQ.pop();
-            // cout << "(replace)" << newMember->speed << "||" << newMember->efficiency << endl;
-
             curSpeedSum += newMember->speed;
             effMinQ.push(newMember); 
             
             minEff = effMinQ.top()->efficiency;
             maxPerformance = max(maxPerformance, (curSpeedSum * minEff));
-            
-            // cout << "(curSpeedSum) : " << curSpeedSum << " | (minEff) : " << minEff << " | (currPf) : " << (curSpeedSum * minEff) <<" | (maxPf) : " << maxPerformance << endl;
+
         }
         
         while (!effMinQ.empty()) {
             curSpeedSum -= effMinQ.top()->speed;
             effMinQ.pop();
-            // cout << "(pop)" << effMinQ.top()->speed << "||" << effMinQ.top()->efficiency << endl;
 
             minEff = effMinQ.top()->efficiency;
             maxPerformance = max(maxPerformance, (curSpeedSum * minEff));
-            // cout << "(curSpeedSum) : " << curSpeedSum << " | (minEff) : " << minEff << " | (currPf) : " << (curSpeedSum * minEff) % MOD <<" | (maxPf) : " << maxPerformance << endl;
         }
         
         return maxPerformance % MOD;
