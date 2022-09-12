ec827bd13253bfe8f57f30bffb32e8c546e021db
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-12 16:01:18 +0900

    Time: 14 ms (48.71%), Space: 8.8 MB (69.89%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,26 +1,30 @@
+class DSU {
+    vector<int> par, rank;
+public:
+    DSU(int n) : par(n), rank(n) {
+        iota(begin(par), end(par), 0);      // initializes each node's parent to be itself - fills as [0,1,2,3,...,n]
+    }
+    int find(int x) {
+        if(x == par[x]) return x;           // x is itself the parent of this component
+        return find(par[x]);                // recurse for parent of x
+    }
+    bool Union(int x, int y) {
+        auto xp = find(x), yp = find(y);    // find parents of x and y, i.e, representatives of components that x and y belong to
+        if(xp == yp) return false;          // x and y already belong to same component - not possible to union
+        return par[xp] = yp;                // union x and y by making parent common
+    }
+    // iterative implementation of find
+    // int find(int x) {  
+    //     while(x != par[x]) x = par[x];
+    //     return par[x];
+    // }
+};
 class Solution {
- public:
-  int cycleStart = -1;
-bool *cycle, *vis, found{};
-void dfs(vector<int> graph[], int cur, int par = -1) {
-	if(vis[cur]) { cycleStart = cur; found = true; return; }   
-	vis[cur] = true;                                        
-	for(auto child : graph[cur]) {                          
-		if(child == par) continue;                          
-		if(!found) dfs(graph, child, cur);     
-		if(cycleStart != -1) cycle[cur]=true;  
-		if(cur == cycleStart) { cycleStart = -1; return; } 
-	}
-}
-vector<int> findRedundantConnection(vector<vector<int>>& e) {
-	int n = size(e);
-	vector<int> graph[n+1];
-	cycle = (bool*)calloc(n+1, sizeof(bool));
-    vis = (bool*)calloc(n+1, sizeof(bool));
-	for(auto& edge : e) graph[edge[0]].push_back(edge[1]), graph[edge[1]].push_back(edge[0]);
-	dfs(graph, 1);   
-	for(int i = n-1; ~i; i--)
-		if(cycle[e[i][0]] && cycle[e[i][1]]) return e[i];  
-	return { };    
-}
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& e) {
+        DSU ds(size(e)+1);
+        for(auto& E : e) 
+            if(!ds.Union(E[0], E[1])) return E;	// not possible to union - adding this edge was causing the cycle
+        return { };    // un-reachable
+    }
 };
