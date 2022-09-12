dfa49e46a30a4a9ee1a382459b9c71b5c45208ca
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-12 17:04:52 +0900

    Time: 19 ms (28.10%), Space: 9.6 MB (25.05%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,25 +1,21 @@
-class DSU {
-    vector<int> par, rank;
-public:
-    DSU(int n) : par(n), rank(n) {
-        iota(begin(par), end(par), 0);
-    }
-    int find(int x) {
-        if(x == par[x]) return x;           // x is itself the parent of this component
-        return par[x] = find(par[x]);       // update parent of x before returning for each call
-    }
-    bool Union(int x, int y) {
-        auto xp = find(x), yp = find(y);    // find parents of x and y, i.e, representatives of components that x and y belong to
-        if(xp == yp) return false;          // x and y already belong to same component - not possible to union
-        return par[xp] = yp;                // union x and y by making parent common
-    }
-};
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& e) {
-        DSU ds(size(e)+1);
-        for(auto& E : e) 
-            if(!ds.Union(E[0], E[1])) return E;	// not possible to union - adding this edge was causing the cycle
-        return { };    // un-reachable
+	int n = size(e);
+	vector<vector<int>> graph(n+1);
+	vector<bool> vis(n+1);                
+	for(auto& E : e) {
+        fill(begin(vis), end(vis), false);     // reset the vis array
+        graph[E[0]].push_back(E[1]), graph[E[1]].push_back(E[0]);
+        if(isCycleDetected(graph, vis, E[0])) return E;
+    }
+	return { };    // un-reachable
+}
+    bool isCycleDetected(vector<vector<int>>& graph, vector<bool>& vis, int cur, int par = -1) {
+        if(vis[cur]) return true;    // reached already visited node -  cycle detected
+        vis[cur] = true;
+        for(auto& child : graph[cur]) 
+            if(child != par && isCycleDetected(graph, vis, child, cur)) return true;
+        return false;    // no cycle found
     }
 };
