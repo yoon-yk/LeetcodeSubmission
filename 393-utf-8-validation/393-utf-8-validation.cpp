570ad147357727b1fa9bb921f838b022168f3490
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-14 01:54:07 +0900

    Time: 117 ms (5.24%), Space: 13.8 MB (100.00%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        
+        int cur = 0;
+        bool pass = true;
+        while (cur < data.size()) {
+
+            int count = 0;
+            int std = cur;
+            while ((data[cur] & 128) == 128 && count < 8) {
+                count ++;
+                data[cur] <<= 1;
+            }
+            
+            cur++;
+            
+            cout << count << endl;
+            if (count == 0) continue;
+            else if (count == 1 || count > 4) return false;
+            
+            count--;
+            for (; count > 0 && cur<data.size(); count--, cur++) {
+                cout << count << endl;
+                pass = false;
+                if ((data[cur] & 128) == 128) {
+                    data[cur] <<= 1;
+                    if ((data[cur] & 128) == 0) {
+                        pass = true;
+                    }
+                }
+                if (!pass) return false;
+            }
+            
+            cout << "pass" << endl;
+            cout << count << endl;
+            if (count!= 0) return false;
+            
+        }        
+        
+        return pass;
+    }
+};
