507cd2c1bc3fa6866b206dff9100800c9cf4afa7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-16 18:26:13 +0900

    Time: 7 ms (41.37%), Space: 7.7 MB (54.84%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
+    /*
+            0
+        1   2   3
+       2 3 1 3 1 2
+       3 2 3 1 2 1
+    
+    */
+    
+    
+    
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> cur = nums;
+        
+        vector<bool> visited(nums.size(), false);
+        vector<int> path;
         vector<vector<int>> ans;
         
-        backtrack(0, nums, ans);
+        helper(nums, visited, path, ans);
         
         return ans;
     }
     
-    void backtrack(int idx, vector<int>& nums, vector<vector<int>>& ans) {
-                
-        if (idx == nums.size()) {
-            ans.push_back(nums);
+    
+    // Swap 하는 방법
+    // visited 하는 방법
+    
+    void helper(vector<int>& nums, vector<bool>& visited, vector<int>& path, vector<vector<int>>& ans) {
+        // condition
+        if (path.size() == nums.size()) {
+            ans.push_back(path);
             return;
         }
-        else {
-            for (int j=idx; j < nums.size(); j++) {
-                swap(nums[idx], nums[j]);
-                backtrack(idx+1, nums, ans);
-                swap(nums[idx], nums[j]);
-            }
+        
+        for (int i=0; i<nums.size(); i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            path.emplace_back(nums[i]);
+            helper(nums, visited, path, ans);
+            path.pop_back();
+            visited[i] = false;
         }
-
+        
+        // 
     }
 };
