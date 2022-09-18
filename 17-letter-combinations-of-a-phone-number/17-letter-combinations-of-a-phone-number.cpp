6f20de3e94114b11c2f7ce9d02d6fd6bfa117b9b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-18 21:20:49 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (54.17%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    const vector<string> pad = {
-        "", "", "abc", "def", "ghi", "jkl",
-        "mno", "pqrs", "tuv", "wxyz"
-    };
-    
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
-		vector<string> result;
-        result.push_back("");
+        vector<string> ans;
+        if (digits.length() == 0){
+            return ans;
+        }  
         
-        for(auto digit: digits) {
-            vector<string> tmp;
-            for(auto candidate: pad[digit - '0']) {
-                for(auto s: result) {
-                    tmp.push_back(s + candidate);
-                }
-            }
-            result.swap(tmp);
+        int index = 0;
+        string output = "";
+        vector<string> mapping = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,index,output,ans,mapping);
+        return ans;
+    }
+    
+    void solve(string& digits, int index, string& output,vector<string>& ans, vector<string>& mapping){
+        
+        if (index >= digits.length()){
+            ans.push_back(output);
+            return;
         }
-        return result;
+        
+        int number = digits[index] - '0';
+        string value = mapping[number];
+        for (int i=0;i<value.length();i++){
+            output.push_back(value[i]);
+            solve(digits,index+1,output,ans,mapping);
+            output.pop_back();
+        }
     }
 };
