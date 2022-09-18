7a6615fec739cfe0d8ba23ec0478cb70d774f359
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-18 20:54:08 +0900

    Time: 0 ms (100.00%), Space: 6.6 MB (54.17%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> res;
-        int len = digits.length();
-        if (len == 0) return res;
+        vector<vector<char>> keys = 
+        {{}, {}, {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'}, {'m', 'n', 'o'}, {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}};
         
-        unordered_map <int, vector<string>> keyboard;
-        keyboard[2] = {"a", "b", "c"};
-        keyboard[3] = {"d", "e", "f"};
-        keyboard[4] = {"g", "h", "i"};
-        keyboard[5] = {"j", "k", "l"};
-        keyboard[6] = {"m", "n", "o"};
-        keyboard[7] = {"p", "q", "r", "s"};
-        keyboard[8] = {"t", "u", "v"};
-        keyboard[9] = {"w", "x", "y", "z"};
-
-        if (len == 1)
-            return keyboard[digits[0]-'0'];
+        deque<string> q;
+        q.push_back("");
+        backtrack(0, digits, keys, q);
         
-        queue<string> q; 
-        //q.push("");
+        vector<string> ans;
         
-        // index 0 chars 모두 넣기
-        for (auto ch : keyboard[digits[0]-'0']) {
-            q.push(ch);
+        while (!q.empty() && q.front()!= "") {
+            ans.push_back(q.front());
+            q.pop_front();
         }
         
-        int i = 1;
-        while (i < len){
-            for (int j = q.size(); j > 0; j--) {
-                string curCh = q.front();
-                q.pop();
-                
-                for (auto ch : keyboard[digits[i]-'0'])
-                    q.push(curCh+ch);
-            }        
-            i++;
+        return ans;
+        
+    }
+    
+    void backtrack(int idx, string& digits, vector<vector<char>>& keys, deque<string>& q) {
+        
+        if (idx == digits.size()) {
+            return;
         }
         
-        while(!q.empty()) {
-            res.push_back(q.front());
-            q.pop();
+        int digit = digits[idx] - '0';
+        
+        int size = q.size();
+        while (size--) {
+            string cur = q.front(); q.pop_front();
+            for (int i=0; i<keys[digit].size(); i++) {
+                q.push_back(cur+keys[digit][i]);
+            }
         }
         
-        return res;
+        backtrack(idx+1, digits, keys, q);
+        
     }
 };
