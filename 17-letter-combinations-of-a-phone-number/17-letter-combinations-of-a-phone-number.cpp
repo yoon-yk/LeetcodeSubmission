4c144396d76eb495fa7633994727d92754929f4f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-18 21:23:52 +0900

    Time: 6 ms (14.35%), Space: 6.4 MB (81.69%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -7,25 +7,25 @@ public:
         }  
         
         int index = 0;
-        string output = "";
+        string curString = "";
         vector<string> mapping = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(digits,index,output,ans,mapping);
+        solve(digits,index,curString,ans,mapping);
         return ans;
     }
     
-    void solve(string& digits, int index, string& output,vector<string>& ans, vector<string>& mapping){
+    void solve(string& digits, int index, string& curString,vector<string>& ans, vector<string>& mapping){
         
         if (index >= digits.length()){
-            ans.push_back(output);
+            ans.push_back(curString);
             return;
         }
         
         int number = digits[index] - '0';
         string value = mapping[number];
         for (int i=0;i<value.length();i++){
-            output.push_back(value[i]);
-            solve(digits,index+1,output,ans,mapping);
-            output.pop_back();
+            curString.push_back(value[i]);
+            solve(digits,index+1,curString,ans,mapping);
+            curString.pop_back();
         }
     }
 };
