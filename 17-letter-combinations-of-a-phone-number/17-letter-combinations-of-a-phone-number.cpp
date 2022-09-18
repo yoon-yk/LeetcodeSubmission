508907167aa2ec64481b8a7cca7ea1e9b632c39d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-18 21:15:11 +0900

    Time: 3 ms (45.93%), Space: 6.6 MB (31.62%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        vector<vector<char>> keys = 
-        {{}, {}, {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'}, {'m', 'n', 'o'}, {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}};
-        
-        deque<string> q;
-        q.push_back("");
-        backtrack(0, digits, keys, q);
-        
-        vector<string> ans;
-        
-        while (!q.empty() && q.front()!= "") {
-            ans.push_back(q.front());
-            q.pop_front();
-        }
-        
-        return ans;
-        
-    }
+    const vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
     
-    void backtrack(int idx, string& digits, vector<vector<char>>& keys, deque<string>& q) {
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+		vector<string> result;
+        result.push_back("");
         
-        if (idx == digits.size()) {
-            return;
-        }
-        
-        int digit = digits[idx] - '0';
-        
-        int size = q.size();
-        while (size--) {
-            string cur = q.front(); q.pop_front();
-            for (int i=0; i<keys[digit].size(); i++) {
-                q.push_back(cur+keys[digit][i]);
+        for(auto digit: digits) {
+            vector<string> tmp;
+            for(auto candidate: pad[digit - '0']) {
+                for(auto s: result) {
+                    tmp.push_back(s + candidate);
+                }
             }
+            result.swap(tmp);
         }
-        
-        backtrack(idx+1, digits, keys, q);
-        
+        return result;
     }
 };
