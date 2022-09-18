a6d5d53cd1e4b9c7496ff3feb8631a4bad87b35b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-19 01:07:21 +0900

    Time: 235 ms (5.01%), Space: 13.9 MB (31.23%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int maxLen = 0;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        unordered_set<string> dict;
+        unordered_map<int, unordered_map<int, bool>> visit;
+        
+        for (string& str : wordDict) {
+            dict.insert(str);
+            maxLen = max(maxLen, (int)str.size());
+        }
+        
+        string curStr = "";
+        return backtrack(0, s, 0, curStr, dict, visit);
+    }
+    
+    bool backtrack(int idx, string& s, int curStrBeg, string& curStr, unordered_set<string>& dict, unordered_map<int, unordered_map<int, bool>>& visit) {
+        
+        if (visit[curStrBeg].count(idx)) 
+            return visit[curStrBeg][idx];
+        
+        // cout << curStr << endl;
+        if (curStr.size() > maxLen)
+            return visit[curStrBeg][idx] = false;
+        
+        // basecase
+        // if all found 
+        if (idx == s.size()) {
+            return visit[curStrBeg][idx] = dict.count(curStr);
+        }
+        
+        bool ans = false;
+        
+        for (int i=idx; i<s.size(); i++) {
+            
+            curStr.push_back(s[idx]);
+            ans |= backtrack(idx+1, s, curStrBeg, curStr, dict, visit);
+            curStr.pop_back();
+            
+            if (dict.count(curStr)) {
+                string origStr = curStr;
+                curStr = s[idx];
+                ans |= backtrack(idx+1, s, idx, curStr, dict, visit);
+                curStr = origStr;
+            } 
+        
+        }
+        
+        return visit[curStrBeg][idx] = ans;
+        
+        
+    }
+};
