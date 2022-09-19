75440a187a44150e1bd5ba7d677d653ffcc3eca0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-19 17:06:24 +0900

    Time: 3 ms (62.47%), Space: 6.3 MB (49.59%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
     
-    /*
-                 106
-                
-                1 0 6
-                10 6
-                1 06 <-- invalid
-                106 <-- invalid
-   
-   [1 0 6]
-    1 2 2
-    
-    dp[i] = (if it is continuing digit - dp[i-2]) + (if it is separate digit - dp[i-1])
-   
-    - prune
-    if the number is not in a range[1, 26] 
-    if the string is starting with 0 and having more than one char
-    
-    */
-    
-    int numDecodings(string s) {
+    int numDecodings(string &s) {
                 
         int n = s.length();
         vector<int> dp(n+1, 0);
@@ -34,9 +15,8 @@ public:
                     dp[i+1] += dp[i-1];
             }
                         
-            if (s[i]!= '0') {
+            if (s[i]!= '0')
                 dp[i+1] += dp[i];
-            }
         }
         
         return dp[n]; // number of ways to decode the string 
