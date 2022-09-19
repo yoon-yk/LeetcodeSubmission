17fee0ba37d7d9ed5548639798db342ddaa1fa97
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-19 15:54:48 +0900

    Time: 28 ms (39.21%), Space: 11.1 MB (53.17%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {     
+
+    bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        vector<bool> dp(n + 1);
-        dp[n] = true;
-        
-        for (int i = n; i >= 0; i--)
-            for (string& word : wordDict) {
-                if (i + word.size() <= n) {
-                    for (int j = 0; j < word.size(); j++){
-                        if (s[i + j] != word[j])
-                            break;
-                        if (j == word.size() - 1)
-                            dp[i] = dp[i + word.size()];                            
+        vector<bool> dp(n + 1, false);
+        unordered_set<string> st;
+        for(auto i: wordDict)
+            st.insert(i);
+        dp[0] = true;
+        for (int i = 1; i < n + 1; i++){
+            // cout << "***" << i << "***" << endl;
+            string temp;
+            
+            for (int j = i; j > 0; j--){
+                temp = s[j - 1] + temp;
+                // cout << temp << endl;
+                
+                if (st.count(temp)){
+                    if (dp[j - 1]){
+                        // cout << j-1 << endl;
+                        // cout << "dp[" << i << "] = true" << endl;  
+                        dp[i] = true;
+                        break;
                     }
                 }
-                if (dp[i]) break;
             }
+        }
         
-        return dp[0];
+//         for (int n : dp)
+//             cout << n << " ";
+//         cout << endl;
+        
+        return dp[n];
     }
 };
