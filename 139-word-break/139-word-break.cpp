43a254ecac4ad482d32d489d1f635d66993fdd4b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-19 14:26:33 +0900

    Time: 46 ms (19.82%), Space: 14.7 MB (24.19%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
-    int maxLen = 0;
-    
     bool wordBreak(string s, vector<string>& wordDict) {
-        
-        int len = s.size();
-        unordered_set<string> dict;
-        
-        vector<int> startingDict(26, 0);
-        vector<int> visit(len+1, -1);
-        
-        for (string& str : wordDict) {
-            dict.insert(str);
-            startingDict[str[0]-'a']++;
-            maxLen = max(maxLen, (int)str.size());
-        }
-        
-        string curStr = "";
-        return backtrack(0, 0, s, curStr, startingDict, dict, visit);
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        vector<int> memo(s.size(), -1);
+        return check(s, wordSet, 0, memo);
     }
-    
-    bool backtrack(int curStrBeg, int idx, string& s, string& curStr, vector<int>& startingDict, unordered_set<string>& dict, vector<int>& visit) {
-        
-        if (visit[curStrBeg] != -1)
-            return visit[curStrBeg];
-        
-        if (curStr.size() > maxLen)
-            return visit[curStrBeg] = false;
-
-        if (idx == s.size()) 
-            return visit[curStrBeg] = dict.count(curStr);
-        
-        bool ans = false;
-        
-        for (int i=idx; i<s.size(); i++) {
-            
-            char curCh = s[idx];
-            
-            curStr.push_back(curCh);
-            ans |= backtrack(curStrBeg, idx+1, s, curStr, startingDict, dict, visit);
-            curStr.pop_back();
-            
-            if (dict.count(curStr) && startingDict[curCh-'a'] > 0) {
-                string newString;
-                newString.push_back(curCh);
-                ans |= backtrack(idx, idx+1, s, newString, startingDict, dict, visit);
-                newString.pop_back();
-            } 
-        
+    bool check(string s, unordered_set<string>& wordSet, int start, vector<int>& memo) {
+        if (start >= s.size()) 
+            return true;
+        if (memo[start] != -1) 
+            return memo[start];
+        for (int i = start + 1; i <= s.size(); ++i) {
+            if (wordSet.count(s.substr(start, i - start)) && check(s, wordSet, i, memo)) {
+                memo[start] = 1;
+                return memo[start];
+            }
         }
-        
-        return visit[curStrBeg] = ans;
-        
+        memo[start] = 0;
+        return memo[start];
     }
 };
