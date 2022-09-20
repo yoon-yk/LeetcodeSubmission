e5d0f4061ccfedf14c0953d910e9a0a2f0924ae1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-21 00:58:11 +0900

    Time: 4 ms (68.99%), Space: 7.5 MB (71.51%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -2,30 +2,20 @@ class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<int> curPath;
-        vector<vector<int>> ans;
-        backtrack(0, nums, curPath, ans);
-        return ans;
-    }
-    
-    void backtrack(int idx, vector<int>& nums, vector<int>& curPath, vector<vector<int>>& ans) {
+        vector<vector<int>> subsets = {{}};
         
-        ans.emplace_back(curPath);
+        int subsetSize = 0;
         
-        int n = nums.size();
-        if (idx == n) 
-            return;
-        
-        // vector<bool> visited(20, false);
-        
-        for (int i=idx; i<n; i++) {
-            // if (visited[nums[i]+10]) continue;
-            if (i > idx && nums[i-1] == nums[i]) continue;
-            // visited[nums[i]+10] = true;
-            curPath.emplace_back(nums[i]);
-            backtrack(i+1, nums, curPath, ans);
-            curPath.pop_back();
+        for (int i=0; i<nums.size(); i++) {
+            int startingIdx = (i>=1 && nums[i] == nums[i-1]) ? subsetSize : 0;
+            subsetSize = subsets.size();
+            for (int j = startingIdx; j<subsetSize; j++) {
+                vector<int> curSubset = subsets[j];
+                curSubset.push_back(nums[i]);
+                subsets.push_back(curSubset);
+            }
         }
         
+        return subsets;
     }
 };
