2e831507dbd5b8877f8cde10f974d88507235bf3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-20 18:40:58 +0900

    Time: 482 ms (51.68%), Space: 11.9 MB (92.72%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int findLength(vector<int>& A, vector<int>& B) {
-        int result = 0;
-        for (int i = 0; i < A.size() + B.size() - 1; ++i) {
-            // The current overlapping window is A[aStart, Math.min(A.length, B.length)] and B[bStart, Math.min(A.length, B.length)].
-            int aStart = max(0, (int)(A.size() - 1 - i));  
-            int bStart = max(0, (int)(i - (A.size() - 1)));
-            int numConsecutiveMatches = 0;
-            for (int aIdx = aStart, bIdx = bStart; aIdx < A.size() && bIdx < B.size(); ++aIdx, ++bIdx) {
-                // Maintain number of equal consecutive elements in the current window (overlap) and the max number ever computed.
-                numConsecutiveMatches = A[aIdx] == B[bIdx] ? numConsecutiveMatches + 1 : 0;
-                result = max(result, numConsecutiveMatches);
-            }
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        
+        int n1 = nums1.size(), n2 = nums2.size();
+
+        // Make sure len(nums1) > len(nums2) to optimize space
+        if (nums1.size() < nums2.size()) return findLength(nums2, nums1); 
+        
+        int ans = 0; 
+        vector<int> prev(n2+1, 0);
+        vector<int> cur(n2+1);
+        
+        for (int i = 1; i<=n1 ; i++) {
+            for (int j = 1; j<=n2; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    cur[j] = prev[j-1] + 1;
+                    if (ans < cur[j]) ans = cur[j];
+                } else cur[j] = 0;
+            } 
+            prev.swap(cur);
         }
-        return result;
+        
+        return ans;
     }
 };
-
