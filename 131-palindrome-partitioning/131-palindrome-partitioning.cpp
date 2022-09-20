4439c6c501ce5d7e8a00fb7b64df374a68fac6ad
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-20 21:21:38 +0900

    Time: 117 ms (94.06%), Space: 49.2 MB (90.38%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -15,20 +15,26 @@ public:
         int n = s.length();
         vector<string> curPath;
         vector<vector<string>> ans;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
         
-        string newStr;
-        backtrack(ans, s, 0, curPath);
+        dfs(0, s, dp, curPath, ans);
+        
         return ans;
     }
     
-    void backtrack(vector<vector<string>>& ans, string &s, int start, vector<string> &curPath) {
-        if (start == s.length())
-            ans.emplace_back(curPath);
+    void dfs(int start, string& s, vector<vector<int>>& dp, vector<string> &curPath, vector<vector<string>>& ans) {
+        int n = s.size();
+        if (start == n){
+            ans.push_back(curPath);
+            return;
+        }
         
-        for (int end = start; end < s.length(); end++) {
-            if (isPalindrome(s, start, end)) {
-                curPath.emplace_back(s.substr(start, end-start+1));
-                backtrack(ans, s, end+1, curPath);
+        for (int end = start; end < n; end++) {
+            if (dp[start][end] == -1)
+                dp[start][end] = isPalindrome(s, start, end);
+            if (dp[start][end] == 1) {
+                curPath.push_back(s.substr(start, end-start+1));
+                dfs(end+1, s, dp, curPath, ans);
                 curPath.pop_back();
             }
         }
