93e4c1583fc1979d503d0dd8657cea0de81efe17
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-21 18:23:22 +0900

    Time: 7 ms (77.50%), Space: 10.5 MB (89.40%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -14,14 +14,14 @@ public:
     void backtrack(int idx, vector<int>& candidates, int target, int &curSum, vector<int>& curPath, vector<vector<int>>& ans) {
         // base case
         if (curSum == target){ 
-            ans.push_back(curPath);
+            ans.emplace_back(curPath);
             return;
         }
 
         for (int i=idx; i<candidates.size(); i++) {
             if (i > idx && (candidates[i-1] == candidates[i])) continue;
             if (curSum + candidates[i] > target) continue;
-            curPath.push_back(candidates[i]);
+            curPath.emplace_back(candidates[i]);
             curSum += candidates[i];
             backtrack(i+1, candidates, target, curSum, curPath, ans);
             curSum -= candidates[i];
