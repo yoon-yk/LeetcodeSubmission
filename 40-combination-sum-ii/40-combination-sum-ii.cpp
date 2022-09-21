a61d06fa0c0b13674d4f34bfa352f375354499eb
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-21 18:21:44 +0900

    Time: 10 ms (57.78%), Space: 10.5 MB (89.40%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        int curSum = 0;
+        vector<int> curPath;
+        vector<vector<int>> ans;
+        
+        backtrack(0, candidates, target, curSum, curPath, ans);
+        return ans;
+        
+    }
+    
+    void backtrack(int idx, vector<int>& candidates, int target, int &curSum, vector<int>& curPath, vector<vector<int>>& ans) {
+        // base case
+        if (curSum == target){ 
+            ans.push_back(curPath);
+            return;
+        }
+        
+        if (curSum > target)
+            return;
+
+        for (int i=idx; i<candidates.size(); i++) {
+            if (i > idx && (candidates[i-1] == candidates[i])) continue;
+            curPath.push_back(candidates[i]);
+            curSum += candidates[i];
+            backtrack(i+1, candidates, target, curSum, curPath, ans);
+            curSum -= candidates[i];
+            curPath.pop_back();
+        }
+    }
+};
