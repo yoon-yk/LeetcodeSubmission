bfbb5c631f9ec72cb5a90b86bdd80a878e2fdf66
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-21 14:28:05 +0900

    Time: 17 ms (14.51%), Space: 8.3 MB (35.73%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -2,20 +2,32 @@ class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> subsets = {{}};
-        
-        int subsetSize = 0;
-        
-        for (int i=0; i<nums.size(); i++) {
-            int startingIdx = (i>=1 && nums[i] == nums[i-1]) ? subsetSize : 0;
-            subsetSize = subsets.size();
-            for (int j = startingIdx; j<subsetSize; j++) {
-                vector<int> curSubset = subsets[j];
-                curSubset.push_back(nums[i]);
-                subsets.push_back(curSubset);
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        unordered_set<string> seen;
+        for (int i = 0; i < p; i++) {
+            string curBit;
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                    curBit += (nums[j]+'0');
+                    curBit += ',';
+                }
             }
+            // cout << curBit << endl;
+            if (seen.count(curBit))
+                subs[i].push_back(-11);
+            else seen.insert(curBit);
         }
         
-        return subsets;
+        vector<vector<int>> ans;
+        
+        for (auto& s : subs) {
+            if (s.size() > 0 && s.back()==-11)   
+                continue;
+            ans.push_back(s);
+        }
+        
+        return ans;
     }
 };
