8166851d18247f057e448c6e88c45fc6cc8d25e6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-23 18:16:50 +0900

    Time: 436 ms (26.31%), Space: 282.9 MB (13.83%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -3,11 +3,8 @@ public:
     bool isPalindrome(string& s, int start, int end, vector<vector<int>>& dp) {
         if (dp[start][end] != -1) 
             return dp[start][end];
-        
-        if (start >= end)
-            return dp[start][end] = true;
-        
-        if (s[start] == s[end] && (isPalindrome(s, start+1, end-1, dp)))
+
+        if (s[start] == s[end] && (end-start <= 1 || isPalindrome(s, start+1, end-1, dp)))
             return dp[start][end] = true;
 
         return dp[start][end] = false;
@@ -17,13 +14,11 @@ public:
         vector<vector<int>> dp(s.length(), vector<int>(s.length(), -1));
         
         int n= s.length(); 
-        for (int i=n-1; i>=0; i--) {
-            for (int j=0; i+j<n; j++) {
-                if (isPalindrome(s, j, j+i, dp)) {
+        for (int i=n-1; i>=0; i--) 
+            for (int j=0; i+j<n; j++) 
+                if (isPalindrome(s, j, j+i, dp)) 
                     return s.substr(j, i+1);
-                }
-            }
-        }
+
         
         return "";
     }
