1990b8830a9f56db15d69ea7c83e822e3103141f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-25 23:14:28 +0900

    Time: 390 ms (49.07%), Space: 9.7 MB (91.78%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -5,22 +5,20 @@ public:
         for (int i=0; i<nums.size(); i++) sum+= nums[i];
         if (sum % 2) return false;
         
-        vector<vector<bool>> dp(nums.size()+1, vector<bool>(sum+1, false));
-        dp[0][0] = true;
+        vector<bool> curr(sum+1, false), prev(sum+1, false);
+        prev[0] = true;
         
-        
-        for (int i=1; i<=nums.size(); i++) {
+        for (int i=0; i<nums.size(); i++) {
             for (int j=0; j<=sum; j++) {
-                if (dp[i-1][j]) {
-                    dp[i][j] = true;
-                    if (j+nums[i-1] <= sum) 
-                        dp[i][j+nums[i-1]] = true;
+                if (prev[j]) {
+                    curr[j] = true;
+                    if (j+nums[i] <= sum) 
+                        curr[j+nums[i]] = true;
                 }
             }
+            prev.swap(curr);
         }
-        
-        // for (int i=0; i< nums.size(); )
-        // return true;
-        return dp[nums.size()][sum >> 1];
+
+        return curr[sum >> 1];
     }
 };
