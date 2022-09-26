ab8879b7d83417860adf34418b74ca2ec5493401
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-26 19:45:01 +0900

    Time: 95 ms (18.69%), Space: 18.3 MB (53.77%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -3,19 +3,19 @@ public:
     int change(int amount, vector<int>& coins) {
         
         int coinCnt = coins.size();
-        vector<int> memo (amount+1, 0);
+        vector<vector<int>> memo (coinCnt, vector<int>(amount+1, 0));
         
-        memo[0] = 1;
-        
+        memo[0][0] = 1;
         for (int i=0; i<coinCnt; i++) { // coin 
-            for (int j=1; j<=amount; j++) { // amount                 
+            for (int j=0; j<=amount; j++) { // amount 
+                if (i > 0) memo[i][j] = memo[i-1][j];
                 if (j-coins[i] >= 0) 
-                memo[j] += memo[j-coins[i]];
+                memo[i][j] += memo[i][j-coins[i]];
 
             }
         }
         
-        return memo[amount];
+        return memo[coinCnt-1][amount];
     }
 
 };
