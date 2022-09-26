9eac7baefeb26dd7be315eac78aa9f95c9ca18c0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-26 20:05:33 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (63.64%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int>(n, 0));
-        memo[0][0] = 1;
+        vector<int> memo(n, 0);
+        memo[0] = 1;
         
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
-                if (i>0) memo[i][j] += memo[i-1][j];
-                if (j>0) memo[i][j] += memo[i][j-1]; 
+                if (j>0) memo[j] += memo[j-1]; 
             }
         }
         
-        return memo[m-1][n-1];
+        return memo[n-1];
     }
 };
