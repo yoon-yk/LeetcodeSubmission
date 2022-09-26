6ee52b9fbe104ac7c36fbc6ce9b4037316825327
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-26 17:49:13 +0900

    Time: 46 ms (46.32%), Space: 7.3 MB (81.49%) - LeetHub

---
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        
+        vector<int> dp(amount+1, 0);
+        dp[0] = 1;
+        
+        for (auto & coin : coins) {
+            for (int i=coin; i<=amount; i++) {
+
+                // cout << i- coin << endl;
+                if (i-coin >= 0 && dp[i-coin] > 0)
+                    dp[i] += (dp[i-coin]);
+            }
+        }
+        
+        for (int i=0; i<=amount; i++) 
+            cout << dp[i] << " ";
+        cout << endl;
+        
+        return dp[amount];
+    }
+};
