5b21898bb90d3b3066e8d79cbd9765e55426fe0c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-26 17:49:47 +0900

    Time: 17 ms (81.22%), Space: 7.2 MB (90.12%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -5,11 +5,9 @@ public:
         vector<int> dp(amount+1, 0);
         dp[0] = 1;
         
-        for (auto & coin : coins) {
-            for (int i=coin; i<=amount; i++) {
+        for (auto & coin : coins) 
+            for (int i=coin; i<=amount; i++) 
                 dp[i] += (dp[i-coin]);
-            }
-        }
         
         return dp[amount];
     }
