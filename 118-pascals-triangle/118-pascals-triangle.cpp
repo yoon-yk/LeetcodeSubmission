82c8463ca962aef80d871d4dd7f7cd95c6e71c88
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-26 16:05:48 +0900

    Time: 7 ms (6.02%), Space: 6.6 MB (31.16%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,16 +3,14 @@ public:
     vector<vector<int>> generate(int numRows) {
         
         vector<vector<int>> dp(numRows);
-        dp[0].push_back(1);
+        // dp[0].push_back(1);
         
         int curN;
-        for (int i=1; i<numRows; i++) {
+        for (int i=0; i<numRows; i++) {
             for (int j=0; j<i; j++) {
                 curN = dp[i-1][j];
                 if (j > 0) curN += dp[i-1][j-1]; 
-
                 dp[i].push_back(curN);
-
             }
             dp[i].push_back(1);
         }
