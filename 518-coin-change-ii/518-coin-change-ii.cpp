e14ff2847abcf0d2f16e937e008571a79001e6ec
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-26 18:15:33 +0900

    Time: 388 ms (5.48%), Space: 18.9 MB (21.16%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        vector<vector<int>> memo (coins.size(), vector<int>(amount+1, -1));
+        return backtrack(0, amount, coins, memo);
+    }
+    
+    int backtrack(int idx, int amount, vector<int>& coins, vector<vector<int>>& memo) {
         
-        vector<int> dp(amount+1, 0);
-        dp[0] = 1;
+        if (memo[idx][amount]!= -1)
+            return memo[idx][amount];
         
-        for (auto & coin : coins) 
-            for (int i=coin; i<=amount; i++) 
-                dp[i] += (dp[i-coin]);
+        if (amount == 0) 
+            return memo[idx][amount] = 1;
         
-        return dp[amount];
+        int ans = 0;
+        int n = coins.size();
+        for (int i=idx; i<n; i++) {
+            if (amount - coins[i] < 0) continue;
+            amount -= coins[i];
+            ans += backtrack(i, amount, coins, memo) ;
+            amount += coins[i];
+        }
+        
+        return memo[idx][amount] = ans;
     }
 };
