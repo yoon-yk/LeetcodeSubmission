3c7c98d55903d3fe4bbbcfd8673cf3dc06aee8bd
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-28 00:56:04 +0900

    Time: 62 ms (18.22%), Space: 13.1 MB (24.57%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
-        vector<vector<int>> memo(n+1, vector<int> (m+1, -1));
-        return dpSolve(text1, text2, 0, 0, memo);
-    }
-    
-    int dpSolve(string & text1, string & text2, int p1, int p2, vector<vector<int>>& memo) {
-               
-        if (memo[p1][p2] != -1) 
-            return memo[p1][p2];
-    
-        if (p1 == text1.size() || p2 == text2.size()) 
-            return memo[p1][p2] = 0; 
+        vector<vector<int>> memo(n+1, vector<int> (m+1, 0));
         
-        int answer = 0;
-        if (text1[p1] == text2[p2])
-            answer = 1 + dpSolve(text1, text2, p1+1, p2+1, memo);
-        else 
-            answer = max(dpSolve(text1, text2, p1, p2+1, memo), dpSolve(text1, text2, p1+1, p2, memo));
+        for (int c = m-1; c >= 0; c--) {
+            for (int r = n-1; r >= 0; r--) {
+                if (text1[r] == text2[c]) {
+                    memo[r][c] = 1 + memo[r+1][c+1];
+                } else {
+                    memo[r][c] = max(memo[r+1][c], memo[r][c+1]);
+                }
+            }
+        }
         
-        return memo[p1][p2] = answer;
+        return memo[0][0];
     }
 
 };
