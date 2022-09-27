27652ed57d863ddf1eb11f448a5cc57c146e2905
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-27 17:06:26 +0900

    Time: 3 ms (79.46%), Space: 6.8 MB (60.20%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -6,21 +6,19 @@ public:
         
         int dist = INT_MAX;
         for (int i=0; i<n; i++) {
-            if (s[i] == c) {
+            if (s[i] == c)
                 dist = 0;
-            } else if (dist != INT_MAX){
+            else if (dist != INT_MAX) 
                 dist++;
-            }
             arr[i] = min(arr[i], dist);
         }
         
         dist = INT_MAX;
         for (int i=n-1; i>=0; i--) {
-            if (s[i] == c) {
+            if (s[i] == c)
                 dist = 0;
-            } else if (dist != INT_MAX){
+            else if (dist != INT_MAX)
                 dist++;
-            }
             arr[i] = min(arr[i], dist);
         }
         
