3a91c0e76f7bbeef9836e90282fa939d49a7e10c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-27 15:54:48 +0900

    Time: 153 ms (11.79%), Space: 17.2 MB (57.37%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,62 +1,21 @@
 class Solution {
 public:
-    string pushDominoes(string dominoes) {
-        // bfs
-        int n = dominoes.size();
-        queue<int> q;
-
-        /*
-        status :   -1 = undecided,
-                    0 = balanced, 
-                    1 = left, 
-                    2 = right 
-                    
-                    
-                    R  R   .  L
-                    2, 2, -1, 1 cnt = 3
-                           0    cnt = 4
-                           
-                   .  L  .  R  .  .  .  L  R  .  .  L  .  .
-                   -1 1  -1 2 -1 -1 -1  1  2 -1 -1  1 -1 -1 cnt = 5
-                   1     0     2     1       2   1          cnt = 6
-                                  0                         cnt = 7
-                                                            cnt = 7
-                   
-                   
-        */
-        
-        int prevCnt = 0, curCnt = 0;
-        for (int i = 0; i<n; i++) {
-            if (dominoes[i] == '.')
-                q.push(i);
+    string pushDominoes(string d) {
+        d = 'L' + d + 'R';
+        string res = "";
+        for (int i = 0, j = 1; j < d.length(); ++j) {
+            if (d[j] == '.') continue;
+            int middle = j - i - 1;
+            if (i > 0)
+                res += d[i];
+            if (d[i] == d[j])
+                res += string(middle, d[i]);
+            else if (d[i] == 'L' && d[j] == 'R')
+                res += string(middle, '.');
+            else
+                res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
+            i = j;
         }
-
-        int i, size;
-        string cur = dominoes, next = dominoes; 
-        while (!q.empty()) { // TODO : if no change in one loop = end of the loop
-
-            size = q.size();
-            while (size--) {
-                i = q.front(); q.pop();
-            
-                // left most , right most handling
-                if (i > 0 && i < n-1 && cur[i-1] == 'R' && cur[i+1] == 'L') {
-                    continue;
-                }
-                else if (i < n-1 && cur[i+1] == 'L') { // righthand side pushed left
-                    next[i] = 'L';
-                    if (i > 0 && cur[i-1] == '.') q.push(i-1);
-                }
-                else if (i > 0 && cur[i-1] == 'R') { // // lefthand side pushed left
-                    next[i] = 'R';
-                    if (i < n-1 && cur[i+1] == '.') q.push(i+1);
-                }
-            }
-            
-            cur = next;
-
-        }
-    
-        return cur;
+        return res;
     }
 };
