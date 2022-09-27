3a33041df3c57841d299acd12c1bf93b50c01836
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-28 00:25:17 +0900

    Time: 72 ms (12.15%), Space: 13.2 MB (24.57%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> memo(n+1, vector<int> (m+1, -1));
+        return dpSolve(text1, text2, 0, 0, memo);
+    }
+    
+    int dpSolve(string & text1, string & text2, int p1, int p2, vector<vector<int>>& memo) {
+        
+        if (p1 == text1.size() || p2 == text2.size()) 
+            return 0;        
+
+        if (memo[p1][p2] != -1) 
+            return memo[p1][p2];
+    
+        int answer = 0;
+        if (text1[p1] == text2[p2])
+            answer = 1 + dpSolve(text1, text2, p1+1, p2+1, memo);
+        else 
+            answer = max(dpSolve(text1, text2, p1, p2+1, memo), dpSolve(text1, text2, p1+1, p2, memo));
+        
+        return memo[p1][p2] = answer;
+    }
+
+};
