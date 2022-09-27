bdebc778dc2b148a898e1c898cb8de6301f7c317
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-27 16:35:31 +0900

    Time: 148 ms (12.83%), Space: 20.5 MB (39.34%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -2,32 +2,35 @@ class Solution {
 public:
    string pushDominoes(string s){
         int n = s.length();
-        vector<int> l(n, INT_MAX), r(n, INT_MAX);
-        
-        int count = -1;
+        vector<int> l(n), r(n); // each value represents the distance from the affecting root 
+       
+        // 1. Calculate the distance from the closest 'R' element
+        int dist = INT_MAX;
+        for (int i=0; i<n; i++){ // Starting from the first element
+            if (s[i] == 'R') 
+                dist = 0;
+            else if (s[i] == 'L') dist = INT_MAX;
+            else if (dist != INT_MAX && s[i] == '.') dist++;
+            r[i] = dist;
+        }
+       
+        // 2. Calculate the distance from the closest 'L' element
+        dist = INT_MAX;
+        for (int i=n-1; i>=0; i--){ // Starting from the last element
+            if (s[i] == 'L')
+                dist = 0;
+            else if(s[i] == 'R') dist = INT_MAX;
+            else if (dist != INT_MAX && s[i] == '.') dist++;
+            l[i] = dist;
+        }
+       
+        // Update the current string
         for (int i=0; i<n; i++){
-            if(s[i] == 'R'){
-                count = 1;
-            }
-            else if (s[i] == 'L') count = -1;
-            else if (count > 0 && s[i] == '.') r[i] = count++;
+            if (l[i] == r[i]) continue; 
+            else if (l[i] > r[i]) s[i] = 'R'; 
+            else s[i] = 'L';
         }
        
-        count = -1;
-        for(int i=n-1; i>=0; i--){
-            if(s[i] == 'L'){
-                count = 1;
-            }
-            else if(s[i] == 'R') count = -1;
-            else if (count > 0 && s[i] == '.') l[i] = count++;
-        }
-       
-        string ans = "";
-        for(int i=0; i<n; i++){
-            if (l[i] == r[i]) ans += s[i];
-            else if (l[i] > r[i]) ans += 'R';
-            else ans += 'L';
-        }
-        return ans;
+        return s;
     }
 };
