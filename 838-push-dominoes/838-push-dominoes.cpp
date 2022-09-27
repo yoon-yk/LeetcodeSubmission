f5117c7f535d2677f6da160f1435e671c3091c58
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-27 16:26:02 +0900

    Time: 144 ms (12.83%), Space: 22.6 MB (25.82%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    string pushDominoes(string d) {
-        d = 'L' + d + 'R';
-        string res = "";
-        for (int i = 0, j = 1; j < d.length(); ++j) {
-            if (d[j] == '.') continue;
-            int middle = j - i - 1;
-            if (i > 0)
-                res += d[i];
-            if (d[i] == d[j])
-                res += string(middle, d[i]);
-            else if (d[i] == 'L' && d[j] == 'R')
-                res += string(middle, '.');
-            else
-                res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
-            i = j;
+   string pushDominoes(string s){
+        int n = s.length();
+        vector<int> l(n, INT_MAX), r(n, INT_MAX);
+        
+        int count = -1;
+        for (int i=0; i<n; i++){
+            if(s[i] == 'R'){
+                count = 1;
+            }
+            else if (s[i] == 'L') count = -1;
+            else if (count > 0 && s[i] == '.') r[i] = count++;
         }
-        return res;
+       
+        count = -1;
+        for(int i=n-1; i>=0; i--){
+            if(s[i] == 'L'){
+                count = 1;
+            }
+            else if(s[i] == 'R') count = -1;
+            else if (count > 0 && s[i] == '.') l[i] = count++;
+        }
+       
+        string ans = "";
+        for(int i=0; i<n; i++){
+            if (l[i] == r[i]) ans += s[i];
+            else if (l[i] > r[i]) ans += 'R';
+            else ans += 'L';
+        }
+        return ans;
     }
 };
