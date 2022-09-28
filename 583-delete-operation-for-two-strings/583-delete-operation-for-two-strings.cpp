becd590ad044e0623f6c4495c84a2f8f53350c9d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-28 21:46:31 +0900

    Time: 55 ms (26.34%), Space: 12.1 MB (54.66%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -4,14 +4,17 @@ public:
         int n = word1.size(), m = word2.size();
         vector<vector<int>> memo(n+1, vector<int>(m+1, 0));
         
-        for (int i = n-1; i>=0; i--) {
-            for (int j = m-1; j>=0; j--) {
-                if (word1[i] == word2[j])
-                    memo[i][j] = 1 + memo[i+1][j+1];
-                else 
-                    memo[i][j] = max(memo[i+1][j], memo[i][j+1]);
+        for (int i = 0; i<=n; i++) {
+            for (int j = 0; j<=m; j++) {
+                if (i == 0 || j == 0 ) {
+                    memo[i][j] = i + j;
+                } else if (word1[i-1] != word2[j-1]) {
+                    memo[i][j] = 1 + min(memo[i][j-1], memo[i-1][j]);
+                } else if (word1[i-1] == word2[j-1]) {
+                    memo[i][j] = memo[i-1][j-1];
+                }
             }
         }
-        return (n-memo[0][0]) + (m-memo[0][0]);
+        return memo[n][m];
     }
 };
