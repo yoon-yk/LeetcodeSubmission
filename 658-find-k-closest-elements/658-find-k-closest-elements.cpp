cbb8a46d46b44a9bb4c168686db51d885d033785
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-09-29 20:56:47 +0900

    Time: 140 ms (33.46%), Space: 34.8 MB (15.86%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    
+        // cout << " ******" << endl;
+        int n = arr.size();
+        vector<int> ans;
+
+        priority_queue<int> maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        int leftBound = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        // int rightBound = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
+        
+        for (int i=0; i<leftBound; i++) {
+            maxHeap.push(arr[i]);
+        }
+        
+        for (int i=leftBound; i<n; i++) {
+            minHeap.push(arr[i]);
+        }
+
+        while (ans.size() < k) {
+
+            if (!maxHeap.empty() && !minHeap.empty()) {
+                
+                int left = maxHeap.top();
+                int right = minHeap.top();
+                
+                if (abs(left-x) <= abs(right-x)) {
+                    // cout << left << endl;
+                    ans.push_back(left);
+                    maxHeap.pop();
+                } else {
+                    // cout << right << endl;
+
+                    ans.push_back(right);
+                    minHeap.pop();
+                }
+            } 
+
+            else if (!maxHeap.empty()) {
+                // cout << maxHeap.top() << endl;
+                ans.push_back(maxHeap.top());
+                maxHeap.pop();
+            }
+            else if (!minHeap.empty()) {
+                // cout << minHeap.top() << endl;
+                ans.push_back(minHeap.top());
+                minHeap.pop();
+            }
+               
+        }
+        
+        sort(ans.begin(), ans.end());
+        return ans;
+
+    }
+};
