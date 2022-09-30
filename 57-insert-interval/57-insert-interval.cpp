85ce0a1607bad0ae8a5a9b3835b67a1fc9d7e063
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-01 00:25:18 +0900

    Time: 43 ms (18.50%), Space: 17.1 MB (37.56%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ans;
-        int idx = lower_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin();
-        
-        int n = intervals.size();
-
-        for (int i=0; i<idx; i++) {
-            ans.push_back(intervals[i]);
-        }
-        
-        if (idx > 0 && ans.back()[1] >= newInterval[0]) {
-            ans.back()[1] = max(ans.back()[1], newInterval[1]);
-        } else {
-            ans.push_back(newInterval);
-        }
-        
-        for (int i=idx; i<n; i++) {
-            if (ans.empty() || ans.back()[1] >= intervals[i][0])
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            else {
-                ans.push_back(intervals[i]);
-            }
-        }
     
-        return ans;    
+    /*
+    [1,2],[3,5],[6,7],[8,10],[12,16]
+    [7,11]
+    
+    [1,2],[3,5],[6,7],[7,11],[8,10],[12,16]
+    [1,2],[3,5],[6,11],[12,16]
+
+    */
+    
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        if (intervals.size() == 0) return {newInterval};
+        
+        int idx = 0, end = 0;
+        int rangeL = newInterval[0], rangeR = newInterval[1];
+        
+        while (idx < intervals.size() && intervals[idx][1] < newInterval[0])
+            res.push_back(intervals[idx++]);
+        
+        while (idx < intervals.size() && newInterval[1] >= intervals[idx][0]){
+            rangeL = min(rangeL, intervals[idx][0]);
+            rangeR = max(rangeR, intervals[idx][1]);
+            idx ++;
+        }
+        
+        res.push_back({rangeL, rangeR});
+
+        while (idx < intervals.size())
+            res.push_back(intervals[idx++]);
+
+        return res;
     }
 };
