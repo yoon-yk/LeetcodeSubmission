00ceea4d0dbaa335191864a2884cdeb1f55173fe
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-01 01:56:31 +0900

    Time: 265 ms (15.34%), Space: 38.3 MB (42.50%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
-        int idx = lower_bound(intervals.begin(), intervals.end(), toBeRemoved) - intervals.begin();
         
-        int i = 0;
         int n = intervals.size();
         vector<vector<int>> ans;
-        for (; i<idx; i++) 
-            ans.push_back(intervals[i]);
-
-        if (i > 0 && ans[i-1][1] > toBeRemoved[0]) {
-            int prevEnd = ans[i-1][1];
-            ans[i-1][1] = toBeRemoved[0];
-            if (prevEnd > toBeRemoved[1]) {
-                ans.push_back({toBeRemoved[1], prevEnd});;
+        for (int i=0; i<n; i++) {
+            if (toBeRemoved[0] > intervals[i][1] || toBeRemoved[1] < intervals[i][0]) {
+                ans.push_back(intervals[i]);
+            } else {
+                if (intervals[i][0] < toBeRemoved[0]) {
+                    ans.push_back({intervals[i][0], toBeRemoved[0]});
+                }
+                if (intervals[i][1] > toBeRemoved[1]) {
+                    ans.push_back({toBeRemoved[1], intervals[i][1]});    
+                }
             }
         } 
-        
-        while (i < n && toBeRemoved[1] > intervals[i][0]) {
-            if (toBeRemoved[1] < intervals[i][1]){
-                ans.push_back({toBeRemoved[1], intervals[i][1]});
-                i++;
-                break;
-            }
-            i++;
-        }
-        
-        for (; i<n; i++)
-            ans.push_back(intervals[i]);
-            
         
         return ans;
     }
