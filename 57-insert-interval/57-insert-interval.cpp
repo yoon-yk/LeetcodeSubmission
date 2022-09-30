d15a274e221309d8d97870bc66f0e9d09d8af920
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-01 00:35:48 +0900

    Time: 36 ms (32.11%), Space: 17.2 MB (37.56%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -15,13 +15,14 @@ public:
         if (intervals.size() == 0) return {newInterval};
         
         int idx = 0;
-        int rangeL = newInterval[0], rangeR = newInterval[1];
         
         while (idx < intervals.size() && intervals[idx][1] < newInterval[0])
             res.emplace_back(intervals[idx++]);
-        
+
+        int rangeL = newInterval[0], rangeR = newInterval[1];
+        if (idx < intervals.size() && intervals[idx][0] < newInterval[0])
+            rangeL = intervals[idx][0];
         while (idx < intervals.size() && newInterval[1] >= intervals[idx][0]){
-            rangeL = min(rangeL, intervals[idx][0]);
             rangeR = max(rangeR, intervals[idx][1]);
             idx ++;
         }
