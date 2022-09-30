67b507a806ae5fe79134da624018976dc646f124
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-01 02:00:07 +0900

    Time: 237 ms (36.02%), Space: 38.3 MB (76.25%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -4,15 +4,15 @@ public:
         
         int n = intervals.size();
         vector<vector<int>> ans;
-        for (int i=0; i<n; i++) {
-            if (toBeRemoved[0] > intervals[i][1] || toBeRemoved[1] < intervals[i][0]) {
-                ans.emplace_back(intervals[i]);
+        for (auto & interval : intervals) {
+            if (toBeRemoved[0] > interval[1] || toBeRemoved[1] < interval[0]) {
+                ans.emplace_back(interval);
             } else {
-                if (intervals[i][0] < toBeRemoved[0]) {
-                    ans.push_back({intervals[i][0], toBeRemoved[0]});
+                if (interval[0] < toBeRemoved[0]) {
+                    ans.push_back({interval[0], toBeRemoved[0]});
                 }
-                if (intervals[i][1] > toBeRemoved[1]) {
-                    ans.push_back({toBeRemoved[1], intervals[i][1]});    
+                if (interval[1] > toBeRemoved[1]) {
+                    ans.push_back({toBeRemoved[1], interval[1]});    
                 }
             }
         } 
