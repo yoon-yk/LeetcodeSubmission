62591f36e5df172a15fe0aa7e328618605e86fee
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-01 15:34:19 +0900

    Time: 4 ms (47.86%), Space: 6.2 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    
-    int numDecodings(string &s) {
-                
+    int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
+        vector<int> memo(n+1, 0);
+        memo[0] = 1;
         
-        for (int i=0; i<n; i++) {
-            
-            if (i > 0 && (s[i-1] == '1' || s[i-1] == '2')) {
-                string newStr = s.substr(i-1, 2);
-                if (stoi(newStr) < 27) 
-                    dp[i+1] += dp[i-1];
-            }
-                        
-            if (s[i]!= '0')
-                dp[i+1] += dp[i];
+        // dp 
+        for (int i=1; i<=n; i++) {
+            if (s[i-1]-'0' > 0) memo[i] += memo[i-1];
+            if (i>1 && ((s[i-2]-'0' == 1) || ((s[i-2]-'0' == 2) && s[i-1]-'0' < 7) )) memo[i] += memo[i-2];
         }
         
-        return dp[n]; // number of ways to decode the string 
+        return memo[s.length()];
     }
 };
