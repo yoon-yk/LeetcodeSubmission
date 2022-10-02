1e605f6375932ceccbd08e04b44ffc4197cbc536
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-02 18:46:39 +0900

    Time: 753 ms (18.21%), Space: 292.3 MB (8.41%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,18 +2,26 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
-        // Initialize stack
-        vector<int> topOfStack(nums.size(), INT_MAX);
-        int currStackN = 0;
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0)); // dp[curIdx][prevIdx] = curCnt
         
-        // 2. Make piles of cards
-        for (int i=0, j; i<nums.size(); i++) {
-            int currCard = nums[i];
-            int stackToPut = lower_bound(topOfStack.begin(), topOfStack.begin()+currStackN, currCard) - topOfStack.begin();
-            if (stackToPut >= currStackN) currStackN++;
-            topOfStack[stackToPut] = currCard;
+        int curMax = 0;
+        for (int i=1; i<=n; i++) {
+            int rowMax = 0;
+            for (int j=0; j<=i; j++) {
+                if (j == 0) // 그 전 원소가 없을 때 
+                    dp[i][j] = 1;
+                else if (j==i)
+                    dp[i][j] = rowMax;
+                else if (nums[i-1] > nums[j-1]) // 그 전 원소보다 클 때 
+                    dp[i][j] = dp[j][j] + 1; // j가 cur 일 때의 최대값 
+                else // 그 전 원소보다 작거나 같을 때 
+                    dp[i][j] = 1; // 그 전 원소 안넣는 걸로
+                rowMax = max(rowMax, dp[i][j]);
+            }
+            curMax = max(curMax, rowMax);
         }
         
-        return currStackN;
+        return curMax;
     }
 };
