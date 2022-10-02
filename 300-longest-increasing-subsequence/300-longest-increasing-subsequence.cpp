573640a5ec2b42fea550e0451c632380f34a2559
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-02 23:19:24 +0900

    Time: 17 ms (75.15%), Space: 10.4 MB (87.89%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
-        int n = nums.size();
-        vector<int> dp(n+1, 0); // dp[curIdx] = maxCnt
-        
-        int curMax = 0, ans;
-        dp[0] = 1;
-        
-        int i, j;
-        for (i=1; i<=n; i++) {
-            int rowMax = 0;
-            for (j=0; j<=i; j++) {
-                ans = 1; // // 그 전 원소가 없거나 그 전 원소보다 작거나 같을 때  
-                if (j > 0 && nums[i-1] > nums[j-1]) // 그 전 원소보다 클 때 
-                    ans = dp[j] + 1; // j가 cur 일 때의 최대값 
-                rowMax = max(rowMax, ans);
-            }
-            dp[j-1] = rowMax; 
-            curMax = max(rowMax, curMax);
+        vector<int> piles;
+        for (int & num : nums) {
+            int idx = lower_bound(piles.begin(), piles.end(), num) - piles.begin();
+            if (idx == piles.size()) piles.push_back(num);
+            else piles[idx] = num;
         }
         
-        return curMax;
+        return piles.size();
     }
 };
