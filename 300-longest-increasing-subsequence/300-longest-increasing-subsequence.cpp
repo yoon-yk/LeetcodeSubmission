d748527fbca617e4f1e801610a25aff4393473e5
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-02 23:03:28 +0900

    Time: 501 ms (33.93%), Space: 10.7 MB (24.98%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -12,13 +12,9 @@ public:
         for (i=1; i<=n; i++) {
             int rowMax = 0;
             for (j=0; j<=i; j++) {
-                int ans;
-                if (j == 0) // 그 전 원소가 없을 때 
-                    ans = 1;
-                else if (nums[i-1] > nums[j-1]) // 그 전 원소보다 클 때 
+                int ans = 1; // // 그 전 원소가 없거나 그 전 원소보다 작거나 같을 때  
+                if (j > 0 && nums[i-1] > nums[j-1]) // 그 전 원소보다 클 때 
                     ans = dp[j] + 1; // j가 cur 일 때의 최대값 
-                else // 그 전 원소보다 작거나 같을 때 
-                    ans = 1; // 그 전 원소 안넣는 걸로
                 rowMax = max(rowMax, ans);
             }
             dp[j-1] = rowMax; 
