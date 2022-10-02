11721ece74d60b79bfd35a746586fc718942d86c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-02 23:04:18 +0900

    Time: 596 ms (25.80%), Space: 10.6 MB (44.10%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -5,14 +5,14 @@ public:
         int n = nums.size();
         vector<int> dp(n+1, 0); // dp[curIdx] = maxCnt
         
-        int curMax = 0;
+        int curMax = 0, ans;
         dp[0] = 1;
         
         int i, j;
         for (i=1; i<=n; i++) {
             int rowMax = 0;
             for (j=0; j<=i; j++) {
-                int ans = 1; // // 그 전 원소가 없거나 그 전 원소보다 작거나 같을 때  
+                ans = 1; // // 그 전 원소가 없거나 그 전 원소보다 작거나 같을 때  
                 if (j > 0 && nums[i-1] > nums[j-1]) // 그 전 원소보다 클 때 
                     ans = dp[j] + 1; // j가 cur 일 때의 최대값 
                 rowMax = max(rowMax, ans);
