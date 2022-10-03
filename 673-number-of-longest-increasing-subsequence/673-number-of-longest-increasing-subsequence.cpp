d698e6bc712e7965e74a372473c40b9124f204df
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-03 20:28:35 +0900

    Time: 358 ms (22.36%), Space: 13.3 MB (53.40%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -4,14 +4,15 @@ public:
      int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size(), res = 0, max_len = 0;
         vector<int> len(n, 1), cnt(n, 1);
-         
          //dp[i]: {length, number of LIS which ends with nums[i]}
+         
+         
         for (int i = 0; i<n; i++){
             for (int j = 0; j <i ; j++){
                 if (nums[i] > nums[j]){
                     if (len[i] == len[j]+ 1)
                         cnt[i] += cnt[j];
-                    if (len[i] < len[j] + 1) {
+                    else if (len[i] < len[j] + 1) {
                         len[i] = len[j] + 1;
                         cnt[i] = cnt[j];
                     }
@@ -20,7 +21,7 @@ public:
             
             if (max_len == len[i])
                 res += cnt[i];
-            if (max_len < len[i]){
+            else if (max_len < len[i]){
                 max_len = len[i];
                 res = cnt[i];
             }
