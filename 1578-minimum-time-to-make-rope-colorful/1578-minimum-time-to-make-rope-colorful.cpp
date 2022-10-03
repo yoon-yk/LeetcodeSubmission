61e2712b82c0ed8ceb9fab91ef84138716c50417
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-03 16:58:07 +0900

    Time: 527 ms (5.84%), Space: 99.6 MB (13.91%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -7,31 +7,29 @@ public:
         
         "a - a a a"
         [4,2,5,4,3]
-        
-        
-        
+
         */
         
         int n = colors.size(), j;
         int ans = 0;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        priority_queue<int, vector<int>, greater<int>> pq;
         for (int i=0; i<n-1; i++) {
             
             if (colors[i] == colors[i+1]) {
 
                 j = i;
-                pq.push({neededTime[j], j});
+                pq.push(neededTime[j]);
                 
                 j++;
                 while (j>0 && j <= n-1 && colors[j-1] == colors[j]) {
-                    pq.push({neededTime[j], j});
+                    pq.push(neededTime[j]);
                     j++;
                 }
                 
                 while (pq.size() > 1) {
                     auto top = pq.top();
                     pq.pop();
-                    ans += top[0];
+                    ans += top;
                     // cout << top[0] << endl; 
                 }
                 
