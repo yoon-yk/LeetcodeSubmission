1b1ee18b1e38e054e37ae82fbe91131139c9e537
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-04 21:34:58 +0900

    Time: 11 ms (5.60%), Space: 6.4 MB (8.38%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,26 +1,16 @@
 class Solution {
+    
 public:
-    bool divisorGame(int n) {
-        
-        // Alice wins when n is 1 in Bob's turn
-        
-        // n = 2
-        // alice choses 1
-        
-        // n = 3
-        // alice chooses 1
-            // n = 2, bob c 1
-            // n = 1, alice loses
-        
-        // alice chooses 2
-            // n = 1 in bob's turn
-        
-        // alice needs to choose the biggest number 
-        // 13 
-        // 13 % x == 0, x == 1 / 12 
-        // 12 % x == 0, x == 0 / 11
-        
-        return n%2 == 0;
-        
+  bool divisorGame(int n) {
+    vector <int> dp(n+1, 0);
+
+    for (int i = 2; i < n + 1; i++) {
+      for (int j = 1; j < i; j++) {
+        if (i % j == 0) 
+            dp[i] |= !dp[i - j]; //(!dp[i-j] checks if i-j is a losing position)
+      }
     }
+    return dp[n];
+  }
+    
 };
