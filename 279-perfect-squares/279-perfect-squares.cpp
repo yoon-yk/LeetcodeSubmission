cbeb9a965f4d39a749cb74615b1f7b7fcc28d513
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-04 21:01:57 +0900

    Time: 204 ms (75.79%), Space: 51.5 MB (11.19%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    set<int> sqrNums;
-    
-    bool isDividedBy(int n, int count) {
-        if (count == 1)
-            return sqrNums.count(n);
-        
-        for (auto & sqr : sqrNums) {
-            if (isDividedBy(n-sqr, count-1))
-                return true;
-        }
-        
-        return false;
-    }
-    
     int numSquares(int n) {
-        sqrNums.clear();
         
-        for (int i=sqrt(n); i >= 0; i--)
-            sqrNums.insert(i*i);
+        vector<int> sqrNums;
         
-        int count = 1;
-        for (;count<=n; count++) {
-            if (isDividedBy(n, count))
-                return count;
+        for (int i=1; i <= sqrt(n); i++)
+            sqrNums.push_back(i*i);
+        
+        queue<int> q;
+        q.push(n);
+        
+        int level = 0, size; 
+        
+        while (q.size() > 0) {
+            level ++;
+            
+            size = q.size();
+            while (size--) {
+                int curN = q.front(); q.pop();
+                for (int & sqr : sqrNums) {
+                    if (curN == sqr) {
+                        return level;
+                    } else if (curN > sqr) {
+                        q.push(curN - sqr);
+                    }
+                }
+            }
+            
         }
         
-        return count;
+        return level;
     }
 };
