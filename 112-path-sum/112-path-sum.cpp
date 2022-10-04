0aff52bd8cb0ad765e0455e1c9aeb0206adc8f53
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-04 16:39:34 +0900

    Time: 31 ms (9.28%), Space: 21.3 MB (38.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -10,35 +10,18 @@
  * };
  */
 class Solution {
-public:    
+public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (root==NULL) return false;
+
+        if (!root) return false;
         
-        stack<TreeNode*> st;
-        st.push(root);
+        if ((!(root->left) && !(root->right)) && targetSum == root->val) 
+            return true;
         
-        TreeNode* cur;
-        while (!st.empty()) {
-            cur = st.top();
-            st.pop();
-            
-            if (!cur->left && !cur->right && cur->val == targetSum) {
-                return true;
-            }
-            
-            if (cur->left) {
-                cur->left->val += cur->val;
-                st.push(cur->left);
-            }
-            
-            if (cur->right) {
-                cur->right->val += cur->val;
-                st.push(cur->right);
-            }
-            
-        }
+        if ((!(root->left) && !(root->right)))
+            return false;
         
-        return false;
+        return hasPathSum(root->left, targetSum-root->val) || 
+            hasPathSum(root->right, targetSum-root->val);
     }
-    
 };
