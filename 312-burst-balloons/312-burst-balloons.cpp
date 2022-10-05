3b19c8285c0fb3020bf408510411814bce88007a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-06 01:41:39 +0900

    Time: 1286 ms (9.60%), Space: 10.4 MB (53.19%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
+
+    
     int maxCoins(vector<int>& nums) {
-        // add 1 before and after nums
+        
         nums.insert(nums.begin(), 1);
         nums.insert(nums.end(), 1);
-        // cache the results of dp
-        vector<vector<int>> memo(nums.size(), vector<int>(nums.size(), 0));
-        // we can not burst the first one and the last one
-        // since they are both fake balloons added by ourselves
-        return dp(memo, nums, 1, nums.size() - 2);
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        
+        return bruteForce(nums, 1, n-2, dp);
     }
-	
-    int dp(vector<vector<int>>& memo, vector<int>& nums, int left, int right) {
-        // return maximum if we burst all nums[left]...nums[right], inclusive
-        if (right - left < 0) {
-            return 0;
+    
+    int bruteForce(vector<int>& nums, int l, int r, vector<vector<int>>& dp) {
+        
+        if (r < l) return 0;
+        
+        if (dp[l][r] != -1)
+            return dp[l][r];
+            
+        int maxSum = 0;
+        int remainingLeft, remainingRight, gain;
+        for (int i=l; i<=r; i++) {
+            gain = nums[l - 1] * nums[i] * nums[r + 1];
+            remainingLeft = bruteForce(nums, l, i-1, dp);
+            remainingRight = bruteForce(nums, i+1, r, dp); 
+            maxSum = max(remainingLeft+remainingRight+gain, maxSum);
         }
-
-        // we've already seen this, return from cache
-        if (memo[left][right] > 0) {
-            return memo[left][right];
-        }
-
-        // find the last burst one in nums[left]...nums[right]
-        int result = 0;
-        for (int i = left; i <= right; i++) {
-            // nums[i] is the last burst one
-            int gain = nums[left - 1] * nums[i] * nums[right + 1];
-            // nums[i] is fixed, recursively call left side and right side
-            int remaining = dp(memo, nums, left, i - 1) + dp(memo, nums, i + 1, right);
-            result = max(result, remaining + gain);
-        }
-        // add to the cache
-        memo[left][right] = result;
-        return result;
+        
+        return dp[l][r] = maxSum;
     }
 };
