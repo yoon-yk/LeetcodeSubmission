1674354f671efea80248f0dd60423d2249d50bc2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-07 18:21:50 +0900

    Time: 28 ms (66.66%), Space: 13.6 MB (20.86%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         
-        int n = intervals.size();
-        vector<int> startTime(n), endTime(n);
-        
-        for (int i=0; i<n; i++) {
-            startTime[i] = intervals[i][0];
-            endTime[i] = intervals[i][1];
+        map<int, int> timeline;
+        for (auto &interval : intervals) {
+            
+            timeline[interval[0]]++;
+            timeline[interval[1]]--;
         }
         
-        sort(startTime.begin(), startTime.end());
-        sort(endTime.begin(), endTime.end());
-
-        int startPtr = 0, endPtr = 0;
-        
-        int cnt = 0;
-        while (startPtr < n) {
-            if (startTime[startPtr] >= endTime[endPtr])
-                endPtr++;
-            else cnt++;
-            startPtr ++;
+        int sum = 0, maxSum = 0;
+        for (auto &[time, count] : timeline) {
+            sum += count;
+            maxSum = max(sum, maxSum);
         }
-        
-        return cnt;
+        return maxSum;
     }
 };
