4e093f924d7ed315cf6d024ae9cbb8167ba4c81d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-08 17:33:22 +0900

    Time: 168 ms (20.29%), Space: 13.4 MB (90.31%) - LeetHub

---
+++ 259-3sum-smaller/259-3sum-smaller.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        
+        /*
+        a + b + c < target
+        */
+        
+        int n = nums.size();
+        int begin, end;
+        int curSum, cnt = 0;
+        sort(nums.begin(), nums.end());
+        
+        for (int i=0; i<n-2; i++) {
+            begin = i+1, end = n-1;
+            while (begin < end) {
+                if (nums[i] + nums[begin] + nums[end] < target) {
+                    cnt += end-begin;
+                    begin++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        
+        return cnt;
+        
+    }
+    
+    
+};
