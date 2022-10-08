2126ee6dc4d564f6ac525e283c5a3090b70c5dfb
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-08 16:07:56 +0900

    Time: 125 ms (74.11%), Space: 20 MB (63.39%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,43 +1,78 @@
 class Solution {
 public:
+    
+    inline void beginSkipDup(vector<int>& nums, int& i) {
+        while (i+1<nums.size() && nums[i] == nums[i+1]) i++;
+    }
+    
+    inline void endSkipDup(vector<int>& nums, int& i) {
+        while (i-1>0 && nums[i] == nums[i-1]) i--;
+    }   
+    
     vector<vector<int>> threeSum(vector<int>& nums) {
-     
+        /*
+        
+        a + b + c = 0
+        
+        모두 0 이거나
+        적어도 하나는 음수여야 함. ==> 적어도 하나는 양수여야 함 . 
+        
+        [-4,-1,-1,0,1,2]
+        
+        
+        
+        [-4, -1, 0, 1, 2]
+          1   2  1  1  1
+                
+        
+          
+         -4 + 2 (+ 2) = 0
+
+        
+        a + b + c = 0
+        a 를 음수라고 하자. 
+          |  
+
+        
+        a + b = -c (target)
+        
+        a + b + c = 0
+        a + b = -c
+        -4 -1 = -5
+        
+        [-1,0,1,2,-1,-4]
+        
+          |       |
+          
+         -4 -2 == -6
+         -4 + 2 == -2
+        
+        */
+        
+        int n = nums.size();
         vector<vector<int>> ans;
         
-        //1. sort
         sort(nums.begin(), nums.end());
         
-        //2. two pointers (skip the duplicates)
-        // a + b + c = 0
-        // --> 'a' should be a negavtive num; at least one elem should be negative
-        // --> c should be starting from the bigger num; 
+        int begin, end;
         
-        int aIdx = 0;
-        for (; aIdx<nums.size()-2;) {
-            if (nums[aIdx] > 0) break;
-            
-            int bIdx = aIdx+1, cIdx = nums.size()-1;
-            
-            while (bIdx < cIdx) {
-                int sum = nums[aIdx]+nums[bIdx]+nums[cIdx];
-                if (sum==0) {
-                    ans.push_back({nums[aIdx], nums[bIdx], nums[cIdx]});
-                    bIdx = upper_bound(nums.begin(), nums.end(), nums[bIdx]) - nums.begin();
-                    cIdx = lower_bound(nums.begin(), nums.end(), nums[cIdx]) - nums.begin();
-                    cIdx--;
+        for (int i=0; i<n && nums[i]<=0; i++) {
+            if (i>0 && nums[i] == nums[i-1]) continue;
+            begin = i+1, end = n-1;
+            while (begin < end) {
+                if (nums[begin] + nums[end] + nums[i] == 0) {
+                    ans.push_back({nums[i], nums[begin], nums[end]});
+                    beginSkipDup(nums, begin);
+                    endSkipDup(nums, end);
+                    begin++, end--;
+                } else if (nums[begin] + nums[end] + nums[i] < 0) {
+                    begin++;
+                } else {
+                    end --;
                 }
-                else if (sum < 0) {
-                    bIdx++;
-                }
-                else {
-                    cIdx--;
-                }
-                
             }
-            aIdx = upper_bound(nums.begin(), nums.end(), nums[aIdx]) - nums.begin();
-        }
+        }        
         
         return ans;
-
     }
 };
