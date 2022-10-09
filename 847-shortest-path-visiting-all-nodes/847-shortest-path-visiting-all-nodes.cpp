bf98a704f8255b1a3cc4019c23d16f6960368af1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-09 18:14:04 +0900

    Time: 195 ms (40.61%), Space: 30 MB (13.77%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> cache;
-    int endingMask;
-    
-    int dp(int node, int mask, vector<vector<int>>& graph) {
-    
-        //First, check if this state has already been visited. If it has, return the cached result.
-        if (cache[node][mask] != 0)
-            return cache[node][mask];
-    
-
-        // Base case - mask only has a single "1", which means
-        // that only one node has been visited (the current node)
-        if ((mask & (mask-1)) == 0)
-            return cache[node][mask] = 0;
-            
+    int shortestPathLength(vector<vector<int>>& graph) {
+        if (graph.size() == 1)
+            return 0;
         
-        cache[node][mask] = INT_MAX-1; // Avoid infinite loop in recursion
+        int n = graph.size();
+        int endingMask = (1 << n) -1;
+        vector<vector<bool>> seen(n, vector<bool>(endingMask));
+        queue<vector<int>> q;
         
-        for (int& neighbor : graph[node]) {
-            if ((mask & (1<<neighbor)) != 0) {
-                int alreadyVisited = dp(neighbor, mask, graph);
-                int notVisited = dp(neighbor, mask ^ (1 << node), graph);
-                int betterOption = min (alreadyVisited, notVisited);
-                cache[node][mask] = min(cache[node][mask], 1+betterOption);
-            }
+        for (int i=0; i<n; i++) {
+            q.push({i, 1<<i});
+            seen[i][1<<i] = true;
         }
         
-        return cache[node][mask];            
-    }
-    
-    int shortestPathLength(vector<vector<int>>& graph) {    
-        int n = graph.size();
-        endingMask = (1<<n) -1; // a bitmask that represents all nodes being visited.
-        cache.resize(n+1, vector<int>(endingMask+1, 0));
+        int steps = 0, size;
+        while (!q.empty()) {
+            queue<vector<int>> nextQ;
+            size = q.size();
+            while (size--) {
+                vector<int> currPair = q.front(); q.pop();
+                int node = currPair[0];
+                int mask = currPair[1];
+                
+                for (int& neighbor : graph[node]) {
+                    int nextMask = mask | (1 << neighbor);
+                    if (nextMask == endingMask)
+                        return 1 + steps;
+                    
+                    if (!seen[neighbor][nextMask]) {
+                        seen[neighbor][nextMask] = true;
+                        nextQ.push({neighbor, nextMask});
+                    }
+                }
+            }
+            
+            steps++;
+            q = nextQ;
+        }
         
-        int best = INT_MAX;
-        for (int node = 0; node <n; node++)
-            best = min(best, dp(node, endingMask, graph));
-        
-        
-        // for (int i=0; i<=n; i++) {
-        //     for (int j=0; j<=endingMask; j++) {
-        //         cout << cache[i][j] << " ";
-        //     }
-            // cout << endl;
-        // }
-        
-        return best;
-        
+        return -1;
     }
 };
