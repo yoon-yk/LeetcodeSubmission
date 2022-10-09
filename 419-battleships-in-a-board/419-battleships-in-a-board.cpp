834be1c79bf370cc5d84292b3c1cb98ea0382e6f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-10 02:49:47 +0900

    Time: 15 ms (51.44%), Space: 8.1 MB (84.41%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -12,24 +12,13 @@ public:
         int cnt = 0;
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
-                if (board[i][j] == 'X') {
-                    cnt ++; 
-                    dfs(board, i, j);
-                }
+                if (board[i][j] == '.') continue;
+                if (i>0 && board[i-1][j] == 'X') continue;
+                if (j>0 && board[i][j-1] == 'X') continue;
+                cnt++;
             }
         }        
         
         return cnt;
-    }
-    
-    void dfs(vector<vector<char>>& board, int i, int j) {
-        
-        if (i < 0 || i >=board.size() || j < 0 || j >=board[0].size() || board[i][j] == '.') return;
-        
-        board[i][j] = '.';
-        
-        dfs(board, i+1, j);
-        dfs(board, i, j+1);
-
     }
 };
