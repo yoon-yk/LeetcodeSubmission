2139232b4896db9c8b187b3522f2dc63fc140447
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-09 21:49:22 +0900

    Time: 72 ms (39.61%), Space: 12.1 MB (21.05%) - LeetHub

---
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSplits(string s) {
+        int n = s.size();
+        vector<int> startingFrom(n+1), endingAt(n+1);
+        
+        unordered_set<int> seen;
+        for (int i=0; i<n; i++) {
+            seen.insert(s[i]-'a');
+            endingAt[i] = seen.size();
+        }
+        
+        seen.clear();
+        for (int i=n-1; i>=0; i--) {
+            seen.insert(s[i]-'a');
+            startingFrom[i] = seen.size();
+        }
+        
+        int cnt = 0;
+        for (int i=0; i<n-1; i++) 
+            cnt += (endingAt[i] == startingFrom[i+1]);
+        
+        return cnt;
+        
+    }
+};
