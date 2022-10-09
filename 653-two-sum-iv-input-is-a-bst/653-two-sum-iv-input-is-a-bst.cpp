9e0055bfed0730ba23293f4762baa9180d7bd8c8
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-10 00:29:40 +0900

    Time: 34 ms (98.57%), Space: 36.9 MB (62.65%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,28 +1,89 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// This will give next greater element in average of O(1) time 
+// and O(h) space
+class BSTIterator {
+private:
+    stack<TreeNode*> stk;
+    
+public:
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode * node = stk.top();
+        stk.pop();
+        
+        pushAll(node->right);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !stk.empty();
+    } 
+    
+    void pushAll(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr) {
+            stk.push(curr);
+            curr = curr->left;
+        }
+    }
+};
+
+// This will give next smaller element average of in O(1) time
+// and O(h) space
+class revBSTIterator {
+private:
+    stack<TreeNode*> stk;
+
+public:
+    revBSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* node = stk.top();
+        stk.pop();
+        
+        pushAll(node->left);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !stk.empty();
+    }
+    
+    void pushAll(TreeNode* root) {
+        TreeNode * curr = root;
+        while(curr) {
+            stk.push(curr);
+            curr = curr->right;
+        }
+    }
+};
+
 class Solution {
 public:
-    unordered_set<int> seen;
-    
     bool findTarget(TreeNode* root, int k) {
-        if (!root) return false;
+        BSTIterator left(root); // to get next greater element
+        revBSTIterator right(root); // to get next smaller element
         
-        if (seen.count(k-root->val))
-            return true;
+        int lo = left.next(), hi = right.next(); // lo is smallest element && hi is greatest element
+        while (lo < hi) {
+            
+            if(lo + hi == k) return true;
+            
+            else if(lo + hi < k) {
+                if(!left.hasNext()) break;
+                lo = left.next();     
+            }
+            
+            else {
+                if(!right.hasNext()) break;
+                hi = right.next();
+            }
+        }
         
-        seen.insert(root->val);
-        int ans = false;
-        if (root->left) ans |= findTarget(root->left, k);
-        if (root->right) ans |= findTarget(root->right, k);
-        return ans;
+        return false;
     }
 };
