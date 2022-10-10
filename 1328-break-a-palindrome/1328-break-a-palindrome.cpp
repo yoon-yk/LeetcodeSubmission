d0582fa6775c27c0174bc778cbefd37cf77bae1b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-10 14:40:02 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -29,20 +29,17 @@ public:
         // str에서 a가 아닌게 발견되면 b로 바꿔주기
         
         int n = s.size(), i;
+        
+        if (n == 1)
+            return "";
+        
         for (i=0; i<(n>>1); i++) {
             if (s[i] != 'a') {
                 s[i] = 'a';
                 return s;
             }
         }
-        int lBound = (n%2) ? (n>>1) : (n>>1)-1;
-        for (i=n-1; i>lBound; i--) {
-            if (s[i] == 'a') {
-                s[i] = 'b';
-                return s;
-            }
-        } 
-        
-        return "";
+        s[n-1] = 'b';
+        return s;
     }
 };
