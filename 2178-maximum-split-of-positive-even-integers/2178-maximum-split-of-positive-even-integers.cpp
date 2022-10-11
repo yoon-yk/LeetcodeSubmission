192e926f06c75d98d364d37ce0c76c86e9c7a3e7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-11 21:29:38 +0900

    Time: 370 ms (39.63%), Space: 40.6 MB (48.78%) - LeetHub

---
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        // only even integer
+        // maximum split --> we want to start with the smallest integer (2)
+        // (2 ~ finalSum)
+        
+        // (1) : brute fore try everything - O(2^(finalSum-2))
+        // (2) : memorize
+        /*
+        12
+                [2, 4, 6, 8, 10, 12, 14]
+        path    [2] (2) 
+                [4 (2,2)] (2,2)
+                [2,4(2,2)] (2,2,2)
+                [2,6(2,2,2)] (2,2,2,2)
+                [2,8(2,2,2,2)] (2,2,2,2,2)
+                [2,10(2,2,2,2,2)] (2,2,2,2,2,2)
+                [2, 12(2,2,2,2,2,2)] (2,2,2,2,2,2,2)
+        */
+        if (finalSum % 2) return {};
+        long long numOfTwo = finalSum>>1;
+
+        bool found = false;
+        vector<long long> path;
+        
+        long long nextNum = 2, curSum = 0;
+        while ((curSum + nextNum)<=finalSum){
+            path.emplace_back(nextNum);
+            curSum += nextNum;
+            nextNum += 2;
+        }
+        
+        if (curSum < finalSum) 
+            path[path.size()-1] += (finalSum - curSum);
+
+        return path;
+    }
+};
