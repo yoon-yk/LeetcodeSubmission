fe589239a97ef03ce5d2155c32e1cd799fbeb2df
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-17 20:50:31 +0900

    Time: 821 ms (54.99%), Space: 23.4 MB (23.61%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
@@ -8,14 +8,16 @@ public:
         sort(points.begin(), points.end());
         unordered_map<int, unordered_set<int>> pointsDict;
 
-        for (auto& pt : points)
+        for (auto& pt : points) // O(N)
             pointsDict[pt[0]].insert(pt[1]);
         
         for (int curPtr=0; curPtr<points.size(); curPtr++) {
             int curX = points[curPtr][0], curY = points[curPtr][1];
-            for (int nextPtr=curPtr+1; nextPtr < points.size(); nextPtr++) {
+            vector<int> nextMin = {curX+1, 0};
+            int nextIdx = lower_bound(points.begin(), points.end(), nextMin) - points.begin();
+            for (int nextPtr=nextIdx; nextPtr < points.size(); nextPtr++) {
                 int nextX = points[nextPtr][0], nextY = points[nextPtr][1];
-                if (curX == nextX || curY == nextY) continue;
+                if (curY == nextY) continue;
                 if (!pointsDict[curX].count(nextY) || 
                     !pointsDict[nextX].count(curY)) continue;
                 minSize = min(minSize, abs((nextX-curX)*(nextY-curY)));
