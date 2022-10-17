a3292390e33dae7a362d304b867249f1d799fc2b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-17 15:14:15 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     bool checkIfPangram(string sentence) {
         long long check = 0;
-        for (auto &ch : sentence) {
+        for (auto &ch : sentence) 
             check |= (1 << (ch-'a'));
-        }
-        // cout << check << endl;
-        // cout << ((1<<26)-1) << endl;
-        // cout << (check == ((1<<26)-1)) << endl;
         return (check == ((1<<26)-1));
     }
 };
