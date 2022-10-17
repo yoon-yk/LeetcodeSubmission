315c61516fae860c0c890e48312b76881ba44b6d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-17 20:56:18 +0900

    Time: 321 ms (87.95%), Space: 22.1 MB (43.26%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
@@ -18,7 +18,7 @@ public:
             int nextIdx = lower_bound(points.begin(), points.end(), nextMin) - points.begin();
             for (int nextPtr=nextIdx; nextPtr < points.size(); nextPtr++) {
                 nextX = points[nextPtr][0], nextY = points[nextPtr][1];
-                if (curY == nextY) continue;
+                if (curY >= nextY) continue;
                 if (!pointsDict[curX].count(nextY) || 
                     !pointsDict[nextX].count(curY)) continue;
                 minSize = min(minSize, abs((nextX-curX)*(nextY-curY)));
