c873528e526e40009d34bc2c0c8429dc4022912a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-17 20:53:46 +0900

    Time: 474 ms (82.25%), Space: 21.9 MB (60.22%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minAreaRect(vector<vector<int>>& points) {
         
-        int ans = 0;
         int minSize = INT_MAX;
         
         sort(points.begin(), points.end());
@@ -11,19 +10,19 @@ public:
         for (auto& pt : points) // O(N)
             pointsDict[pt[0]].insert(pt[1]);
         
+        int curX, curY, nextX, nextY;
         vector<int> nextMin;
         for (int curPtr=0; curPtr<points.size(); curPtr++) {
-            int curX = points[curPtr][0], curY = points[curPtr][1];
+            curX = points[curPtr][0], curY = points[curPtr][1];
             nextMin = {curX+1, 0};
             int nextIdx = lower_bound(points.begin(), points.end(), nextMin) - points.begin();
             for (int nextPtr=nextIdx; nextPtr < points.size(); nextPtr++) {
-                int nextX = points[nextPtr][0], nextY = points[nextPtr][1];
+                nextX = points[nextPtr][0], nextY = points[nextPtr][1];
                 if (curY == nextY) continue;
                 if (!pointsDict[curX].count(nextY) || 
                     !pointsDict[nextX].count(curY)) continue;
                 minSize = min(minSize, abs((nextX-curX)*(nextY-curY)));
             }
-            
         }      
         return (minSize == INT_MAX)? 0 : minSize;
     }
