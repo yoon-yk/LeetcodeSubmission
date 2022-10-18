1dcce4551f071ecb352d0d4b998c4d12707b62df
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-18 19:37:25 +0900

    Time: 10 ms (71.76%), Space: 6.4 MB (96.57%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     string countAndSay(int n) {
         
-        if (n == 1)
-            return "1";
-        string curStr = countAndSay(n-1);
+        if (n == 0) return "";
         
-        string ans;
-        int j, count, digit;
-        for (int i=0; i<curStr.size();) {
-            j = i;
-            while (j<curStr.size() && curStr[j]==curStr[i]) {
-                j++;
+        string curStr = "1", nextStr;
+        for (int i=2; i<=n; i++) {
+            nextStr = "";
+            int j, count, digit;
+            for (int i=0; i<curStr.size();) {
+                j = i;
+                while (j<curStr.size() && curStr[j]==curStr[i]) j++;
+                count = j-i;
+                digit = curStr[i];
+                nextStr += to_string(j-i); nextStr+= digit;
+                i = j;
             }
-            count = j-i;
-            digit = curStr[i];
-            ans += to_string(j-i); ans+= digit;
-            i = j;
-        }
-        
-        return ans;
+            curStr = nextStr;
+        }        
+        return curStr;
     }
 };
