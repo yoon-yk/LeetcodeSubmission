129263f95ae03a62a09d06eda7dfb7564e59c7d6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-18 15:58:22 +0900

    Time: 143 ms (68.19%), Space: 23.2 MB (19.04%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,53 +1,31 @@
 class Solution {
 public:
     const int K = 10001;
-
-    int find (vector<int>& rep, int x) {
-        if (x == rep[x])
-            return x;
-        return rep[x] = find(rep, rep[x]);
-    }
-    
-    int performUnion(vector<int>& rep, vector<int>& size, int x, int y) {
-        x = find(rep, x);
-        y = find(rep, y);
+    void dfs (vector<vector<int>>& stones, vector<int> adj[], vector<int>& visited, int src) {
+        visited[src] = 1;
         
-        if (x == y) 
-            return 0;
-        
-        if (size[x] > size[y]) {
-            rep[y] = x;
-            size[x] += size[y];
-        } else {
-            rep[x] = y;
-            size[y] += size[x];
+        for (int & adjc : adj[src]) {
+            if (visited[adjc] == 0)
+                dfs(stones, adj, visited, adjc);
         }
-        
-        return 1;
     }
     
     int removeStones(vector<vector<int>>& stones) {
-        vector<int> rep(2*K + 1);
-        vector<int> size(2*K + 1);
         
-        for (int i=0; i< 2*K + 1; i++) {
-            rep[i] = i, size[i] = 1;
-        }
-        
-        int componentCount = 0;
-        unordered_map<int, int> marked;
-        for (vector<int>& stone : stones) {
-            if (!marked.count(stone[0]))
-                componentCount++;
-            if (!marked.count(stone[1]+K))
-                componentCount++;
-            marked[stone[0]] = 1;
-            marked[stone[1]+K] = 1;
-        }
-        
-        for (int i=0; i<stones.size(); i++) {
+        vector<int> adj[2 * K + 1];
+        for (int i=0; i < stones.size(); i++) {
             int x = stones[i][0], y = stones[i][1] + K;
-            componentCount -= performUnion(rep, size, x, y);
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+        
+        vector<int> visited(2*K + 1, 0);
+        int componentCount = 0;
+        for (int i=0; i<2*K + 1; i++) {
+            if (visited[i] == 0 && adj[i].size() > 0) {
+                componentCount ++;
+                dfs (stones, adj, visited, i);
+            }
         }
         
         return stones.size() - componentCount;
