e0680673e40cda69bbb6a0fbdb4b5e7efffc0c72
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-18 14:41:15 +0900

    Time: 222 ms (62.53%), Space: 14.4 MB (69.02%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -5,34 +5,45 @@ public:
         return a[0] == b[0] || a[1] == b[1];
     }
     
-    void dfs (vector<vector<int>>& stones, vector<int> adj[], vector<int>& visited, int src) {
-        visited[src] = 1;
+    int find (vector<int>& rep, int x) {
+        if (x == rep[x])
+            return x;
+        return rep[x] = find(rep, rep[x]);
+    }
+    
+    int performUnion(vector<int>& rep, vector<int>& size, int x, int y) {
+        x = find(rep, x);
+        y = find(rep, y);
         
-        for (int & adjc : adj[src]) {
-            if (visited[adjc] == 0)
-                dfs(stones, adj, visited, adjc);
+        if (x == y) 
+            return 0;
+        
+        if (size[x] > size[y]) {
+            rep[y] = x;
+            size[x] += size[y];
+        } else {
+            rep[x] = y;
+            size[y] += size[x];
         }
+        
+        return 1;
     }
     
     int removeStones(vector<vector<int>>& stones) {
+     
+        vector<int> rep(stones.size());
+        vector<int> size(stones.size());
         
-        vector<int> adj[stones.size()];
-        for (int i=0; i < stones.size(); i++) {
-            for (int j=i+1; j<stones.size(); j++) {
-                if (shareSameRowOrCol(stones[i], stones[j])) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
+        for (int i=0; i<stones.size(); i++) {
+            rep[i] = i, size[i] = 1;
         }
         
-        
-        vector<int> visited(stones.size(), 0);
-        int componentCount = 0;
+        int componentCount = stones.size();
         for (int i=0; i<stones.size(); i++) {
-            if (visited[i] == 0) {
-                componentCount ++;
-                dfs (stones, adj, visited, i);
+            for (int j=i+1; j<stones.size(); j++) {
+                if (shareSameRowOrCol(stones[i], stones[j])) {
+                    componentCount -= performUnion(rep, size, i, j);
+                }
             }
         }
         
