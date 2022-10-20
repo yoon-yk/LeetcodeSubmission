7d541350067e629ec20f1a0418440a99aa95af5e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-21 00:29:14 +0900

    Time: 13 ms (17.84%), Space: 8.8 MB (89.33%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPeak(vector<int>& nums, int idx) {
+        // if (idx < 0 || idx >= nums.size())
+            // return false;
+        if (((idx-1 < 0) || nums[idx-1] < nums[idx]) && (idx+1 >= nums.size() || nums[idx+1] < nums[idx]))
+            return true;
+        return false;
+    }
+    
+    bool isAscending(vector<int>& nums, int idx){
+        return (idx-1 < 0 || nums[idx-1] < nums[idx]);
+    }
+    
+    int findPeakElement(vector<int>& nums) {
+        int start = 0, end = nums.size()-1;
+        while (start <= end) {
+            int mid = start + ((end-start) >> 1);
+            if (isPeak(nums, mid)) return mid;
+            if (isAscending(nums, mid)) start = mid + 1;
+            else end = mid-1;
+        }
+        
+        return start;
+    }
+};
