8c41ae50fe431d4db6523d1b449c58b9fcf8ce49
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-22 17:04:48 +0900

    Time: 842 ms (15.92%), Space: 102.4 MB (54.67%) - LeetHub

--- 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cpp
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    map<string, vector<int>> userQ;
-
-    void insertTime(string& userName, string& time) {
-        int curTime = stringTimeToNumber(time);
-        userQ[userName].push_back(curTime);
-    }
     
-    int stringTimeToNumber(string& time) {
+    inline int stringTimeToNumber(string& time) {
         int hour = stoi(time.substr(0, 2));
         int min = stoi(time.substr(3, 2));
-        
-        int totalMin = hour * 60 + min;
-        return totalMin;
+        return hour * 60 + min;
     }
     
     vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
         
+        vector<string> ans;
+        map<string, vector<int>> userQ;
+
         int n = keyName.size();
-        set<string> setAns;
         
         for (int i=0; i<n; i++)
-            insertTime(keyName[i], keyTime[i]);
+            userQ[keyName[i]].push_back(stringTimeToNumber(keyTime[i]));   
         
-        vector<string> ans;
         for (auto & user : userQ) {
-            string name = user.first;
             auto logs = user.second;
-            sort (logs.begin(), logs.end());
             if (logs.size() < 3) continue;
+            sort (logs.begin(), logs.end());
             
-            for (int i=2; i<logs.size(); i++) {
+            for (int i=2; i<logs.size(); i++)
                 if (logs[i-2] + 60 >= logs[i]) {
-                    ans.push_back(name);
+                    ans.push_back(user.first);
                     break;
                 }
-            }
-
         }
         
         return ans;
