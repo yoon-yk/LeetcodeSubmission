71114b97a78f9afa8ec1e24f6b5a48943588124c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-22 16:43:59 +0900

    Time: 449 ms (70.91%), Space: 8.1 MB (18.41%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,17 +4,13 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int n=board.size(), m = board[0].size();
         vector<vector<int>> visited(n, vector<int>(m, false));
-        string str;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (board[i][j] != word[0]) continue;
-                visited[i][j] = true;
-                if (backtrack(1, i, j, board, visited, word))
-                    return true;
-                visited[i][j] = false;
-            }
-        }
-        
+
+        for (int i=0; i<n; i++)
+            for (int j=0; j<m; j++) 
+                if (isInRange(0, i, j, board, visited, word) 
+                    && backtrack(0, i, j, board, visited, word))
+                        return true;
+
         return false;
     }
     
@@ -22,21 +18,22 @@ public:
         return (!(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || visited[i][j] || word[idx] != board[i][j]));
     }
     
-    inline bool backtrack(int idx, int i, int j, vector<vector<char>>& board, vector<vector<int>> &visited, string& word){
+    inline bool backtrack(int idx, int i, int j, vector<vector<char>>& board, vector<vector<int>>& visited, string& word){
         
-        if (idx == word.size())
+        visited[i][j] = true;
+
+        if (idx == word.size()-1)
             return true;
-            
+        
         for (int d=0; d<4; d++) {
             int newI = i+dir[d], newJ = j+dir[d+1];
-            if (isInRange(idx, newI, newJ, board, visited, word)) {
-                visited[newI][newJ] = true;
+            if (isInRange(idx+1, newI, newJ, board, visited, word)) {
                 if (backtrack(idx+1, newI, newJ, board, visited, word))
                     return true;
-                visited[newI][newJ] = false;
             }
         }
         
+        visited[i][j] = false;
         return false;
     }
 };
