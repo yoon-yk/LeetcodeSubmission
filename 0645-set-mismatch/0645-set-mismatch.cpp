20405a4ce0facf80a7f24adcd771657742b67bf5
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-24 00:46:51 +0900

    Time: 69 ms (61.90%), Space: 21.4 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        int expected = (int) (pow(n, 2) + n) / 2; // gausses property 
-        int duplicate = 0, actualSum = 0;
-        set<int> sset;
-        for (int i = 0; i < n; i++) {
-            if (sset.count(nums[i])) duplicate = nums[i];
-            sset.insert(nums[i]);
-            actualSum += nums[i];
+        int xorr = 0, xor0 = 0, xor1 = 0;
+        for (int n: nums)
+            xorr ^= n;
+        for (int i = 1; i <= n; i++)
+            xorr ^= i;
+        int rightmostbit = xorr & ~(xorr - 1);
+        for (int n: nums) {
+            if ((n & rightmostbit) != 0)
+                xor1 ^= n;
+            else
+                xor0 ^= n;
         }
-        int differential =  expected - actualSum;
-        return {duplicate, duplicate + differential};
+        for (int i = 1; i <= n; i++) {
+            if ((i & rightmostbit) != 0)
+                xor1 ^= i;
+            else
+                xor0 ^= i;
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == xor0)
+                return {xor0, xor1};
+        }
+        return {xor1, xor0};
     }
 };
