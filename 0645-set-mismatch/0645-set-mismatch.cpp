c139b74b7bb5adb7e5d8fd8f12945b31d971cd08
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-28 13:55:08 +0900

    Time: 79 ms (57.50%), Space: 21.4 MB (70.61%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        int xorr = 0, xor0 = 0, xor1 = 0;
-        for (int n: nums)
-            xorr ^= n;
-        for (int i = 1; i <= n; i++)
-            xorr ^= i;
-        int rightmostbit = xorr & ~(xorr - 1);
-        for (int n: nums) {
-            if ((n & rightmostbit) != 0)
-                xor1 ^= n;
-            else
-                xor0 ^= n;
+vector<int> findErrorNums(vector<int>& nums) {
+        for (int i = 0; i<nums.size(); i++){
+            while (nums[i] != nums[nums[i] - 1])
+                swap(nums[i], nums[nums[i] - 1]);
         }
-        for (int i = 1; i <= n; i++) {
-            if ((i & rightmostbit) != 0)
-                xor1 ^= i;
-            else
-                xor0 ^= i;
+        for (int i = 0; i<nums.size() ; i++){
+            if (nums[i] != i + 1) 
+                return {nums[i], i + 1};
         }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == xor0)
-                return {xor0, xor1};
-        }
-        return {xor1, xor0};
+        return {};
     }
 };
