e8dd2526dd4ffa3a643284897ea7224aea3c65e6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-28 13:01:01 +0900

    Time: 290 ms (7.09%), Space: 25.6 MB (14.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
+    string cntToString(string& str) {
+        vector<int> cnt(26, 0);
+        for (auto& ch : str)
+            cnt[ch-'a']++;
+        
+        string modified_str;
+        for (int i=0; i<26; i++) {
+            modified_str += to_string(cnt[i]);
+            modified_str += '/';
+        }
+        return modified_str;
+    }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         unordered_map<string, vector<int>> mp;
         for (int i=0; i<n; i++) {
-            string sorted = strs[i];
-            sort(sorted.begin(), sorted.end());
-            mp[sorted].push_back(i);
+            mp[cntToString(strs[i])].push_back(i);
         }
         
         vector<vector<string>> ans;
         for (auto& [_, idxes]: mp) {
             ans.push_back({});
-            for (auto & idx : idxes) {
+            for (auto & idx : idxes) 
                 ans.back().push_back(strs[idx]);
-            }
         }
         
         return ans;
