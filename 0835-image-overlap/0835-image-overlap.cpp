78f096825b1a69d50f003f59e507325168cb0037
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-29 00:46:54 +0900

    Time: 101 ms (76.39%), Space: 12 MB (43.06%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-  int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+      int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
         vector<int> LA, LB;
         int N = A.size();
         unordered_map<int, int> count;
-        for (int i = 0; i < N * N; ++i)
-            if (A[i / N][i % N] == 1)
-                LA.push_back(i / N * 100 + i % N);
-        for (int i = 0; i < N * N; ++i)
-            if (B[i / N][i % N] == 1)
-                LB.push_back(i / N * 100 + i % N);
-        for (int i : LA) for (int j : LB) count[i - j]++;
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j<N; ++j){
+                if (A[i][j] == 1)
+                    LA.push_back(i*100+j); 
+                if (B[i][j] == 1)
+                    LB.push_back(i*100+j);
+            }
+
+        for (int &i : LA) 
+            for (int &j : LB) 
+                count[i - j]++;
         int res = 0;
-        for (auto it : count) res = max(res, it.second);
+        for (auto &it : count) 
+            res = max(res, it.second);
         return res;
     }
 };
