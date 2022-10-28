aa7b2ad4c16f4f3f692b34bddac72da948899758
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-28 20:31:18 +0900

    Time: 58 ms (62.43%), Space: 18.6 MB (57.67%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,37 +1,59 @@
 class Solution {
 public:
     int maxLength(vector<string>& arr) {
-        int n = arr.size();
-        vector<int> bits(n);
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<arr[i].size(); j++) {
-                if (bits[i] & (1 << (arr[i][j]-'a'))){
-                    bits[i] = -1;
-                    break;
-                }
-                bits[i] |= (1 << (arr[i][j]-'a'));
-            }
+        // Results initialized as a Set to prevent duplicates
+        unordered_set<int> results({0});
+        int best = 0;
+        
+        // Check each string in arr and find the best length
+        for (string word : arr) 
+            best = max(best, addWord(word, results));
+        return best;
+    }
+
+    // Helper to check if string has no duplicate characters
+    int addWord(string& word, unordered_set<int>& results) {
+        // Initialize an int to use as a character set
+        int charBitSet = 0;
+        int best = 0;
+        for (char& c : word) {
+            // Define character mask for currrent char
+            int mask = 1 << (c - 'a');
+
+            // Bitwise AND check using character mask
+            // to see if char already found and if so, exit
+            if ((charBitSet & mask) > 0)
+                return 0;
+
+            // Mark char as seen in charBitSet
+            charBitSet += mask;
+        }
+
+        // If the initial bitset is already a known result,
+        // then any possible new results will have already been found
+        if (results.find(charBitSet) != results.end())
+            return 0;
+
+        // Iterate through previous results only
+        // and store new results in newResults
+        unordered_set<int> newResults;
+        for (int res : results) {
+            // If the two bitsets overlap, skip to the next result
+            if (res & charBitSet)
+                continue;
+
+            // Build the new entry with bit manipulation
+            int newResLen = (res >> 26) + word.size();
+            int newCharBitSet = (charBitSet + res) & ((1 << 26) - 1);
+
+            // Merge the two into one, add it to results,
+            // and keep track of the longest so far
+            newResults.insert((newResLen << 26) + newCharBitSet);
+            best = max(best, newResLen);
         }
         
-        vector<unordered_map<int, int>> dp(n);
-        return backtrack(arr, bits, 0, 0, dp);
-    }
-    
-    int backtrack(vector<string>& arr, vector<int>& bits, int idx, int curBits, vector<unordered_map<int, int>>& dp) {
-                    
-        if (idx == arr.size())
-            return 0;
-        
-        if (dp[idx].count(curBits))
-            return dp[idx][curBits];
-
-        int ans = 0;
-        
-        if (bits[idx] != -1 && (curBits & bits[idx]) == 0) 
-            ans = max(ans, backtrack(arr, bits, idx+1, curBits | bits[idx], dp) + (int)arr[idx].size());
-        
-        ans = max(ans, backtrack(arr, bits, idx+1, curBits, dp));
-        
-        return dp[idx][curBits] = ans;
+        // Merge newResults into results
+        results.insert(newResults.begin(), newResults.end());
+        return best;
     }
 };
