252a481da9ddd840277ae99d59a34aadc1cde7fc
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-28 13:21:43 +0900

    Time: 16 ms (12.70%), Space: 11.3 MB (80.97%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-       queue<char> Q;
-        for (auto & word : word1) {
-            for (auto & ch : word) {
-                Q.push(ch);
+        int w1Idx = 0, w2Idx = 0; 
+        int c1Idx = 0, c2Idx = 0;
+        
+        while (w1Idx<word1.size() && w2Idx< word2.size()) {
+            
+            if (word1[w1Idx][c1Idx] != word2[w2Idx][c2Idx])
+                return false;
+            
+            c1Idx++, c2Idx++;
+            
+            if (c1Idx == word1[w1Idx].size()) {
+                w1Idx ++;
+                c1Idx = 0;
             }
+            
+            if (c2Idx == word2[w2Idx].size()) {
+                w2Idx ++;
+                c2Idx = 0;
+            }
+            
         }
         
+        if (w1Idx<word1.size() || w2Idx< word2.size() ||
+            c1Idx || c2Idx) return false;
         
-        int cnt = 0;
-        for (auto & word : word2) {
-            for (auto & ch : word) {
-                if (Q.empty() || ch != Q.front()) return false;
-                Q.pop();
-            }
-        }
-        
-        return Q.empty();
-   }
+        return true;
+    }
 };
