234b468634755ffd88107f66fee406fdb575cf15
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-28 21:50:52 +0900

    Time: 55 ms (78.41%), Space: 28.1 MB (91.82%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -3,14 +3,14 @@ public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int n = time.size();
         int curTime, cnt = 0;
-        unordered_map<int, int> hashM;
+        vector<int> remainders(60, 0);
         for (int i=0; i<n; i++) {
             curTime = time[i] % 60;
-            if (curTime == 0 && hashM[0] > 0)
-                cnt+= hashM[0];
-            else if (hashM.count(60-curTime)) 
-                cnt+= hashM[60-curTime];
-            hashM[curTime]++;
+            if (curTime == 0)
+                cnt+= remainders[0];
+            else 
+                cnt+= remainders[60-curTime];
+            remainders[curTime]++;
         }
 
         return cnt;
