7f328ab3ca22a949bb4d77f44f831e4b7a61dc5a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-10-31 13:54:58 +0900

    Time: 29 ms (59.44%), Space: 17.5 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        for (int r=0; r<matrix.size(); r++) {
-            for (int c=0; c<matrix[0].size(); c++) {
-                if (r>0 && c>0 && matrix[r-1][c-1] != matrix[r][c])
+        for (int r=1; r<matrix.size(); r++) {
+            for (int c=1; c<matrix[0].size(); c++) {
+                if (matrix[r-1][c-1] != matrix[r][c])
                     return false;
             }
         }
