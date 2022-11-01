252bcb5102e710490ce41de1b4b8f847a92121e1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-01 21:01:58 +0900

    Time: 0 ms (100.00%), Space: 8 MB (99.61%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -4,11 +4,11 @@ public:
         unordered_map<string, pair<string, double>> gidWeight;
         
         int idx = 0;
+        string dividend, divisor, dividendGid, divisorGid;
+        double quotient, dividendWeight, divisorWeight;
         for (auto &equation : equations) {
-            string dividend = equation[0];
-            string divisor = equation[1];
-            double quotient = values[idx];
-            
+            dividend = equation[0], divisor = equation[1];
+            quotient = values[idx];
             unionn (gidWeight, dividend, divisor, quotient);
             idx++;
         }
@@ -16,18 +16,18 @@ public:
         idx=0;
         vector<double>results(queries.size());
         for (auto& query : queries) {
-            string dividend = query[0], divisor = query[1];
+            dividend = query[0], divisor = query[1];
             
             if (!gidWeight.count(dividend) || !gidWeight.count(divisor)) {
                 results[idx] = -1.0;
             } else {
-                pair<string, double> dividendEntry = find(gidWeight, dividend);
-                pair<string, double> divisorEntry = find(gidWeight, divisor);
+                auto dividendEntry = find(gidWeight, dividend);
+                auto divisorEntry = find(gidWeight, divisor);
                 
-                string dividendGid = dividendEntry.first,
-                       divisorGid = divisorEntry.first;
-                double dividendWeight = dividendEntry.second,
-                       divisorWeight = divisorEntry.second;
+                dividendGid = dividendEntry.first,
+                divisorGid = divisorEntry.first;
+                dividendWeight = dividendEntry.second,
+                divisorWeight = divisorEntry.second;
                 
                 if (dividendGid!=divisorGid) 
                     results[idx] = -1;
@@ -44,27 +44,28 @@ public:
     pair<string, double> find(unordered_map<string, pair<string, double>>& gidWeight, string nodeId) {
         if (!gidWeight.count(nodeId))
             gidWeight[nodeId] = pair<string, double>{nodeId, 1.0};
-        pair<string, double> entry = gidWeight[nodeId];
+        
+        auto entry = gidWeight[nodeId];
         
         if (entry.first != nodeId) {
-            pair<string, double> newEntry = find(gidWeight, entry.first);
-            gidWeight[nodeId] = pair<string, double> {newEntry.first, entry.second * newEntry.second};
+            auto newEntry = find(gidWeight, entry.first);
+            gidWeight[nodeId] = {newEntry.first, entry.second * newEntry.second};
         }
         
         return gidWeight[nodeId];
     }
     
     void unionn(unordered_map<string, pair<string, double>>& gidWeight, string dividend, string divisor, double value) {
-        pair<string, double> dividendEntry = find(gidWeight, dividend);
-        pair<string, double> divisorEntry = find(gidWeight, divisor);
+        auto dividendEntry = find(gidWeight, dividend);
+        auto divisorEntry = find(gidWeight, divisor);
         
-        string dividendGid = dividendEntry.first,
-               divisorGid = divisorEntry.first;
-        double dividendWeight = dividendEntry.second,
-               divisorWeight = divisorEntry.second;
+        string dividendGid, divisorGid;
+        double dividendWeight, divisorWeight;
         
-        if (dividendGid!=divisorGid) {
-            gidWeight[dividendGid] = pair<string, double>{divisorGid, divisorWeight*value/dividendWeight};
-        }
+        dividendGid = dividendEntry.first, divisorGid = divisorEntry.first;
+        dividendWeight = dividendEntry.second, divisorWeight = divisorEntry.second;
+        
+        if (dividendGid!=divisorGid) 
+            gidWeight[dividendGid] = {divisorGid, divisorWeight*value/dividendWeight};
     }
 };
