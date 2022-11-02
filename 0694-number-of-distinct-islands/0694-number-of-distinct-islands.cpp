57f3d8adbc8db334b51fde82c88441906409190e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-02 20:57:07 +0900

    Time: 44 ms (90.09%), Space: 27.6 MB (64.05%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    int m, n;
+    vector<int> dir = {-1, 0, 1, 0, -1};
+    
+    bool isInRange(int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n) return false;
+        return true;
+    }
+    
+    int numDistinctIslands(vector<vector<int>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        vector<vector<bool>> visited (m, vector<bool> (n, false));
+        unordered_set<string> visitedSet;
+        
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (!grid[i][j] || visited[i][j]) continue;
+                visited[i][j] = true;
+                string curPath = to_string(5);
+                dfs(grid, i, j, curPath, visited);
+                visitedSet.insert(curPath);
+            }
+        }
+        // for (auto c : visitedSet)
+            // cout << c << endl;
+        
+        return visitedSet.size();
+    }
+    
+    void dfs(vector<vector<int>>& grid, int i, int j, string& curPath, vector<vector<bool>>& visited) {
+        
+        curPath += '.';
+        for (int d=0; d<4; d++) {
+            int newI = i+dir[d], newJ = j+dir[d+1];
+            if (isInRange(newI, newJ, m, n) && grid[newI][newJ] && !visited[newI][newJ]) {
+                visited[newI][newJ] = true;
+                curPath += to_string(d);
+                dfs(grid, newI, newJ, curPath, visited);
+            }
+        }
+        curPath += '.';
+
+    }
+};
