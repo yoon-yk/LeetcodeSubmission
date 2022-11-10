7e73d9ac910ebcb09a3d3642fd04d75ad57603f8
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-10 22:03:33 +0900

    Time: 245 ms (87.84%), Space: 62.8 MB (79.50%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -4,22 +4,27 @@ public:
         
         vector<vector<int>> adjList(n);
         vector<vector<int>> ans(n);
-
-        for (auto& e : edges) 
+        for (auto & e : edges) {
             adjList[e[0]].push_back(e[1]);
+        }
         
-        for (int i=0; i<n; i++) 
-            dfs(i, i, ans, adjList);
+        for (int i=0; i<n; i++) {
+            vector<bool> visited(n, false);
+            visited[i] = true;
+            dfs(i, i, visited, adjList, ans);
+        }
         
         return ans;
     }
     
-    void dfs(int root, int curr, vector<vector<int>>& ans, vector<vector<int>>& adjList) {
-        for (auto& ch : adjList[curr]) {
-            if (ans[ch].empty() || ans[ch].back()!=root) {
-                ans[ch].push_back(root);
-                dfs(root, ch, ans, adjList);
-            }
+    void dfs(int root, int cur, vector<bool> &visited, vector<vector<int>>& adjList, vector<vector<int>>& ans) {
+        
+        if (cur!=root) ans[cur].push_back(root);
+        
+        for (auto & nei : adjList[cur]) {
+            if (visited[nei]) continue;
+            visited[nei] = true;
+            dfs(root, nei, visited, adjList, ans);
         }
-    }
+    }    
 };
