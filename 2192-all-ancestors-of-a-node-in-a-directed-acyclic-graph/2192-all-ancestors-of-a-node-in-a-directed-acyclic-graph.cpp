0ff45df1e899588255db256d5fc1690edaa3ba47
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-10 22:47:36 +0900

    Time: 290 ms (79.20%), Space: 59.6 MB (95.88%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -9,22 +9,19 @@ public:
         }
         
         for (int i=0; i<n; i++) {
-            vector<bool> visited(n, false);
-            visited[i] = true;
-            dfs(i, i, visited, adjList, ans);
+            dfs(i, i, adjList, ans);
         }
         
         return ans;
     }
     
-    void dfs(int root, int cur, vector<bool> &visited, vector<vector<int>>& adjList, vector<vector<int>>& ans) {
+    void dfs(int root, int cur, vector<vector<int>>& adjList, vector<vector<int>>& ans) {
         
         if (cur!=root) ans[cur].push_back(root);
         
         for (auto & nei : adjList[cur]) {
-            if (visited[nei]) continue;
-            visited[nei] = true;
-            dfs(root, nei, visited, adjList, ans);
+            if (!ans[nei].empty() && ans[nei].back() == root) continue;
+            dfs(root, nei, adjList, ans);
         }
     }    
 };
