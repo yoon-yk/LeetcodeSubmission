aef25ac99180cb6646e921f62bc07a7694767414
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-11 22:27:19 +0900

    Time: 251 ms (64.56%), Space: 37.3 MB (96.42%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> parent;
+    
+    int find(int idx) {
+        if (parent[idx] == idx) return idx;
+        return parent[idx] = find(parent[idx]);
+    }
+    
+    void unionn(int idx1, int idx2) {
+        int p1 = find(idx1);
+        int p2 = find(idx2);
+        
+        parent[p1] = p2;
+    }
+    
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        // 1) # of redundant cable in total
+        // 2) # of components -- needs at least (# of c)-1
+        
+        // if (1) < (2) return -1
+        // else then return (# of c)-1
+        
+        // union and find 
+        
+        int numE = connections.size();
+        
+        if (n-1 > numE) return -1;
+        
+        int redundN = 0;
+        int numOfComp = n;
+        parent.resize(n);
+        for (int i=0; i<n; i++) parent[i] = i;
+        
+        for (auto & c : connections) {
+            if (find(c[0]) == find(c[1])) {
+                redundN ++;
+            } else {
+                unionn(c[0], c[1]);
+                numOfComp --;
+            }
+        }
+        
+        // cout << redundN << " " << numOfComp << endl;
+        if (redundN < (numOfComp - 1))
+            return -1;
+        else return (numOfComp -1);
+        
+    }
+};
