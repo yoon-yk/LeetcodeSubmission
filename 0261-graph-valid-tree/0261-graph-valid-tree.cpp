857870db02a19e5e5390f36ab62f7723d0654549
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-12 14:28:38 +0900

    Time: 30 ms (77.05%), Space: 12 MB (86.91%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> parent;
+    vector<int> size;
     
     int find(int e) {
         if (parent[e] == e)
@@ -15,6 +16,7 @@ public:
             return false;
         // ONLY one component
         parent.resize(n);
+        size.resize(n, 1);
         for (int i=0; i<n; i++) parent[i] = i;
         
         int compN = n;
@@ -22,9 +24,17 @@ public:
         for (auto& e : edges) {
             p1 = find(e[0]), p2 = find(e[1]);
             if (p1 != p2) {
-                parent[p1] = p2;
+
+                if (size[p1] > size[p2]) {
+                    parent[p2] = p1;
+                    size[p1] += size[p2];
+                } else {
+                    parent[p1] = p2;
+                    size[p2] += size[p1];
+                }
                 compN--;
-            }
+                
+            } 
         }
         
         return (compN==1);
