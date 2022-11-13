f201383bc721a6dabb9d4ccc5e83b8193c3add76
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-13 19:55:31 +0900

    Time: 66 ms (14.59%), Space: 19.9 MB (42.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<int>> col(10, vector<int>(10, false));
-        vector<vector<int>> row(10, vector<int>(10, false));
-        vector<vector<int>> cell(10, vector<int>(10, false));
+        
+        vector<vector<int>> row(9, vector<int>(10, false)),
+                            col(9, vector<int>(10, false)),
+                            block(9, vector<int>(10, false));
         
         for (int i=0; i<9; i++) {
             for (int j=0; j<9; j++) {
                 if (board[i][j] == '.') continue;
-                
-                int curN = board[i][j]-'0';
-                if (col[i][curN] == true)
+                int curVal = board[i][j]-'0';
+                int blockN = (i/3)*3+(j/3); 
+                if (row[i][curVal] || col[j][curVal] || block[blockN][curVal])
                     return false;
-                col[i][curN] = true;
-                
-                if (row[j][curN] == true)
-                    return false;
-                row[j][curN] = true;
-                
-                int curC = (i/3)*3 + (j/3);
-                if (cell[curC][curN] == true)
-                    return false;
-                cell[curC][curN] = true;
+                row[i][curVal] = true;
+                col[j][curVal] = true;
+                block[blockN][curVal] = true;
             }
         }
-        
         return true;
     }
 };
