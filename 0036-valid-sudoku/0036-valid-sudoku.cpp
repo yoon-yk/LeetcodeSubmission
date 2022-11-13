d4fdf5216dd498424bc802984c2451ddceb339a8
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-13 20:10:02 +0900

    Time: 24 ms (91.52%), Space: 18.3 MB (62.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
+
+    
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        vector<vector<int>> row(9, vector<int>(10, false)),
-                            col(9, vector<int>(10, false)),
-                            block(9, vector<int>(10, false));
+        vector<bitset<10>> row(9), col(9), block(9);
         
         for (int i=0; i<9; i++) {
             for (int j=0; j<9; j++) {
                 if (board[i][j] == '.') continue;
                 int curVal = board[i][j]-'0';
                 int blockN = (i/3)*3+(j/3); 
+                
                 if (row[i][curVal] || col[j][curVal] || block[blockN][curVal])
                     return false;
-                row[i][curVal] = true;
-                col[j][curVal] = true;
-                block[blockN][curVal] = true;
+                row[i].set(curVal);
+                col[j].set(curVal);
+                block[blockN].set(curVal);
             }
         }
         return true;
