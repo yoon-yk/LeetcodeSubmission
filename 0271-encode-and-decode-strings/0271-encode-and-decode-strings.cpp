d9949673b4cf20d69b98ecb52225aab018ea342c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-14 01:03:33 +0900

    Time: 45 ms (95.03%), Space: 25.1 MB (60.00%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
@@ -1,44 +1,46 @@
 class Codec {
 public:
-    string Int2String(int x) {
-        string result;
-        for(int i = 0; i < 4; i++) {
-            unsigned char c = (x >> (i * 8)) & 0xFF;
-            // DO TAKE CARE that we need to use static_cast convert it to unsigned!!
-            result.push_back(static_cast<char>(c));
-        }
-        return result;
-    }
-
-    // Encodes a list of strings to a single string.
-    string encode(vector<string>& strs) {
-        string encoded;
-        for(string& s : strs) {
-            encoded += Int2String(s.size());
-            encoded += s;
-        }
-        return encoded;
+    int INFOSIZE = 5;
+    
+    string& getZeroSize(int size, string& res) {
+        
+        string sizeInfo = to_string(size);
+        int sizeInfoLen = sizeInfo.size();
+        int zeroCnt = (int)(INFOSIZE-sizeInfoLen);
+        
+        res = (string(zeroCnt, '0') + sizeInfo);
+        return res;
     }
     
-    int String2Int(string s) {
-        int result = 0;
-        for(int i = 0; i < 4; i++) {
-            // DO TAKE CARE that we need to use static_cast convert it to unsigned!!
-            result += static_cast<unsigned char>(s[i] << (i * 8));
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        
+        string ret;
+        
+        string res;
+        ret += getZeroSize(strs.size(), res);
+        
+        for (auto & str : strs) {
+            ret += getZeroSize(str.size(), res) + str;
         }
-        return result;
+        return ret;
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
-        vector<string> decoded;
-        int i = 0;
-        while(i < s.size()) {
-            int len = String2Int(s.substr(i, 4));
-            i += 4;
-            decoded.push_back(s.substr(i, len));
-            i += len;
+        vector<string> ans;
+        int cnt = stoi(s.substr(0, INFOSIZE));
+        int startIdx = INFOSIZE, len;
+        for (int i=0; i<cnt; i++) {
+            len = stoi(s.substr(startIdx, INFOSIZE));
+            startIdx += INFOSIZE;
+            ans.push_back(s.substr(startIdx, len));
+            startIdx += len;
         }
-        return decoded;
+        return ans;
     }
 };
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.decode(codec.encode(strs));
