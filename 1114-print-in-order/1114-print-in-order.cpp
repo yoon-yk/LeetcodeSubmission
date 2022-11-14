d5553be45ffd10f8470d317f9ea02de5ea221f5b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-15 00:42:06 +0900

    Time: 58 ms (65.80%), Space: 7.2 MB (19.04%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cpp
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -1,5 +1,7 @@
 class Foo {
 protected :
+    mutex m;
+    condition_variable_any cv;
     int state = 0;
 public:
     Foo() {
@@ -9,26 +11,27 @@ public:
     void first(function<void()> printFirst) {
         
         // printFirst() outputs "first". Do not change or remove this line.
+        lock_guard<mutex> lg(m);
         printFirst();
         state = 2;
+        if (state == 2) cv.notify_all();
     }
 
     void second(function<void()> printSecond) {
         
         // printSecond() outputs "second". Do not change or remove this line.
-        while (state != 2) {
-             std::this_thread::yield();
-        }
+        lock_guard<mutex> lg(m);
+        while (state < 2) cv.wait(m);
         printSecond();
         state = 3;
+        if (state == 3) cv.notify_all();
     }
 
     void third(function<void()> printThird) {
         
         // printThird() outputs "third". Do not change or remove this line.
-        while (state != 3) {
-             std::this_thread::yield();
-        }
+        lock_guard<mutex> lg(m);
+        while (state < 3) cv.wait(m);
         printThird();
     }
 };
