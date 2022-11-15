4de895bf6f3aad4b42fb9de24f88e49ab9e44a91
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-15 15:49:43 +0900

    Time: 17 ms (62.15%), Space: 6.7 MB (52.64%) - LeetHub

--- 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -1,43 +1,60 @@
 class FizzBuzz {
 private:
     int n;
-    int i;
+    int count;
     mutex m;
     condition_variable cv;
 
 public:
     FizzBuzz(int n) {
         this->n = n;
-        i = 1;
+        this->count = 1;
     }
-    
-    void run(function<bool(int)> test, function<void(int)> print) {
+
+    void fizz(function<void()> printFizz) {
         while (true) {
             unique_lock<mutex> lock(m);
-            cv.wait(lock, [&]{ return i > n || test(i); });
-            if (i > n) break;
-            print(i++);
-            lock.unlock();
+            while (count <= n && (count % 3 != 0 || count % 5 == 0))
+                cv.wait(lock);
+            if (count > n) return;
+            printFizz();
+            ++count;
             cv.notify_all();
         }
     }
 
-    void fizz(function<void()> printFizz) {
-        run([](int i){ return i % 3 == 0 && i % 5 != 0; },
-            [&](int i){ printFizz(); });
-    }
-
     void buzz(function<void()> printBuzz) {
-        run([](int i){ return i % 3 != 0 && i % 5 == 0; },
-            [&](int i){ printBuzz(); });
+        while (true) {
+            unique_lock<mutex> lock(m);
+            while (count <= n && (count % 5 != 0 || count % 3 == 0))
+                cv.wait(lock);
+            if (count > n) return;
+            printBuzz();
+            ++count;
+            cv.notify_all();
+        }
     }
 
 	void fizzbuzz(function<void()> printFizzBuzz) {
-        run([](int i){ return i % 3 == 0 && i % 5 == 0; },
-            [&](int i){ printFizzBuzz(); });
+        while (true) {
+            unique_lock<mutex> lock(m);
+            while (count <= n && (count % 5 != 0 || count % 3 != 0))
+                cv.wait(lock);
+            if (count > n) return;
+            printFizzBuzz();
+            ++count;
+            cv.notify_all();
+        }
     }
 
     void number(function<void(int)> printNumber) {
-        run([](int i){ return i % 3 != 0 && i % 5 != 0; }, printNumber);
+        while (true) {
+            unique_lock<mutex> lock(m);
+            while (count <= n && (count % 5 == 0 || count % 3 == 0))
+                cv.wait(lock);
+            if (count > n) return;
+            printNumber(count++);
+            cv.notify_all();
+        }
     }
 };
