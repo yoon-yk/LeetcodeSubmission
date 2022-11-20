99f43b447602fe9943f08509b7131d90de4d815a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-20 21:35:10 +0900

    Time: 206 ms (90.81%), Space: 103.1 MB (24.51%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,21 +4,20 @@ public:
         /*
         [73,74,75,71,69,72,76,73]
         [ 1, 1, 4, 2, 1, 1, 0, 0]
+        
         */
         
-        int n = temp.size();
-        vector<int> arr(n, 0);
-        stack<pair<int, int>> st;
-        st.push({temp[0], 0});
-        for (int i=1; i<n; i++) {
-            while (!st.empty() && temp[i] > st.top().first) {
-                int idx = st.top().second;
-                arr[idx] = i-idx;
+        int dist, tp, n = temp.size();
+        stack<int> st;
+        vector<int> ans(n, 0);
+        for (int i=0; i<n; i++) {
+            tp = temp[i];
+            while (!st.empty() && temp[st.top()] < tp) {
+                ans[st.top()] = i-st.top();
                 st.pop();
             }
-            st.push({temp[i], i});
+            st.push(i);
         }
-        return arr;
-        
+        return ans;
     }
 };
