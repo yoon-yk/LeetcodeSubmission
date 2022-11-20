7d15412c94944b38aeb7d6aa950fdaf970f01bdb
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-21 01:00:58 +0900

    Time: 669 ms (46.78%), Space: 134.7 MB (61.88%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int curN;
         deque<int> dq;
         vector<int> ans;
+        
         for (int i=0; i<nums.size(); i++) {
-            if (!dq.empty() && dq.front() == i-k) dq.pop_front();
+
+            if (!dq.empty() && (i-dq.front()+1) > k)
+                dq.pop_front();
             while (!dq.empty() && nums[dq.back()] < nums[i])
                 dq.pop_back();
             dq.push_back(i);
-            if (i>=k-1) ans.push_back(nums[dq.front()]);
+            if (i >= k-1) ans.push_back(nums[dq.front()]);
         }
+        
         return ans;
     }
 };
