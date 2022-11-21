0ddeee936ecc4a1288321105dff1303c31d6954b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-21 21:22:36 +0900

    Time: 10 ms (71.17%), Space: 9 MB (13.94%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        vector<int> ans(n, -1);
+        unordered_map<int, int> dict2;
+        for (int i=0; i<n; i++)
+            dict2[nums2[i]] = i;
+        
+        stack<int> st;
+        for (int i=0; i<n; i++) {
+            while (!st.empty() && nums2[st.top()] < nums2[i]) {
+                ans[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        
+        vector<int> ret;
+        for (int i : nums1) {
+            if (ans[dict2[i]] > -1)
+                ret.push_back(nums2[ans[dict2[i]]]);
+            else ret.push_back(-1);
+        }
+        
+        return ret;
+    }
+};
