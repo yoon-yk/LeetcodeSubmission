c80fa4e049936da64e8200ff4ecae19cef2bd246
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-21 20:36:20 +0900

    Time: 275 ms (62.06%), Space: 52.2 MB (61.86%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    int longestSubarray(vector<int>& A, int limit) {
-        int start = 0, end, ans = 0;
-        multiset<int> m;
-        for (end = 0; end < A.size(); ++end) {
-            m.insert(A[end]);
-            while (*m.rbegin() - *m.begin() > limit)
-                m.erase(m.find(A[start++]));
-            ans = max(ans, (int)m.size());
+    int longestSubarray(vector<int>& nums, int limit) {
+        int n = nums.size(), start = 0, ans = 0;
+        deque<int> minD, maxD;
+        for (int end = 0; end < n; end++) {
+            int curN = nums[end];
+            
+            while (!minD.empty() && nums[minD.back()] > curN)
+                minD.pop_back();
+            
+            while (!maxD.empty() && nums[maxD.back()] < curN)
+                maxD.pop_back();
+            
+            minD.push_back(end);
+            maxD.push_back(end);
+            
+            // start 줄이기
+            int minN = nums[minD.front()], maxN = nums[maxD.front()];
+            while (abs(maxN - minN) > limit) {
+                if (nums[start] == minN) minD.pop_front(); 
+                if (nums[start] == maxN) maxD.pop_front();
+                start++;
+                minN = nums[minD.front()], maxN = nums[maxD.front()];
+            }
+            
+            ans = max(end-start+1, ans);
         }
         return ans;
+        
     }
 };
