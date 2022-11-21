fcab788d9a20962220f6fc97574a2ca03188df90
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-21 19:59:31 +0900

    Time: 458 ms (20.98%), Space: 78.7 MB (11.08%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& A, int limit) {
-        int i = 0, j, ans = 0;
+        int start = 0, end, ans = 0;
         multiset<int> m;
-        for (j = 0; j < A.size(); ++j) {
-            m.insert(A[j]);
+        for (end = 0; end < A.size(); ++end) {
+            m.insert(A[end]);
             while (*m.rbegin() - *m.begin() > limit)
-                m.erase(m.find(A[i++]));
+                m.erase(m.find(A[start++]));
             ans = max(ans, (int)m.size());
         }
         return ans;
