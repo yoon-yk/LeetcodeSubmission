9fe94838540a68bcd779c302cd25b65e55622c67
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-22 21:42:55 +0900

    Time: 261 ms (81.06%), Space: 85.5 MB (54.56%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -18,24 +18,4 @@ public:
         return maxLen;
     }
     
-    int backtrack(int idx1, int idx2, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp, int &maxLen) {
-                
-        if (dp[idx1][idx2]!= -1)
-            return dp[idx1][idx2];
-        
-        if (idx1>= nums1.size() || idx2>= nums2.size())
-            return dp[idx1][idx2] = 0;
-        
-        int ans = 0;
-        
-        if (nums1[idx1] == nums2[idx2])
-            ans = 1 + backtrack(idx1+1, idx2+1, nums1, nums2, dp, maxLen);
-        
-        int skip1 = backtrack(idx1+1, idx2, nums1, nums2, dp, maxLen);
-        int skip2 = backtrack(idx1, idx2+1, nums1, nums2, dp, maxLen);
-        
-        // ans = max(ans, max(skip1, skip2));
-        maxLen = max(maxLen, ans);
-        return dp[idx1][idx2] = ans;
-    }
 };
