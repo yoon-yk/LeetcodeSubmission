b5ba9295e6ae89b125b931b9c56bc3001a7f72c2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-22 21:36:46 +0900

    Time: 1434 ms (5.01%), Space: 85.8 MB (10.01%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int len1 = nums1.size(), len2 = nums2.size();
+        int n1 = nums1.size(), n2 = nums2.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
         
-        vector<int> cur(len2+1, 0), prev(len2+1, 0);
-        int maxVal = 0;
-        for (int i=len1-1; i>=0; i--) {
-            for (int j=len2-1; j>=0; j--) {
-                if (nums1[i] == nums2[j]) {
-                    cur[j] = prev[j+1] + 1;
-                    if (maxVal < cur[j] ) 
-                        maxVal = cur[j];
-                } else cur[j] = 0;
-            }
-            cur.swap(prev);
-        }
+        int maxLen = 0;
+        backtrack(0, 0, nums1, nums2, dp, maxLen);
+        // for (int i=0; i<=n1; i++) {
+        //     for (int j=0; j<=n2; j++) 
+        //         printf("%2d ", dp[i][j]);
+        //     cout << endl;
+        // }
         
-        return maxVal;
+        return maxLen;
+    }
+    
+    int backtrack(int idx1, int idx2, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp, int &maxLen) {
+                
+        if (dp[idx1][idx2]!= -1)
+            return dp[idx1][idx2];
+        
+        if (idx1>= nums1.size() || idx2>= nums2.size())
+            return dp[idx1][idx2] = 0;
+        
+        int ans = 0;
+        
+        if (nums1[idx1] == nums2[idx2])
+            ans = max(ans, 1 + backtrack(idx1+1, idx2+1, nums1, nums2, dp, maxLen));
+        
+        int skip1 = backtrack(idx1+1, idx2, nums1, nums2, dp, maxLen);
+        int skip2 = backtrack(idx1, idx2+1, nums1, nums2, dp, maxLen);
+        
+        maxLen = max(maxLen, ans);
+        return dp[idx1][idx2] = ans;
     }
 };
