0b528515c456b07998994492422f996f864504e7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-23 01:50:26 +0900

    Time: 139 ms (6.79%), Space: 29.1 MB (7.85%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        
-        int n = nums.size(), minL = INT_MAX, left = 0, sum = 0;
-        for (int right=0; right<n; right++) {
-            sum += nums[right];
-            
-            while (sum >= target) {
-                minL = min(right-left+1, minL);
-                sum -= nums[left];
-                left ++;
+    int minSubArrayLen(int s, vector<int>& nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        int ans = INT_MAX;
+        vector<int> sums(n + 1, 0); //size = n+1 for easier calculations
+        //sums[0]=0 : Meaning that it is the sum of first 0 elements
+        //sums[1]=A[0] : Sum of first 1 elements
+        //ans so on...
+        for (int i = 1; i <= n; i++)
+            sums[i] = sums[i - 1] + nums[i - 1];
+        for (int i = 1; i <= n; i++) {
+            int to_find = s + sums[i - 1];
+            auto bound = lower_bound(sums.begin(), sums.end(), to_find);
+            if (bound != sums.end()) {
+                ans = min(ans, static_cast<int>(bound - (sums.begin() + i - 1)));
             }
         }
-        return (minL < INT_MAX) ? minL : 0;
+        return (ans != INT_MAX) ? ans : 0;
     }
 };
