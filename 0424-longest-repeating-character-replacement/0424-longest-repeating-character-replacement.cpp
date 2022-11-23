e585bb536aabb8e19c416f7ee66e08022da6c031
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-23 14:27:36 +0900

    Time: 24 ms (48.49%), Space: 7 MB (85.93%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int len = s.size();
-        vector<int> count(26);
-        int start = 0, maxCount = 0, maxLength = 0;
-        int curCh; 
-        for (int end = 0; end < len; end++) {
-            curCh = s[end] - 'A';
-            count[curCh]++;
+        
+        int n = s.size(), curCh, left = 0, right = 0, ans = 0, maxCount = 0;
+        vector<int> dict(26, 0);
+        for (;right<n; right++) {
+            curCh = s[right]-'A';
+            dict[curCh]++;
+            maxCount = max(maxCount, dict[curCh]);
             
-            maxCount = max(maxCount, count[curCh]);
-            
-            while (end - start + 1 - maxCount > k) {
-                count[s[start]- 'A']--;
-                start++;
-            }
-            
-            maxLength = max(maxLength, end - start + 1);
+            while ((right-left+1) - maxCount > k) {
+            // while ((right-left+1) - dict[curCh] > k) {
+                dict[s[left]-'A']--;
+                maxCount = max(maxCount, dict[s[left]-'A']);
+                left++;
+            } 
+            ans = max(ans, right-left+1);
         }
-        return maxLength;
+        
+        return ans;
     }
 };
