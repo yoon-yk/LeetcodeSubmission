7632eae1480927dd475356e0f20c352fe8370a56
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-23 18:22:55 +0900

    Time: 6 ms (22.30%), Space: 11.1 MB (11.76%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string decodeString(string s) {
+     
+        stack<char> st;
+        stack<int> cntSt; 
+        int n = s.size();
+        for (int i=0; i<n; i++) {
+            if (s[i] == ']') {
+                string ans;
+                string finalStr;
+                while (st.top() != '[') {
+                    ans = st.top() + ans;
+                    st.pop();
+                }
+                st.pop();
+                // cout << ans << endl;
+                int cnt = cntSt.top(); cntSt.pop();
+                // cout << cnt << endl;
+                for (int i=0; i<cnt; i++) {
+                    finalStr += ans;
+                }
+                // cout << finalStr << endl;
+                for (char c : finalStr) {
+                    st.push(c);
+                }
+            } else if (isdigit(s[i])) {
+                int j = i;
+                string num;
+                while (isdigit(s[j])) {
+                    num += s[j++];
+                }
+                i = j-1;
+                cntSt.push(stoi(num));
+            } else {
+                st.push(s[i]);
+            }
+        }
+        
+        string ans; 
+        while(!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
