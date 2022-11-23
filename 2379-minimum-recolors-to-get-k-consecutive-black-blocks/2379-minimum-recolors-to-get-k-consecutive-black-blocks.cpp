6bdd71ccf1dbe047b17380e16c410c27e04f1191
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-24 02:08:50 +0900

    Time: 3 ms (56.80%), Space: 6.1 MB (92.48%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
+       
+        // make a window
+        // count the black blocks
+        // curans = (k-cntBlk)
         
-        int beginIdx = 0, endIdx = 0;
-        int blackCnt = 0;
+        // while (end<n) 
+        //  add next blck
+        //  discard head blck
+        //  update curans
         
-        for (; endIdx<k; endIdx++)
-            if (blocks[endIdx] == 'B') ++blackCnt;               
-
-        if (blackCnt == k) 
-            return 0;
-        
-        int len = blocks.length();
-        int curMax = blackCnt;
-        endIdx--;
-        
-        while (endIdx < len) {
-            
-            ++endIdx;
-            if (blocks[endIdx] == 'B') ++blackCnt;
-            
-            if (blocks[beginIdx] == 'B') --blackCnt;
-            ++beginIdx;
-
-            curMax = max(curMax, blackCnt);
+        int blckCnt = 0;
+        for (int i=0; i<k; i++) {
+            if (blocks[i] == 'B')
+                blckCnt ++;
         }
         
-        return k-curMax;
+        int maxBCnt = blckCnt;
+        
+        for (int end=k, start=0; end<blocks.size(); end++, start++) {
+            if (blocks[end] == 'B')
+                blckCnt ++;
+            if (blocks[start] == 'B')
+                blckCnt --;
+            maxBCnt = max(maxBCnt, blckCnt);
+        }
+        
+        return k - maxBCnt;
         
     }
 };
