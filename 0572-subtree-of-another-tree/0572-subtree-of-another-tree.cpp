52c2a60947dada81682d94d2c0b976dcee835e49
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-24 20:53:25 +0900

    Time: 56 ms (37.62%), Space: 29.2 MB (9.64%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+
+        queue<TreeNode*> Q;
+        Q.push(root);
+        while(!Q.empty()) {
+            auto cur = Q.front(); Q.pop();
+            if (cur->val == subRoot->val) {
+                if (isSameTree(cur, subRoot))
+                    return true;
+            }
+            if (cur->left) Q.push(cur->left);
+            if (cur->right) Q.push(cur->right);
+        }
+        return false;
+    }
+    
+    bool isSameTree(TreeNode* a, TreeNode* b) {
+        if (!a && !b) return true;
+        else if (!a || !b) return false;
+        else if (a->val != b->val) return false;
+        
+        bool isLeftSame = isSameTree(a->left, b->left);
+        bool isRightSame = isSameTree(a->right, b->right);
+        
+        return isLeftSame && isRightSame;
+    }
+    
+};
