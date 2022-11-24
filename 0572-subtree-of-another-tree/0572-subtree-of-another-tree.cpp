22c918cd83f43d09ae2d99d4877a68d73d72107a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-25 03:11:25 +0900

    Time: 66 ms (12.70%), Space: 28.9 MB (54.95%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -1,49 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* sR;
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-
-        sR = subRoot;
-        queue<TreeNode*> Q;
-        Q.push(root);
-        while(!Q.empty()) {
-            auto cur = Q.front(); Q.pop();
-            if (cur->val == subRoot->val) {
-                if (isSameTree(cur, subRoot))
-                    return true;
-            }
-            if (cur->left) Q.push(cur->left);
-            if (cur->right) Q.push(cur->right);
-        }
-        return false;
+        if (!root && !subRoot) return true;
+        if (!root || !subRoot) return false;
+        
+        return matchSubtree(root, subRoot) || isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
     
-    bool isSameTree(TreeNode* a, TreeNode* b) {
-        if (!a && !b) return true;
-        else if (!a || !b) return false;
-        else if (a->val != b->val) return false;
+    bool matchSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot) return true;
         
-        if (!sR && a->val == sR->val) {
-            TreeNode * temp = sR;
-            if (isSameTree(a, temp))
-                return true;
-        }
+        if (!root|| !subRoot) return false;
         
-        bool isLeftSame = isSameTree(a->left, b->left);
-        bool isRightSame = isSameTree(a->right, b->right);
-        
-        return isLeftSame && isRightSame;
+        return root->val == subRoot->val &&
+            matchSubtree(root->left, subRoot->left) &&
+            matchSubtree(root->right, subRoot->right);
     }
-    
 };
