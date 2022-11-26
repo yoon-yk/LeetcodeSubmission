bfaa9d651b90ab91496ff78b7571830654a50b39
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-26 18:02:14 +0900

    Time: 3 ms (92.38%), Space: 11.1 MB (28.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
-    int findStartingIdx(vector<int>& nums, int target) {
-        int start = 0, end = nums.size()-1;
-        while (start <= end) {
-            int mid = (start+end) >> 1;
-            if (mid+1 >= nums.size()) break;
-            if (nums[mid] > nums[mid+1])
-                return mid+1;
-            if (nums[mid] < nums[start])
-                end = mid-1;
-            else start = mid+1;
-        }
-        return end;
-    }
-    
     int search(vector<int>& nums, int target) {
-        int startingIdx = findStartingIdx(nums, target);
-        int findLeft = lower_bound(nums.begin(), nums.begin() + startingIdx-1, target) - nums.begin();
-        int findRight = lower_bound(nums.begin() + startingIdx, nums.end(), target) - (nums.begin());
-        if (findLeft >= 0 && findLeft < startingIdx && nums[findLeft] == target)
-            return findLeft;
-        if (findRight >= startingIdx && findRight < nums.size() && nums[findRight] == target)
-            return findRight;
+        int lo = 0, hi = nums.size()-1;
+        /*
+        0 1 2 3 4 5 6
         
+        */
+        
+        while (lo < hi) {
+            int mid = lo + ((hi-lo) >> 1);
+            if (nums[mid] == target) return mid;
+            else if (nums[lo] == target) return lo;
+            else if (nums[hi] == target) return hi;
+            
+            if (nums[lo] > nums[hi]) {
+                if (nums[mid] > nums[hi])
+                    lo = mid+1;
+                else hi = mid;
+            } else {
+                hi --;
+            }
+        }
+        
+        int origIdx = hi;
+        
+        if (nums[hi] == target) return hi;
+        
+        if (target <= nums[nums.size()-1]) 
+            lo = origIdx, hi = nums.size()-1;
+        else lo = 0, hi = origIdx-1;
+        
+        while (lo <= hi) {
+            int mid = lo + ((hi-lo) >> 1);
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) lo = mid+1;
+            else hi = mid-1;
+        }
+            
         return -1;
     }
 };
