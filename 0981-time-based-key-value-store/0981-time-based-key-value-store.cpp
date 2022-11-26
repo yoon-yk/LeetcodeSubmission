2df7f61e639a7ea4a2bfba485c8fd6481e4d14bc
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-26 20:34:15 +0900

    Time: 764 ms (37.45%), Space: 128.6 MB (67.28%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -0,0 +1,29 @@
+class TimeMap {
+public:
+    /*
+    foo 
+    1 bar
+    3 bar2 ...
+    */
+    
+    unordered_map<string, vector<pair<int, string>>> mp;
+    TimeMap() {    
+        // vector<string> v= {"a a"," a a", "", "a", "az"};
+        // sort(v.begin(), v.end());
+        // for (auto & s : v) cout << s << "/" ;
+        // cout << endl;
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (!mp.count(key)) return ""; 
+        auto & vectorList = mp[key];
+        auto ptr = upper_bound(vectorList.begin(), vectorList.end(), pair<int, string>(timestamp+1,""));
+        if (ptr == vectorList.begin())
+            return "";
+        return mp[key][ptr-vectorList.begin()-1].second;
+    }
+};
