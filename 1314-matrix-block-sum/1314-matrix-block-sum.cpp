09a00776756a08243fc40bf9decae97628268160
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-26 14:32:40 +0900

    Time: 7 ms (98.29%), Space: 9.6 MB (8.78%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -2,21 +2,8 @@ class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int n = mat.size(), m = mat[0].size();
-        
         vector<vector<int>> blockSum(n+1, vector<int>(m+1, 0));
         
-        int sum = 0;
-        
-        /*
-        1 2 3
-        4 5 6
-        7 8 9
-        
-         1  3  6
-         5 12 18
-        12 27 45
-        
-        */
         for (int i=1; i<=n; i++) { // row
             for (int j=1; j<=m; j++) { // col
                 blockSum[i][j] = mat[i-1][j-1]
@@ -27,7 +14,6 @@ public:
         }
         
         vector<vector<int>> res(n, vector<int>(m, 0));
-
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
                 int maxI = i+1+k, minI = i+1-k, maxJ = j+1+k, minJ = j+1-k;
