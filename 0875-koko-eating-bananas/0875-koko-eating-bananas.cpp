773581fe9c53b6d3c3275d4644f8dd768d21d87b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-26 16:52:31 +0900

    Time: 119 ms (38.71%), Space: 18.9 MB (75.46%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+    int totalTime(vector<int>& piles, int bananaN) {
+        int cnt = 0;
+        for (int i=0; i<piles.size(); i++) {
+            cnt += ceil((double)piles[i] / (double)bananaN);
+        }
+        return cnt;
+    }
+    
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-            long long hourSpent = 0;
-            
-            for (int &pile : piles) 
-                hourSpent += (pile / mid + (pile % mid != 0));
-            
-            if (hourSpent <= h) right = mid-1;
-            else left = mid + 1;
+    
+        int lo = 1, hi = *max_element(piles.begin(), piles.end());
+        int mid;
+        
+        // cout << printf("%.10f", ((float)100000000 / (float)49999998)) << endl;
+        // cout << printf("%.10f", ceil((double)100000000 / (double)49999998)) << endl;
+
+        
+        while (lo < hi) {
+            mid = (lo + ((hi-lo) >> 1));
+            // cout << lo << " " << mid << " " << hi << endl;
+
+            if (totalTime(piles, mid) <= h) {
+                hi = mid;
+            } else {
+                lo = mid+1;
+            }
         }
         
-        return left;
+        return lo;
     }
 };
