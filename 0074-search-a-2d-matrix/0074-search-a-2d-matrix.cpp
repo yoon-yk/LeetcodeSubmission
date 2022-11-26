0c2ca22fb49ea40f2373955072900f0dc929ca81
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-26 16:09:28 +0900

    Time: 3 ms (94.93%), Space: 9.6 MB (51.69%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        
-        // which row?
-        int lr = 0, hr = mat.size()-1;
-        while (lr < hr) { // find the lower bound
-            int mr = (lr + hr + 1) >> 1;
-            if (mat[mr][0] == target) return true;
-            if (mat[mr][0] < target) lr = mr;
-            else hr = mr-1;
+        int n = mat.size(), m = mat[0].size();
+        int lo = 0, hi = (n*m)-1, mid, midR, midC;
+        while (lo <= hi) {
+            mid = (lo + hi) >> 1;
+            midR = mid/m, midC = mid%m;
+            if (mat[midR][midC] == target)
+                return true;
+            else if (mat[midR][midC] < target)
+                lo = mid+1;
+            else hi = mid-1;
         }
-        if (hr == -1) return false;
-
-        // which col?
-        int lc = 0, hc = mat[0].size()-1;
-        while (lc <= hc) {
-            int mc = (lc + hc) >> 1;
-            if (mat[hr][mc] == target) return true;
-            else if (target < mat[hr][mc]) hc = mc-1;
-            else lc = mc+1;
-        }
-
         return false;
     }
 };
