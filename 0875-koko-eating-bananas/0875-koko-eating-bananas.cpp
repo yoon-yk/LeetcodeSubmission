76c87e6357b030fc647ceb4f35ed454f5a16adfa
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-26 16:57:18 +0900

    Time: 64 ms (80.64%), Space: 18.9 MB (75.46%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,7 +3,8 @@ public:
     int totalTime(vector<int>& piles, int bananaN) {
         int cnt = 0;
         for (int i=0; i<piles.size(); i++) {
-            cnt += ceil((double)piles[i] / (double)bananaN);
+            // cnt += ceil((double)piles[i] / (double)bananaN);
+            cnt += (piles[i]/bananaN) + (piles[i]%bananaN>0);
         }
         return cnt;
     }
@@ -12,14 +13,9 @@ public:
     
         int lo = 1, hi = *max_element(piles.begin(), piles.end());
         int mid;
-        
-        // cout << printf("%.10f", ((float)100000000 / (float)49999998)) << endl;
-        // cout << printf("%.10f", ceil((double)100000000 / (double)49999998)) << endl;
 
-        
         while (lo < hi) {
             mid = (lo + ((hi-lo) >> 1));
-            // cout << lo << " " << mid << " " << hi << endl;
 
             if (totalTime(piles, mid) <= h) {
                 hi = mid;
