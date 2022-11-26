1366172ed18932de3f54734cff52304bb83dee18
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-26 18:14:14 +0900

    Time: 12 ms (19.88%), Space: 11.1 MB (75.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int search(vector<int> A, int target) {
-        int n = A.size();
-        int lo=0,hi=n-1;
-        // find the index of the smallest value using binary search.
-        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
-        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
-        while(lo<hi){
-            int mid=(lo+hi)/2;
-            if(A[mid]>A[hi]) lo=mid+1;
-            else hi=mid;
+    int search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size()-1;
+        /*
+        0 1 2 3 4 5 6
+        
+        */
+        
+        while (lo < hi) {
+            int mid = lo + ((hi-lo) >> 1);
+            if (nums[mid] == target) return mid;
+            
+            if (nums[lo] > nums[hi]) {
+                if (nums[mid] > nums[hi]) lo = mid+1;
+                else hi = mid;
+            } else hi --;
         }
-        // lo==hi is the index of the smallest value and also the number of places rotated.
-        int rot=lo;
-        lo=0;hi=n-1;
-        // The usual binary search and accounting for rotation.
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int realmid=(mid+rot)%n;
-            if(A[realmid]==target)return realmid;
-            if(A[realmid]<target)lo=mid+1;
-            else hi=mid-1;
+        
+        int origIdx = hi;
+        lo = 0, hi = nums.size()-1;
+        int n = nums.size();
+        while (lo <= hi) {
+            int mid = lo + ((hi-lo) >> 1);
+            int sortedMid = (origIdx + mid) % n;
+
+            if (nums[sortedMid] == target) return sortedMid;
+            else if (nums[sortedMid] < target) lo = mid+1;
+            else hi = mid-1;
         }
+            
         return -1;
     }
 };
