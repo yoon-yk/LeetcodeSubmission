b03803feeb5d86cab879425db1bdbbd3566f9bdf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-28 01:30:58 +0900

    Time: 303 ms (77.45%), Space: 97 MB (58.40%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,45 +1,34 @@
 class RandomizedSet {
 public:
-    list<int> li;
-    unordered_map<int, list<int>::iterator> m;
-    
+    /** Initialize your data structure here. */
     RandomizedSet() {
         
     }
     
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        if (m.count(val)) return false;
-        li.push_back(val);
-        m[val] = li.end();
+        if (m.find(val) != m.end()) return false;
+        nums.emplace_back(val);
+        m[val] = nums.size() - 1;
         return true;
     }
     
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        if (!m.count(val)) return false;
-        li.remove(val);
+        if (m.find(val) == m.end()) return false;
+        int last = nums.back();
+        m[last] = m[val];
+        nums[m[val]] = last;
+        nums.pop_back();
         m.erase(val);
         return true;
     }
     
+    /** Get a random element from the set. */
     int getRandom() {
-        float rand = (float) random() / RAND_MAX;
-        float randIdx = floor(rand * (li.size()));
-        // cout << rand << " " << randIdx << endl;
-        int cnt = 0;
-        
-        for (auto &l : li) {
-            if (cnt++ == randIdx)
-                return l;
-        }
-        
-        return -1;
+        return nums[rand() % nums.size()];
     }
+private:
+    vector<int> nums;
+    unordered_map<int, int> m;
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
