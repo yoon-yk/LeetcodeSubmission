c8e187dbf1ec2723e5a9a2281675a9ddacc12cb1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-28 20:07:26 +0900

    Time: 517 ms (49.32%), Space: 97.1 MB (58.31%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,34 +1,44 @@
 class RandomizedSet {
 public:
-    /** Initialize your data structure here. */
+    vector<int> arr;
+    unordered_map<int, int> map;
+    
     RandomizedSet() {
         
     }
     
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        if (m.find(val) != m.end()) return false;
-        nums.emplace_back(val);
-        m[val] = nums.size() - 1;
+        if (map.count(val)) return false;
+        arr.push_back(val);
+        map[val] = arr.size()-1;
         return true;
     }
     
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        if (m.find(val) == m.end()) return false;
-        int last = nums.back();
-        m[last] = m[val];
-        nums[m[val]] = last;
-        nums.pop_back();
-        m.erase(val);
+        if (!map.count(val)) return false;
+        int origIdx = map[val];
+        map[arr.back()] = origIdx;
+        swap(arr[map[val]], arr.back());
+        arr.pop_back();
+        map.erase(val);
         return true;
     }
     
-    /** Get a random element from the set. */
+    /*
+     0  1  2  3
+    01 10 20 30
+    
+    */
     int getRandom() {
-        return nums[rand() % nums.size()];
+        int randIdx = random() % (arr.size());
+        return arr[randIdx];
     }
-private:
-    vector<int> nums;
-    unordered_map<int, int> m;
 };
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
