d333fac3f5b3d0b5961e89c858c2dc3ac195da51
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-28 21:35:32 +0900

    Time: 356 ms (77.31%), Space: 107.9 MB (20.65%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,43 +1,29 @@
 class RandomizedCollection {
 public:
-    
-    vector<int> arr;
-    unordered_map<int, priority_queue<int>> map;
-    
-    RandomizedCollection() {
+  vector<int> v;
+  unordered_map<int, unordered_set<int>> m;
+
+  bool insert(int val) {
+    v.push_back(val);
+    m[val].insert(v.size() - 1);
+    return m[val].size() == 1;
+  }
         
+  bool remove(int val) {
+    auto it = m.find(val);
+    if (it != end(m)) {
+      auto free_pos = *it->second.begin(); // 삭제할 원소의 인덱스
+      it->second.erase(it->second.begin()); // 위에서 찾은 인덱스 삭제
+      v[free_pos] = v.back(); // 삭제할 원소가 위치한 자리에 맨 뒷 원소 넣기
+      m[v.back()].insert(free_pos); // 새로 바뀐 인덱스 넣기
+      m[v.back()].erase(v.size() - 1); // 맨 뒤 원소의 인덱스 셋에서 인덱스 삭제
+      v.pop_back(); // arr자체에서 맨 뒤 원소 지우기
+      if (it->second.size() == 0) m.erase(it); // 만약에 지운 인덱스가 해쉬셋의 유일한 원소였다면 m에서 해당 엔트리 지우기 
+      return true;
     }
-    
-    bool insert(int val) {
-        bool ans = (!map.count(val) || map[val].size() == 0);
-        arr.push_back(val);
-        map[val].push(arr.size()-1);
-        
-        return ans;
-    }
-    
-    bool remove(int val) {
-        
-        if (!map.count(val) || map[val].empty()) return false;
-        int origIdx = map[val].top();
-        int backVal = arr.back(), backIdx = map[backVal].top();
-        
-        if (origIdx != backIdx) {
-            map[backVal].pop();
-            arr[origIdx] = backVal;
-            map[backVal].push(origIdx);            
-        }
-        
-        map[val].pop();
-        arr.pop_back();
-        
-        return true;
-    }
-    
-    int getRandom() {
-        int randIdx = random() % arr.size();
-        return arr[randIdx];
-    }
+    return false;
+  }
+  int getRandom() { return v[rand() % v.size()]; }
 };
 
 /**
