e2f348052596a80cad7f9a6426f2ac1cc5428545
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-28 17:07:33 +0900

    Time: 804 ms (7.09%), Space: 31.6 MB (18.39%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,61 +1,32 @@
-class TrieNode {
-public:
-    int idx;
-    vector<TrieNode*> ch;
-    
-    TrieNode() {
-        idx = -1;
-        ch.resize(26);
-    }
-};
-
 class Solution {
 public:
-    void insert(TrieNode* root, string& w, int curIdx) {
-        TrieNode *cur = root;
-        for (int i=0; i<w.size(); i++) {
-            int curCh = w[i] - 'a';
-            if (cur->ch[curCh] == NULL)
-                cur->ch[curCh] = new TrieNode();
-            cur = cur->ch[curCh];
-        }
-        cur->idx = curIdx;
-    }
-    
-    void findPredecessor (TrieNode* root, string& w, int startIdx, bool skipped, int curWordIdx, int len, vector<vector<int>>& adjList) {
-        TrieNode* cur = root;
-        for (int i=startIdx; i<=w.size(); i++) {
-            if (!skipped) {
-                for (int k=0; k<26; k++) {
-                    if (cur->ch[k] == NULL) continue; 
-                    findPredecessor (cur->ch[k], w, i, true, curWordIdx, len-1, adjList);
-                }
-            } 
-            if (i < w.size()) {
-                int curCh = w[i] - 'a';
-                if (cur->ch[curCh] == NULL) return;
-                cur = cur->ch[curCh];
-                len --; 
-            }
-
-        }
-        if (len == 0 && cur->idx > -1 && cur->idx != curWordIdx)
-            adjList[curWordIdx].push_back(cur->idx);        
-    }
-    
     int longestStrChain(vector<string>& words) {
         /*
         a ba bca bdca 
         b ba bda bdca
         */
         int n = words.size();
-        
-        TrieNode * root = new TrieNode();
-        for (int i=0; i<n; i++) insert(root, words[i], i);
+        unordered_map<string, int> map;
+        for (int i=0; i<n; i++) map[words[i]] = i;
         
         vector<vector<int>> adjList(n);
-        for (int i=0; i<n; i++)
-            findPredecessor(root, words[i], 0, false, i, words[i].size()+1, adjList);
+        
+        for (int i=0; i<n; i++) {
+            string first = "";
+            string second = words[i];
+            for (int j=0; j<=words[i].size(); j++) {
+                string newWord = first + '0' + second;
+                for (int k=0; k<26; k++) {
+                    newWord[j] = k+'a';
+                    if (map.count(newWord))
+                        adjList[i].push_back(map[newWord]);
+                }
+                if (second.size() > 0) {
+                    first.push_back(second[0]);
+                    second = second.substr(1);
+                }
+            }
+        }
         
         int len = 0;
         vector<int> memo(n, -1);
