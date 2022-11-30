270c54e752a0a0a7118820598e0be040523e4b06
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-01 01:42:47 +0900

    Time: 124 ms (81.21%), Space: 16.7 MB (60.92%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -2,29 +2,26 @@ class Solution {
 public:
     int longestStrChain(vector<string>& words) {
         
-        sort(begin(words), end(words), [](string& w1, string& w2) {
+        sort(begin(words), end(words), [](const string& w1, const string& w2) {
             return w1.size() < w2.size();
         });
         
         int n = words.size();
-        vector<int> dp(n);
-        unordered_map<string, int> mp;
+        unordered_map<string, int> dp;
         
-        int idx = 0, ans = -1, maxLen, preIdx;
+        int ans = -1, maxLen, preIdx;
         for (auto & w : words) {
             maxLen = 1;
             for (int i=0; i<w.size(); i++) {
                 string combined = w.substr(0, i) + w.substr(i+1);
-                auto findPre = mp.find(combined);
-                if (findPre == mp.end()) continue;
+                auto findPre = dp.find(combined);
+                if (findPre == dp.end()) continue;
                 preIdx = findPre->second;
-                maxLen = max(dp[preIdx] + 1, maxLen);
+                maxLen = max(dp[combined] + 1, maxLen);
             }
             
-            mp[w] = idx;
-            dp[idx] = maxLen;
-            ans = max(ans, dp[idx]);
-            idx++;
+            dp[w] = maxLen;
+            ans = max(ans, dp[w]);
         }
         
         return ans;
