29d1eaa634b0612b255bf3c3fcea4f83a1719ec0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-11-30 18:42:08 +0900

    Time: 75 ms (81.52%), Space: 42.4 MB (43.13%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
+    int maxScore(vector<int> &cardPoints, int k) {
+        int frontScore = 0;
+        int rearScore = 0;
         int n = cardPoints.size();
 
-        /*
-        arr = [1 2 3 4 5 6 1]
-        fc  = [0 1 3 6 10 15 21 22]
-        rc  = [0 1 7 12 16 19 21 22]
-        */
-        
-        int curSum = 0;
-        for (int i=0; i<k; i++) {
-            curSum += cardPoints[i];
+        for (int i = 0; i < k; i++) {
+            frontScore += cardPoints[i];
         }
-        
-        int maxAns = curSum;
-        for (int i=0; i<k; i++) {
-            curSum = curSum - cardPoints[k-1-i]+ cardPoints[n-1-i];
-            maxAns = max(maxAns, curSum);
+
+        // take all k cards from the beginning
+        int maxScore = frontScore;
+
+        // take i from the beginning and k - i from the end
+        for (int i = k - 1; i >= 0; i--) {
+            rearScore += cardPoints[n - (k - i)];
+            frontScore -= cardPoints[i];
+            int currentScore = rearScore + frontScore;
+            maxScore = max(maxScore, currentScore);
         }
-        
-        return maxAns;
+
+        return maxScore;
     }
 };
