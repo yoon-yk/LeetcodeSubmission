6e9873cddf0819e7c563f87df3ade495a2ba6177
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-01 01:52:40 +0900

    Time: 1413 ms (5.08%), Space: 167.3 MB (49.97%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.cpp
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.cpp
@@ -1,40 +1,34 @@
 class StockPrice {
 public:
-    StockPrice() {}
-
-    // price - timestamp
-    int latestTime = -1;
-    multiset<int> pq;
-    unordered_map<int, int> mp; // timestamp - price
+    unordered_map<int, int> timeStamps;
+    multiset<int> prices;
+    int latestTS;
+    
+    StockPrice() {
+        latestTS = -1;
+    }
     
     void update(int timestamp, int price) {
-        // correcting the price 
-        // if same time stamp - correction
-        if (mp.count(timestamp)) 
-            pq.erase(pq.find(mp[timestamp]));
-        // else - update
-        mp[timestamp] = price;
-        if (timestamp > latestTime)
-            latestTime = timestamp;
-        pq.insert(price);
+        
+        if (timeStamps.count(timestamp)) {
+            prices.erase(prices.find(timeStamps[timestamp]));
+        }
+        
+        latestTS = max(latestTS, timestamp);
+        timeStamps[timestamp] = price;
+        prices.insert(price);
     }
     
     int current() {
-        // latest 
-        if (mp.empty()) return -1;
-        return mp[latestTime];
-        // needs to be sorted?
+        return timeStamps[latestTS];
     }
     
     int maximum() {
-        // maximum 
-        return *pq.rbegin();
-        // what if it has been corrected afterward?
+       return *prices.rbegin(); 
     }
     
     int minimum() {
-        // minimum 
-        return *pq.begin();
+        return *prices.begin();
     }
 };
 
