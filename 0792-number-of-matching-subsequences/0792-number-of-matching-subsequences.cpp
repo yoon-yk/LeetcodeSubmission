d817a7811c5948a5d38d87d57496976c92ef6c31
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-01 15:01:59 +0900

    Time: 173 ms (98.27%), Space: 53 MB (57.52%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    int numMatchingSubseq(string s, vector<string>& words) {
-        vector<vector<int>> dict(26);
-        
-        for (int i=0; i<s.size(); i++) {
-            dict[s[i]-'a'].push_back(i);
-        }
-        
-        int cnt = 0;
-        for (auto & word : words) {
-            int curIdx = -1;
-            bool found = true;
-            for (int i=0; i<word.size(); i++) {
-                vector<int>& curVector = dict[word[i]-'a'];
-                auto nextIdx = upper_bound(begin(curVector), end(curVector), curIdx);
-                if (nextIdx == end(curVector)) {
-                    found = false;
-                    break;
-                }
-                curIdx = curVector[nextIdx-begin(curVector)];
-            }
-            cnt += found;
-        }
-        
-        return cnt;
+    int numMatchingSubseq(string S, vector<string>& words) {
+    vector<const char*> waiting[128];
+    for (auto &w : words)
+        waiting[w[0]].push_back(w.c_str());
+    for (char c : S) {
+        auto advance = waiting[c];
+        waiting[c].clear();
+        for (auto it : advance)
+            waiting[*++it].push_back(it);
     }
+    return waiting[0].size();
+}
 };
