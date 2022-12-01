b4ec908b0f32e387bf61167ef4904524a5eb7b65
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-01 15:22:23 +0900

    Time: 9 ms (33.09%), Space: 6.5 MB (78.99%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
@@ -1,35 +1,11 @@
 class Solution {
 public:
-    int shortestWay(string source, string target) {
-        /*
-          x z y x z
-          1 1 2 2 3
-        */
-        
-        vector<vector<int>> idxDict(26);
-        for (int i=0; i<source.size(); i++) {
-            idxDict[source[i]-'a'].push_back(i);
+    int shortestWay(string s, string t) {
+        int sz = s.size(), i = 0, res = 0;
+        for (int j = 0; i < t.size() && res <= i + 1; ++j) {
+            res += j % sz == 0;
+            i += t[i] == s[j % sz];
         }
-        
-        int curIdx = -1;
-        int windowsCount = 1;
-        
-        for (int end=0; end<target.size(); end++) {
-            int curCh = target[end] - 'a';
-
-            if (!idxDict[curCh].size())
-                return -1;
-            
-            auto findIdx = upper_bound(idxDict[curCh].begin(), idxDict[curCh].end(), curIdx);
-
-            if (findIdx == idxDict[curCh].end()){
-                windowsCount++;
-                curIdx = idxDict[curCh][0]; 
-            } else
-                curIdx = idxDict[curCh][findIdx - idxDict[curCh].begin()]; 
-            
-        }
-        
-        return windowsCount;
+        return i == t.size() ? res : -1;
     }
 };
