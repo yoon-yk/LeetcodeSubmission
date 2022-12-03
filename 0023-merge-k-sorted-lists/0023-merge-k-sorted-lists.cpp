ce18ce3f8eada1bf006720e772ae1bfeea9fabf0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-03 16:47:34 +0900

    Time: 22 ms (96.44%), Space: 22.8 MB (5.04%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,49 +11,40 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        return divide(lists, 0, lists.size()-1);
+        int n = lists.size();
+        return divide(lists, 0, n-1);
     }
     
-    ListNode* divide(vector<ListNode*>& lists, int l, int r) {
-        if (l==r)
-            return lists[l];
+    ListNode* divide(vector<ListNode*>& ls, int start, int end) {
         
-        if (l < r) {
-            int mid = l + ((r-l) >> 1);
-            ListNode* left = divide(lists, l, mid);
-            ListNode* right = divide(lists, mid+1, r);
-            return mergeTwoLists(left, right);
+        if (start == end)
+            return ls[start];
+        while (start < end) {
+            int mid = start + ((end-start) >> 1);
+            ListNode* left = divide (ls, start, mid);
+            ListNode* right = divide (ls, mid+1, end);
+            return conquer(left, right);
         }
-        
         return NULL;
     }
     
-    /*
-    ㅇ ㅇ ㅇ ㅇㅡㅇ
-    ㅇ ㅇ ㅇㅡㅇ
-    ㅇ ㅇㅡㅇ
-    ㅇㅡㅇ 
-    ㅇ     
-    (N-1)번 반복 * K
-    
-    */
-    ListNode* mergeTwoLists(ListNode*& l1, ListNode*& l2) {
+    ListNode* conquer(ListNode* l1, ListNode* l2) {
         ListNode* head = new ListNode();
-        ListNode* cPtr = head;
-        
+        ListNode* curPtr = head;
         while (l1 && l2) {
             if (l1->val < l2->val) {
-                cPtr->next = l1;
-                l1 = l1->next;
+                curPtr->next = l1;
+                l1 = l1->next; 
             } else {
-                cPtr->next = l2;
-                l2 = l2->next;
+                curPtr->next = l2;
+                l2 = l2->next; 
             }
-            cPtr = cPtr->next;
-        }
-        if (l1) cPtr->next = l1;
-        else if (l2) cPtr->next = l2;
+            curPtr = curPtr->next;
+        } 
+        if (l1) curPtr->next = l1;
+        else curPtr->next = l2;
         
         return head->next;
     }
+    
 };
