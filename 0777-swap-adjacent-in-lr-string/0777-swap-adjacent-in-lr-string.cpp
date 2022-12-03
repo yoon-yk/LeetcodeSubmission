01f65d5f0abf0590da3b5fe5eaf93dac97a9d5e4
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-04 00:22:33 +0900

    Time: 17 ms (48.36%), Space: 7.5 MB (68.67%) - LeetHub

--- 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.cpp
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.cpp
@@ -1,82 +1,27 @@
 class Solution {
 public:
     bool canTransform(string start, string end) {
-        
-        /*
-        "XL" => "LX" // moving x forward
-        "RX" => "XR" // moving x backward
-        
-        start = "RXXLRXRXL"
-                "XRXLRXRXL"
-                "XRLXRXRXL"
-                "XRLXXRRXL"
-                "XRLXXRRLX"
-        end   = "XRLXXRRLX"
-        
-        "LXR"
-        "LRX"
-        "XLR"
-        
-        "RXL"
-        
-
-        // "L and R order shouldn't be changed"
-        
-        start = "XLLR"
-        end   = "LLXR"
-        
-        start = "LLLXR" // can move x 3 times
-                "LXLLR"
-                "XLLLR"
-                "LXRLL"
-        end   = "LXLRL"
-        
-        
-        1. # of L , # of R should be the same
-        2. "L can't go more righter side"
-        3. "R can't go more lefter side"
-        4. LR should be in same order
-        
-        X
-        0
-        
-        L
-        
-        "RLX"
-        "XLR"
-        
-        "RLX"
-        "RXL"
-        "XRL"
-        
-        
-        
-        */
-        
-        vector<pair<int, int>> V; // <isL, val> = can't go further than this value
-        for (int i=0; i<start.size(); i++) {
-            if (start[i] == 'L')
-                V.push_back(pair(true,i));
-            else if (start[i] == 'R')
-                V.push_back(pair(false,i));
-        }
-        
-        for (int i=end.size()-1; i>=0; i--) {
-            if (end[i] == 'X') continue;
-            if (V.empty()) return false;
+        int sIdx = 0, eIdx = 0;
+        while (sIdx < start.size() || eIdx < end.size()) {
+                        
+            while (sIdx < start.size() && start[sIdx] == 'X')
+                ++sIdx;
+            while (eIdx < end.size() && end[eIdx] == 'X')
+                ++eIdx;
             
-            if (end[i] == 'L') {
-                if (!V.back().first 
-                    || V.back().second < i) 
-                    return false;
-                V.pop_back();
-            } else if (end[i] == 'R') {
-                if (V.back().first 
-                    || V.back().second > i) 
-                    return false;
-                V.pop_back();
-            }
+            if (sIdx == start.size() && eIdx == end.size())
+                return true;
+            else if (sIdx == start.size() || eIdx == end.size())
+                return false;
+            
+            if ((start[sIdx] != end[eIdx]) ||
+                (start[sIdx] == 'L' && sIdx < eIdx) ||
+                (end[eIdx] == 'R' && sIdx > eIdx)) return false;
+            
+            ++sIdx, ++eIdx;
         }
-        return V.empty();
+
+        
+        return (sIdx == start.size()) && (eIdx == end.size());
     }
 };
