5c951b1cb98b6babc4e170d56d1986d284ca4b45
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-04 00:39:49 +0900

    Time: 584 ms (81.13%), Space: 22 MB (55.51%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
@@ -4,20 +4,14 @@ public:
         
         unordered_map<int, unordered_set<int>> mp;
         int n = points.size(); 
-        
-        
-        /*
-        [x,  y] [cx,  y]
-            
-        [x, cy] [cx, cy]
-        
-        
-        */
+ 
         int minArea = INT_MAX;
         for (int i=0; i<n; i++) {
             int cX = points[i][0], cY = points[i][1];
-            
+                        mp[cX].insert(cY);
+
             for (auto &[x, ys] : mp) { // diagonal points
+                
                 if (x == cX || ys.size() < 2) continue;
                 for (auto & y : ys) {
                     // x, y
@@ -27,7 +21,6 @@ public:
                     }
                 } 
             }
-            mp[cX].insert(cY);
         }
         
         return minArea == INT_MAX ? 0 : minArea;
