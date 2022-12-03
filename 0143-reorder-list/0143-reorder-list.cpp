504c7f836b8b9c3c93b67086c94d914fff4996bc
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-03 18:53:27 +0900

    Time: 93 ms (44.57%), Space: 17.8 MB (77.38%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,28 +10,66 @@
  */
 class Solution {
 public:
+    
+    ListNode* getMiddleNode(ListNode* head) {
+        // find a middle node 
+        ListNode* first = head, *second = head, 
+        *third;
+        while (first && first->next) {
+            first = first->next->next;
+            third = second;
+            second = second->next;
+        }
+        ListNode* mid = second;
+        if (third) third->next = NULL;
+        
+        return mid;
+    }
+    
+    ListNode* getRListHead(ListNode* mid) {
+        ListNode* first = mid, *second = mid, 
+        *third=NULL;
+         while (first) {
+            first = first->next;
+            second->next = third;
+            third = second;
+            second = first;
+        }
+        
+        return third;
+    }
+    
+    ListNode* mergeLists(ListNode* l1, ListNode* l2) {
+        ListNode* pt1 = l1, *pt2 = l2;
+        ListNode* newHead = new ListNode();
+        ListNode* cur = newHead;
+        
+        while (pt1 || pt2) {
+            
+            if (pt1) {
+                cur->next = pt1;
+                pt1 = pt1->next;
+                cur = cur->next;
+            }
+            
+            if (pt2) {
+                cur->next = pt2;
+                pt2 = pt2->next; 
+                cur = cur->next;
+            }
+
+        }
+        
+        return newHead->next;
+    }
+    
     void reorderList(ListNode* head) {
-        unordered_map<int, ListNode*> mp;
-        ListNode* ptr = head, *temp;
+
+        if (!head->next) return;
         
-        int idx=0;
-        for(; ptr; idx++) {
-            mp[idx] = ptr;
-            temp = ptr->next;
-            ptr->next = NULL;
-            ptr = temp;
-        }
-        
-        ListNode* dHead = new ListNode();
-        ptr = dHead;
-        for (int i=0; i< (idx >> 1); i++) {
-            ptr->next = mp[i];
-            ptr->next->next = mp[idx-1-i];
-            ptr = ptr->next->next;
-        }
-        
-        if (idx % 2) 
-            ptr->next = mp[idx>>1];
-        
+        ListNode* mid = getMiddleNode(head);
+        ListNode* rlHead = getRListHead(mid);
+        head = mergeLists(head, rlHead);
+
     }
 };
