8771fe721c1c3f814f9f75e6085fd8e48915eae5
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-03 17:53:18 +0900

    Time: 11 ms (82.20%), Space: 11.3 MB (60.58%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,21 +17,43 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        if (!head) return head;
         
-        Node* cur = head;
-        unordered_map<Node*, Node*> mp;
-        while (cur) {
-            mp[cur] = new Node(cur->val);
-            cur = cur->next;
+        Node* curPtr = head;        
+        while (curPtr) {
+            Node* nNode = new Node(curPtr->val);
+            nNode->next = curPtr->next;
+            curPtr->next = nNode;
+            curPtr = nNode->next;
         }
-        
-        cur = head;
-        while (cur) {
-            mp[cur]->next = mp[cur->next];
-            mp[cur]->random = mp[cur->random];
-            cur = cur->next;
-        }
-        return mp[head];
 
+        curPtr = head;
+        while (curPtr) {
+            curPtr->next->random = 
+                (curPtr->random) ? curPtr->random->next : NULL;
+            curPtr = curPtr->next->next;
+        }
+        
+        // 7 7 13 13 11 11 10 10 1 1 
+        Node* nHead = head->next, *oPtr, *cPtr;
+        oPtr = head, cPtr = head->next;
+        while (oPtr) {
+            cPtr = oPtr->next;
+            oPtr->next = cPtr->next;
+            if (cPtr->next) cPtr->next = cPtr->next->next;
+            oPtr = oPtr->next;
+        }
+        
+        // Node* pPtr = nHead;
+        // while (pPtr) {
+        //     int v = pPtr->val;
+        //     int nv = (pPtr->next) ? pPtr->next->val : 0;
+        //     int rv = (pPtr->random) ? pPtr->random->val : 0;
+        //     cout << v << "/" << nv << "/" << rv << endl;
+        //     pPtr = pPtr->next;
+        // }
+        // cout << endl;
+        
+        return nHead;
     }
 };
