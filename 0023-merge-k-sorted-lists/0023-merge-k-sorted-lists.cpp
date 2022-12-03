1605959caba1f5f3358fdb7c0411926581beecb4
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-03 15:22:33 +0900

    Time: 381 ms (14.92%), Space: 13.4 MB (46.59%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,29 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-        auto compare = [](const ListNode* l1, const ListNode* l2){
-            return l1->val > l2->val;
-        };
-        
-        priority_queue<ListNode*, vector<ListNode*>, decltype(compare)> pq(compare);
-        
-        for (auto & list : lists) {
-            if (list) pq.push(list);
-        }
-        
-        ListNode* retHead = new ListNode();
-        ListNode* retPtr = retHead;
-        
-        while (!pq.empty()) {
-            auto cur = pq.top(); pq.pop();
-            if (cur->next) pq.push(cur->next);
-            cur->next = NULL;
-            retPtr->next = cur;
-            retPtr = retPtr->next;
-        }
-        
-        return retHead->next;
+ListNode *mergeKLists(vector<ListNode *> &lists) {
+    if(lists.empty()){
+        return nullptr;
     }
+    while(lists.size() > 1){
+        lists.push_back(mergeTwoLists(lists[0], lists[1]));
+        lists.erase(lists.begin());
+        lists.erase(lists.begin());
+    }
+    return lists.front();
+}
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    if(l1 == nullptr){
+        return l2;
+    }
+    if(l2 == nullptr){
+        return l1;
+    }
+    if(l1->val <= l2->val){
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    }
+    else{
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
 };
