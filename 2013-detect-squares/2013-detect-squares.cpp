182302830d9acdd80dc2bc3145f7ed11755cedf2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-03 23:30:47 +0900

    Time: 525 ms (72.57%), Space: 93.8 MB (79.56%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.cpp
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -1,20 +1,36 @@
-class DetectSquares { // 216 ms, faster than 66.67%
+class DetectSquares {
 public:
-    int cntPoints[1001][1001] = {};
-    vector<pair<int, int>> points;
-
-    void add(vector<int> p) {
-        cntPoints[p[0]][p[1]]++;
-        points.emplace_back(p[0], p[1]);
+    
+    unordered_map<int, unordered_map<int, int>> mp;
+    
+    DetectSquares() {}
+    
+    void add(vector<int> point) {
+        ++mp[point[0]][point[1]];
     }
-
-    int count(vector<int> p1) {
-        int x1 = p1[0], y1 = p1[1], ans = 0;
-        for (auto& [x3, y3] : points) {
-            if (abs(x1-x3) == 0 || abs(x1-x3) != abs(y1-y3))
-                continue; // Skip empty square or invalid square point!
-            ans += cntPoints[x1][y3] * cntPoints[x3][y1];
+    
+    int count(vector<int> point) {
+        
+        int curX = point[0], curY = point[1];
+        int ans = 0;
+        
+        for (auto & [xv, yvs] : mp) {
+            if (xv == curX) continue;
+            
+            for (auto & [yv, cnt] : yvs) {
+                if (yv == curY || abs(yv-curY) != abs(xv-curX)) continue;
+                
+                ans += mp[xv][curY] * mp[curX][yv] * mp[xv][yv];
+            }
         }
+        
         return ans;
     }
 };
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
