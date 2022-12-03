80949bb93adae8dace873d0030a49e8fad618f4c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-04 02:19:46 +0900

    Time: 465 ms (82.34%), Space: 165.2 MB (64.69%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,35 +1,46 @@
 class LRUCache {
 public:
-    int max_size;
-    list<pair<int, int>> list_pair; // key, value;
-    unordered_map<int, list<pair<int, int>>::iterator> keys_map;
+    using lit = list<pair<int, int>>::iterator;
+    unordered_map<int, lit> mp;
+    list<pair<int, int>> l;
+    int cap;
     
     LRUCache(int capacity) {
-        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-        max_size = capacity;
+        cap = capacity;
     }
     
-    // O(logn) time ( maybe use map? )
     int get(int key) {
-        unordered_map<int, list<pair<int, int>>::iterator>::iterator it = keys_map.find(key);
-        if(it==keys_map.end()) return -1;
-        list_pair.splice( list_pair.begin(), list_pair, it->second );
-        return (it->second)->second;
+        
+        if (!mp.count(key)) 
+            return -1;
+        
+        l.splice(l.begin(), l, mp[key]);
+        mp[key] = l.begin();
+        return mp[key]->second;
     }
     
     void put(int key, int value) {
-        unordered_map<int, list<pair<int, int>>::iterator>::iterator it = keys_map.find(key);
-        if(it==keys_map.end()){ // not in the map;
-            list_pair.push_front(pair<int, int>{key, value});
-            keys_map[key] = list_pair.begin();
-        } else {
-            it->second->second = value; // keys_map[key]->second = value;
-            list_pair.splice( list_pair.begin(), list_pair, it->second ); // list_pair.splice( list_pair.begin(), list_pair, keys_map[key] );
+        if (!mp.count(key))
+            l.push_front({key, value});
+        else {
+            l.splice(l.begin(), l, mp[key]);
+            l.front().second = value;
         }
-        if(keys_map.size()>max_size){
-            pair<int, int> it = list_pair.back();
-            keys_map.erase(it.first);
-            list_pair.pop_back();
+        mp[key] = l.begin();
+        
+        if (l.size() > cap) {
+            int delKey = l.back().first;
+            l.pop_back();
+            mp.erase(delKey);
         }
+ 
     }
+
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
