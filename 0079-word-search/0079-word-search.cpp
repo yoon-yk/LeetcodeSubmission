adf75572fa0eb687db63e34b2bc55d70b07074c2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-04 20:44:40 +0900

    Time: 781 ms (37.39%), Space: 8.1 MB (15.88%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,52 @@
 class Solution {
 public:
-    vector<int> dir = {-1, 0, 1, 0, -1};
+
+    bool isValidRange(int r, int c, vector<vector<char>>& board) {
+        if (r<0 || r>=board.size() || c < 0 || c>= board[0].size()) return false;
+        
+        return true;
+    }
+    
     bool exist(vector<vector<char>>& board, string word) {
-        int n=board.size(), m = board[0].size();
-        vector<vector<int>> visited(n, vector<int>(m, false));
-
-        for (int i=0; i<n; i++)
-            for (int j=0; j<m; j++) 
-                if (isInRange(0, i, j, board, visited, word) 
-                    && backtrack(0, i, j, board, visited, word))
+        
+        int r = board.size(), c = board[0].size();
+        vector<vector<bool>> visited(r, vector<bool>(c, false));
+        
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                if (word[0] == board[i][j]) {
+                    visited[i][j] = true;
+                    if (backtrack(i, j, 0, visited, board, word))
                         return true;
-
-        return false;
-    }
-    
-    inline bool isInRange(int idx, int i, int j, vector<vector<char>>& board, vector<vector<int>> &visited, string &word) {
-        return (!(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || visited[i][j] || word[idx] != board[i][j]));
-    }
-    
-    inline bool backtrack(int idx, int i, int j, vector<vector<char>>& board, vector<vector<int>>& visited, string& word){
-        
-        visited[i][j] = true;
-
-        if (idx == word.size()-1)
-            return true;
-        
-        for (int d=0; d<4; d++) {
-            int newI = i+dir[d], newJ = j+dir[d+1];
-            if (isInRange(idx+1, newI, newJ, board, visited, word)) {
-                if (backtrack(idx+1, newI, newJ, board, visited, word))
-                    return true;
+                    visited[i][j] = false;
+                }
             }
         }
-        
-        visited[i][j] = false;
         return false;
+    }
+    
+    vector<int> dir = {-1, 0, 1, 0, -1};
+    
+    bool backtrack(int r, int c, int idx, vector<vector<bool>>& visited, vector<vector<char>>& board, string& word) {
+
+        if (idx == word.size() - 1 && word[idx] == board[r][c])
+            return true;
+        
+        if (word[idx] != board[r][c])
+            return false; 
+
+        bool ans = false;
+        int nextR, nextC;
+        for (int d=0; d<4; d++) {
+            nextR = r+dir[d], nextC = c+dir[d+1];
+            if (!isValidRange(nextR, nextC, board) || 
+                visited[nextR][nextC]) continue;
+            visited[nextR][nextC] = true;
+            ans |= backtrack(nextR, nextC, idx+1, visited, board, word);
+            visited[nextR][nextC] = false;
+        }
+        
+        return ans; 
+        
     }
 };
