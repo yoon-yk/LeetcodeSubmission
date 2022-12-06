916d4bdeb36f9eb7542dad6901c96a2cd315cf7f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-07 00:48:55 +0900

    Time: 8 ms (85.82%), Space: 13.8 MB (26.50%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,14 +3,6 @@ public:
     int maxProduct(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> maxArr(n, 1), minArr(n, 1);
-        
-        /*
-             2,  3, -2,  4, -2,  0, -1
-        min  2   3  -6 -24  -2 가 아니라 -8
-        max 
-        */
-        
         int curMinChain, curMaxChain, temp, ans;
         curMinChain = curMaxChain = ans = nums[0];
         for (int i=1; i<n; i++) {
