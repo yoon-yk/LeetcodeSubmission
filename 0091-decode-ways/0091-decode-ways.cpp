a0b669fecd5015849c1c548380c6fbab982467d3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-06 20:53:42 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (74.81%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n+1);
-        dp[0] = 1;
-        
         /*
         "1"
         dp 
@@ -22,14 +18,19 @@ public:
         1 1 1 1
         */
         
-        for (int i=1; i <= s.size(); i++) {
-            if ( 0 < (s[i-1]-'0')) dp[i] = dp[i-1]; // as an independent num 
+        int n = s.size();        
+        int prev2 = 1, prev1 = 1, cur = 0;
+        for (int i=0; i < s.size(); i++) {
+            cur = 0;
+            if ( 0 < (s[i]-'0')) cur = prev1; // as an independent num 
             
-            if (i-2 < 0) continue;
-            int nn = stoi(s.substr(i-2, 2));
-            if (nn > 9 && nn <= 26) dp[i] += dp[i-2]; // as a 2-digit num
+            if (i-1 >= 0){
+                int nn = stoi(s.substr(i-1, 2));
+                if (nn > 9 && nn <= 26) cur += prev2; // as a 2-digit num                
+            } 
+            prev2 = prev1, prev1 = cur;
         }
         
-        return dp[n];
+        return cur;
     }
 };
