178cdfcdd99432ad9d6f7f7b364f8691ca92dbe1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-06 18:06:56 +0900

    Time: 6 ms (21.33%), Space: 8.1 MB (6.93%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        
         int n = nums.size();
-        if (n == 1) return nums[0];
+        if (n==1) return nums[0];
+        vector<vector<int>> dp(n+1, vector<int>(2)); // include/exclude
         
-        vector<vector<int>> dp(n, vector<int>(2, -1)); 
-        int include1 = backtrack(2, nums, true, dp) + nums[0];
-        int exclude1 = backtrack(1, nums, false, dp);
-        return max(include1, exclude1);
+        dp[0][0] = dp[0][1] = 0;
+        dp[1][0] = nums[0];
+        dp[1][1] = 0;
+        
+        for (int i=1; i<n; i++) {
+            for (int k=0; k<2; k++) {
+                dp[i+1][k] = max(dp[i-1][k] + nums[i], dp[i][k]);
+            }
+        }
+        
+        return max(dp[n-1][0], dp[n][1]);
+
     }
-    
-    int backtrack(int idx, vector<int>& nums, bool fIncluded, vector<vector<int>>& dp) {
-        
-        if (idx >= nums.size())
-            return 0;
-        if (dp[idx][fIncluded] != -1)
-            return dp[idx][fIncluded];
-        
-        int ans = backtrack(idx + 1, nums, fIncluded, dp); // skip
-        if (!fIncluded || idx != nums.size()-1) 
-        ans = max(ans, backtrack(idx + 2, nums, fIncluded, dp) + nums[idx]); // include
-        return dp[idx][fIncluded] = ans;
-        
-    }
-    
 };
