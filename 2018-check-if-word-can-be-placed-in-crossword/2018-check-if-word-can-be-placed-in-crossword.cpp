a46d74af9deda08fe96f002d19ab5e989f79dea4
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-07 18:01:20 +0900

    Time: 233 ms (94.86%), Space: 59.6 MB (96.84%) - LeetHub

--- 2018-check-if-word-can-be-placed-in-crossword/2018-check-if-word-can-be-placed-in-crossword.cpp
+++ 2018-check-if-word-can-be-placed-in-crossword/2018-check-if-word-can-be-placed-in-crossword.cpp
@@ -1,57 +1,62 @@
 class Solution {
 public:
-    enum dir{T, R};
+    typedef enum {H, V} direction;
     
-    bool placeWordInCrossword(vector<vector<char>>& board, string word) {
+    bool placeWordInCrossword(vector<vector<char>>& board, string& word) {
+        
+        string revStr = word;
+        reverse(revStr.begin(), revStr.end());
+        
         int r = board.size(), c = board[0].size();
-        
-        // 각 cell 에 대해서 vertical, horizontal하게 word를 하나씩 넣어본다. 
-        // 이 때, vertical하게 놓을거면 위에 빈칸이거나 다른 알파벳이 있으면 안됨
-        // horizontal -> 맨 왼쪽에 빈칸이거나 다른 알파벳이 있으면 안됨
-        // Dfs 와중에 # 이나 현재 와야하는 알파벳과 맞지 않는 애가 존재하면 바로 return false
-        
-        // word를 모두 끝낸 다음에, 다음 cell에 빈칸이나 다른 알파벳이 있는지 확인하고 그렇다면 return false;
-        
-        string rev = word;
-        reverse(rev.begin(), rev.end());
-        
-        for (int i=0; i<r; i++) {
-            for (int j=0; j<c; j++) {
-                if (i == 0 || board[i-1][j] == '#') { // vertical
-                    if (dfs(i, j, board, word, 0, T))
-                        return true;
-                    if (dfs(i, j, board, rev, 0, T))
-                        return true;
+        for (int i=0; i<r; ++i) {
+            for (int j=0; j<c; ++j) {
+                // horizontal 
+                if (i == 0 || board[i-1][j] == '#') {
+                    if (dfs(i, j, board, 0, word, V)) return true;
+                    if (dfs(i, j, board, 0, revStr, V)) return true;
                 }
-                if (j == c-1 || board[i][j+1] == '#') { // horizontal 
-                    if (dfs(i, j, board, word, 0, R))
-                        return true;
-                    if (dfs(i, j, board, rev, 0, R))
-                        return true;
-                } 
 
+                // vertical
+                if (j == 0 || board[i][j-1] == '#') {
+                    if (dfs(i, j, board, 0, word, H)) return true;
+                    if (dfs(i, j, board, 0, revStr, H)) return true;
+                }
             }
         }
-        
         
         return false;
     }
     
-    bool dfs(int i, int j, vector<vector<char>>& board, string& word, int idx, int dir) {
-        
+    /*
+    
+    # #
+    # #
+    # c
+    
+    */
+    
+    
+    bool dfs(int r, int c, vector<vector<char>>& board, int idx, string& word,  direction dir) {
+                
+        // if reached the end, 
+            // return true if no more space in the end
+            // else return false;
         if (idx == word.size()) {
-            if (dir == T) 
-                return (i >= board.size() || board[i][j] == '#');
+            if (dir == H) 
+                return (c == board[0].size() || board[r][c] == '#');
             else 
-                return(j < 0 || board[i][j] == '#');
+                return (r == board.size() || board[r][c] == '#');
         }
         
-        
-        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == '#' || (board[i][j]!=' ' && board[i][j] != word[idx]))
+        // exit
+        // if out of range/ or different char exists or is blocked by the wall
+        if (r >= board.size() || c >= board[0].size() || board[r][c] == '#' || 
+            (board[r][c]!=' ' && board[r][c] != word[idx]))
             return false;
         
-        if (dir == T) return dfs(i+1, j, board, word, idx+1, dir);
-        else return dfs(i, j-1, board, word, idx+1, dir);
+        // keep exploring
+        if (dir == H) return dfs(r, c+1, board, idx+1, word, dir);
+        return dfs(r+1, c, board, idx+1, word, dir);
         
     }
 };
