f504f7c9761fdcc39f4855beac57e1ea52cf6145
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-07 23:13:04 +0900

    Time: 8 ms (10.70%), Space: 7.7 MB (85.83%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
         int n = nums.size();
-        if (n==1) return nums[0];
-        
-        return max(rob_simple(nums, 0, n-1), rob_simple(nums, 1, n));
+        if (n == 1) return nums[0];
+        return max(rob(nums, 0, n-1), rob(nums, 1, n));
     }
     
-    int rob_simple(vector<int>& nums, int start, int end) {
+    int rob(vector<int>& nums, int start, int end) {
+        if (end-start == 1) return nums[start];
         
-        if (end-start+1 == 2) return nums[start];
-
-        int prev_2 = 0, prev_1 = 0;
+        int prevSec = 0, prevFirst = 0, cur = prevFirst;
+        
         for (int i=start; i<end; i++) {
-            int curChoice = max(prev_2 + nums[i], prev_1);
-            prev_2 = prev_1;
-            prev_1 = curChoice;
+            cur = max(prevSec + nums[i], prevFirst);
+            prevSec = prevFirst, prevFirst = cur;
         }
-        
-        return max(prev_2, prev_1);
+        return cur;
     }
 };
