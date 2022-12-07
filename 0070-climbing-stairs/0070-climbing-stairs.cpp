1347b1cf94ccf136460662ab293d3160bb3c83dc
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-07 21:52:24 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (15.00%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     int climbStairs(int n) {
         
-        if (n < 3) return n;
+        vector<int> dp(n+1, -1);
+        return backtrack(n, dp);
+    }
+    
+    int backtrack(int left, vector<int>& dp){
         
-        int prev = 2, secPrev = 1, res;
-        for (int i=3; i<=n; i++) {
-            res = prev + secPrev;
-            secPrev = prev;
-            prev = res;
-        }
-        return res;
+        if (left < 0) return 0;
+        if (left == 0) return dp[left] = 1;
+        
+        if (dp[left] != -1) return dp[left];
+        
+        int ans = 0;
+        ans += backtrack(left-1, dp);
+        ans += backtrack(left-2, dp);
+        
+        return dp[left] = ans;
     }
 };
