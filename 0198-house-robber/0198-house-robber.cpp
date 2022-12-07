278044a0d0657dd5b44faed3489ef4c9e3a9d0f5
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-07 23:02:35 +0900

    Time: 4 ms (43.32%), Space: 7.7 MB (50.00%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,14 +4,11 @@ public:
         int n = nums.size();
         if (n == 1) return nums[0];
         
-        vector<int> dp(n, 0);
-        dp[0] = nums[0], dp[1] = nums[1];
+        int prevSec = nums[0], prevFirst = max(nums[0], nums[1]), cur = prevFirst;
         for (int i=2; i<n; i++) {
-            for (int j=0; j<i-1; j++) {
-                dp[i] = max(dp[i], dp[j] + nums[i]);
-            }
+            cur = max(prevSec + nums[i], prevFirst);
+            prevSec = prevFirst, prevFirst = cur;
         }
-        
-        return max(dp[n-1], dp[n-2]);
+        return cur;
     }
 };
