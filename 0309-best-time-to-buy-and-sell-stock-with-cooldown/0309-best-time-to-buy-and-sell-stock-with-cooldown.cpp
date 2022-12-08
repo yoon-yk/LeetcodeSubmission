05567865c97ceffa0f411c67b55886dfa431a98c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-08 21:53:02 +0900

    Time: 0 ms (100.00%), Space: 11.1 MB (90.76%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size(), ret;
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
         
-        dp[1][true] = -prices[0]; // 첫쨋날에 구입.
-        for (int d=2; d<=prices.size(); ++d) {
-            // 오늘 구매 (판매한지 2일 이상 지났음) 혹은 기다리기
-            dp[d][true] = max(dp[d-2][false] - prices[d-1], dp[d-1][true]);
+        int sold = INT_MIN, held = INT_MIN, reset = 0;
+        for (int price : prices) {
+            int preSold = sold;
             
-            // 오늘 판매 혹은 기다리기
-            dp[d][false] = max(dp[d-1][true] + prices[d-1], dp[d-1][false]);
+            sold = held + price;
+            held = max(held, reset-price); // 구매하거나..?
+            reset = max(reset, preSold); // 
         }
-        return max(dp[n][0], dp[n][1]);
+        return max(sold, reset);
     }
     
 };
