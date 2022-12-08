770e6019cde42176a8cbfac1edde54b513845035
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-09 03:34:22 +0900

    Time: 35 ms (37.50%), Space: 9.1 MB (41.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int len1= word1.size(), len2 = word2.size();
-        vector<vector<int>> dp(len1, vector<int>(len2, -1));
-        return minDist(0, 0, word1, word2, dp);
-    }
-    
-    int minDist(int idx1, int idx2, string &w1, string &w2, vector<vector<int>>& dp) {
+    int minDistance(string w1, string w2) {
+        int len1= w1.size(), len2 = w2.size();
+        vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
+
+        int ret;
+        for (int idx1=len1; idx1>=0; --idx1) {
+            for (int idx2=len2; idx2>=0; --idx2) {
+                if (idx1 == len1 || idx2 == len2) {
+                    dp[idx1][idx2] = max(len2-idx2, len1-idx1);
+                    continue;
+                }    
+                ret = len2+len1;
+                if (w1[idx1] == w2[idx2]) 
+                    ret = dp[idx1+1][idx2+1];
+                ret = min(ret, dp[idx1+1][idx2] + 1);
+                ret = min(ret, dp[idx1][idx2+1] + 1);
+                ret = min(ret, dp[idx1+1][idx2+1] + 1);
+                dp[idx1][idx2] = ret;
+            }
+        }
         
-        if (idx1 == w1.size() || idx2 == w2.size()) {
-            return max(w2.size()-idx2, w1.size()-idx1);
-        } 
-        
-        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        
-        int ans = w1.size() + w2.size();
-        if (w1[idx1] == w2[idx2]) ans = min(ans, minDist(idx1+1, idx2+1, w1, w2, dp));
-        ans = min(ans, minDist(idx1+1, idx2, w1, w2, dp) + 1); // delete/insert w1
-        ans = min(ans, minDist(idx1, idx2+1, w1, w2, dp) + 1); // delete/insert w2
-        ans = min(ans, minDist(idx1+1, idx2+1, w1, w2, dp) + 1); // replace
-        
-        return dp[idx1][idx2] = ans;
+        return dp[0][0];
     }
 };
