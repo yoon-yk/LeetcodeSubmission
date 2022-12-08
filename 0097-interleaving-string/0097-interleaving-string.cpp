62befe9e34e9ba6f80849b8dd9591e3dc02fdfaf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-08 23:09:25 +0900

    Time: 3 ms (88.07%), Space: 7.2 MB (43.12%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+                
+        int len1 = s1.size(), len2 = s2.size();
+        vector<vector<int>> dp(len1+1, vector<int>(len2+1, -1));
+        return backtrack(s1, s2, s3, 0, 0, dp);
+    }
+    
+    bool backtrack(string& s1, string& s2, string& s3, int idx1, int idx2, vector<vector<int>>& dp) {
+        
+        int idx3 = idx1 + idx2;
+        if (idx3 == s3.size()) {
+            return (idx1 == s1.size() && idx2 == s2.size());
+        }
+        
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+        bool ans = false;
+        // s1 끼우기 (하나씩!)
+        if (idx1 < s1.size() && s1[idx1] == s3[idx3]) {
+            ans |= backtrack(s1, s2, s3, idx1+1, idx2, dp);
+        }
+        
+        // s2 끼우기 (하나씩!)
+        if (idx2 < s2.size() && s2[idx2] == s3[idx3]) {
+            ans |= backtrack(s1, s2, s3, idx1, idx2+1, dp);
+        }
+        
+        return dp[idx1][idx2] = ans;
+    }
+};
