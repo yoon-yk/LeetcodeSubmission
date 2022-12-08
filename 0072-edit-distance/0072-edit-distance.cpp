e48c2b5a90b0309e2f6239094e0096bd4f6a9868
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-09 03:27:32 +0900

    Time: 33 ms (40.43%), Space: 9 MB (61.38%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int minD(int idx1, int idx2, string word1, string word2, vector<vector<int>>& dp) {
-        if (dp[idx1][idx2] != INT_MAX)
-            return dp[idx1][idx2];
-        
-        if (idx1==word1.size()) 
-            return dp[idx1][idx2] = word2.size()-idx2;
-    
-        if (idx2==word2.size()) 
-            return dp[idx1][idx2] = word1.size()-idx1;
-        
-        int ans = (word2.size()-idx2) + (word1.size()-idx1);
-        if (word1[idx1]==word2[idx2]) {
-            ans = min (ans, minD(idx1+1, idx2+1, word1, word2, dp));
-        } else {
-            int delW1 = minD(idx1+1, idx2, word1, word2, dp) + 1;
-            int delW2 = minD(idx1, idx2+1, word1, word2, dp) + 1;
-            int insOrRep = minD(idx1+1, idx2+1, word1, word2, dp) + 1;
-            ans = min(ans, min(delW1, min(delW2, insOrRep)));
-        }
-        
-        return dp[idx1][idx2] = ans;
+    int minDistance(string word1, string word2) {
+        int len1= word1.size(), len2 = word2.size();
+        vector<vector<int>> dp(len1, vector<int>(len2, -1));
+        return minDist(0, 0, word1, word2, dp);
     }
     
-    int minDistance(string word1, string word2) {
-        int len1 = word1.size(), len2 = word2.size();
-        vector<vector<int>> dp(len1+1, vector<int>(len2+1, INT_MAX));
-        return minD(0, 0, word1, word2, dp);
+    int minDist(int idx1, int idx2, string &w1, string &w2, vector<vector<int>>& dp) {
+        
+        if (idx1 == w1.size() || idx2 == w2.size()) {
+            return max(w2.size()-idx2, w1.size()-idx1);
+        } 
+        
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+        int ans = w1.size() + w2.size();
+        if (w1[idx1] == w2[idx2]) ans = min(ans, minDist(idx1+1, idx2+1, w1, w2, dp));
+        ans = min(ans, minDist(idx1+1, idx2, w1, w2, dp) + 1); // delete/insert w1
+        ans = min(ans, minDist(idx1, idx2+1, w1, w2, dp) + 1); // delete/insert w2
+        ans = min(ans, minDist(idx1+1, idx2+1, w1, w2, dp) + 1); // replace
+        
+        return dp[idx1][idx2] = ans;
     }
 };
