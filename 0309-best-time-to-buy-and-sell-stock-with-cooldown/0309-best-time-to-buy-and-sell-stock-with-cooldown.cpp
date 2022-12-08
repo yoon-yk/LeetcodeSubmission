52b23e33e862813dbffa604a586e3ab9808861d7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-08 21:37:57 +0900

    Time: 15 ms (22.16%), Space: 11.8 MB (53.88%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // you can't buy the day after you sell the stock
-        // today choice = buy(if no cool day) / sell (if i had bought)
+        int n = prices.size(), ret;
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
         
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        
-        return backtrack(0, false, prices, dp);
+        dp[1][true] = -prices[0]; // 첫쨋날에 구입.
+        for (int d=2; d<=prices.size(); ++d) {
+            // 오늘 구매 (판매한지 2일 이상 지났음) 혹은 기다리기
+            dp[d][true] = max(dp[d-2][false] - prices[d-1], dp[d-1][true]);
+            
+            // 오늘 판매 혹은 기다리기
+            dp[d][false] = max(dp[d-1][true] + prices[d-1], dp[d-1][false]);
+        }
+        return max(dp[n][0], dp[n][1]);
     }
     
-    int backtrack(int idx, bool hold, vector<int>& prices, vector<vector<int>>& dp) {
-        
-        if (idx >= prices.size())
-            return 0;
-        
-        if (dp[idx][hold] != -1)
-            return dp[idx][hold];
-        
-        int ans = 0;
-        if (hold) {
-            // sell - and move to +2
-            ans = backtrack(idx+2, !hold, prices, dp) + prices[idx];
-            
-            // not sell
-            ans = max(ans, backtrack(idx+1, hold, prices, dp));
-        } else {
-            // buy 
-            ans = backtrack(idx+1, true, prices, dp) - prices[idx];
-            // or not buy
-            ans = max(ans, backtrack(idx+1, false, prices, dp));
-        }
-        
-        return dp[idx][hold] = ans;
-    }
 };
