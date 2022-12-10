2d8391759db5f10141bca942313d9d64029196e6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-10 16:19:30 +0900

    Time: 132 ms (82.92%), Space: 67.8 MB (55.89%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // Prefix sum?
+        /*
+        prefix sum
+        [-2, 1,-3, 4,-1, 2, 1,-5, 4]
+        [-2,-1,-4, 0,-1, 1, 2,-3, 1]
         
-        //    -2  1 -3  4 -1  2  1 -5  4
-        //  0 -2 -1 -4  0 -1  1  2 -3  1 - prefix
-        //  0 -2 -1 -1  4                - minimum Sum
-        // current - minimum 
+        [ 5, 4,-1, 7, 8]
+        [ 5, 9, 8,15,23]
+        maximum Sum - minimum Sum 
+        */
         
-        int n = nums.size(), preSum = 0;
-        int ans = nums[0], minN = 0;
-        for (int i=0; i<n; ++i) {
-            preSum += nums[i];
-            ans = max(ans, preSum - minN);
-            minN = min(preSum, minN);
+        int n = nums.size();
+        int prefixSum = 0, minSum = 0;
+        int ans = nums[0];
+        for(int i=0; i<n; ++i) {
+            prefixSum += nums[i];
+            ans = max(ans, prefixSum-minSum);
+            minSum = min(minSum, prefixSum);
         }
         
         return ans;
