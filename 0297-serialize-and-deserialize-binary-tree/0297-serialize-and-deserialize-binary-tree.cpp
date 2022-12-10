bece0e34c20a07dd4776c62a2a7012d568846ad4
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-11 03:10:16 +0900

    Time: 119 ms (38.25%), Space: 38.2 MB (28.40%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -10,7 +10,6 @@
 class Codec {
 public:
 
-    // * Preorder로 해야 한다는 것 * //
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         if (!root) return "#";
@@ -24,21 +23,21 @@ public:
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        stringstream str(data);
-        return inorderTraversal(str);
+        stringstream s(data);
+        return preorderTraversal(s);
     }
     
-    TreeNode* inorderTraversal(stringstream &s) {
+    TreeNode* preorderTraversal(stringstream& s) {
         string str;
         getline(s, str, ',');
         if (str == "#") return NULL;
         
         TreeNode* root = new TreeNode(stoi(str));
-        root->left = inorderTraversal(s);
-        root->right = inorderTraversal(s);
+        root->left = preorderTraversal(s);
+        root->right = preorderTraversal(s);
+        
         return root;
     }
-    
     
 };
 
