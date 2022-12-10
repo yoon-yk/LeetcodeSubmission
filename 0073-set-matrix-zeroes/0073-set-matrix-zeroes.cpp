eae514e1170715a5a56d7f67c6821c48658ef079
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-10 21:37:11 +0900

    Time: 15 ms (92.07%), Space: 13.3 MB (55.86%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,25 +3,20 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         
         int r = matrix.size(), c = matrix[0].size();
-        bool isFirstCol = false, isFirstRow = false;
+        bool isFirstCol = false;
         for (int i=0; i<r; ++i) {
-            for (int j=0; j<c; ++j) {
-                if (i==0 && matrix[i][j] == 0) isFirstRow = true;
-                if (j==0 && matrix[i][j] == 0) isFirstCol = true;
-                if (i!=0 && j!=0 && matrix[i][j] == 0){
+            if (matrix[i][0] == 0) isFirstCol = true;
+
+            for (int j=1; j<c; ++j) 
+                if (matrix[i][j] == 0)
                     matrix[i][0] = matrix[0][j] = 0;
-                }
-            }
         }
         
         for (int i=r-1; i>=0; --i) {
-            for (int j=c-1; j>=0; --j) {
-                if (i==0 && isFirstRow) matrix[i][j] = 0;
-                if (j==0 && isFirstCol) matrix[i][j] = 0;
-                if (i!=0 && j!=0 && (matrix[i][0] == 0 || matrix[0][j] == 0)){
+            for (int j=c-1; j>0; --j) 
+                if ((matrix[i][0] == 0 || matrix[0][j] == 0))
                     matrix[i][j] = 0;
-                }
-            }
+            if (isFirstCol) matrix[i][0] = 0;
         }
         
         
