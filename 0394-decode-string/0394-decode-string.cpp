aa7e0ed5c541dfa741968df08a0521ab0bc9b31c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-10 13:58:46 +0900

    Time: 2 ms (56.42%), Space: 6.5 MB (55.76%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     string decodeString(string s) {
-        int n = s.size();
-        string ans;
-        dfs(s, 0, n-1, ans);
-        return ans;
+        stringstream ss(s);
+        return ProcessExp(ss);
     }
-    
-    void dfs(string& s, int left, int right, string& subStr){
-        
-        int cntN; 
-        string ans;
-        for (int i=left; i<=right; i++) {
-            if (isdigit(s[i])) {
-                int j = i;
-                string curN;
-                while (j<=right && isdigit(s[j]))
-                    curN += s[j++];
-                cntN = stoi(curN);
-                i = j-1;
-            } else if (s[i] == '[') {
-                int j = i+1;
-                int leftB = 1, rightB = 0;
-                while (j <= right && leftB > rightB) {
-                    if (s[j] == '[') leftB ++;
-                    else if (s[j] == ']') rightB++;
-                    ++j;
-                }
-                if (leftB > 0) {
-                    string tempStr;
-                    dfs(s, i+1, j-2, tempStr);
-                    for (int c=0; c<cntN; c++) 
-                        ans += tempStr;
-                }
-                i = j-1;
-            } else {
-                ans += s[i];
-            } 
+    string ProcessExp(stringstream& in)
+    {
+        stringstream out;
+        char c;
+        int times = 0;
+        while (in >> c)
+        {
+            if (isdigit(c)) times = 10 * times + (c - '0');
+            else if (c == '[')
+            {
+                string inner = ProcessExp(in);
+                for (int i = 0; i < times; i++) out << inner;
+                times = 0;
+            }      
+            else if (c == ']') break;
+            else out << c;
         }
-        subStr = ans;
+        return out.str();
     }
 };
