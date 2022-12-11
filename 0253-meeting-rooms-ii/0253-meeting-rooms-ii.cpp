288269624f7683e2d592dab23b895ce8be03ac58
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-12 02:26:39 +0900

    Time: 23 ms (82.54%), Space: 12.2 MB (56.95%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        
-        sort(intervals.begin(), intervals.end());        
+        priority_queue<int, vector<int>, greater<int>> startTimes, endTimes;
+        for (auto & in : intervals) {
+            startTimes.push(in[0]);
+            endTimes.push(in[1]);
+        }
         
         /*
-        0 5 10 15 20 30
-        0 ---------- 30
-          5-10
-               15-20
-        ===============
-        1 1 -1 1  -1  -1
-        1 2 1  2   1   0
-        
+        0  5  15
+        10 20 30
         */
-        map<int, int> maxRooms;
-        for (auto & in : intervals) {
-            maxRooms[in[0]] ++, maxRooms[in[1]] --;
+        int usedRooms = 0;
+        while (!startTimes.empty()) {
+            if (startTimes.top() >= endTimes.top()) {
+                usedRooms --;
+                endTimes.pop();
+            }
+            usedRooms ++;
+            startTimes.pop();
         }
-    
-        int ans = 0;
-        int prefixSum = 0;
-        for (auto & i : maxRooms) {
-            prefixSum += i.second;
-            ans = max(prefixSum, ans);
-        }
-        
-        return ans;
-    
+        return usedRooms;
     }
 };
