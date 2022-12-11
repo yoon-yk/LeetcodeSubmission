49f32f48010cee53aa78dc717f6d1efea7bc59c6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-11 18:36:58 +0900

    Time: 2 ms (71.07%), Space: 6 MB (48.58%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        bitset<32> s(n);
-        return s.count();
+        int bits = 0;
+        uint32_t mask = 1;
+        for (int i=0; i<32; ++i) {
+            if ((n & mask) != 0)
+                bits++;
+            mask <<= 1;
+        }
+        return bits;
     }
 };
