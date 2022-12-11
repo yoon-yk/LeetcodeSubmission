c7b295034b72bc6d2b6ccb6542b6b7b3e31b73af
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-12 01:01:03 +0900

    Time: 292 ms (7.61%), Space: 38.3 MB (38.04%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
-        /*
-        
-        [0,2] [3,4] [5,7]
-           [1,6]
-        
-        
-        <----> <------> <-->
-    
-        */
-        
-        vector<vector<int>> ans;
-        auto it = upper_bound(intervals.begin(), intervals.end(), toBeRemoved);
-
-
-        if (it != intervals.begin()) {
-
-            for (auto i = intervals.begin(); i!=prev(it); ++i) 
-                ans.push_back(*i);
-            
-            auto pre = prev(it);
-            if ((*pre)[1] > toBeRemoved[0]) {
-                if ((*pre)[0] < toBeRemoved[0])
-                    ans.push_back({(*pre)[0], toBeRemoved[0]});
-                if (toBeRemoved[1] < (*pre)[1]) 
-                    ans.push_back({toBeRemoved[1], (*pre)[1]});
-            } else ans.push_back(*pre);            
+        vector<vector<int>> result;
+        for (auto & in : intervals) {
+            if (in[0] > toBeRemoved[1] || in[1] < toBeRemoved[0])
+                result.push_back({in[0], in[1]});
+            else {
+                if (in[0] < toBeRemoved[0])
+                    result.push_back({in[0], toBeRemoved[0]});
+                if (in[1] > toBeRemoved[1]) 
+                    result.push_back({toBeRemoved[1], in[1]});
+            }
         }
-
-        while (it!= intervals.end() && (*it)[1] <= toBeRemoved[1]) it++;
-
-        if (it != intervals.end() && (*it)[0] < toBeRemoved[1] && 
-            toBeRemoved[0] < (*it)[1]) {
-            ans.push_back({toBeRemoved[1], (*it)[1]});
-            it++;
-        }
-        
-        while (it!= intervals.end()) {
-            ans.push_back(*it); it++;
-        }
-        
-        return ans;
+        return result;
     }
 };
