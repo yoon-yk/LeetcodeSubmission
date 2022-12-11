7b46d503e6092fd57ee2e540288a9cb623dffffe
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-11 23:20:13 +0900

    Time: 9 ms (9.61%), Space: 7 MB (74.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-
-        int n = matrix.size();
-        int m = n-1;
-        int limit = m;
-        
-        for (int r=0; r< (n>>1); ++r) {
-            for (int c=r; c< limit; ++c) {
-                int tmp = matrix[r][c];
-                matrix[r][c] = matrix[m-c][r];
-                matrix[m-c][r] = matrix[m-r][m-c];
-                matrix[m-r][m-c] = matrix[c][m-r];
-                matrix[c][m-r] = tmp;
+        transpose(matrix);
+        reflect(matrix);
+    }
+    /*
+    
+    1 2
+    3 4
+    5 6
+    
+    */
+    void transpose(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        for (int i=0; i<n; ++i) {
+            for (int j=i+1; j<m; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
             }
-            limit--;
         }
-
+    }
+    
+    void reflect(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<(m>>1); ++j) {
+                swap(matrix[i][j], matrix[i][m-1-j]);
+            }
+        }
         
     }
 };
