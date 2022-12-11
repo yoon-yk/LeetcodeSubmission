0627db3e792f84b0966531acd66ef4c1b42ca772
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-12 00:57:30 +0900

    Time: 247 ms (33.69%), Space: 38.4 MB (38.04%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+        /*
         
-        int n = intervals.size();
+        [0,2] [3,4] [5,7]
+           [1,6]
+        
+        
+        <----> <------> <-->
+    
+        */
+        
         vector<vector<int>> ans;
-        for (auto & interval : intervals) {
-            if (toBeRemoved[0] > interval[1] || toBeRemoved[1] < interval[0]) {
-                ans.emplace_back(interval);
-            } else {
-                if (interval[0] < toBeRemoved[0]) {
-                    ans.push_back({interval[0], toBeRemoved[0]});
-                }
-                if (interval[1] > toBeRemoved[1]) {
-                    ans.push_back({toBeRemoved[1], interval[1]});    
-                }
-            }
-        } 
+        auto it = upper_bound(intervals.begin(), intervals.end(), toBeRemoved);
+
+
+        if (it != intervals.begin()) {
+
+            for (auto i = intervals.begin(); i!=prev(it); ++i) 
+                ans.push_back(*i);
+            
+            auto pre = prev(it);
+            if ((*pre)[1] > toBeRemoved[0]) {
+                if ((*pre)[0] < toBeRemoved[0])
+                    ans.push_back({(*pre)[0], toBeRemoved[0]});
+                if (toBeRemoved[1] < (*pre)[1]) 
+                    ans.push_back({toBeRemoved[1], (*pre)[1]});
+            } else ans.push_back(*pre);            
+        }
+
+        while (it!= intervals.end() && (*it)[1] <= toBeRemoved[1]) it++;
+
+        if (it != intervals.end() && (*it)[0] < toBeRemoved[1] && 
+            toBeRemoved[0] < (*it)[1]) {
+            ans.push_back({toBeRemoved[1], (*it)[1]});
+            it++;
+        }
+        
+        while (it!= intervals.end()) {
+            ans.push_back(*it); it++;
+        }
         
         return ans;
     }
