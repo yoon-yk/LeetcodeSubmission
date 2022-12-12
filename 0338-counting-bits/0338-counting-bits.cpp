eb4b83fa660b0bf722e1f0cc5bcb48b59bbdb792
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-12 16:14:44 +0900

    Time: 15 ms (20.97%), Space: 7.8 MB (89.23%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
+    /*
+    0000  0 0
+    0001  1 1
+    0010  2 1
+    0011  3 2
+    /////////
+    0100  4 1
+    0101  5 2
+    0110  6 2
+    0111  7 3
+    /////////
+    1000  8 1
+    1001  9 2
+    1010 10 2
+    1011 11 3
+    1100 12 2
+    
+    */
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
-        for (int i=0; i<=n; i++) {
-            int j = i, cnt = 0;
-            while (j != 0) {
-                cnt ++;
-                j &= (j-1);
-            }
-            ans[i] = cnt;
+        for (int i=1; i<=n; i++) {
+            ans[i] = ans[i>>1] + (i & 1);
         }
+        
         return ans;
     }
 };
