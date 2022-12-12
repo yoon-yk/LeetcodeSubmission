88c8c840c1c209f6f17e0064175152d0f68708b3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-13 03:56:02 +0900

    Time: 2064 ms (13.88%), Space: 16.3 MB (30.32%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,55 +1,38 @@
-class Trie {
-public :
-    Trie* next[26] = {};
-    int wordIdx;
+class TrieNode {
+public: 
     bool isWord;
-    bool isInserted;
-    
-    Trie() {
+    vector<TrieNode*> children;
+
+    TrieNode() {
         isWord = false;
-        isInserted = false;
-    }
-    
-    void insertWord(int idx, string &word) {
-        int c;
-        Trie* curNode = this;
-        for (int i=0; i<word.size(); i++) {
-            c = word[i]-'a';
-            if (!curNode->next[c]) curNode->next[c] = new Trie();
-            curNode = curNode->next[c];
-        }
-        curNode->isWord = true;
-        curNode->wordIdx = idx;
-    }
+        children.resize(26, NULL);
+    };
 };
 
 class Solution {
 public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        // 필요한 DS - Trie, ans 
-        
-        vector<string> ans;
-        Trie* root = new Trie();
-        
-        // Trie에 주어진 Words 다 추가해놓기 
-        int idx = 0;
-        for (auto &word : words) {
-            root->insertWord(idx++, word);
+    TrieNode* root = new TrieNode();
+    
+    void insertWord(string& word) {
+        TrieNode* cur = root;
+        for (auto & c : word) {
+            if (cur->children[c-'a'] == NULL)
+                cur->children[c-'a'] = new TrieNode();
+            cur = cur->children[c-'a'];
         }
+        cur->isWord = true;
+    }    
+    
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        for (auto & w : words) insertWord(w);
         
-        vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
-
-        // backtracking 하면서 trie에 존재하는지 확인하기
-        string curStr;
-        Trie* curNode;
-        int n=board.size(), m = board[0].size();
-        for (int r=0; r<n; r++) {
-            for (int c=0; c<m; c++) {
-                if (!root->next[board[r][c]-'a']) continue;
-                curNode = root->next[board[r][c]-'a'];
-                visited[r][c] = true;
-                backtrack (board, visited, words, r, c, curNode, ans);
-                visited[r][c] = false;
+        int n = board.size(), m = board[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        vector<string> ans;
+        for (int i=0; i<board.size(); ++i) {
+            for (int j=0; j<board[0].size(); ++j) {
+                string curStr;
+                dfs(board, visited, i, j, curStr, root, ans);
             }
         }
         
@@ -58,32 +41,22 @@ public:
     
     vector<int> dir = {-1, 0, 1, 0, -1};
     
-    bool isRangeValid(vector<vector<char>>& board, int r, int c){
-        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size())
-            return false;
-        return true;
-    }
-    
-    void backtrack (vector<vector<char>>& board, vector<vector<bool>>& visited, vector<string>& words, int r, int c, Trie* curNode, vector<string>& ans) {
-    
-        Trie* root = curNode;
-        if (root->isWord && !root->isInserted) {
-            ans.push_back(words[root->wordIdx]);
-            root->isInserted = true;
-        }
-            
-        int newR, newC;
-        Trie* nextRoot;
-        for (int d=0; d<4; d++) {
-            newR = r+dir[d], newC = c+dir[d+1];
-            if (!isRangeValid(board, newR, newC) || visited[newR][newC]) continue;
-            nextRoot = root->next[board[newR][newC]-'a'];
-            if (nextRoot) {
-                visited[newR][newC] = true;
-                backtrack (board, visited, words, newR, newC, nextRoot, ans);
-                visited[newR][newC] = false;
-            }
-        }
+    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int i, int j, string& curStr, TrieNode* cur, vector<string>& ans) {
         
+        if (i<0 || j<0 || i>=board.size() || j>=board[0].size() || visited[i][j] || cur->children[board[i][j]-'a'] == NULL) return;
+        
+        cur = cur->children[board[i][j]-'a'];
+        curStr.push_back(board[i][j]);
+
+        if (cur->isWord) ans.push_back(curStr);
+        cur->isWord = false;
+        visited[i][j] = true;
+        
+        for (int d=0; d<4; d++) 
+            dfs(board, visited, i + dir[d], j+dir[d+1], curStr, cur, ans);
+
+        
+        visited[i][j] = false;
+        curStr.pop_back();
     }
 };
