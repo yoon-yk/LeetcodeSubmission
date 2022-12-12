608db7afb7e94c7c5527f01a3924c88294c670e9
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-13 00:22:49 +0900

    Time: 45 ms (18.22%), Space: 13.5 MB (97.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,21 +1,60 @@
 class Solution {
 public:
+    vector<int> unique;
+    unordered_map<int, int> count_map;
+
+    int partition(int left, int right, int pivot_index) {
+        int pivot_frequency = count_map[unique[pivot_index]];
+        
+        // 1. Move pivot to the end
+        swap(unique[pivot_index], unique[right]);
+        
+        // 2. Move all less frequent elements to the left
+        int store_index = left;
+        for (int i=left; i<=right; i++) {
+            if (count_map[unique[i]] < pivot_frequency) {
+                swap(unique[store_index], unique[i]);
+                store_index++;
+            }
+        }
+        
+        // 3. move pivot to its final place
+        swap(unique[right], unique[store_index]);
+        
+        return store_index;
+    }
+        
+    void quickselect(int left, int right, int k_smallest) {
+        
+        // base case : the list contains only one element
+        if (left == right) return;
+        int pivot_index = left + rand() % (right-left+1);
+        
+        // find the pivot position in a sorted list
+        pivot_index = partition(left, right, pivot_index);
+        
+        // if the pivot is in its final sorted position
+        if (k_smallest == pivot_index) {
+            return;
+        } else if(k_smallest < pivot_index) {
+            // go left
+            quickselect(left, pivot_index-1, k_smallest);
+        } else {
+            // go right
+            quickselect(pivot_index+1, right, k_smallest);
+        }
+    }
+    
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        for (auto & n : nums)
-            mp[n]++;
+        for (int & n : nums) count_map[n] ++;
         
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        for (auto & [val, freq] : mp) {
-            pq.push({freq, val});
-            if (pq.size() == k+1) pq.pop();
-        }
+        int n = count_map.size();
+        for (auto & [val, preq] : count_map)
+            unique.push_back(val);
         
-        vector<int> ans(k);
-        for (int i=k-1; i>=0; i--) {
-            auto cur = pq.top(); pq.pop();
-            ans[i] = cur.second;
-        }
-        return ans;
+        quickselect(0, n-1, n-k);
+        vector<int> top_k_frequent(k);
+        copy(unique.begin() + n-k, unique.end(), top_k_frequent.begin());
+        return top_k_frequent;
     }
 };
