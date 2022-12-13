65bd10555d75ea1d9e721330132db182d34150bd
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-13 23:02:01 +0900

    Time: 118 ms (5.02%), Space: 90.1 MB (21.91%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // naive
-        // merge -> find median
-        
-        int len1 = nums1.size(), len2 = nums2.size();
-        int mid = (len1 + len2) >> 1;
-        
-        int p1 = 0, p2 = 0;
-        int cnt = 0, cur = 0, prev = 0;
-        while (cnt <= mid) {
-            prev = cur;
-            if (p1 < len1 && p2 < len2) {
-                if (nums1[p1] < nums2[p2])
-                    cur = nums1[p1++];
-                else cur = nums2[p2++];
-            } else if (p1 < len1) cur = nums1[p1++];
-            else cur = nums2[p2++]; 
-            cnt++;
+        vector<int> v = nums1;
+        for (auto &i : nums2) v.push_back(i);
+       
+        int totalLen = nums1.size() + nums2.size();
+        int targetIdx = totalLen >> 1;
+        if (totalLen % 2) { // odd
+            quicksort(v, 0, totalLen-1, targetIdx);
+            return v[targetIdx];
         }
-        return ((len1 + len2) % 2) ? cur : (cur+prev) * 0.5;
-        
-        /*
-        
-        1 3 5
-          ^
-        2 4 6
-          ^
-        1 2 3 4 5 6 
-            ^
-        */
-        
-        
+        quicksort(v, 0, totalLen-1, targetIdx-1);
+        int t1 = v[targetIdx-1];
+        quicksort(v, 0, totalLen-1, targetIdx);
+        int t2 = v[targetIdx];
+        return (t1 + t2) * 0.5;
         
     }
+    
+    void quicksort(vector<int>&v, int start, int end, int targetIdx) {
+        if (start == end) return;
+        
+        int ptIdx = partition(v, start, end);
+        if (ptIdx == targetIdx) return;
+        if (ptIdx < targetIdx) quicksort(v, ptIdx+1, end, targetIdx);
+        else quicksort(v, start, ptIdx-1, targetIdx);
+    }
+    
+    int partition (vector<int>& v, int start, int end) {
+        
+        int mid = start + ((end-start) >> 1);
+        swap(v[mid], v[end]);
+        
+        int lo = start;
+        for (int i=start; i< end; i++) {
+            if (v[i] < v[end]) {
+                swap(v[i], v[lo]);
+                lo++;
+            }
+        }
+        swap(v[lo], v[end]);
+        
+        return lo;
+    }
+    
 };
