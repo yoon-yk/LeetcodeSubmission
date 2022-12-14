b10890286cef65406c5f1deac6bcb04128b4565f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-14 14:22:09 +0900

    Time: 569 ms (8.63%), Space: 39.9 MB (86.53%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    vector<pair<int, int>> adj[101];
+    vector<int> minCost;
+    vector<vector<pair<int, int>>> adjList;
     
-    void DFS(vector<int>& signalReceivedAt, int currNode, int currTime) {
-        if (currTime>=signalReceivedAt[currNode])
-            return;
+    void dfs(int idx, int curCost) {
+        if (minCost[idx] <= curCost) return;
+        minCost[idx] = curCost;
         
-        signalReceivedAt[currNode] = currTime;
-        
-        for (auto & edge : adj[currNode]) {
-            int travelTime = edge.first;
-            int neighborNode = edge.second;
-            
-            DFS(signalReceivedAt, neighborNode, currTime+travelTime);
+        for (auto & [next, cost] : adjList[idx]) {
+            dfs(next, curCost+cost);
         }
-    }   
+    }
     
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        for (vector<int>& time : times) {
-            int src = time[0];
-            int dst = time[1];
-            int travelTime = time[2];
-            
-            adj[src].push_back({travelTime, dst});
+        minCost.resize(n+1, INT_MAX);
+        adjList.resize(n+1);
+        
+        for (auto & time : times) {
+            int src = time[0], dst = time[1], cost = time[2];
+            adjList[src].push_back({dst, cost});
         }
         
-        for (int i=1; i<=n; i++) {
-            sort(adj[i].begin(), adj[i].end());
-        }
+        dfs(k, 0);
         
-        vector<int> signalReceivedAt(n+1, INT_MAX);
-        DFS(signalReceivedAt, k, 0);
+        int ans = *max_element(minCost.begin()+1, minCost.end());
         
-        int ans = *max_element(signalReceivedAt.begin()+1, signalReceivedAt.end());
         return (ans == INT_MAX) ? -1 : ans;
+        
     }
 };
