6ddfc0596ba90956a2c96ce90d00440dda60a86a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-14 21:11:40 +0900

    Time: 24 ms (93.67%), Space: 13.5 MB (62.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+    using dpair = pair<int, int>;
+    using tpair = pair<int, dpair>;
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
+        vector<vector<dpair>> adj(n);
+        for (auto & e : flights)
+            adj[e[0]].push_back(pair(e[1], e[2]));
         
-        for (int i=0; i<=k; ++i) {
-            vector<int> temp(dist);
-            for (auto & flight : flights) {
-                if (dist[flight[0]] != INT_MAX) {
-                    temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
-                }
-            }
-            dist = temp;
+        vector<int> stops(n, INT_MAX);
+        
+        priority_queue<tpair, vector<tpair>, greater<tpair>> pq;
+        pq.push({0, {src, 0}});
+    
+        
+        while (!pq.empty()) {
+            auto [dist, nums] = pq.top(); pq.pop();
+            auto& [curV, steps] = nums;
+            
+            if (steps > stops[curV] || steps > k+1) continue;
+            
+            stops[curV] = steps;
+            if (curV == dst) return dist;
+            
+            for (auto & [nei, price] : adj[curV])
+                pq.push({dist+price, {nei, steps+1}});
         }
         
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        return -1;
+        
     }
 };
