88f26f6d9305098348583908bbf91b3c8f20712e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-15 15:39:21 +0900

    Time: 264 ms (47.15%), Space: 17.6 MB (20.06%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,15 +3,30 @@ public:
     int jump(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n, INT_MAX);
-        dp[0] = 0;
-        for (int i=0; i<n; ++i) {
-            for (int j=1; j<=nums[i]; ++j) {
-                if (i + j <= n-1)
-                    dp[i+j] = min(dp[i]+1, dp[i+j]);
+        if (n == 1) return 0;
+        
+        queue<int> Q;
+        vector<int> visited(n, false);
+        Q.push(0);
+        visited[0] = true;
+        
+        int size = 0, idx = 0, cnt = 0;
+        while (!Q.empty()) {
+            size = Q.size();
+            while (size--) {
+                auto cur = Q.front(); Q.pop();
+                
+                for (int i=0; i<=nums[cur]; i++) {
+                    if (cur+i == n-1) return cnt + 1;
+                    if (visited[cur+i]) continue;
+                    Q.push(cur+i);
+                    visited[cur+i] = true;
+                }
             }
+            cnt ++;
         }
-        return dp[n-1];
+        
+        return cnt;
         
     }
 };
