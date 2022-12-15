a549937d13c687b02f7b9fde3c6dddd5c2fe63fd
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-15 17:46:17 +0900

    Time: 74 ms (97.95%), Space: 28.2 MB (75.90%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int k) {
-        
-        /*
-        1 2 3 6 2 3 4 7 8
-        
-        1 2 3 4 6 7 8 9 2 3 4 5 3 4 5 6, k = 4
-        
-        1 2 2 3 3 3 4 4 4 5 5 6 6 7 8 9, k = 4
-        
-        2 
-        1 2 2 
-        
-        // max freq <= k
-        // size % k == 0
-        */
+        if (hand.size() % k ) return false;
         
         map<int, int> hashM;
         for (auto & i : hand) ++hashM[i];
