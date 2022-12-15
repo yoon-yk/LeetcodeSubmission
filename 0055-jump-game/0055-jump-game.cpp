336e78547c3b7fd490ea06b64d62916fddb37764
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-15 16:04:25 +0900

    Time: 70 ms (92.55%), Space: 48.4 MB (48.90%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int curLv = 0, curEnd = 0, curMax = 0;
+        int curEnd = 0, curMax = 0;
         for (int i=0; i<n-1; i++) {
             curMax = max(curMax, nums[i] + i);
             if (i == curEnd) {
                 if (curMax <= curEnd) return false;
                 curEnd = curMax;
-                curLv ++;
             }
         }
         return true;
