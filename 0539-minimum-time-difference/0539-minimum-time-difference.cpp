5adc7d32c9ae39d14866e38f566aea01190c36f9
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-26 02:33:00 +0900

    Time: 15 ms (88.92%), Space: 13.3 MB (98.02%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,46 +1,19 @@
 class Solution {
 public:
-    int dayInMins = 24*60;
-    /*
-    00 : 00 == 24 : 00
-    13 : 00
     
-    13시간, 11시간 차이
-    
-    01:00
-    15:00
-    
-    14시간 차이, 10시간 차이 
-    */
-    
-    int stringToMiniutes(vector<string>& timePoints, int idx) {
-        // return stoi(timePoints[idx].substr(0, 2)) * 60 + stoi(timePoints[idx].substr(3, 2));
-        return timePoints[idx][0] * 600 + timePoints[idx][1] * 60 + timePoints[idx][3] * 10 + timePoints[idx][4];
+    int getDiff(string& time1, string& time2) {
+        int t1 = ((time1[0]-'0')*10 + (time1[1]-'0'))*60 + (time1[3]-'0')*10 + (time1[4]-'0');
+        int t2 = ((time2[0]-'0')*10 + (time2[1]-'0'))*60 + (time2[3]-'0')*10 + (time2[4]-'0');
+
+        return min(abs(t1-t2), 24*60-abs((t1-t2)));
     }
-    
-    int getTimeDifference(vector<string>& timePoints, int left, int right) {
-        int leftM = stringToMiniutes(timePoints, left);
-        int rightM = stringToMiniutes(timePoints, right);
-        int diff = abs(leftM-rightM);
-        
-        return min(diff, dayInMins - diff);
-    }
-    
-    int timeDifference(vector<string>& timePoints, int idx) {
-        int left = idx, right = idx+1;
-        if (right >= timePoints.size()) right %= timePoints.size();
-        
-        return getTimeDifference(timePoints, left, right);
-    }
-    
     int findMinDifference(vector<string>& timePoints) {
         sort(timePoints.begin(), timePoints.end());
-        
-        int ans = dayInMins;
-        for (int i=0; i<timePoints.size(); i++) {
-            ans = min(ans, timeDifference(timePoints, i));
+        int diff, minDiff = 24*60;
+        for (int i=0; i<timePoints.size(); ++i) {
+            diff = getDiff(timePoints[i], timePoints[(i+1)%(timePoints.size())]);
+            minDiff = min(minDiff, diff);
         }
-                
-        return ans;
+        return minDiff;
     }
 };
