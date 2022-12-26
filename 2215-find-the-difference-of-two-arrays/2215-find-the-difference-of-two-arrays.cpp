cc6a0a70183f8be1f1837eec1498a0f0ba66f3fc
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-27 00:54:24 +0900

    Time: 35 ms (99.08%), Space: 27.1 MB (91.26%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret1, ret2;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int p1 = 0, p2 = 0, tmp;
+        while (p1 < nums1.size() || p2 < nums2.size()) {
+            if (p1 < nums1.size() && p2 < nums2.size()) {
+                if (nums1[p1] == nums2[p2]) {
+                    tmp = nums1[p1];
+                    while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
+                    while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
+                } else if (nums1[p1] < nums2[p2]) {
+                    tmp = nums1[p1];
+                    ret1.push_back(nums1[p1]);
+                    while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
+                } else {
+                    tmp = nums2[p2];
+                    ret2.push_back(nums2[p2]);
+                    while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
+                }
+            } else {
+                if (p1 < nums1.size()) {
+                    tmp = nums1[p1];
+                    ret1.push_back(nums1[p1]);
+                    while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
+                } else {
+                    tmp = nums2[p2];
+                    ret2.push_back(nums2[p2]);
+                    while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
+                }
+            }
+        }
+        
+        
+        return {ret1, ret2};
+    }
+};
