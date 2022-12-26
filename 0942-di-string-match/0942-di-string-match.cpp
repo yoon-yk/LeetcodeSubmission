e66c17b01c6f61a8dad9c824708f0016247b4c4d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-26 23:06:57 +0900

    Time: 10 ms (56.22%), Space: 8.4 MB (93.96%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.cpp
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
-        
-        /*
-        I  D  I  D
-        0 -1  0  1
-        
-        */
+     
         vector<int> v(s.size()+1);
-        v[0] = 0;
-        for (int i=1; i<=s.size(); ++i){
-            if (s[i-1] == 'I') {
-               v[i] = v[i-1]+1;
+        int lo = 0, hi = s.size();
+        for (int i=0; i<s.size(); ++i){
+            if (s[i] == 'I') {
+               v[i] = lo++;
             } else {
-               v[i] = v[i-1]-1;
+               v[i] = hi--;
             }
         }
-        priority_queue<pair<int, int>> pq;
-        for (int i=0; i<v.size(); ++i) pq.push({v[i], i});
-        
-        int n = v.size()-1;
-        while (!pq.empty()){
-            auto cur = pq.top(); pq.pop();
-            v[cur.second] = n--;
-        }
+        v[s.size()] = lo;
         
         return v;
     }
