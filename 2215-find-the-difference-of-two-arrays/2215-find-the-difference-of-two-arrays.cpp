af781116af1a9bc3a6f495f5a2bbaf415422269b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-27 00:57:28 +0900

    Time: 35 ms (99.08%), Space: 26.8 MB (92.00%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -6,32 +6,32 @@ public:
         sort(nums2.begin(), nums2.end());
         
         int p1 = 0, p2 = 0, tmp;
-        while (p1 < nums1.size() || p2 < nums2.size()) {
-            if (p1 < nums1.size() && p2 < nums2.size()) {
-                if (nums1[p1] == nums2[p2]) {
-                    tmp = nums1[p1];
-                    while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
-                    while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
-                } else if (nums1[p1] < nums2[p2]) {
-                    tmp = nums1[p1];
-                    ret1.push_back(nums1[p1]);
-                    while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
-                } else {
-                    tmp = nums2[p2];
-                    ret2.push_back(nums2[p2]);
-                    while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
-                }
+        while (p1 < nums1.size() && p2 < nums2.size()) {
+            if (nums1[p1] == nums2[p2]) {
+                tmp = nums1[p1];
+                while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
+                while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
+            } else if (nums1[p1] < nums2[p2]) {
+                tmp = nums1[p1];
+                ret1.push_back(nums1[p1]);
+                while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
             } else {
-                if (p1 < nums1.size()) {
-                    tmp = nums1[p1];
-                    ret1.push_back(nums1[p1]);
-                    while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
-                } else {
-                    tmp = nums2[p2];
-                    ret2.push_back(nums2[p2]);
-                    while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
-                }
+                tmp = nums2[p2];
+                ret2.push_back(nums2[p2]);
+                while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
             }
+        } 
+
+        while (p1 < nums1.size()) {
+            tmp = nums1[p1];
+            ret1.push_back(nums1[p1]);
+            while (p1 < nums1.size() && nums1[p1] == tmp) ++p1;
+        }
+        
+        while (p2 < nums2.size()) {
+            tmp = nums2[p2];
+            ret2.push_back(nums2[p2]);
+            while (p2 < nums2.size() && nums2[p2] == tmp) ++p2;
         }
         
         
