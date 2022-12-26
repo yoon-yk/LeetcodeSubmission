c007d863f78a644e38f87e88f31df5e7bf338f2c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-26 21:33:37 +0900

    Time: 10 ms (92.76%), Space: 9.9 MB (77.63%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
         int cnt = 0;
-        for (auto & word : words) 
-            if (word.substr(0, pref.size()) == pref) ++cnt;
+        for (auto & word : words) {
+            int i;
+            for (i=0; i<pref.size() && pref[i] == word[i]; ++i);
+            if (i==pref.size()) ++cnt;
+        }
         return cnt;
     }
 };
