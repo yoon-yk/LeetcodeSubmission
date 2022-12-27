137ba83799860815c36a7255556016910a5fe505
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-27 16:00:32 +0900

    Time: 3 ms (98.28%), Space: 6 MB (66.08%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -11,7 +11,6 @@ public:
         Explanation: L = 50, V= 5, III = 3.
         */
         
-        unordered_map<char, int> mp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         int ans = 0, n = s.size();
         
         char curCh;
