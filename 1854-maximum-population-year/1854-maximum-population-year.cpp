dd8e4e057cd88ffce1cd7e2f9d629de7e112f5d3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-27 15:37:58 +0900

    Time: 0 ms (100.00%), Space: 8 MB (72.98%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        map<int, int> years;
+        int base = 1950;
+        vector<int> years(105);
         for (auto & log : logs) {
-            ++years[log[0]], --years[log[1]];
+            ++years[log[0]-base], --years[log[1]-base];
         }
         
-        int prefix = 0, maxPop = 0, maxYear = -1;
-        for (auto & [year, pop] : years) {
+        int pop, prefix = 0, maxPop = 0, maxYear = -1;
+        for (int i=0; i<=101; ++i) {
+            pop = years[i]; 
             prefix += pop;
             if (maxPop < prefix) {
                 maxPop = prefix;
-                maxYear = year;
+                maxYear = i + base;
             }
         }
         
