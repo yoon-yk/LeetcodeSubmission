495fa8facbff83e45624971291e39c8d2980004f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-27 14:10:08 +0900

    Time: 0 ms (100.00%), Space: 6 MB (40.40%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        /*
+        9 
+        2 : 1001
+        3 : 1*9 + 0*3 + 0*1 = 100
+        4 : 2*4 + 1*1 = 21
+        5 : 1*5 + 4*1 = 14
+        6 : 1*6 + 3*1 = 13
+        7 : 1*7 + 2*1 = 12
+        
+        
+        3 :  true
+        */
+        return false;
+    }
+};
