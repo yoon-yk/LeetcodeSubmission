6b87aba2c087bafd3c322c3a86d983637ed1ef17
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-27 17:47:46 +0900

    Time: 58 ms (96.84%), Space: 55.6 MB (19.14%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int zeroCnt = 0, start = 0, ans = 0;
-        for (int end=0; end<nums.size(); ++end) {
+        int zeroCnt = 0, start = 0, end, ans = 0;
+        for (end=0; end<nums.size(); ++end) {
             if (nums[end] == 0) {
                 ++zeroCnt;
             }
             
-            while (zeroCnt > k) {
+            if (zeroCnt > k) {
                 if (nums[start] == 0) --zeroCnt;
                 ++start;
             }
             
-            ans = max(ans, end-start+1);
         }
-        return ans;
+        return end-start;
     }
 };
