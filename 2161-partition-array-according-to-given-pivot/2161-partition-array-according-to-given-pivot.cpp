2e2d4607709f8187a28c1285b8b19fed51e3ddd3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-27 13:20:34 +0900

    Time: 245 ms (77.74%), Space: 127.5 MB (69.11%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     void partition(vector<int>& nums, int pivot) {
-        vector<int> less, more;
-        int equal = 0;
+        int equal = 0, less = 0, more = 0;
         for (int i=0; i<nums.size(); ++i) {
-            if (nums[i] < pivot) less.push_back(i);
+            if (nums[i] < pivot) ++less;
             else if (nums[i] == pivot) ++equal;
-            else more.push_back(i);
+            else ++more;
         }
-        int idx = 0;
+        int lIdx = 0, eIdx = less, mIdx = less + equal;
         vector<int> ans(nums.size());
-        for (auto & i : less) ans[idx++] = nums[i];
-        for (int i=0; i<equal; ++i) ans[idx++] = pivot;
-        for (auto & i : more) ans[idx++] = nums[i];
+        for (int i=0; i<nums.size(); ++i) {
+            if (nums[i] < pivot) ans[lIdx++] = nums[i];
+            else if (nums[i] == pivot) ans[eIdx++] = nums[i];
+            else ans[mIdx++] = nums[i];
+        }
         nums.swap(ans);
     }
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         partition(nums, pivot);
-        
         return nums;
     }
 };
