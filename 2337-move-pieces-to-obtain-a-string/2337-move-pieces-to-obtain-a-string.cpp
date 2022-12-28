e14aa292290ca1f570a8ac88247e5cfe8eb8655b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-28 13:56:55 +0900

    Time: 58 ms (95.03%), Space: 19 MB (67.28%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    int left = 0, right = 1;
     bool canChange(string start, string target) {
         // l can go only lefter side, r can go only righter side
         int p1 = 0, p2 = 0;
         int n = start.size();
-        int lastCh = -1;
-        int lastChIdx = -1;
         while (true) {
             while (p1 < n && start[p1] == '_') ++p1;
             while (p2 < n && target[p2] == '_') ++p2;
