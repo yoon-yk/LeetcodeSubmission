542bc31cce0541624f7f104255fe7d5391e841e3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-28 18:44:45 +0900

    Time: 48 ms (78.74%), Space: 22.8 MB (21.84%) - LeetHub

--- 0379-design-phone-directory/0379-design-phone-directory.cpp
+++ 0379-design-phone-directory/0379-design-phone-directory.cpp
@@ -4,27 +4,45 @@ public:
     // taken
     unordered_set<int> taken;
     // not taken 
-    unordered_set<int> notTaken;
+    deque<int> available;
+    void print() {
+        cout << "**********" << endl;
+        cout << "available" << endl;
+        for (auto i : available) cout << i << " ";
+        cout << endl;
+        cout << "taken" << endl;
+        for (auto i : taken) cout << i << " ";
+        cout << endl;
+        cout << "**********" << endl;
+    }
     
     PhoneDirectory(int maxNumbers) {
-        for (int i=0; i<maxNumbers; ++i) notTaken.insert(i);
+        for (int i=0; i<maxNumbers; ++i) available.push_back(i);
+        // print();
     }
     
     int get() {
-        if (notTaken.empty()) return -1;
-        int rand = *notTaken.begin();
+        // cout << "Get" << endl;
+
+        if (available.empty()) return -1;
+        int rand = available.front(); available.pop_front();
         taken.insert(rand);
-        notTaken.erase(rand);
+        // print();
         return rand;
     }
     
     bool check(int number) {
-        return (notTaken.count(number));
+        // cout << "Check" << endl;
+        // print();
+        return (!taken.count(number));
     }
     
     void release(int number) {
+        if (!taken.count(number)) return;
         taken.erase(number);
-        notTaken.insert(number);
+        available.push_back(number);
+        // cout << "Release" << endl;
+        // print();
     }
 };
 
