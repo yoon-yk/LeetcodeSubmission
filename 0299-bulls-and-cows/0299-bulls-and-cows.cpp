c6c4756ef535744b18d2e1df148e3d136c16194e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-28 19:04:32 +0900

    Time: 6 ms (73.02%), Space: 6.4 MB (99.66%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    string getHint(string secret, string guess) {
-        unordered_map<int, int> mp;
-        for (int i=0; i<secret.size(); ++i) ++mp[secret[i]];
+    string getHint(string s, string g) {
+        vector<int> mp(10, 0);
         
         int nB = 0, nC = 0;
-        for (int i=0; i<guess.size(); ++i) {
-            if (secret[i] == guess[i]) {
-                ++nB; --mp[guess[i]];
-            }            
-        }
-        
-        for (int i=0; i<guess.size(); ++i) {
-            if (secret[i] != guess[i] && mp[guess[i]] > 0) {
-                ++nC; --mp[guess[i]];
-            }
+        for (int i=0; i<g.size(); ++i) {
+            if (s[i] == g[i]) {
+                ++nB;
+            } else {
+                if (mp[s[i]-'0'] < 0) ++nC;
+                if (mp[g[i]-'0'] > 0) ++nC;
+                ++mp[s[i]-'0'], --mp[g[i]-'0'];
+            }      
         }
         
         return to_string(nB)+'A'+to_string(nC)+'B';
