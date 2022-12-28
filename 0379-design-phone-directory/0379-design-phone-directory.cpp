e24f5ed9101a610d3a465f85b83215c4bc7b98ad
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-28 18:45:04 +0900

    Time: 45 ms (80.46%), Space: 22.8 MB (21.84%) - LeetHub

--- 0379-design-phone-directory/0379-design-phone-directory.cpp
+++ 0379-design-phone-directory/0379-design-phone-directory.cpp
@@ -5,35 +5,19 @@ public:
     unordered_set<int> taken;
     // not taken 
     deque<int> available;
-    void print() {
-        cout << "**********" << endl;
-        cout << "available" << endl;
-        for (auto i : available) cout << i << " ";
-        cout << endl;
-        cout << "taken" << endl;
-        for (auto i : taken) cout << i << " ";
-        cout << endl;
-        cout << "**********" << endl;
-    }
-    
+
     PhoneDirectory(int maxNumbers) {
         for (int i=0; i<maxNumbers; ++i) available.push_back(i);
-        // print();
     }
     
     int get() {
-        // cout << "Get" << endl;
-
         if (available.empty()) return -1;
         int rand = available.front(); available.pop_front();
         taken.insert(rand);
-        // print();
         return rand;
     }
     
     bool check(int number) {
-        // cout << "Check" << endl;
-        // print();
         return (!taken.count(number));
     }
     
@@ -41,8 +25,6 @@ public:
         if (!taken.count(number)) return;
         taken.erase(number);
         available.push_back(number);
-        // cout << "Release" << endl;
-        // print();
     }
 };
 
