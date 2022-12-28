d23a154091512a0e639a175bdc5c17de936284dd
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-28 16:39:16 +0900

    Time: 0 ms (100.00%), Space: 6.8 MB (48.17%) - LeetHub

---
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        int len = 0, cur = 0;
+        
+        for (int i=s.size()-1; i>=0; --i) {
+            if (s[i] == '1') {
+                if (cur + pow(2, len) <= k) {
+                    cur += pow(2, len);
+                    ++len;
+                }
+            } else {
+                ++len;
+            }
+        }
+        return len;
+    }
+};
