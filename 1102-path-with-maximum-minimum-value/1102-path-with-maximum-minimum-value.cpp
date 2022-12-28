45cf565784f84415076d0ae0532cc38d5cdcd2af
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-28 15:41:20 +0900

    Time: 597 ms (52.25%), Space: 68.8 MB (25.00%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
@@ -1,65 +1,42 @@
 class Solution {
 public:
-    int base;
-    vector<int> parent, size;
-    vector<int> dir = {-1, 0, 1, 0, -1};
-    vector<vector<bool>> visited;
-
-    int find(int v) {
-        if (parent[v] == v) return v;
-        return parent[v] = find(parent[v]);
-    }
-    
-    void unionn(pair<int, int> a, pair<int, int> b) {
-        int v1 = a.first*base + a.second, v2 = b.first*base + b.second;
-        int p1 = find(v1), p2 = find(v2);
-        if (p1 == p2) return;
-        
-        if (size[p1] < size[p2]) {
-            size[p2] += size[p1];
-            parent[p1] = parent[p2];
-        } else {
-            size[p1] += size[p2];
-            parent[p2] = parent[p1];
+    int maximumMinimumPath(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int lo = 0, hi = min(grid[0][0], grid[n-1][m-1]);
+        while (lo < hi) {
+            int mi = (lo+hi+1) >> 1;
+            if (exist(grid, mi)) lo = mi;
+            else hi = mi-1;
         }
+        return lo;
     }
     
-    bool isValid (vector<vector<int>>& grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+    vector<int> dir = {-1, 0, 1, 0, -1};
+    bool isValid(vector<vector<int>>& grid, int i, int j){
+        if (i<0 || j<0 || i>=grid.size() || j>=grid[0].size())
             return false;
         return true;
     }
-
-    int maximumMinimumPath(vector<vector<int>>& grid) {
-        
-        base = grid[0].size();
-        parent.resize(10005);
-        size.resize(10005, 1);
-        visited.resize(grid.size(), vector<bool>(grid[0].size(), false));
-        
-        for (int i=0; i<parent.size(); ++i) parent[i] = i;
-        
-        priority_queue<pair<int, pair<int, int>>> pq;
-        for (int i=0; i<grid.size(); ++i)
-            for (int j=0; j<grid[0].size(); ++j)
-                pq.push({grid[i][j], {i, j}});
-    
-        int target = (grid.size()-1)*base + (grid[0].size()-1);
-        
-        int ans = INT_MAX;
-        while (!pq.empty()) {
-            auto [r, c] = pq.top().second; 
-            ans = pq.top().first; pq.pop();
-            visited[r][c] = true;
+    bool exist(vector<vector<int>>& grid, int target){ 
+        queue<pair<int, int>> Q;
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        Q.push({0, 0});
+        int nR, nC;
+        while (!Q.empty()) {
+            auto cur = Q.front(); Q.pop();
+            if (cur.first == grid.size()-1 &&
+                cur.second == grid[0].size()-1) return true;
             for (int d=0; d<4; ++d) {
-                int newI = r+dir[d], newJ = c+dir[d+1];
-                if (isValid(grid, newI, newJ) && visited[newI][newJ]) {
-                    unionn({r,c}, {newI, newJ});
-                    if (find(0) == find(target)) return ans;
+                nR = cur.first + dir[d], nC = cur.second + dir[d+1];
+                if (isValid(grid, nR, nC) && !visited[nR][nC] && 
+                   grid[nR][nC] >= target) {
+                    Q.push({nR, nC});
+                    visited[nR][nC] = true;
                 }
+            
             }
         }
         
-        return ans;
+        return false;
     }
 };
