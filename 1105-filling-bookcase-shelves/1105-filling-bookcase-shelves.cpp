f42bc220330ea4c468dc1da465d9a2e56bc17131
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-30 00:47:47 +0900

    Time: 7 ms (79.06%), Space: 18.1 MB (15.47%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int maxWidth;
+    int maxW;
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        maxWidth = shelfWidth;
-        vector<vector<int>> dp(books.size(), vector<int>(shelfWidth+1, -1));
+        // 어쨌든 큰 애들은 우겨넣는게 최선, greedy는 안될걸?
+        int n = books.size();
+        maxW = shelfWidth;
+        vector<vector<int>> dp(n+1, vector<int>(shelfWidth+1, -1));
         return backtrack(books, 0, 0, 0, dp);
     }
     
     int backtrack(vector<vector<int>>& books, int idx, int curW, int curH, vector<vector<int>>& dp) {
+        
         if (idx == books.size()) 
             return curH;
-        if (dp[idx][curW] != -1)
-            return dp[idx][curW];
         
-        int include = INT_MAX, exclude;
-        if (curW + books[idx][0] <= maxWidth) 
-            include = backtrack(books, idx+1, curW+books[idx][0], max(curH, books[idx][1]), dp);
-        exclude = curH + backtrack(books, idx+1, books[idx][0], books[idx][1], dp);
+        if (dp[idx][curW]!=-1) return dp[idx][curW];
         
-        return dp[idx][curW] = min(include, exclude);
+        int bw = books[idx][0], bh = books[idx][1];
+        
+        int ans = backtrack(books, idx+1, bw, bh, dp) + curH;
+        
+        if (curW + bw <= maxW)
+            ans = min(ans, backtrack(books, idx+1, curW+bw, max(curH, bh), dp));
+        
+        return dp[idx][curW] = ans;
     }
 };
