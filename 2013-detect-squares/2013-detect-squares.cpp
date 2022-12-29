a88c5d0f8b7ead3ffffbdb52eaec941642ce253e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-29 23:14:37 +0900

    Time: 850 ms (18.08%), Space: 187.9 MB (5.92%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.cpp
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -1,9 +1,9 @@
 class DetectSquares {
 public:
-    
-    unordered_map<int, unordered_map<int, int>> mp;
-    
-    DetectSquares() {}
+    vector<vector<int>> mp;
+    DetectSquares() {
+        mp.resize(1005, vector<int>(1005, 0));
+    }
     
     void add(vector<int> point) {
         ++mp[point[0]][point[1]];
@@ -11,19 +11,23 @@ public:
     
     int count(vector<int> point) {
         
-        int curX = point[0], curY = point[1];
         int ans = 0;
-        
-        for (auto & [xv, yvs] : mp) {
-            if (xv == curX) continue;
-            
-            for (auto & [yv, cnt] : yvs) {
-                if (yv == curY || abs(yv-curY) != abs(xv-curX)) continue;
-                
-                ans += mp[xv][curY] * mp[curX][yv] * mp[xv][yv];
+        int x = point[0], y = point[1];
+        for (int i=1; i<=1000; ++i) {
+            if (x+i <= 1000 && y+i <= 1000) {
+                ans += mp[x+i][y+i] * mp[x+i][y] * mp[x][y+i];
             }
+            if (x+i <= 1000 && y-i >= 0) {
+                ans += mp[x+i][y-i] * mp[x+i][y] * mp[x][y-i];
+            }
+            if (x-i >= 0 && y-i >= 0) {
+                ans += mp[x-i][y-i] * mp[x-i][y] * mp[x][y-i];
+            }
+            if (x-i >= 0 && y+i <= 1000) {
+                ans += mp[x-i][y+i] * mp[x-i][y] * mp[x][y+i];
+            }
+            
         }
-        
         return ans;
     }
 };
