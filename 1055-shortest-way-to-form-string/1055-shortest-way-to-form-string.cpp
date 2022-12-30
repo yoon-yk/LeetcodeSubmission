a606007e1bb2f994ad57c672097ad3ebd02db2c6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-30 19:54:57 +0900

    Time: 3 ms (83.97%), Space: 6.8 MB (37.98%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int shortestWay(string src, string tgt) {
-        int s = 0, t = 0, ans = 1, cycle = false;
-        while (t < tgt.size()) {
-            cycle = false;
-            if (s == src.size()) {++ans; s=0;}
-            while (src[s] != tgt[t]) {
-                ++s;
-                if (s == src.size()) {
-                    if (cycle) return -1;
-                    cycle = true;
-                    ++ans; s = 0;
-                }
-            }
-            ++s, ++t;
+    int shortestWay(string source, string target) {
+        vector<vector<int>> dict(26);
+        for (int i=0; i<source.size(); ++i) dict[source[i]-'a'].push_back(i);
+        int ans = 1, curIdx = -1, curCh;
+        for (int i=0; i<target.size(); ++i) {
+            curCh = target[i]-'a';
+            if (dict[curCh].empty()) return -1;
+            auto it = upper_bound(dict[curCh].begin(), dict[curCh].end(), curIdx);
+            if (it == dict[curCh].end()) ++ans, curIdx = dict[curCh][0];
+            else curIdx = dict[curCh][it-dict[curCh].begin()];
         }
+        
         return ans;
     }
 };
