dd5a37d6e4a70d93f08295229ff825941e6c882d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-31 01:51:02 +0900

    Time: 4 ms (98.62%), Space: 8.2 MB (53.08%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int ans = 0;
+        for (int i=0; i<board.size(); ++i) {
+            for (int j=0; j<board[0].size(); ++j) {
+                if (board[i][j] != 'X') continue;
+                if (i>0 && board[i-1][j] == 'X' || j>0 && board[i][j-1] == 'X') continue;
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
