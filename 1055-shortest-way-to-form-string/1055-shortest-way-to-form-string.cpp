1503522eeedf5cf380d7e2ddd991f912a328be60
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-30 18:03:25 +0900

    Time: 5 ms (59.58%), Space: 6.5 MB (59.76%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
@@ -4,13 +4,13 @@ public:
         int s = 0, t = 0, ans = 1, cycle = false;
         while (t < tgt.size()) {
             cycle = false;
-            if (s == src.size()) {++ans; s%=src.size();}
+            if (s == src.size()) {++ans; s=0;}
             while (src[s] != tgt[t]) {
                 ++s;
                 if (s == src.size()) {
                     if (cycle) return -1;
                     cycle = true;
-                    ++ans; s%=src.size();
+                    ++ans; s = 0;
                 }
             }
             ++s, ++t;
