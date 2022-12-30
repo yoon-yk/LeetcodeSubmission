6dbab73702618fd3e234083a55c6524e91a26a58
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-30 22:07:24 +0900

    Time: 14 ms (65.45%), Space: 7.5 MB (60.13%) - LeetHub

--- 0418-sentence-screen-fitting/0418-sentence-screen-fitting.cpp
+++ 0418-sentence-screen-fitting/0418-sentence-screen-fitting.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+    int wordsTyping(vector<string>& words, int rows, int cols) {
+        /*
+        hello---
+        world---
+        hello---
+        world---
         
-        int n = sentence.size();
-        vector<int> cycleNext(n, -1), cycleCnt(n, -1);
-
-        bool cycle = false, space = false;
-        int startStr = 0;
-        int rowsLeft = rows-1, colsLeft = cols;
-        int cycleStart = -1, currCnt = 0, prevCnt = 0;
-        while (!cycle) {
-            for (int i=0; !cycle && i<n; i++){
-                int wordSize = sentence[i].size();
-                if (colsLeft < (wordSize + space)) {
-                    if (rowsLeft == 0) return currCnt;
-                    cycleNext[startStr] = i;
-                    cycleCnt[startStr] = currCnt - prevCnt;
-                    if (cycleCnt[i]!= -1) { cycle = true, cycleStart = i; break;}
-                    --rowsLeft;
-                    colsLeft = cols;
-                    space = false;
-                    startStr = i;
-                    prevCnt = currCnt;
-                }
-                colsLeft -= (wordSize + space);
-                space = true;
+        
+        a_bcd_e_
+        a_bcd_e_
+        
+        a_bcd_
+        e_a___
+        bcd_e_
+        a_bcd_
+        */
+        
+        int wid = 0, cleft = cols, ans = 0, prevCnt, curCnt, startIdx;
+        vector<pair<int, int>> dp(words.size(), {-1, -1});
+        for (int r=0; r<rows; ++r, cleft = cols) {
+            if (dp[wid].first!=-1) {
+                ans += dp[wid].first;
+                wid = dp[wid].second;
+                continue;
             }
-            if (cycle) break;
-            ++currCnt;
+            startIdx = wid;
+            prevCnt = ans;
+            while ((cleft-=words[wid].size()) >= 0) {
+                if (++wid == words.size()) {++ans; wid=0;}
+                cleft -= 1;
+            }
+            curCnt = ans;
+            dp[startIdx] = {curCnt-prevCnt, wid};
         }
-
-        int idx = cycleStart;
-        while (rowsLeft--) {
-            currCnt += cycleCnt[idx];
-            idx = cycleNext[idx];
-        }        
         
-        return currCnt;
-        
+        return ans;
     }
 };
