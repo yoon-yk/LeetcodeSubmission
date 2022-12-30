5cc365d798a75ab57a5463434093cfbc085855cd
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-30 20:30:19 +0900

    Time: 63 ms (97.22%), Space: 18.3 MB (47.68%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        
-        sort(begin(words), end(words), [](const string& w1, const string& w2) {
-            return w1.size() < w2.size();
+        // sort 
+        sort(words.begin(), words.end(), [](string& a, string& b) {
+           return a.size() < b.size(); 
         });
         
+        // for (auto & w : words) cout << w << endl;
+        // if chain - put into adjList
+        
         int n = words.size();
-        unordered_map<string, int> dp;
-        
-        int ans = -1, maxLen, preIdx;
-        for (auto & w : words) {
-            maxLen = 1;
-            for (int i=0; i<w.size(); i++) {
-                string combined = w.substr(0, i) + w.substr(i+1);
-                auto findPre = dp.find(combined);
-                if (findPre == dp.end()) continue;
-                preIdx = findPre->second;
-                maxLen = max(dp[combined] + 1, maxLen);
+        unordered_map<string, int> mp;
+        vector<vector<int>> adjList(n);
+        for (int i=0; i<words.size(); ++i) {
+            for (int j=0; j<words[i].size(); ++j) {
+                string next = words[i].substr(0, j) + words[i].substr(j+1);
+                if (mp.count(next))
+                    adjList[i].push_back(mp[next]);
             }
-            
-            dp[w] = maxLen;
-            ans = max(ans, dp[w]);
+            mp[words[i]] = i;
         }
+        int ans = 0;
         
+        vector<int> dp(n, -1);
+        for (int i=0; i<n; ++i)
+            ans = max(ans, dfs(i, adjList, dp));
+        
+        // dfs + dp
         return ans;
+    }
+    
+    int dfs(int cur, vector<vector<int>>& adjList, vector<int>& dp) {
+        
+        if (dp[cur]!=-1) return dp[cur];
+        
+        int ans = 1;
+        for (auto & nei : adjList[cur])
+            ans = max(ans, dfs(nei, adjList, dp)+1);
+        
+        return dp[cur] = ans;
     }
 };
