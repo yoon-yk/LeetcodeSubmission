885497fe47cc6fc95a548570044bc8ee9edd7166
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-30 15:25:14 +0900

    Time: 21 ms (86.49%), Space: 11.5 MB (98.96%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.cpp
+++ 0529-minesweeper/0529-minesweeper.cpp
@@ -2,16 +2,6 @@ class Solution {
 public:
     vector<pair<int, int>> dir = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
     
-    void print(vector<vector<char>>& board) {
-        for (int i=0; i<board.size(); ++i) {
-            for (int j=0; j<board[0].size(); ++j) {
-                cout << board[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-    
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
         
         if (board[click[0]][click[1]] == 'M') {
@@ -19,40 +9,35 @@ public:
             return board;
         }
         
-        // print(board);
-        
-        unordered_map<int, unordered_map<int, int>> mp;
-        for (int i=0; i<board.size(); ++i) {
-            for (int j=0; j<board[0].size(); ++j) {
-                if (board[i][j] == 'M') {
-                    mark(mp, board, i, j);
-                }
-            }
-        }
-        
         queue<pair<int, int>> Q;
-        if (!mp[click[0]].count(click[1])) Q.push({click[0], click[1]});
-        else {
-            board[click[0]][click[1]] = mp[click[0]][click[1]] + '0';
+        
+        int r = click[0], c = click[1];
+        int res = mark(board, r, c);
+        if (res > 0) {
+            board[r][c] = (res + '0');
             return board;
+        } else {
+            board[r][c] = 'B';
         }
+        
+        Q.push({click[0], click[1]});
         int nr, nc;
         while (!Q.empty()) {
             auto [r, c] = Q.front(); Q.pop();
-
+            
             for (auto& [nr, nc] : dir) {
                 if (!isValidRange(board, r+nr, c+nc) || board[r+nr][c+nc] != 'E') continue;
-                if (mp[r+nr].count(c+nc)){
-                    board[r+nr][c+nc] = mp[r+nr][c+nc]+'0';
+                int res = mark(board, r+nr, c+nc);
+                if (res > 0) {
+                    board[r+nr][c+nc] = (res + '0');
                     continue;
+                } else {
+                    board[r+nr][c+nc] = 'B';
+                    Q.push({r+nr, c+nc});
                 }
-                board[r+nr][c+nc] = 'B';
-                Q.push({r+nr, c+nc});
             }
         }
-        
-        // print(board);
-        
+                
         return board;
     }
     
@@ -61,14 +46,14 @@ public:
         return true;
     }
     
-    void mark(unordered_map<int, unordered_map<int, int>>& mp, vector<vector<char>>& board, int i, int j) {
-        int nr, nc;
+    int mark(vector<vector<char>>& board, int i, int j) {
+        int nr, nc, res = 0;
         for (auto & [r, c] : dir) {
             nr = i+r, nc = j+c;
-            if (!isValidRange(board, nr, nc)||board[nr][nc] != 'E') continue;
-            ++mp[nr][nc];
-            // cout << nr << " " << nc << " " << mp[nr][nc] << endl;
+            if (!isValidRange(board, nr, nc)) continue;
+            if (board[nr][nc] == 'M') ++res;
         }
+        return res;
     }
 
 };
