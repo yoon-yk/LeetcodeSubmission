e17a4a410e705aa91bde1c8f20d5c2c502e65639
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-31 00:49:49 +0900

    Time: 11 ms (73.42%), Space: 7.6 MB (19.60%) - LeetHub

--- 0418-sentence-screen-fitting/0418-sentence-screen-fitting.cpp
+++ 0418-sentence-screen-fitting/0418-sentence-screen-fitting.cpp
@@ -3,7 +3,7 @@ public:
     int wordsTyping(vector<string>& words, int rows, int cols) {
 
         int n = words.size();
-        int wid = 0, cleft = cols, ans = 0, prevCnt, curCnt, startIdx;
+        int wid = 0, cleft = cols, startIdx;
         vector<int> dp(n, -1);
         for (int r=0; r<rows; ++r, cleft = cols) {
             if (dp[wid%n]!=-1) {
