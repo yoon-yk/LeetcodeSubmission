2663a203a77b82e568d7110d13941fc884dfefe6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-31 00:48:33 +0900

    Time: 11 ms (73.42%), Space: 7.5 MB (60.13%) - LeetHub

--- 0418-sentence-screen-fitting/0418-sentence-screen-fitting.cpp
+++ 0418-sentence-screen-fitting/0418-sentence-screen-fitting.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     int wordsTyping(vector<string>& words, int rows, int cols) {
-        /*
-        hello---
-        world---
-        hello---
-        world---
-        
-        
-        a_bcd_e_
-        a_bcd_e_
-        
-        a_bcd_
-        e_a___
-        bcd_e_
-        a_bcd_
-        */
-        
+
+        int n = words.size();
         int wid = 0, cleft = cols, ans = 0, prevCnt, curCnt, startIdx;
-        vector<pair<int, int>> dp(words.size(), {-1, -1});
+        vector<int> dp(n, -1);
         for (int r=0; r<rows; ++r, cleft = cols) {
-            if (dp[wid].first!=-1) {
-                ans += dp[wid].first;
-                wid = dp[wid].second;
+            if (dp[wid%n]!=-1) {
+                wid += dp[wid%n];
                 continue;
             }
             startIdx = wid;
-            prevCnt = ans;
-            while ((cleft-=words[wid].size()) >= 0) {
-                if (++wid == words.size()) {++ans; wid=0;}
-                cleft -= 1;
+            while ((cleft-=words[wid%n].size()) >= 0) { 
+                cleft -= 1, ++wid;
             }
-            curCnt = ans;
-            dp[startIdx] = {curCnt-prevCnt, wid};
+            dp[startIdx%n] = wid-startIdx;
         }
         
-        return ans;
+        return wid/n;
     }
 };
