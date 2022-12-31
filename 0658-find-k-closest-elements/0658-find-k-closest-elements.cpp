f9dcc2888d497386302aa280734f556f87dce35c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-31 20:40:55 +0900

    Time: 47 ms (87.34%), Space: 31.2 MB (69.13%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        if (k == arr.size()) return arr;
-        auto lo = lower_bound(arr.begin(), arr.end(), x);
-        auto hi = upper_bound(arr.begin(), arr.end(), x);
-        int lb = lo - arr.begin(), hb = hi - arr.begin();
-        if (hb-lb > k) return vector<int> (k, x);
-        
-        int left, right;        
-        while (hb-lb-1 < k) {
-            if (lb < 0) ++hb;
-            else if (hb >= arr.size()) --lb;
-            else {
-                left = x-arr[lb];
-                right = arr[hb]-x;
-                if (left == right) (arr[lb] < arr[hb]) ? --lb : ++hb;
-                else (left < right) ? --lb : ++hb;    
-            }
+        int left = 0, right = arr.size()-k;
+        // left bound
+        while (left < right) {
+            int mid = left + ((right-left) >> 1);
+            if (x-arr[mid] /*left*/ > arr[mid+k]-x /*right*/) left = mid+1;
+            else right = mid;
         }
-
-        lb = max(0, lb+1);
-        hb = min(hb, (int)arr.size());
-        return vector<int> (arr.begin()+lb, arr.begin()+hb);
+        
+        return vector<int> (arr.begin()+left, arr.begin()+left+k);
     }
 };
