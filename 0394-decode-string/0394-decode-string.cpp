ee570e86824e23d31759460156711346d38e1dcf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-31 14:48:56 +0900

    Time: 4 ms (35.49%), Space: 6.7 MB (25.24%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stringstream ss(s);
-        return ProcessExp(ss);
-    }
-    string ProcessExp(stringstream& in)
-    {
-        stringstream out;
-        char c;
-        int times = 0;
-        while (in >> c)
-        {
-            if (isdigit(c)) times = 10 * times + (c - '0');
-            else if (c == '[')
-            {
-                string inner = ProcessExp(in);
-                for (int i = 0; i < times; i++) out << inner;
-                times = 0;
-            }      
-            else if (c == ']') break;
-            else out << c;
+        string ans;
+        deque<int> nums;
+        deque<string> msgs;
+        string num, msg;
+        int cnt;
+        for (char & c : s) {
+            if (isdigit(c)) {
+                if (!msg.empty()) {
+                    msgs.push_back(msg);
+                    msg.clear();
+                }
+                num += c;
+            } else if (c == ']') {
+                string cur;
+                while (!msgs.empty() && msgs.back() != "[") {
+                    cur = msgs.back() + cur;
+                    msgs.pop_back();
+                }
+                msgs.pop_back();
+                int cnt = nums.back(); nums.pop_back();
+                msgs.push_back(cur);
+                for (int i=0; i<cnt-1; ++i) {
+                    msgs.back() += cur;
+                }
+            } else {
+                if (c == '[') {
+                    nums.push_back(stoi(num));
+                    num.clear();
+                }
+                msgs.push_back(string(1, c));
+            }
         }
-        return out.str();
+        
+        string ret;
+        while (!msgs.empty()) {
+            ret += msgs.front();
+            msgs.pop_front();
+        }
+        return ret;
     }
 };
