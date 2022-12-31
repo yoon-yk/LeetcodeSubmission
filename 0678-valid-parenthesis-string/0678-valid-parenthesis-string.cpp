0f48e9ff2ee27065eabbe0c7b6d5d90763736208
Yoon <63157606+yoon-yk@users.noreply.github.com>
2022-12-31 17:15:07 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (33.21%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
-        return check(s, 0, 0, dp); 
-    }
-    
-    bool check(string &s, int idx, int opened, vector<vector<int>>& dp) {
-        if (idx == s.size()) 
-            return (opened == 0);
-        
-        if (dp[idx][opened] != -1)
-            return dp[idx][opened];
-
-        bool ret = false;
-        if (s[idx] == ')' || s[idx] == '*') {
-            if (opened > 0)
-                ret |= check(s, idx+1, opened-1, dp);
-        } 
-
-        if (s[idx] == '(' || s[idx] == '*') 
-            ret |= check(s, idx+1, opened+1, dp);
-        
-        if (s[idx] == '*') 
-            ret |= check(s, idx+1, opened, dp);
-            
-        return dp[idx][opened] = ret;
+        int cmin = 0, cmax = 0;
+        for (char& c : s) {
+            if (c == '(')
+                cmax++, cmin++;
+            if (c == ')')
+                cmax--, cmin = max(cmin - 1, 0);
+            if (c == '*')
+                cmax++, cmin = max(cmin - 1, 0);
+            if (cmax < 0) return false;
+        }
+        return cmin == 0;
     }
 };
