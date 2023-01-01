e2cb0b9f47c8065b4eadc6a91ddc20ec7aa6e7a7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-01 21:55:12 +0900

    Time: 283 ms (68.15%), Space: 93.1 MB (82.22%) - LeetHub

--- 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.cpp
+++ 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     long long countPairs(vector<int>& nums1, vector<int>& nums2) {
-        // nums1[i] + nums1[j] > nums2[i] + nums2[j].
-        // nums1[i] - nums2[i] > nums2[j] - nums1[j].
-        // nums1[i] - nums2[i] > - (nums1[j] - nums2[j]).
-        // ans[i] > - ans[j]
-        // ans[i] + ans[j] > 0
-
         /*
-        n1 :  2  1  2  1
-        n2 :  1  2  1  2
-        -----------------
-        re :  1 -1  1 -1  
-        re : -1  1 -1  1
-
-        */
-        int n = nums1.size();
-        long long cnt = 0;
-        vector<int> ans(nums1.size());
-        for (int i=0; i<n; ++i) {
-            ans[i] = (nums1[i] - nums2[i]);
-        }
-        sort(ans.begin(), ans.end());
+        [2, 1, 2, 1]
+        [1, 2, 1, 2]
         
-        for (int i=0; i<n; ++i) {
-            // cur + counter > 0
-            // counter > 0-cur
-            // counter >= 0-cur+1
-            // counter >= 1-cur
-            int idx =  lower_bound(ans.begin(), ans.begin()+i, 1-ans[i]) - ans.begin();
-            cnt += i - idx;
+        nums1[i] + nums1[j] > nums2[i] + nums2[j].
+        nums1[i] - nums2[i] > - (nums1[j]-nums2[j])
+        
+        arr[i] + arr[j] > 0
+        a + b > 0
+        b > -a
+        */
+        
+        for (int i=0; i<nums1.size(); ++i) {
+            nums1[i] -= nums2[i];
         }
-        return cnt;
+        sort(nums1.begin(), nums1.end());
+
+        int target, idx;
+        long long ans = 0;
+        for (int i=0; i<nums1.size(); ++i) {
+            target = (-1) * nums1[i];
+            idx = upper_bound(nums1.begin(), nums1.begin()+i, target)-nums1.begin();
+            ans += (i-idx);
+        }
+        
+        return ans;
     }
 };
