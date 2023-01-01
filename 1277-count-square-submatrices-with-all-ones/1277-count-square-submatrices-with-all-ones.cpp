d0305244f29d1429987ec1d8140e7e2cfbbbceef
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-02 03:28:47 +0900

    Time: 520 ms (9.54%), Space: 29 MB (5.88%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -8,14 +8,6 @@ public:
     [1,1,0]
     
     */
-    void print(vector<vector<int>>& matrix) {
-        for (int i=0; i<matrix.size(); ++i) {
-            for (int j=0; j<matrix[0].size(); ++j) {
-                cout << matrix[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
     
     int countSquares(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
@@ -30,24 +22,16 @@ public:
             }
         }
         
-        // cout << ans << endl;
-        // print(prev);
-        
         for (int sz=2; sz<=maxi; ++sz) {
             for (int i=sz-1; i<m; ++i) {
                 for (int j=sz-1; j<n; ++j) {
                     cur[i][j] = false;
-                    if (i-1>=0 && j-1>=0 && prev[i-1][j] && prev[i][j-1] && prev[i-1][j-1] && prev[i][j]) {
+                    if (prev[i-1][j] && prev[i][j-1] && prev[i-1][j-1] && prev[i][j]) {
                         cur[i][j] = true;
                         ++ans;
                     }
                 }
             }
-            // cout<<sz << "prev" << endl;
-            // print(prev);
-            // cout<<sz << "cur" << endl;
-            // print(cur);
-            // cout << ans << endl;
             prev.swap(cur);
         }
         
