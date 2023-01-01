ca37ebed763abd53ba82d7a7e6f547c555b2388c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-02 06:13:39 +0900

    Time: 0 ms (100.00%), Space: 8.2 MB (88.54%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    string transform(string s) {
+    string transform(string &s) {
+        
+        string ans;
         int cur = 0;
         vector<int> seen(26, -1);
         for (auto &c : s) {
             if (seen[c-'a'] == -1) seen[c-'a'] = cur++;
-            c = (seen[c-'a']+'0');
+            ans += (seen[c-'a']+'0');
         }
-        return s;
+        return ans;
     }
     
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         
         vector<string> ans;
         string target = transform(pattern);
-        // cout << target << endl;
         for (auto & word : words) {
-            // cout << transform(word) << endl;
             if (transform(word) == target)
                 ans.push_back(word);
         }
