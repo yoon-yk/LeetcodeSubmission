120d2342a76b51299f0ec2c5620fb16550ed1100
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-01 23:26:54 +0900

    Time: 473 ms (57.96%), Space: 46.3 MB (58.46%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    
     int base = 300;
-    vector<int> dirX = {-2, -2, -1, -1, 1, 1, 2, 2};
-    vector<int> dirY = {1, -1, 2, -2, 2, -2, 1, -1};
-    
-    
     int minKnightMoves(int x, int y) {
-        
-        if (x==0 && y == 0) return 0;
-        
-        x+= base, y+= base;
-        vector<vector<bool>> visited(605, vector<bool>(605, false));
+        if (x == 0 && y == 0) return 0;
+        x += base, y += base;
+        vector<pair<int, int>> dir = {{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}};
         queue<pair<int, int>> Q;
+        vector<vector<bool>> visited(605, vector<bool>(605, false));
+        
+        int steps = 0;
         Q.push({base, base});
         visited[base][base] = true;
         
-        int newX, newY, size, steps = 0, visitedHash;
-        
-        while (!Q.empty()) {    
-            size = Q.size();
+        while (!Q.empty()) {
+            int size = Q.size();
             while (size--) {
-                auto cur = Q.front(); Q.pop();
-                int curX = cur.first, curY = cur.second;
+                auto [r, c] = Q.front(); Q.pop();
                 
-                for (int i=0; i<8; ++i) {
-                    newX = curX + dirX[i], newY = curY + dirY[i];
-                    if (newX == x && newY == y) return steps + 1;
-                    if (visited[newX][newY]) continue;
-                    visited[newX][newY] = true;
-                    Q.push({newX, newY});
+                for (auto &[nr, nc] : dir) {
+                    if (r+nr == x && c+nc == y) return steps+1;
+                    if (!visited[r+nr][c+nc] ) {
+                        Q.push({r+nr, c+nc});
+                        visited[r+nr][c+nc] = true;
+                    }
                 }
             }
-            ++ steps;
+            ++steps;
         }
         
         return steps;
