04bbfabbf09d7d61b37a69baa117106aeef85862
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-01 22:58:26 +0900

    Time: 68 ms (80.31%), Space: 19.1 MB (37.31%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
-        // l can go only lefter side, r can go only righter side
-        int p1 = 0, p2 = 0;
-        int n = start.size();
-        while (true) {
-            while (p1 < n && start[p1] == '_') ++p1;
-            while (p2 < n && target[p2] == '_') ++p2;
-            if (p1 == n || p2 == n) break;
-            if (start[p1] != target[p2]) return false;
-            if (start[p1] == 'L' && p1 < p2 || 
-                start[p1] == 'R' && p2 < p1) return false;
-            ++p1, ++p2;
+        int sid = 0, tid = 0;
+        while (sid < start.size() || tid < target.size()) {
+            while (sid < start.size() && start[sid] == '_') ++sid;
+            while (tid < target.size() && target[tid] == '_') ++tid;
+            if (sid == start.size() || tid == target.size()) break;
+            if (start[sid] != target[tid]) break;
+            if (start[sid] == 'L' && sid < tid) break;
+            if (start[sid] == 'R' && sid > tid) break;
+            ++sid, ++tid;
         }
-        return (p1 == n && p2 == n);
+        return (sid == start.size() && tid == target.size());
     }
 };
