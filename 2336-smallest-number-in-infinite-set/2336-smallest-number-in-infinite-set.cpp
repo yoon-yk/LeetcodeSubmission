2cd0a5c047c72011d0507e8c54f8f0b655997592
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-02 17:59:04 +0900

    Time: 75 ms (97.00%), Space: 38.4 MB (59.78%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,25 +1,20 @@
 class SmallestInfiniteSet {
 public:
-    // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     multiset<int> ms;
-    
-    SmallestInfiniteSet() {
-        ms.insert(1);
-    }
+
+    SmallestInfiniteSet() {ms.insert(1);}
     
     int popSmallest() {
         int ret = *ms.begin();
         if (ms.size() == 1) 
             ms.insert(*ms.begin()+1);
         ms.erase(ms.begin());
-
         return ret;
     }
-    
+
     void addBack(int num) {
-        if (num < *ms.rbegin() && ms.find(num) == ms.end()) {
+        if (num < *ms.rbegin() && ms.find(num) == ms.end())
             ms.insert(num);
-        }
     }
 };
 
