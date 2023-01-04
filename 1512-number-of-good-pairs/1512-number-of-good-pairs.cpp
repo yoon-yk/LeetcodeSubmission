6df00ed110b0be8169d4acea01de7d43b39fb04b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-04 13:58:11 +0900

    Time: 0 ms (100.00%), Space: 7.4 MB (15.75%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size(), ans = 0;
         vector<int> count(101, 0);
         for (int & i : nums) ++count[i];
-        for (int & i : count) {ans += ((i * (i-1))>>1);}
+        for (int & i : count) ans += ((i * (i-1))>>1);
         
         return ans;
     }
