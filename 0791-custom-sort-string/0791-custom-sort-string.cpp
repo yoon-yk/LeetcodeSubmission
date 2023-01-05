7e74d682c1331b862ac6c9aeae296c6f3e73b865
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-05 21:47:15 +0900

    Time: 7 ms (14.88%), Space: 6.2 MB (65.53%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -3,14 +3,11 @@ public:
     string customSortString(string order, string s) {
         vector<int> dict(26, INT_MAX);
         int oIdx = 0;
-        for (auto & c : order)
-            dict[c-'a'] = oIdx++;
-        
-        auto compare = [&dict](char a, char b){
+        for (auto & c : order) dict[c-'a'] = oIdx++;
+                
+        sort(s.begin(), s.end(), [&dict](char& a, char& b){
             return dict[a-'a'] < dict[b-'a'];
-        };
-        
-        sort(s.begin(), s.end(), compare);
+        });
         
         return s;
     }
