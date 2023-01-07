8a07fd17c3c50be4e16e722ab5f602f2567269f9
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-08 02:55:35 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (55.79%) - LeetHub

--- 1138-alphabet-board-path/1138-alphabet-board-path.cpp
+++ 1138-alphabet-board-path/1138-alphabet-board-path.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    int kRowSize=5;
-    vector<string> board = {"abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"};
-    
-    inline int getIndex(int r, int c) {
-        return r*kRowSize + c;
-    }
-    
     string alphabetBoardPath(string target) {
-        
+        int cur = 0, idx;
+        int kRowSize = 5;
         string ans;
-        int curTarget;
-        int r = 0, c = 0, idx = 0;
-        while (idx < target.size()) {
-            curTarget = target[idx]-'a';
-            while (getIndex(r, c) != curTarget) {
-                if (getIndex(r, c) < curTarget) {
-                    if (curTarget == 25 && c > 0) {
-                        --c, ans += 'L';
-                    } else {
-                        if (curTarget% kRowSize <= c) {
-                            ++r, ans += 'D'; // z?
-                        } else ++c, ans += 'R';                        
-                    }
-                } else {
-                    if ((curTarget/kRowSize) == r) {
-                        --c, ans += 'L'; // z?
-                    } else --r, ans+= 'U';
-                }
-            }
+        for (auto& c : target) {
+            idx = c-'a';
+            ans += string(max(0, (cur/kRowSize-idx/kRowSize)), 'U');
+            ans += string(max(0, (idx%kRowSize-cur%kRowSize)), 'R');
+            ans += string(max(0, (cur%kRowSize-idx%kRowSize)), 'L');
+            ans += string(max(0, (idx/kRowSize-cur/kRowSize)), 'D');
             ans += '!';
-            ++idx;
+            cur = idx;
         }
         return ans;
     }
-    
-    
 };
