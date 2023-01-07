f5e54e81e89257569e011a20c52481b33f1e7300
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-08 02:16:45 +0900

    Time: 389 ms (83.78%), Space: 122.8 MB (64.86%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
-    int getMin(vector<int>& arr, int idx, int col, vector<vector<int>>& dp) {
-        int left = idx, right = idx+1, dist = 0, ans;
-        int lMin = INT_MAX, rMin = INT_MAX;
-        while (left >= 0) {
-            if (dp[idx][col-1] == -1) break;
-            else if (dp[idx][col-1] != INT_MAX) {
-                lMin = dp[idx][col-1] + dist;
-                break;
-            } else if (arr[left] == col) {
-                lMin = dist;
-                break;
-            }
-            --left, ++dist;
-        }
-        dist = 1;
-        while (right < arr.size() && dist < lMin) {
-            if (dp[idx][col-1] == -1) break;
-            else if (dp[idx][col-1] != INT_MAX) {
-                rMin = dp[idx][col-1] + dist;
-                break;
-            } else if (arr[right] == col) {
-                rMin = dist;
-                break;
-            }
-            ++right, ++dist;
-        }
-        ans = min(lMin, rMin);
-        return dp[idx][col-1] = (ans == INT_MAX ? -1 : ans);
-    }
-    
     vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
-        int n = colors.size();
         vector<int> ans;
-        vector<vector<int>> dp(n, vector<int>(3, INT_MAX));
-        int idx, col;
-        for (auto & query : queries) {
-            idx = query[0], col = query[1];
-            ans.push_back(getMin(colors, idx, col, dp));
+        vector<vector<int>> arr(3);
+        for (int i=0; i<colors.size(); ++i) {
+            if (colors[i] == 1) arr[0].push_back(i);
+            else if (colors[i] == 2) arr[1].push_back(i);
+            else arr[2].push_back(i);
+        }
+
+        int idx, col, hi, lo;
+        for (auto & q : queries) {
+            idx = q[0], col = q[1];
+            if (arr[col-1].empty()) {ans.push_back(-1); continue;}
+            auto it = lower_bound(arr[col-1].begin(), arr[col-1].end(), idx);
+            if (it == arr[col-1].end()) ans.push_back(abs(*prev(it)-idx));
+            else if (it == arr[col-1].begin()) ans.push_back(abs(*it-idx));
+            else ans.push_back(min(abs(*it-idx), abs(*prev(it)-idx)));
         }
         
         return ans;
