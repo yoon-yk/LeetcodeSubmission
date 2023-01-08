f44dc4760abebce269bf6011de33b9753aea2a51
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-08 20:47:31 +0900

    Time: 367 ms (94.12%), Space: 124.2 MB (56.21%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
-        vector<int> ans;
-        vector<vector<int>> arr(3);
-        for (int i=0; i<colors.size(); ++i) {
-            if (colors[i] == 1) arr[0].push_back(i);
-            else if (colors[i] == 2) arr[1].push_back(i);
-            else arr[2].push_back(i);
-        }
-
-        int idx, col, hi, lo;
-        for (auto & q : queries) {
-            idx = q[0], col = q[1];
-            if (arr[col-1].empty()) {ans.push_back(-1); continue;}
-            auto it = lower_bound(arr[col-1].begin(), arr[col-1].end(), idx);
-            if (it == arr[col-1].end()) ans.push_back(abs(*prev(it)-idx));
-            else if (it == arr[col-1].begin()) ans.push_back(abs(*it-idx));
-            else ans.push_back(min(abs(*it-idx), abs(*prev(it)-idx)));
+        int n = colors.size();
+        vector<int> rightMost = {0, 0, 0}, leftMost = {n-1, n-1, n-1};
+        vector<vector<int>> dist(3, vector<int>(n, -1));
+        
+        for (int i=0; i<n; ++i) {
+            int color = colors[i]-1;
+            for (int j=rightMost[color]; j<i+1; ++j) 
+                dist[color][j] = i-j;
+            rightMost[color] = i+1;
         }
         
-        return ans;
+        for (int i=n-1; i>-1; --i) {
+            int color = colors[i]-1;
+            for (int j=leftMost[color]; j>i-1; --j)
+                if (dist[color][j] == -1 || dist[color][j] > j-i)
+                    dist[color][j] = j-i;
+            leftMost[color] = i-1;
+        }
+        
+        vector<int> queryResults;
+        for (auto &q : queries) 
+            queryResults.push_back(dist[q[1]-1][q[0]]);
+        
+        return queryResults;
+        
     }
 };
