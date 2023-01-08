594b6981259f4b17e13332d66adc961f0a681912
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-08 23:16:17 +0900

    Time: 48 ms (90.54%), Space: 16 MB (56.06%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
+    vector<int> dir = {-1, 0, 1, 0, -1};
+    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int r = matrix.size(), c = matrix[0].size();
-        vector<vector<bool>> visited(r, vector<bool>(c, false));
-        vector<vector<int>> dp(r, vector<int>(c, -1));
+        
+        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), -1));
         
         int ans = 0;
-        for (int i=0; i<r; ++i) {
-            for (int j=0; j<c; ++j) {
-                visited[i][j] = true;
-                ans = max(ans, dfs(i, j, matrix, visited, dp));
-                visited[i][j] = false;
-            }
-        }
+        for (int i=0; i<matrix.size(); ++i) 
+            for (int j=0; j<matrix[0].size(); ++j) 
+                ans = max(ans, dfs(i, j, matrix, dp));
         
         return ans;
     }
     
-    vector<int> dir = {-1, 0, 1, 0, -1};
-    
-    bool validRange(int i, int j, vector<vector<int>>& matrix, int curMax) {
-        if (i< 0 || i >= matrix.size() || j < 0 || j >= matrix[0].size() || matrix[i][j] <= curMax)
-            return false;
+    bool isValid(int r, int c, vector<vector<int>>& matrix) {
+        if (r < 0 || c < 0 || r>=matrix.size() || c>=matrix[0].size()) return false;
         return true;
     }
     
-    int dfs(int i, int j, vector<vector<int>>& matrix, vector<vector<bool>>& visited, vector<vector<int>>& dp) {
+    int dfs(int r, int c, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
         
-        if (dp[i][j] != -1) return dp[i][j];
+        if (dp[r][c] != -1) return dp[r][c];
         
-        int ans = 1;
-        int curVal = matrix[i][j];
-        int nI, nJ;
+        int ans = 1, nr, nc;
         
         for (int d=0; d<4; ++d) {
-            nI = i+dir[d], nJ = j+dir[d+1];
-            if (!validRange(nI, nJ, matrix, curVal) || visited[nI][nJ]) continue;
-            visited[nI][nJ] = true;
-            ans = max(ans, dfs(nI, nJ, matrix, visited, dp)+1);
-            visited[nI][nJ] = false;
+            nr = r+dir[d], nc = c+dir[d+1];
+            if (isValid(nr, nc, matrix) && matrix[nr][nc] > matrix[r][c]) 
+                ans = max(ans, dfs(nr, nc, matrix, dp)+1);
         }
         
-        return dp[i][j] = ans;
+        return dp[r][c] = ans;
     }
 };
