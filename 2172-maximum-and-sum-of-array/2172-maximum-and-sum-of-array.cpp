af7d6fc12fae2ab1783d971ee5460c4360582afc
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-10 16:25:42 +0900

    Time: 43 ms (87.31%), Space: 70.9 MB (14.25%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int nSlots;
     int maximumANDSum(vector<int>& nums, int numSlots) {
-        // sort(nums.begin(), nums.end(), greater<int>());
+        sort(nums.begin(), nums.end(), greater<int>());
         int singleSlots = 0, doubleSlots = 0;
         int maxSize = pow(2, numSlots+1);
         vector<vector<int>> dp(maxSize, vector<int>(maxSize, -1));
@@ -18,8 +18,8 @@ public:
         }
 
         int ans = 0;
-        // for (int i=slots.size()-1; i>=0; --i) {
-        for (int i=0; i<nSlots; ++i) {  
+        for (int i=nSlots-1; i>=0; --i) {  
+        // for (int i=0; i<nSlots; ++i) {  
 
             if (doubleSlots & (1 << i)) continue;
             if (singleSlots & (1 << i)) { // has one elem
