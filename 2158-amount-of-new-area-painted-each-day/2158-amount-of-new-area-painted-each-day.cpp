1ef17f58383d0100805bb0eefc731caf4fccf153
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-10 15:00:15 +0900

    Time: 409 ms (98.70%), Space: 149.1 MB (49.87%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
+    void print(vector<int>& arr) {
+    for (int i=0; i<10; ++i) cout << arr[i] << " ";
+        cout << endl;
+    }
     vector<int> amountPainted(vector<vector<int>>& paint) {
         vector<int> arr(50001, -1), ans;
-        int sum;
+        int sum, maxEnd;
         for (auto & p : paint) {
-            sum = 0;
+            sum = 0, maxEnd = p[1]-1;
             for (int i=p[0]; i<p[1]; ++i) {
                 if (arr[i] == -1) {
                     ++ sum;
                     arr[i] = p[1]-1;
                 } else {
+                    maxEnd = max(arr[i], maxEnd);
                     i = arr[i];
                 } 
             }
+            arr[p[0]] = maxEnd;
+            // print(arr);
             ans.push_back(sum);
         }
+        
+
+        
         return ans;
     }
 };
