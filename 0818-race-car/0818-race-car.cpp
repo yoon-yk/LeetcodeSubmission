6ef03a1412eea4642f15fd18760e1a4b8434de43
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-15 17:01:37 +0900

    Time: 32 ms (48.90%), Space: 14.9 MB (38.76%) - LeetHub

--- 0818-race-car/0818-race-car.cpp
+++ 0818-race-car/0818-race-car.cpp
@@ -9,47 +9,24 @@ public:
 class Solution {
 public:
     int racecar(int target) {
-        /*
-        A
-        pos += speed;
-        speed *= 2;
-        
-        R
-        speed = speed > 0 ? speed-1 : 1;
-        
-        
-        0 --> 3
-        pos += 1; // pos = 1, speed = 2;
-        pos += 2; // pos = 3, speed = 4;
-        
-        
-        0 --> 6
-        pos += 1; // pos = 1, speed = 2;
-        pos += 2; // pos = 3, speed = 4;
-        pos += 4; // pos = 7, speed = 8;
-        --speed; // speed = 7;
-        --speed; // speed = 6;
-        */
         queue<info> Q;
         Q.push(info(0, 1, 0));
         unordered_map<int, unordered_set<int>> visited;
-        visited[0].insert(1);
         
         long ap, as, rp, rs;
         while (!Q.empty()) {
             auto cur = Q.front(); Q.pop();
             if (cur.pos == target) return cur.len;
+            if (visited[cur.pos].count(cur.speed)) continue;
+            visited[cur.pos].insert(cur.speed);
             
             ap = cur.pos+cur.speed, as = cur.speed*2;
-            if (!visited[ap].count(as) && 0 < ap ) {
-                Q.push(info(ap, as, cur.len+1));
-                visited[ap].insert(as);
-            }
+            Q.push(info(ap, as, cur.len+1));
             
-            rp = cur.pos, rs = cur.speed > 0 ? -1 : 1;
-            if (!visited[rp].count(rs) && 0 < rp) {
-                Q.push(info(rp, rs, cur.len+1));
-                visited[rp].insert(rs);
+            if ((cur.pos+cur.speed > target && cur.speed > 0) ||
+                 (cur.pos+cur.speed < target && cur.speed < 0)) {
+                rp = cur.pos, rs = cur.speed > 0 ? -1 : 1;
+                Q.push(info(rp, rs, cur.len+1));   
             }
                 
         }
