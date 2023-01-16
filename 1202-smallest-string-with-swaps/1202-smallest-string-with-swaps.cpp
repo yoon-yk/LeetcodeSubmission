fe7fecb08f377ce34dd1d80184ae5c696690f2b1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-17 02:39:01 +0900

    Time: 236 ms (63.07%), Space: 59.6 MB (37.45%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> parent;
+
+    int findd(int v) {
+        if (parent[v] == v) return v;
+        return parent[v] = findd(parent[v]);
+    }
+    
+    void unionn(int v1, int v2) {
+        int p1 = findd(v1), p2 = findd(v2);
+        if (p1 == p2) return;
+        parent[p1] = p2;
+    }
+    
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        int n = s.size();
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        
+        for (auto & p : pairs) {
+            unionn(p[0], p[1]);
+        }
+        
+        unordered_map<int, set<int>> mp;
+        unordered_map<int, priority_queue<char, vector<char>, greater<char>>> cmp;
+        for (int i=0, grp; i<n; ++i) {
+            grp = findd(i);
+            mp[grp].insert(i);
+            cmp[grp].push(s[i]);
+        }
+        
+        for (auto &[idx, set] : mp) {
+            auto pq = cmp[idx];
+            for (auto & id : set) {
+                s[id] = pq.top(); pq.pop();
+            }
+        }
+        
+        return s;
+    }
+};
