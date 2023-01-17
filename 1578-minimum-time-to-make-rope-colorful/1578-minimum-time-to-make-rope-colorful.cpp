e0ad7115fafbcec0ffdc08e8cd6fd3d75b4b0979
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-17 18:31:16 +0900

    Time: 169 ms (70.76%), Space: 99.7 MB (11.06%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& time) {
-        // totalTime: total time needed to make rope colorful;
-        // currMaxTime: maximum time of a balloon needed in this group.
-        int totalTime = 0, currMaxTime = 0;
+    int minCost(string colors, vector<int>& neededTime) {
+        int n = colors.size(), ans = 0;
+        vector<int> dp(n, 0);
+        int sum = neededTime[0];
+        dp[0] = neededTime[0];
         
-        // For each balloon in the array:
-        for (int i = 0; i < colors.size(); ++i) {
-            // If this balloon is the first balloon of a new group
-            // set the currMaxTime as 0.
-            if (i > 0 && colors[i] != colors[i - 1]) {
-                currMaxTime = 0;
+        /*
+        a a a a b b b
+        1 2 3 4 2 3 4
+        1 2 3 4 2 3 4
+        
+        */
+        for (int i=1; i<=n; ++i) {
+            if (i > 0 && colors[i] == colors[i-1]) {
+                dp[i] = max(dp[i-1], neededTime[i]);
+                sum += neededTime[i];
+            } else {
+                if (i > 0) ans += (sum - dp[i-1]);
+                if (i == n) break;
+                dp[i] = neededTime[i];
+                sum = neededTime[i];
             }
-            
-            // Increment totalTime by the smaller one.
-            // Update currMaxTime as the larger one.
-            totalTime += min(currMaxTime, time[i]);
-            currMaxTime = max(currMaxTime, time[i]);
         }
         
-        // Return totalTime as the minimum removal time.
-        return totalTime;
+        
+        return ans;
     }
 };
