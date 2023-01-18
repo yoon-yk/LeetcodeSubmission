9c1f911e11e8f99e6f4aec3631858f34410aadd4
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-18 16:54:17 +0900

    Time: 248 ms (87.05%), Space: 97.2 MB (19.28%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,37 +1,31 @@
 class RandomizedSet {
 public:
-    vector<int> arr;
-    unordered_map<int, int> map;
-    
-    RandomizedSet() {
-        
-    }
+    vector<int> v;
+    unordered_map<int, int> st; // val, loc
+    RandomizedSet() {}
     
     bool insert(int val) {
-        if (map.count(val)) return false;
-        arr.push_back(val);
-        map[val] = arr.size()-1;
+        if (st.count(val)) return false;
+        v.push_back(val);
+        st[val] = v.size()-1;
         return true;
     }
     
     bool remove(int val) {
-        if (!map.count(val)) return false;
-        int origIdx = map[val];
-        map[arr.back()] = origIdx;
-        swap(arr[map[val]], arr.back());
-        arr.pop_back();
-        map.erase(val);
+        if (!st.count(val)) return false;
+        
+        int valIdx = st[val]; 
+        int lastV = v.back();
+        st[lastV] = valIdx;
+        v[valIdx] = lastV;
+        v.pop_back();
+        st.erase(val);
         return true;
     }
     
-    /*
-     0  1  2  3
-    01 10 20 30
-    
-    */
     int getRandom() {
-        int randIdx = random() % (arr.size());
-        return arr[randIdx];
+        int ri = rand() % v.size(); // [0, n-1]
+        return v[ri];
     }
 };
 
