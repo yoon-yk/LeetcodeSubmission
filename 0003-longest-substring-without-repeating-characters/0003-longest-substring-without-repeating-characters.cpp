0c9cf2404aa5e969aaff33fb559ee2d3ea259afb
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-18 17:17:36 +0900

    Time: 8 ms (91.81%), Space: 8.3 MB (62.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,11 +3,9 @@ public:
     int lengthOfLongestSubstring(string s) {
         vector<int> m(267, -1);
         int curCh, start = 0, ans = 0;
-        
         for (int end=0; end<s.size(); ++end) {
             curCh = s[end];
-            if (m[curCh] >= start)
-                start = m[curCh]+1;
+            if (m[curCh] >= start) start = m[curCh]+1;
             m[curCh] = end;
             ans = max(ans, end-start+1);
         }
