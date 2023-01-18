d716f49b8be93fe12111a1457c34593a00493858
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-18 14:44:39 +0900

    Time: 34 ms (94.71%), Space: 28.3 MB (12.53%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int n = nums.size(), ans = n+1, sum = 0, tt, idx;
-        vector<int> psum;
-        psum.push_back(0);
-        /*
-           0, 1, 2, 3,  4,  5
-        0, 2, 5, 6, 8, 12, 15
-        
-        sum - tt >= target;
-        sum - target >= tt;
-        */
-        
-        for (int i=0; i<n; ++i) {
-            if (nums[i] >= target) return 1;
-            sum += nums[i];
-            tt = sum-target;
-            psum.push_back(sum);
+        int n = nums.size();
+        int start = 0, ans = n+1, sum = 0;
+        for (int end=0; end<n; ++end){
+            sum += nums[end];
             
-            auto it = upper_bound(psum.begin(), psum.end(), tt);
-            if (it == psum.begin() || *prev(it) > tt) continue;
-            idx = it-psum.begin()-1;
-            if (ans > i-idx+1) ans = i-idx+1;
-            
+            while (sum >= target) {
+                sum -= nums[start];
+                ans = min(ans, end-start+1);
+                ++start;
+            }
         }
-        return ans == n+1 ? 0 : ans;
+        return (ans == n+1) ? 0 : ans;
     }
 };
