b6ec1812fa6653704743968047da99143bfe5e1b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-19 20:41:20 +0900

    Time: 40 ms (92.24%), Space: 31 MB (97.97%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        auto hi = lower_bound(arr.begin(), arr.end(), x); // equal or bigger thaan x
-        
-        auto lo = upper_bound(arr.begin(), arr.end(), x);
-        if (lo != arr.begin()) --lo;
+        auto lo = prev(upper_bound(arr.begin(), arr.end(), x)); // less than x
+        auto hi = lower_bound(arr.begin(), arr.end(), x); // equal or bigger than x
                 
         while (hi-arr.begin() - (lo-arr.begin()) <= k) {
             if (lo != prev(arr.begin()) && hi != arr.end()) {
