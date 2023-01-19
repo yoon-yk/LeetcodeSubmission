37afb7e94fc34cc880e2b02e2c7c05d53232dc8c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-20 02:42:24 +0900

    Time: 7 ms (81.00%), Space: 7.3 MB (74.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -5,8 +5,7 @@ public:
         
         int cnt = 0;
         vector<int> freq1(26, 0), freq2(26, 0);
-        for (int i=0; i<s1.size(); ++i) ++freq1[s1[i]-'a'];
-        for (int i=0; i<s1.size(); ++i) ++freq2[s2[i]-'a'];
+        for (int i=0; i<s1.size(); ++i) ++freq1[s1[i]-'a'], ++freq2[s2[i]-'a'];
         for (int i=0; i<26; ++i) {
             if (freq1[i] == freq2[i]) ++cnt;
         }
