c33782f56a3da8c5d8094eba0fc7d85219588da3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-20 21:09:09 +0900

    Time: 188 ms (46.03%), Space: 103.2 MB (9.52%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -3,13 +3,12 @@ public:
     long long getDescentPeriods(vector<int>& prices) {
         int n = prices.size();
         long long ans = 1;
-        int cur, prev = 1;
+        int len = 1;
         for (int i=1; i<n; ++i) {
             if (i>0 && prices[i-1]-1 == prices[i]) {
-                cur = prev+1;
-            } else cur = 1;
-            ans += cur;
-            swap(prev, cur);
+                ++len;
+            } else len = 1;
+            ans += len;
         }
         return ans;
     }
