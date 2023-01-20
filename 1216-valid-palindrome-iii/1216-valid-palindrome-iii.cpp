918509c70db642ac28b083ff36e6178120655324
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-20 19:00:24 +0900

    Time: 19 ms (93.25%), Space: 6.4 MB (91.16%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool isValidPalindrome(string s, int k) {
-        int n = s.size(), ans = 1, end;
-        vector<vector<int>> dp(n, vector<int>(n));
-        for (int sz=1; sz<=n; ++sz){
-            for (int start=0; start+sz-1<n; ++start) {
-                end = start+sz-1;
-                if (sz == 1) dp[start][end] = 1;
-                else if (sz == 2) dp[start][end] = (s[start] == s[end]) + 1;
-                else {
-                    if (s[start] == s[end]) {
-                        dp[start][end] = dp[start+1][end-1] + 2;
-                    } else {
-                        dp[start][end] = max(dp[start+1][end], dp[start][end-1]);
-                    }
-                }
-                ans = max(ans, dp[start][end]);
-
+        int n = s.size();
+        vector<int> memo(n, 0);
+        int temp, prev;
+        
+        for (int i=n-2; i>=0; --i) {
+            prev = 0; // memo[i+1][j-1]
+            for (int j = i+1; j<n; ++j) {
+                temp = memo[j]; // memo[i+1][j]
+                
+                if (s[i] == s[j]) memo[j] = prev;
+                else memo[j] = 1 + min(memo[j], memo[j-1]); // memo[i+1][j] vs memo[i][j-1]
+                
+                prev = temp;
             }
         }
-        return ans >= n-k;
+        
+        return memo[n-1] <= k;
     }
 };
