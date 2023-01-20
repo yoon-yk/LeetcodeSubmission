9688c0b0e25e6be1938f63a2a4819631cbf3fb6a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-20 21:22:17 +0900

    Time: 60 ms (88.61%), Space: 55.4 MB (68.77%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,11 +4,8 @@ public:
         int zcnt = 0, l=0, r;
         for (r=0; r<nums.size(); ++r) {
             zcnt += !nums[r];
-            
-            if (zcnt > k) {
-                zcnt -= !nums[l];
-                ++l;
-            }
+            if (zcnt > k) 
+                zcnt -= !nums[l], ++l;
         }
         return (r-l);
     }
