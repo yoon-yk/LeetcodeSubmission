a9b7128992e8743a25d218c7bb4d11b222174fef
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-20 17:14:36 +0900

    Time: 303 ms (88.65%), Space: 11.6 MB (89.13%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         
-        int n = s.size();
+        int n = s.size(), ans = 0;
         vector<vector<int>> seen(26, vector<int>(26, 0));
         vector<int> left(26, 0), right(26, 0);
         for (int i=n-1; i>=0; --i) 
@@ -12,18 +12,16 @@ public:
             --right[s[i]-'a'];
             for (int k=0; k<26; ++k) {
                 if (left[k] > 0 && right[k] > 0)  {
-                    seen[s[i]-'a'][k] = true;
+                    if (!seen[s[i]-'a'][k]) {
+                        ++ans;
+                        seen[s[i]-'a'][k] = true;
+                    }
                 }
             }
             ++left[s[i]-'a'];
         }
         
-        int ans = 0;
-        for (int i=0; i<26; ++i) {
-            for (int j=0; j<26; ++j) {
-                ans += seen[i][j];
-            }
-        }
+
         return ans;
     }
 };
