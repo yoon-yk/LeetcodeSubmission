0919ae42ab059aac8d4a6f2448fecf765fc001b2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-25 18:24:34 +0900

    Time: 28 ms (50.08%), Space: 14.1 MB (30.86%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    int unvisited = 0, visiting = 1, visited = 2;
     bool validTree(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adjList(n);
-        vector<bool> v(n, unvisited);
+        vector<bool> v(n, false);
         for (auto & e : edges) {
             adjList[e[0]].push_back(e[1]);
             adjList[e[1]].push_back(e[0]);
@@ -14,14 +13,13 @@ public:
     
     int dfs(int cur, int par, vector<vector<int>>& adjList, vector<bool>& v) {
         int nVisited = 1;
-        v[cur] = visiting;
+        v[cur] = true;
         
         for (auto & nei : adjList[cur]) {
             if (nei == par) continue;
-            if (v[nei] != unvisited) return -1;
+            if (v[nei]) return -1;
             nVisited += dfs(nei, cur, adjList, v);
         }
-        v[cur] = visited;
         
         return nVisited;
     }
