e32373024ee38412a025480ed7c9d8bc21d90b39
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-25 16:49:21 +0900

    Time: 26 ms (54.39%), Space: 11.8 MB (96.60%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     vector<int> parent;
-    vector<int> size;
     
-    int find(int e) {
-        if (parent[e] == e)
-            return e;
-        return parent[e] = find(parent[e]);
+    int findd (int v) {
+        if (parent[v] == v) return v;
+        return parent[v] = findd(parent[v]);
+    }
+    
+    bool isConnected(int v1, int v2) {
+        int p1 = findd(v1), p2 = findd(v2);
+        if (p1 == p2) return true;
+        parent[p1] = p2;
+        return false;
     }
     
     bool validTree(int n, vector<vector<int>>& edges) {
-        // TREE 
-        // NO Cycle -- # edges == n-1
-        if (edges.size() != n-1)
-            return false;
-        // ONLY one component
+        // valid tree - one component, no cycle
+        
+        if (edges.size() != n-1) return false;
+        
         parent.resize(n);
-        size.resize(n, 1);
-        for (int i=0; i<n; i++) parent[i] = i;
-        
-        int p1, p2;
-        for (auto& e : edges) {
-            p1 = find(e[0]), p2 = find(e[1]);
-            if (p1 == p2) 
-                return false;
-        
-            if (size[p1] > size[p2]) {
-                parent[p2] = p1;
-                size[p1] += size[p2];
-            } else {
-                parent[p1] = p2;
-                size[p2] += size[p1];
+        iota(parent.begin(), parent.end(), 0);
+        int nComp = n;
+        for (auto & e : edges) {
+            if (!isConnected(e[0], e[1])) {
+                --nComp;
             }
-
         }
         
-        return true;
+        return (nComp == 1);
     }
 };
