a8eb0d0b79594414933afa20809ee983962dc509
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-26 15:07:18 +0900

    Time: 41 ms (47.89%), Space: 7.3 MB (82.98%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1));
-        dp[0][0] = 1;
+        vector<int> dp(amount+1);
+        dp[0] = 1;
         for (int c=1; c<=coins.size(); ++c) {
             for (int a=0; a<=amount; ++a) {
                 auto & coin = coins[c-1];
-                dp[c][a] = dp[c-1][a];
                 if (a-coin >= 0) {
-                    dp[c][a] += dp[c][a-coin];
+                    dp[a] += dp[a-coin];
                 } 
             }
         }
     
-        return dp[coins.size()][amount];
+        return dp[amount];
     }
 };
