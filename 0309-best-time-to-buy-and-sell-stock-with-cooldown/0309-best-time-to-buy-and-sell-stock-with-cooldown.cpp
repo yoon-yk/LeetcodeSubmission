9bef3d600656b25dfd00bf4f5008762a37c90536
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-26 14:29:21 +0900

    Time: 0 ms (100.00%), Space: 11.1 MB (98.38%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        int hold = -prices[0], nothold = 0, pprevNotHold = 0, prevNotHold = 0;
+        int hold = -prices[0], nothold = 0, pprevNotHold, prevNotHold = 0;
         for (int i=1; i<prices.size(); ++i) {
             pprevNotHold = prevNotHold, prevNotHold = nothold;
             nothold = max(nothold, hold + prices[i]);
