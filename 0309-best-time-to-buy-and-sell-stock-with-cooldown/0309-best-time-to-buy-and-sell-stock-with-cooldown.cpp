846f28da6a63d34e345d652a343b5bf059c4393a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-26 14:28:33 +0900

    Time: 13 ms (21.18%), Space: 11.1 MB (98.38%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size()+1, vector<int>(2)); // hold, not hold 
-        dp[1][1] = -prices[0]; // buy on the first day
-        dp[1][0] = 0; // not buy on the first day
         
-        int sell, buy, hold;
-        for (int i=2; i<=prices.size(); ++i) {
-            // sell
-            dp[i][false] = dp[i-1][true] + prices[i-1];
-            
-            // buy
-            if (i>1) dp[i][true] = dp[i-2][false] - prices[i-1];
-            
-            // hold
-            dp[i][false] = max(dp[i-1][false], dp[i][false]);
-            dp[i][true] = max(dp[i-1][true], dp[i][true]);
+        int hold = -prices[0], nothold = 0, pprevNotHold = 0, prevNotHold = 0;
+        for (int i=1; i<prices.size(); ++i) {
+            pprevNotHold = prevNotHold, prevNotHold = nothold;
+            nothold = max(nothold, hold + prices[i]);
+            hold = max(hold, pprevNotHold - prices[i]);
         }
-        
-        
-//         for (int i=0; i<=prices.size(); ++i) {
-//             cout << dp[i][true] << " " << dp[i][false] << endl;
-//         }
-        
-        return max(dp.back()[true], dp.back()[false]);
+
+        return max(hold, nothold);
     }
 };
