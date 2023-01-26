3ccec3e1e23985639c6c031f02157967c07fd238
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-26 13:45:56 +0900

    Time: 9 ms (36.38%), Space: 12.7 MB (7.33%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size(), ret;
-        
-        int sold = INT_MIN, held = INT_MIN, reset = 0;
-        for (int price : prices) {
-            int preSold = sold;
-            
-            sold = held + price;
-            held = max(held, reset-price); // 구매하거나..?
-            reset = max(reset, preSold); // 
-        }
-        return max(sold, reset);
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1)); // hold, not hold 
+        return maxProfits(0, false, prices, dp);
     }
     
+    int maxProfits(int day, bool hold, vector<int>& prices, vector<vector<int>>& dp) {
+        
+        if (day >= prices.size()) return 0;
+
+        if (dp[day][hold] != -1) return dp[day][hold];
+
+        // sell 
+        int sell = (hold) ? maxProfits(day+2, false, prices, dp) + prices[day] : INT_MIN;
+        
+        // buy
+        int buy = (!hold) ? maxProfits(day+1, true, prices, dp) - prices[day] : INT_MIN;
+        
+        int skip = maxProfits(day+1, hold, prices, dp);
+        
+        return dp[day][hold] = max({sell, buy, skip});
+    }
 };
