75f173f4994e5b47a3e8e4550e2f6aa1e0c87de2
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-26 15:05:13 +0900

    Time: 180 ms (27.35%), Space: 14.6 MB (60.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, amount+1));
-        dp[0][0] = 0;
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
         for (int c=1; c<=coins.size(); ++c) {
             for (int a=0; a<=amount; ++a) {
-                dp[c][a] = dp[c-1][a];
                 auto & coin = coins[c-1];
                 if (a-coin >= 0) {
-                    dp[c][a] = min(dp[c][a], dp[c][a-coin] + 1);
+                    dp[a] = min(dp[a], dp[a-coin] + 1);
                 } 
             }
         }
     
-        return dp[coins.size()][amount] > amount ? -1 : dp[coins.size()][amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
