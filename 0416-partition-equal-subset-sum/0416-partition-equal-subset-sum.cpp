8082e0cd626e5d38c59363b4cb802d7399bc2575
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 19:23:39 +0900

    Time: 682 ms (15.29%), Space: 95.7 MB (19.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
+    int mod = 1e9 + 7;
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % 2) return false;
-        sum = sum >> 1;
-        if (*max_element(nums.begin(), nums.end()) > sum) return false;
+        if (sum & 1) return false;
+        int target = sum >> 1;
         
-        int n = nums.size();
-        
-        vector<int> cur(sum+1, 0), prev(sum+1, 0);
-        prev[0] = cur[0] = 1;
-        
-        for (int i=0; i<n; ++i) {
-            for (int s=0; s<=sum; ++s)
-                if (s-nums[i] >= 0) cur[s] |= prev[s-nums[i]];
-            prev = cur;
+        return subarraySumEqualKExist(nums, target);
+    }
+    
+    bool subarraySumEqualKExist(vector<int>& nums, int target) {
+        vector<vector<int>> dp(nums.size()+1, 
+                               vector<int>(target+1, 0));
+        dp[0][0] = 1;
+        for (int i=1; i<=nums.size(); ++i) {
+            if (nums[i-1] > target) continue;
+            for (int s=0; s<=target; ++s) {
+                dp[i][s] = dp[i-1][s];
+                if (s-nums[i-1] >= 0) {
+                    dp[i][s] = (dp[i-1][s-nums[i-1]] + dp[i][s]) % mod;
+                }
+            }
         }
         
-        return cur[sum];
+        
+        return dp[nums.size()][target];
     }
 };
