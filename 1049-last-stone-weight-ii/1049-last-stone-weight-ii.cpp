8ecd03607767e72eb28405fed443a2b570cc14bf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-28 00:48:35 +0900

    Time: 3 ms (96.01%), Space: 8.4 MB (68.58%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -10,22 +10,21 @@ public:
         */
         int sum = accumulate(stones.begin(), stones.end(), 0);
         int target = sum>>1;
-        vector<vector<int>> dp(stones.size()+1, vector<int>(target + 1));
-        dp[0][0] = 1;
-        for (int i=1; i<=stones.size(); ++i) {
-            for (int s=0; s<=target; ++s) {
-                dp[i][s] = dp[i-1][s];
-                if (s-stones[i-1] >= 0) {
-                    dp[i][s] += dp[i-1][s-stones[i-1]];
+        vector<int> prev(target + 1, 0), cur(target + 1, 0);
+        prev[0] = 1, cur[0] = 1;
+        for (int i=0; i<stones.size(); ++i) {
+            for (int s=target; s>=0; --s) {
+                if (s-stones[i] >= 0) {
+                    cur[s] += prev[s-stones[i]];
                 } 
+                
+                if (i==stones.size()-1) {
+                    if (cur[s] > 0) return (sum-s*2);
+                }            
             }
+            prev = cur;
         }
 
-        int n = stones.size();
-        for (int s=target; s>=0; --s) {
-            if (dp[n][s] > 0) return (sum-s*2);
-        }
-        
         return -1;
     }
 };
