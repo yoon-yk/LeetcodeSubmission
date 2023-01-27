dd6b589d449230482419e273cfa09bedf772174e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 19:53:34 +0900

    Time: 7 ms (75.26%), Space: 13.7 MB (57.02%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        
         int n = cost.size();
-        int prevFirst = 0, prevSec = 0, cur = 0;
-        for (int i=2; i<=n; ++i) {
-            cur = min(cost[i-2] + prevSec, cost[i-1] + prevFirst);
-            prevSec = prevFirst;
-            prevFirst = cur;
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 0;
+        
+        /*
+        - base case
+        dp[0] = 0, dp[1] = 0
+        
+        - recurrence relation 
+        dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
+        */
+        
+        for (int i=2; i<n+1; ++i) {
+            dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1]);
         }
-        return cur;
+        
+        return dp.back();
     }
 };
