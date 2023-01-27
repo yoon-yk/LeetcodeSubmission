4f52d4a0019ca25435b319bcd9cae341a3f44982
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 18:56:00 +0900

    Time: 197 ms (44.25%), Space: 37.7 MB (23.29%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-      
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        vector<int> prev((sum << 3) + 1, 0), curr((sum << 3) + 1, 0);
-        /*
-           0  1  2  3  4  5  6  7  8  9 10
-          -5 -4 -3 -2 -1  0  1  2  3  4  5
-        0        0  0  0  1  0  0  0
-        1        0  0  1  1  1  0  0
-        2        0  1  1  3  1  1  0
-        3        1  2  5  5  5  2  1 
-        4        3  
-  
         
-        
-        */
-        int base = sum * 2;
-        prev[base] = 1;
-        for (int i=0; i<n; ++i) {
-            for (int s = base - sum; s <= base + sum; ++s) {
-                curr[s] = 0;
-                curr[s] += prev[s - nums[i]];
-                curr[s] += prev[s + nums[i]];
-            }
-            prev = curr;
+        unordered_map<int, int> prev, now;
+        prev[0] = 1;
+        for (int i=0; i<nums.size(); ++i) {
+            for (auto & [v, f] : prev) 
+                now[v+nums[i]] += f, now[v-nums[i]] += f;
+            prev = now;
+            now.clear();
         }
-        
-        return curr[base + target];
+        return prev[target];
     }
 };
