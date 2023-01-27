5602fd155383b04c87a09206b4928bb5e3233289
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 17:46:41 +0900

    Time: 38 ms (24.92%), Space: 8.9 MB (61.58%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        if (word1.size() == 0 || word2.size() == 0) 
-            return max(word1.size(), word2.size());
+        vector<vector<int>> dp(word1.size(), 
+                               vector<int> (word2.size(), -1));
+        return backtrack(0, 0, word1, word2, dp);
+    }
     
-        vector<vector<int>> dp
-            (word1.size()+1, vector<int>(word2.size()+1));
+    int backtrack(int i1, int i2, string &word1, string &word2, 
+                  vector<vector<int>>& dp) {
+                
+        // base case
+        if (i1 == word1.size() || i2 == word2.size()) 
+            return max(word1.size()-i1, word2.size()-i2);
         
-        for (int i1 = word1.size(); i1 >= 0; --i1) {
-            for (int i2=word2.size(); i2 >= 0; --i2) {
-                if (i1 == word1.size() || i2 == word2.size()) {
-                    dp[i1][i2] = max(word1.size()-i1, 
-                                     word2.size()-i2); // 이만큼 지워줘야 함 
-                } else {
-                    if (word1[i1] == word2[i2]) 
-                        dp[i1][i2] = dp[i1+1][i2+1];
-                    else {
-                      dp[i1][i2] = min({
-                          dp[i1+1][i2], // delete word1[i1]
-                          dp[i1][i2+1], // insert word2[i2] to word1
-                          dp[i1+1][i2+1] // replace
-                      }) + 1;
-                    }
-                }
-            }
-        }
-
+        if (dp[i1][i2] != -1)
+            return dp[i1][i2];
         
-        return dp[0][0];
+        
+        int max = word1.size() + word2.size();
+        int skip = max, insDel = max, rep = max;
+        
+        // if same
+        if (word1[i1] == word2[i2]) 
+            skip = backtrack(i1+1, i2+1, word1, word2, dp);
+        
+        // insert, delete
+        insDel = min(backtrack(i1+1, i2, word1, word2, dp),
+                  backtrack(i1, i2+1, word1, word2, dp)) + 1;
+        
+        // replace
+        rep = backtrack(i1+1, i2+1, word1, word2, dp) + 1;
+        
+        return dp[i1][i2] = min({skip, insDel, rep});
     }
 };
