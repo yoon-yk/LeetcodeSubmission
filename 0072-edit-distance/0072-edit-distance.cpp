143ee5b00e736ff987f496f63f5b5bbf5c95ccff
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 17:36:34 +0900

    Time: 24 ms (38.48%), Space: 9.1 MB (41.27%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        if (word1.size() == 0 || word2.size() == 0) {
+        if (word1.size() == 0 || word2.size() == 0) 
             return max(word1.size(), word2.size());
-        }
-        
+    
         vector<vector<int>> dp
             (word1.size()+1, vector<int>(word2.size()+1));
         
@@ -13,12 +12,13 @@ public:
                 if (i1 == word1.size() || i2 == word2.size()) {
                     dp[i1][i2] = max(word1.size()-i1, 
                                      word2.size()-i2); // 이만큼 지워줘야 함 
-                }else {
+                } else {
                     if (word1[i1] == word2[i2]) 
                         dp[i1][i2] = dp[i1+1][i2+1];
                     else {
                       dp[i1][i2] = min({
-                          min(dp[i1+1][i2], dp[i1][i2+1]) + 1,// insert, delete
+                          dp[i1+1][i2] + 1, // delete word1[i1]
+                          dp[i1][i2+1] + 1, // insert word2[i2] to word1
                           dp[i1+1][i2+1] + 1 // replace
                       });
                     }
