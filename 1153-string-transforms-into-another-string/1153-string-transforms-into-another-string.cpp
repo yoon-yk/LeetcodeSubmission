1988d45b80671ef84ceadf479d3c26f6d7ceb496
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 15:27:25 +0900

    Time: 4 ms (89.39%), Space: 7.3 MB (69.70%) - LeetHub

--- 1153-string-transforms-into-another-string/1153-string-transforms-into-another-string.cpp
+++ 1153-string-transforms-into-another-string/1153-string-transforms-into-another-string.cpp
@@ -5,20 +5,15 @@ public:
         
         int sid1 = 0, sid2 = 0;
         vector<int> ids1(26, -1), ids2(26, -1);
-        vector<int> parent1(str1.size());
-        
-        for (int i=0; i<str1.size(); ++i) {
-            char & c = str1[i];
-            if (ids1[c-'a'] == -1) ids1[c-'a'] = sid1++;
-            parent1[i] = ids1[c-'a'];
-        }
         
         for (int i=0; i<str2.size(); ++i) {
-            char & c = str2[i];
-            if (ids2[c-'a'] == -1) ids2[c-'a'] = sid2++;
+            char & c1 = str1[i];
+            char & c2 = str2[i];
             
+            if (ids1[c1-'a'] == -1) ids1[c1-'a'] = sid1++;
+            if (ids2[c2-'a'] == -1) ids2[c2-'a'] = sid2++;
             if (sid2 == 26 ||
-                (parent1[i] != i && parent1[i] != ids2[c-'a'])) return false;
+                (ids1[c1-'a'] != i && ids1[c1-'a'] != ids2[c2-'a'])) return false;
         }
 
         return true;
