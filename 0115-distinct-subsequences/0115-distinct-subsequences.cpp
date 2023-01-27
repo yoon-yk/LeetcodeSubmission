11b21d8576a6d70fdf394211d9da1ac8746e97a7
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 18:34:16 +0900

    Time: 62 ms (40.44%), Space: 18.5 MB (66.34%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,55 +1,29 @@
 class Solution {
 public:
-//     int numDistinct(string s, string t) {
-//         int len1=s.size(), len2 = t.size();
-//         vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
-        
-//         for (int i=len1; i>=0; --i) {
-//             for (int j=len2; j>=0; --j) {
-
-//                 // i == len1 || j == len2
-//                 if (i==len1 || j == len2){
-//                     dp[i][j] = 1;
-//                 }
-                
-//                 else if (s[i] == t[j]) {
-//                     dp[i][j] += dp[i+1][j+1];
-//                 } else {
-//                     dp[i][j] = dp[i+1][j];
-//                 }
-                
-//             }
-//         }
-        
-//         for (int i=0; i<=len1; ++i) {
-//             for (int j=0; j<=len2; ++j) {
-//                 cout << dp[i][j] << " ";
-//             }
-//             cout << endl;
-//         }
-        
-//         return 0;
-//     }
-    
     int numDistinct(string s, string t) {
-        int len1 = s.size(), len2 = t.size();
-        long long ret;
-        vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
-        dp[len1][len2] = 1;
-        for (int i=len1; i>=0; --i) {
-            for (int j=len2; j>=0; --j) {
-                if (i==len1 || j == len2) {
-                    dp[i][j] = (j==len2);
-                    continue;
-                }
-                ret = 0;
-                if (s[i] == t[j]) ret += dp[i+1][j+1];
-                ret += dp[i+1][j]; 
-                dp[i][j] = ret;
-            }
-        } 
+        vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
         
-        
-        return dp[0][0];
+        return backtrack(0, 0, s, t, dp);
     }
+    
+    int backtrack(int sid, int tid, string &s, string &t, 
+                  vector<vector<int>>& dp) {
+        
+        int ans = 0;
+        
+        // base case
+        if (tid == t.size()) return 1;
+        if (sid == s.size()) return 0;
+
+        if (dp[sid][tid] != -1) return dp[sid][tid];
+            
+        // recurrence relation
+        if (s[sid] == t[tid]) 
+            ans += backtrack(sid+1, tid+1, s, t, dp);
+        
+        ans += backtrack(sid+1, tid, s, t, dp);
+        
+        return dp[sid][tid] = ans;
+    }
+    
 };
