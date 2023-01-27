c4efd8762972211ba631dfc59d42ae0946edde5b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-27 17:58:36 +0900

    Time: 57 ms (64.32%), Space: 16.2 MB (44.49%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    vector<int> dir = {-1, 0, 1, 0, -1};
-    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
+        // certain path every time
+        int m = matrix.size(), n = matrix[0].size(), ans = 0;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
         
-        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), -1));
-        
-        int ans = 0;
-        for (int i=0; i<matrix.size(); ++i) 
-            for (int j=0; j<matrix[0].size(); ++j) 
-                ans = max(ans, dfs(i, j, matrix, dp));
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                ans = max(ans, longestPath(i, j, matrix, dp));
+            }
+        }
         
         return ans;
     }
     
-    bool isValid(int r, int c, vector<vector<int>>& matrix) {
-        if (r < 0 || c < 0 || r>=matrix.size() || c>=matrix[0].size()) return false;
+    vector<vector<int>> dirs = {
+        {-1, 0}, // up
+        {1, 0}, // down
+        {0, -1}, // left
+        {0, 1} // right
+    };
+    
+    bool valid(int i, int j, vector<vector<int>>& matrix){
+        if (i < 0 || j < 0 || i >= matrix.size() || j >= matrix[0].size()) 
+            return false;
         return true;
     }
     
-    int dfs(int r, int c, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+    int longestPath(int i, int j, vector<vector<int>>& matrix,
+                   vector<vector<int>>& dp) {
         
-        if (dp[r][c] != -1) return dp[r][c];
+        if (dp[i][j] != -1) return dp[i][j];
         
         int ans = 1, nr, nc;
-        
-        for (int d=0; d<4; ++d) {
-            nr = r+dir[d], nc = c+dir[d+1];
-            if (isValid(nr, nc, matrix) && matrix[nr][nc] > matrix[r][c]) 
-                ans = max(ans, dfs(nr, nc, matrix, dp)+1);
+        for (auto & dir : dirs) {
+            nr = i+dir[0], nc = j+dir[1];
+            if (!valid(nr, nc, matrix) || matrix[nr][nc] <= matrix[i][j]) 
+                continue;
+            ans = max(ans, longestPath(nr, nc, matrix, dp)+1);
         }
         
-        return dp[r][c] = ans;
+        return dp[i][j] = ans;
     }
 };
