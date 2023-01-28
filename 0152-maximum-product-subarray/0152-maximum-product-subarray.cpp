e1a838c245d0620be8860cd11b5ffab24be907bb
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-29 02:33:45 +0900

    Time: 19 ms (14.52%), Space: 14.1 MB (9.80%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        
-        
-        /*
-        2 3 -2 4
-        2 3 -6 -24
-        2 6 -2 4
-        
-        -2 -3 7
-        -2 -3 -21
-        -2  6 
-        
-        
-        */
-        int n = nums.size(), ans = nums[0], prevMaxChain, prevMinChain, minChain, maxChain;
-        minChain = maxChain = nums[0];
+        int n = nums.size(), ans = nums[0];
+        vector<int> minn(n), maxx(n);
+        minn[0] = maxx[0] = nums[0];
         for (int i=1; i<n; ++i) {
-            prevMaxChain = maxChain, prevMinChain = minChain;
-            minChain = min(nums[i], min(prevMinChain * nums[i], prevMaxChain * nums[i]));
-            maxChain = max(nums[i], max(prevMaxChain * nums[i], prevMinChain * nums[i]));
-            ans = max(maxChain, ans);
+            minn[i] = min({minn[i-1]*nums[i], maxx[i-1]*nums[i], nums[i]});
+            maxx[i] = max({minn[i-1]*nums[i], maxx[i-1]*nums[i], nums[i]});
+            ans = max(ans, maxx[i]);
         }
-        
         return ans;
     }
 };
