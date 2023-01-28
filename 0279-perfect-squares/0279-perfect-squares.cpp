a6284cc0a99c1258c9e090fb20c829b6bf540959
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-28 18:50:57 +0900

    Time: 275 ms (42.58%), Space: 9 MB (69.75%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int numSquares(int n) {
         int maxN = sqrt(n);
-        vector<vector<int>> dp(maxN+1, vector<int>(n+1, n+1));
-        dp[0][0] = 0;
+        vector<int> dp(n+1, n+1);
+        dp[0] = 0;
         for (int i=1; i<=maxN; ++i) {
             for (int s=0; s<=n; ++s) {
-                dp[i][s] = dp[i-1][s];
                 if (s-(i*i) >= 0) {
-                    dp[i][s] = min(dp[i][s], dp[i][s-(i*i)] + 1);
+                    dp[s] = min(dp[s], dp[s-(i*i)] + 1);
                 }
             }
         }
-        return dp[maxN][n];
+        return dp[n];
     }
 };
