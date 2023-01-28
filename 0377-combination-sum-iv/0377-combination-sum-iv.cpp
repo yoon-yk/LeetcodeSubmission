40290e4a99013b76b00cd34b89ed2fc0f2db7540
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-29 01:26:47 +0900

    Time: 4 ms (44.19%), Space: 6.7 MB (33.64%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1, -1);
-        return backtrack(nums, target, dp);
-    }
-    
-    int backtrack(vector<int>& nums, int target, vector<int>& dp) {
-        
-        if (target == 0) return 1;
-        if (dp[target] != -1) return dp[target];
-        
-        int ans = 0;
-        for (int i=0; i<nums.size(); ++i) {
-            if (target-nums[i] < 0) continue;
-            ans += backtrack(nums, target-nums[i], dp);
+        vector<unsigned long long> dp(target+1, 0);
+        dp[0] = 1;
+        for (int i=1; i<=target; ++i) {
+            for (int j=0; j<nums.size(); ++j) {
+                if (i-nums[j] >= 0) {
+                    dp[i] += dp[i-nums[j]];
+                }
+            }
         }
-        return dp[target] = ans;
+        return dp[target];
     }
     
 };
