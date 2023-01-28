21205b60bb5f5d8e415018b110fcf3553e88ed19
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-29 00:49:49 +0900

    Time: 3 ms (36.56%), Space: 7.9 MB (6.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
         
-        int prevSec = nums[0], prevFirst = max(nums[0], nums[1]), cur = prevFirst;
-        for (int i=2; i<n; i++) {
-            cur = max(prevSec + nums[i], prevFirst);
-            prevSec = prevFirst, prevFirst = cur;
+        /*
+        base case
+        dp[0] = 0, dp[1] = nums[0];
+        
+        recurrence relation
+        dp[i] = max(dp[i-1], dp[i-2]+nums[i-1]);
+        지금 여기서 훔치거나, 넘기거나
+        */
+        
+        int n = nums.size();
+        vector<int> dp(n+1);
+        dp[0] = 0, dp[1] = nums[0];
+        for (int i=2; i<=n; ++i) {
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i-1]);
         }
-        return cur;
+        return dp.back();
     }
 };
