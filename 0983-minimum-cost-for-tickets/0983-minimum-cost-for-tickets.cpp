4ef7cbacac9e5fd19dfe8400f3700eeb71024c26
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-28 21:50:27 +0900

    Time: 0 ms (100.00%), Space: 9.9 MB (28.39%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -4,11 +4,11 @@ public:
         unordered_set<int> travel(begin(days), end(days));
         vector<int> dp(30);
         dp[0] = 0;
-        for (int d=days.front(); d<=days.back(); ++d) {
-            if (!travel.count(d)) dp[d%30] = dp[(d-1)%30];
-            else dp[d%30] = min({dp[(d-1)%30]+costs[0], 
-                              dp[max(0, d-7)%30]+costs[1],
-                              dp[max(0, d-30)%30] + costs[2]});
+        for (int d = days.front(); d <= days.back(); ++d) {
+            if (!travel.count(d)) dp[d % 30] = dp[(d-1) % 30];
+            else dp[d % 30] = min({dp[(d-1) % 30]+costs[0], 
+                              dp[max(0, d-7) % 30]+costs[1],
+                              dp[max(0, d-30) % 30] + costs[2]});
         }
         return dp[days.back() % 30];
     }
