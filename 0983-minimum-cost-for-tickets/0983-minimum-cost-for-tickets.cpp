e621f902e2baa388c7bb059a4624e2caa5eeef36
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-28 21:46:13 +0900

    Time: 10 ms (25.42%), Space: 10.2 MB (14.65%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
-    using ppair = pair<int, pair<int, int>>;
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        priority_queue<ppair, vector<ppair>, greater<ppair>> Q; // cost, {today, idx}
-        Q.push({0, {1, 0}});
-        
-        vector<int> d = {1, 7, 30};
-        vector<int> minCost(400, INT_MAX);
-        minCost[1] = 0;
-        
-        int nextDay, nextIdx;
-        while (!Q.empty()) {
-            auto [cost, info] = Q.top(); Q.pop();
-            auto &[day, idx] = info;
-            // cout << cost << "/" << day << "/" << idx << endl;
-            
-            if (day > days.back()) return cost;
-            
-            if (day < days[idx] && minCost[day+1] > cost) {
-                Q.push({cost, {day+1, idx}});
-                minCost[day+1] = cost;
-            }
-            
-            for (int i=0; i<d.size(); ++i) {
-                nextDay = day + d[i];
-                if (minCost[nextDay] <= cost + costs[i]) continue;
-                nextIdx = lower_bound(days.begin(), days.end(), nextDay) - days.begin();
-                minCost[nextDay] = cost+costs[i];
-                Q.push({cost+costs[i], {nextDay, nextIdx}});
-            }
-            
+        unordered_set<int> travel(begin(days), end(days));
+        vector<int> dp(366);
+        dp[0] = 0;
+        for (int d=1; d<366; ++d) {
+            if (!travel.count(d)) dp[d] = dp[d-1];
+            else dp[d] = min({dp[d-1]+costs[0], dp[max(0, d-7)]+costs[1],
+                              dp[max(0, d-30)] + costs[2]});
         }
-        
-        return -1;
-        
+        return dp[365];
     }
 };
