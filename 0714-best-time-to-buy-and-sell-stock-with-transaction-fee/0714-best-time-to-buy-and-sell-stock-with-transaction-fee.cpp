c74a43eb66679456ef3e24cd242ae44bcb27ba91
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-30 02:12:28 +0900

    Time: 259 ms (38.94%), Space: 90.9 MB (40.38%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        vector<vector<int>> dp(2, vector<int>(prices.size(), -1));
-        return backtrack(false, 0, prices, fee, dp);
-    }
-    
-    int backtrack(bool hold, int day, vector<int>& prices, int fee, 
-                  vector<vector<int>>& dp) {
-        if (day == prices.size()) return 0;
-        if (dp[hold][day] != -1) return dp[hold][day];
-        
-        int keep = backtrack(hold, day+1, prices, fee, dp), option;
-        if (hold) {
-            option = backtrack(!hold, day+1, prices, fee, dp) - fee + prices[day];
-        } else {
-            option = backtrack(!hold, day+1, prices, fee, dp) - prices[day];
+        int sell, buy;
+        vector<vector<int>> dp(prices.size(), vector<int>(2));
+        dp[0][0] = 0, dp[0][1] = -prices[0];
+        for (int i=1; i<prices.size(); ++i) {
+            sell = dp[i-1][true] - fee + prices[i];
+            buy = dp[i-1][false] - prices[i];
+            dp[i][true] = max(dp[i-1][true], buy);
+            dp[i][false] = max(dp[i-1][false], sell);
         }
-        return dp[hold][day] = max(keep, option);
+
+        return max(dp.back()[true], dp.back()[false]);
     }
 };
