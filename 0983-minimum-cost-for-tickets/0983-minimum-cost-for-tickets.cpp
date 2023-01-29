59b55054a00a9518981848ac07e2319aea9296b0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-29 18:50:24 +0900

    Time: 12 ms (17.57%), Space: 10.2 MB (14.53%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -10,15 +10,15 @@ public:
     int backtrack(int day, unordered_set<int>& dict, vector<int>& costs, vector<int>& dp) {
         if (day > 365) return 0;
         if (dp[day] != -1) return dp[day];
-        
-        int oneDay, oneWeek, oneMonth, nextDay = INT_MAX;
-        oneDay = backtrack(day+1, dict, costs, dp) + costs[0];
-        oneWeek = backtrack(day+7, dict, costs, dp) + costs[1];
-        oneMonth = backtrack(day+30, dict, costs, dp) + costs[2];
-        
-        if (!dict.count(day))
-            nextDay = backtrack(day+1, dict, costs, dp);
-        
-        return dp[day] = min({oneDay, oneWeek, oneMonth, nextDay});
+                
+        if (dict.count(day)) {
+            int oneDay, oneWeek, oneMonth, nextDay = INT_MAX;
+            oneDay = backtrack(day+1, dict, costs, dp) + costs[0];
+            oneWeek = backtrack(day+7, dict, costs, dp) + costs[1];
+            oneMonth = backtrack(day+30, dict, costs, dp) + costs[2];
+            return dp[day] = min({oneDay, oneWeek, oneMonth, nextDay});
+        }
+        return backtrack(day+1, dict, costs, dp);
+
     }
 };
