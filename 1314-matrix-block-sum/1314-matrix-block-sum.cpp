5b38d0e67552e97faa427487f908bea87d2ce84e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-30 00:52:34 +0900

    Time: 13 ms (60.65%), Space: 9.3 MB (76.67%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> blockSum(n+1, vector<int>(m+1, 0));
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        calculateMatrixSum(mat, dp);
+        calculateMatrixBlockSum(dp, mat, k);
+        return mat;
+    }
+    
+    void calculateMatrixBlockSum(vector<vector<int>>& dp, vector<vector<int>>& ans, int k) {
+        int m = ans.size(), n = ans[0].size();
+        int maxIRange, maxJRange, minIRange, minJRange;
+        int whole, top, left, diag;
         
-        for (int i=1; i<=n; i++) { // row
-            for (int j=1; j<=m; j++) { // col
-                blockSum[i][j] = mat[i-1][j-1]
-                    + blockSum[i-1][j]
-                    + blockSum[i][j-1]
-                    - blockSum[i-1][j-1];
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                maxIRange = min(i+1+k, m), maxJRange = min(j+1+k, n);
+                minIRange = max(i+1-k-1, 0), minJRange = max(j+1-k-1, 0);
+                whole = dp[maxIRange][maxJRange];
+                top = dp[minIRange][maxJRange];
+                left = dp[maxIRange][minJRange];
+                diag = dp[minIRange][minJRange];
+                ans[i][j] = whole-(top+left)+diag;
             }
         }
-        
-        vector<vector<int>> res(n, vector<int>(m, 0));
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                int maxI = i+1+k, minI = i+1-k, maxJ = j+1+k, minJ = j+1-k;
-                res[i][j] = blockSum[min(maxI, n)][min(maxJ, m)]
-                    - blockSum[max(minI-1, 0)][min(maxJ, m)]
-                    - blockSum[min(maxI, n)][max(minJ-1, 0)]
-                    + blockSum[max(minI-1, 0)][max(minJ-1, 0)];
+
+    }
+    
+    void calculateMatrixSum(vector<vector<int>>& mat, vector<vector<int>>& dp) {
+        int m = mat.size(), n = mat[0].size();
+        int top, left, diag;
+        for (int i=1; i<=m; ++i) {
+            for (int j=1; j<=n; ++j) {
+                top = dp[i-1][j];
+                left = dp[i][j-1];
+                diag = dp[i-1][j-1];
+                dp[i][j] = (top+left-diag) + mat[i-1][j-1];
             }
         }
-        return res;
     }
 };
