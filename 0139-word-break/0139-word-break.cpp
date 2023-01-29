2a17cff1b2d80725e2383ca75357097759ef7157
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-29 13:40:01 +0900

    Time: 21 ms (42.90%), Space: 13.4 MB (34.14%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        /*
+        base case  
+        dp[0][0] = true
+        
+        recurrence relation
+        dp[i][j] = dp[0][i-1] && s.substr(i-1, j-i+1)
+        */
+        
         int n = s.size();
-        vector<int> dp(n+1, false);
-        dp[0] = true;
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        for (int i=1; i<=s.size(); ++i) {
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+        for (int i=1; i<=n; ++i) {
             for (int j=1; j<=i; ++j) {
                 if (dp[j-1] && dict.count(s.substr(j-1, i-j+1))) {
                     dp[i] = true;
@@ -13,6 +21,7 @@ public:
                 }
             }
         }
+        
         return dp[n];
     }
 };
