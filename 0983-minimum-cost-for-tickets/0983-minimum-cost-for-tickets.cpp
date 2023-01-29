ebb839c3529d61d2f87abc2cab82faf91fc59a2c
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-29 18:48:57 +0900

    Time: 8 ms (38.96%), Space: 10.3 MB (12.61%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int kDays = 365;
-        unordered_set<int> travelDays(days.begin(), days.end());
-        vector<int> dp(kDays+1);
-        dp[0] = 0;
-        for (int i = days.front(); i<=days.back(); ++i) {
-            if (!travelDays.count(i)) {
-                dp[i] = dp[i-1];
-            } else {
-                dp[i] = min({
-                    dp[i-1] + costs[0],
-                    dp[max(0, i-7)] + costs[1],
-                    dp[max(0, i-30)] + costs[2]
-                });   
-            }
-        }
-        return dp[days.back()];
+        unordered_set<int> dict(days.begin(), days.end());
+        vector<int> dp(366, -1);
+        
+        return backtrack(1, dict, costs, dp);
+    }
+    
+    int backtrack(int day, unordered_set<int>& dict, vector<int>& costs, vector<int>& dp) {
+        if (day > 365) return 0;
+        if (dp[day] != -1) return dp[day];
+        
+        int oneDay, oneWeek, oneMonth, nextDay = INT_MAX;
+        oneDay = backtrack(day+1, dict, costs, dp) + costs[0];
+        oneWeek = backtrack(day+7, dict, costs, dp) + costs[1];
+        oneMonth = backtrack(day+30, dict, costs, dp) + costs[2];
+        
+        if (!dict.count(day))
+            nextDay = backtrack(day+1, dict, costs, dp);
+        
+        return dp[day] = min({oneDay, oneWeek, oneMonth, nextDay});
     }
 };
