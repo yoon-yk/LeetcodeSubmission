fa096aa06cd0c3e23ba869525b03763d03afff16
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-29 13:20:02 +0900

    Time: 2 ms (62.72%), Space: 6.1 MB (77.11%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n, 0);
-        dp[0] = (s[0]-'0' > 0);
         
-        int cn;
-        for (int i=1; i<n; ++i) {
-            if (s[i]-'0' > 0) dp[i] = dp[i-1];
-            
-            cn = stoi(s.substr(i-1, 2));
-            if (cn <= 26 && cn > 9) dp[i] += (i-2 >= 0) ? dp[i-2] : 1;
+        /*
+        base case :
+        dp[0] = 1
+        recurrence relation :
+        dp[i] += dp[i-1] (if s[i] is valid)
+        dp[i] += dp[i-2] (if s.substr(i-1,2) is valid)
+        */
+        
+        int num;
+        vector<int> dp(s.size()+1);
+        dp[0] = 1;
+        for (int i=1; i<=s.size(); ++i) {
+            if ('1' <= s[i-1] && s[i-1] <= '9') dp[i] += dp[i-1];
+            if (i > 1) {
+                num = stoi(s.substr(i-2, 2));
+                if (10 <= num && num <= 26) dp[i] += dp[i-2]; 
+            }
         }
-        
-        return dp[n-1];
+        return dp[s.size()];
     }
 };
