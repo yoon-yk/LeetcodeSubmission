bfba08677889cbfef02018c7650b36d71c8244d3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-30 17:54:41 +0900

    Time: 42 ms (78.20%), Space: 21.7 MB (49.59%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    
+    struct Node{
+        int removal;
+        int index;
+        int valid;
+        string str;
+        
+        Node(int _removal, int _index, int _valid, string _str) : 
+        removal(_removal), index(_index), valid(_valid), str(_str) {};
+    };
+    
+    vector<string> removeInvalidParentheses(string s) {
+        vector<string> ans;
+        string cur;
+        int minV = s.size()+1, idx, rem, valid;
+        auto greater = [](const Node* a, const Node* b) {
+            return (a->removal > b->removal);
+        };
+        
+        unordered_map<string, unordered_set<int>> visited;
+        priority_queue<Node*, vector<Node*>, decltype(greater)> pq(greater);
+        pq.push(new Node(0, 0, 0, ""));
+        
+        while (!pq.empty()) {
+            auto cur = pq.top(); pq.pop();
+            idx = cur->index, rem = cur->removal, valid = cur->valid;
+            string & str = cur->str;
+            if (visited[str].count(rem)) continue;
+            visited[str].insert(rem);
+            // cout << str << endl;
+            
+            if (rem > minV) break;
+            
+            if (idx == s.size()) {
+                if (valid != 0) continue;
+                minV = rem;
+                ans.push_back(str);
+                continue;
+            }
+            
+            
+            if (s[idx] == '(') {
+                pq.push(new Node(rem, idx+1, valid+1, str+s[idx])); // include
+                pq.push(new Node(rem+1, idx+1, valid, str)); // remove
+
+            } else if (s[idx] == ')') {
+                if (valid > 0) 
+                    pq.push(new Node(rem, idx+1, valid-1, str+s[idx])); // include
+                pq.push(new Node(rem+1, idx+1, valid, str)); // remove
+
+            } else {
+                pq.push(new Node(rem, idx+1, valid, str+s[idx])); // include
+            }
+            
+        }
+        
+        return ans;
+    }
+};
