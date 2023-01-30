08eda9c980438393aa3b4007ef392cbe74de0926
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-30 12:49:50 +0900

    Time: 8 ms (77.13%), Space: 11.4 MB (30.33%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,43 +17,28 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head) return head;
         
-        Node* curPtr = head;        
-        while (curPtr) {
-            Node* nNode = new Node(curPtr->val);
-            nNode->next = curPtr->next;
-            curPtr->next = nNode;
-            curPtr = nNode->next;
-        }
-
-        curPtr = head;
-        while (curPtr) {
-            curPtr->next->random = 
-                (curPtr->random) ? curPtr->random->next : NULL;
-            curPtr = curPtr->next->next;
+        unordered_map<Node*, Node*> dup;
+        
+        // next
+        Node* ptr = head;
+        if (ptr) dup[ptr] = new Node(ptr->val);
+        
+        while (ptr && ptr->next) {
+            dup[ptr->next] = new Node(ptr->next->val);
+            dup[ptr]->next = dup[ptr->next];
+            ptr = ptr->next;
         }
         
-        // 7 7 13 13 11 11 10 10 1 1 
-        Node* nHead = head->next, *oPtr, *cPtr;
-        oPtr = head, cPtr = head->next;
-        while (oPtr) {
-            cPtr = oPtr->next;
-            oPtr->next = cPtr->next;
-            if (cPtr->next) cPtr->next = cPtr->next->next;
-            oPtr = oPtr->next;
+        ptr = head;
+        while (ptr) {
+            if (ptr->random) {
+               dup[ptr]->random = dup[ptr->random]; 
+            } 
+            ptr = ptr->next;
         }
         
-        // Node* pPtr = nHead;
-        // while (pPtr) {
-        //     int v = pPtr->val;
-        //     int nv = (pPtr->next) ? pPtr->next->val : 0;
-        //     int rv = (pPtr->random) ? pPtr->random->val : 0;
-        //     cout << v << "/" << nv << "/" << rv << endl;
-        //     pPtr = pPtr->next;
-        // }
-        // cout << endl;
-        
-        return nHead;
+        // random
+        return dup[head];
     }
 };
