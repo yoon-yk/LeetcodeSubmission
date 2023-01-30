43b5032eaf97fbc434122b677fb869b78db8c991
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-30 21:45:10 +0900

    Time: 48 ms (76.86%), Space: 18.9 MB (41.41%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int totalTime(vector<int>& piles, int bananaN) {
-        int cnt = 0;
-        for (int i=0; i<piles.size(); i++) {
-            // cnt += ceil((double)piles[i] / (double)bananaN);
-            cnt += (piles[i]/bananaN) + (piles[i]%bananaN>0);
+    int hours(vector<int>& piles, int bananas) {
+        int ans = 0;
+        for (int & p : piles) {
+            ans += (p/bananas) + (p % bananas != 0);
         }
-        return cnt;
+        return ans;
     }
-    
     int minEatingSpeed(vector<int>& piles, int h) {
-    
-        int lo = 1, hi = *max_element(piles.begin(), piles.end());
-        int mid;
-
+        /*
+        sum, ...., h, ..., ..., number of piles ...
+        1개씩       k개씩         maximum element 
+        
+        sum >= h >= number of piles
+        
+        k를 구해야 하는데, h값을 갖게 하는 가장 작은 수
+        */
+        
+        int lo = 1, hi = *max_element(piles.begin(), piles.end()), mi;
+        
         while (lo < hi) {
-            mid = (lo + ((hi-lo) >> 1));
-
-            if (totalTime(piles, mid) <= h) {
-                hi = mid;
+            mi = lo + ((hi-lo) >> 1);
+            if (hours(piles, mi) > h) {
+                lo = mi+1;
             } else {
-                lo = mid+1;
+                hi = mi;
             }
         }
         
