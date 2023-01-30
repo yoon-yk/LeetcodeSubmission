7d0b2e4f1fa563351de6248e10af932431409fd8
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-01-31 01:08:33 +0900

    Time: 170 ms (99.05%), Space: 101.2 MB (57.41%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temp) {
-        /*
-        [73,74,75,71,69,72,76,73]
-        [ 1, 1, 4, 2, 1, 1, 0, 0]
-        
-        */
-        
-        int currTp, n = temp.size(), hottest = 0;
-        vector<int> ans(n, 0);
-        for (int currDay=n-1; currDay>=0; currDay--) {
-            currTp = temp[currDay];
-            if (currTp >= hottest) {
-                hottest = currTp;
-            } else {
-                int days = 1;
-                while (temp[currDay + days] <= currTp) {
-                    days += ans[currDay + days];
-                }
-                ans[currDay] = days;
+    vector<int> dailyTemperatures(vector<int>& t) {
+        int n = t.size();
+        vector<int> ans(n, 0), incSt;
+        for (int i=0; i<n; ++i) {
+            auto & temp = t[i];
+            while (!incSt.empty() && t[incSt.back()] < temp) {
+                ans[incSt.back()] = i-incSt.back();
+                incSt.pop_back();
             }
+            incSt.push_back(i);
         }
         return ans;
     }
