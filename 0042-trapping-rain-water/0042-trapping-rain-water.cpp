96bb2eac683a6ad7d5f22246c79dc21b485b5847
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-02-01 00:47:55 +0900

    Time: 17 ms (69.69%), Space: 19.8 MB (65.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
-        int n = h.size();
-        vector<int> l(n);
-        /// min(l[i], r[i])
-        l[0] = 0;
-        for (int i=1; i<n-1; ++i) 
-            l[i] = max(l[i-1], h[i-1]);
-        
-        int ans = 0, maxR = 0;
-        for (int i=n-2; i>=0; --i) {
-            maxR = max(maxR, h[i+1]);
-            ans += max(0, (min(l[i], maxR)-h[i]));
+        int left = 0, right = h.size() - 1;
+        int ans = 0;
+        int lMax = 0, rMax = 0;
+        while (left < right) {
+            if (h[left] < h[right]) {
+                if (h[left] >= lMax) {
+                    lMax = h[left];
+                } else {
+                    ans += (lMax - h[left]);
+                }
+                ++left;
+            } else {
+                if (h[right] >= rMax) {
+                    rMax = h[right];
+                } else {
+                    ans += (rMax - h[right]);
+                }
+                --right;
+            }
         }
         return ans;
-        
     }
 };
