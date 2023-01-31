e64b7074023df65b41d56c94e36eccf3a8d66fda
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-02-01 01:21:16 +0900

    Time: 15 ms (77.78%), Space: 16.9 MB (93.68%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& ins, vector<int>& newI) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& target) {
+        
+        // smaller ..... overlap .... greater
+        // push_back ... merge ... push_back
+        // interval[1] < target[0]
+                            // target[1] < interval[0]
+        // overlap merge
+            // smallest starting point, biggest ending point
+        
+        int idx = 0;
         vector<vector<int>> ans;
-        int i = 0;
-        for (; i<ins.size(); ++i) {
-            if (ins[i][1] < newI[0]) ans.push_back(ins[i]);
-            else break;
+        for (; idx < intervals.size(); ++idx) {
+            if (intervals[idx][1] >= target[0]) break;
+            else ans.push_back(intervals[idx]);
         }
         
-        while(i<ins.size() && ins[i][0] <= newI[1]) {
-            newI[0] = min(ins[i][0], newI[0]);
-            newI[1] = max(ins[i][1], newI[1]);
-            ++i;
-        }
-        ans.push_back(newI);
         
-        for (;i<ins.size(); ++i) ans.push_back(ins[i]);
+        for (; idx < intervals.size(); ++idx) {
+            if (target[1] < intervals[idx][0]) break;
+            else {
+                target[0] = min(intervals[idx][0], target[0]);
+                target[1] = max(intervals[idx][1], target[1]);
+            }
+        }
+        ans.push_back(target);
+        
+        for (; idx < intervals.size(); ++idx) {
+            ans.push_back(intervals[idx]);
+        }
         
         return ans;
+        
     }
 };
