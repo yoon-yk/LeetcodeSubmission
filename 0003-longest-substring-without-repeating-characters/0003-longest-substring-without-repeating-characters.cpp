86fffa5d7a1bae9b96055422b6f041b59bdc5db1
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-02-01 01:08:44 +0900

    Time: 4 ms (96.76%), Space: 8.3 MB (76.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,14 +4,13 @@ public:
         // [start ... end] 
         // end가 이미 본 애라면 start+1
         // 그렇지 않다면 계속 유지
-        vector<int> freq(256, 0);
+        vector<int> lastSeen(256, -1);
         int start = 0, end = 0, n = s.size(), ans = 0;
         for (; end < n; ++end) {
-            ++freq[s[end]];
-            while (freq[s[end]] > 1) {
-                --freq[s[start]];
-                ++start;
+            if (lastSeen[s[end]] >= start) {
+                start = lastSeen[s[end]] + 1;
             }
+            lastSeen[s[end]] = end;
             ans = max(end-start+1, ans);
         }
         return ans;
