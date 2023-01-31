ba996a751f38d63dca91f3ae102aa5a915d61536
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-02-01 00:06:53 +0900

    Time: 12 ms (50.90%), Space: 13.7 MB (69.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,12 +1,37 @@
 class Solution {
 public:
+    int findLowerBound(vector<int>& nums, int target, int lo, int hi) {
+        int mi;
+        // 1 2 3 4 5 
+        while (lo < hi) {
+            mi = lo + ((hi-lo) >> 1);
+            if (nums[mi] < target)
+                lo = mi + 1;
+            else hi = mi;
+        }
+        return lo;
+    }
+    
+    int findUpperBound(vector<int>& nums, int target, int lo, int hi) {
+        // 1 2 2 2 2 3 
+        int mi;
+        while (lo < hi) {
+            mi = lo + ((hi-lo+1) >> 1);
+            if (nums[mi] > target)
+                hi = mi-1;
+            else lo = mi;
+        }
+        return lo;
+    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
-        // lower bound
-        auto lo = lower_bound(nums.begin(), nums.end(), target);
-        if (lo == nums.end() || *lo != target) return {-1, -1};
-        
-        // upper bound
-        auto hi = upper_bound(lo, nums.end(), target); // greater 
-        return {(int)(lo-nums.begin()), (int)(prev(hi) - nums.begin())};
+        vector<int> ans (2, -1);
+        if (nums.empty()) return ans;
+        int lb = findLowerBound(nums, target, 0, nums.size()-1);
+        if (nums[lb] != target) return ans;
+        else ans[0] = lb;
+        int ub = findUpperBound(nums, target, lb, nums.size()-1);
+        ans[1] = ub;
+        return ans;
     }
 };
