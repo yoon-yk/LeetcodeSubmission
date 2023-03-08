7e0f81dffccf2fa05d21033a2f3fef625175a927
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-03-09 00:55:35 +0900

    Time: 3 ms (43.00%), Space: 5.9 MB (60.44%) - LeetHub

--- 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
@@ -18,19 +18,13 @@ public:
                 hourComb = 10;
             } else if (time[0]-'0' == 2) {
                 hourComb = 4;
-            } else {
-                hourComb = 0;
             }
         }
         
-        
-        if (time.substr(3, 2) == "??") {
-            minComb = 60;
-        } else if (time[3] == '?') {
+        if (time[3] == '?') 
             minComb = 6;
-        } else if (time[4] == '?') {
-            minComb = 10;
-        }   
+        if (time[4] == '?') 
+            minComb *= 10;
         
         
         return hourComb * minComb;
