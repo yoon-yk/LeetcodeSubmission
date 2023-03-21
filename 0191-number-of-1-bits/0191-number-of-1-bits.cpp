3ee54c1edd3b0b23204d553e0b29a077ba364557
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-03-22 00:54:44 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (36.65%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    
-    /*
-    1011
-    1010
-    
-    1001
-    1000
-    0001
-    0000
-    
-    */
     int hammingWeight(uint32_t n) {
-        bitset<32> i(n);
-        return i.count();
+        int cnt = 0;
+        while (n) {
+            n &= (n-1);
+            ++cnt;
+        }
+        return cnt;
     }
 };
