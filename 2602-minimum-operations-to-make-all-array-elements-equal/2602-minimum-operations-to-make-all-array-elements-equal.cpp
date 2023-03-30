5122db60296149ed98f0d376e0dc0d73e53b682b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-03-31 01:24:33 +0900

    Time: 275 ms (55.23%), Space: 84.4 MB (21.83%) - LeetHub

--- 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
-        
         sort(nums.begin(), nums.end());
-        vector<long long> ls(nums.size());
-        long long lps = 0;
-        for (int i=0; i<nums.size(); ++i) {
-            lps += nums[i];
-            ls[i] = lps;
-        }
-        long long lb;
-        long long left = 0, right = 0;
-        vector<long long> ans;
         
+        vector<long long> ps;
+        long long s = 0;
+        for (int i=0; i<nums.size(); ++i) {
+            s += nums[i];
+            ps.push_back(s);
+        }
+        
+        // 1 3 6 8
+        
+        long long idx;
+        long long ret;
+        vector<long long> ans;
         for (auto & q : queries) {
             q = (long long) q;
-            lb = lower_bound(nums.begin(), nums.end(), q) - nums.begin();
-            
-            left = (lb > 0) ? (q * lb) - ls[lb-1] : 0;
-            right = (lb > 0) ? (lps - ls[lb-1]) - (q * (nums.size()-lb)) : lps - (q * (nums.size() - lb));
-            
-            ans.push_back(left + right);
+            idx = upper_bound(nums.begin(), nums.end(), q) - nums.begin();
+            ret = ((idx) * q);
+            if (idx > 0) ret -= ps[idx-1];
+            if (idx > 0) ret += (s - ps[idx-1]);
+            else ret += s;
+            ret -= ((nums.size()-idx) * q);
+            ans.push_back(ret);
         }
-        
         return ans;
     }
 };
