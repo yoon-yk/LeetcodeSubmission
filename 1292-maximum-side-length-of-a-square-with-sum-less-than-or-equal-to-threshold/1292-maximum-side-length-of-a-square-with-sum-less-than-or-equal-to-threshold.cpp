57bc8936d8447ad39ff1267487f75ea35d72b35a
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-01 17:52:25 +0900

    Time: 857 ms (13.02%), Space: 28.6 MB (43.12%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int getSum (vector<vector<int>>& dp, int i, int j, int l) {
+        int sum = dp[i][j];
+        if (i-l>=0) sum -= dp[i-l][j];
+        if (j-l>=0) sum -= dp[i][j-l];
+        if (i-l>=0 && j-l>=0) sum += dp[i-l][j-l];
+        return sum;
+    }
+    
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        vector<vector<int>> dp = mat;
+        for (int i=0; i<mat.size(); ++i) {
+            for (int j=0; j<mat[0].size(); ++j) {
+                // +up
+                if (i>0) dp[i][j] += dp[i-1][j];
+                // +left
+                if (j>0) dp[i][j] += dp[i][j-1];
+                // -dup
+                if (i>0 && j>0) dp[i][j] -= dp[i-1][j-1];
+            }
+        }
+        int lenLim = min(mat.size(), mat[0].size()), ret;
+        for (int l=lenLim; l>0; --l) {
+            for (int i=l-1; i<mat.size(); ++i) {
+                for (int j=l-1; j<mat[0].size(); ++j) {
+                    ret = getSum(dp, i, j, l);
+                    if (ret <= threshold) {
+                        return l;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
