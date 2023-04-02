6118ec660ad926fcc7489c52d4958aba7fb5d3ec
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-03 01:27:53 +0900

    Time: 157 ms (80.35%), Space: 47.3 MB (85.43%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<bool> isTerminal(graph.size(), false);
+        vector<int> isSafe(graph.size(), -1);
+        for (int i=0; i<graph.size(); ++i) {
+            auto & elist = graph[i];
+            if (elist.empty()) {
+                isTerminal[i] = true;
+                isSafe[i] = true;
+            }
+        }
+        
+        vector<int> ans;
+        for (int i=0; i<graph.size(); ++i) {
+            if (testSafety(i, isSafe, graph)) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+    
+    
+    bool testSafety(int i, vector<int>& isSafe, vector<vector<int>>& graph) {
+        if (isSafe[i] != -1) return isSafe[i];
+        
+        isSafe[i] = false;
+        bool ans = true;
+        for (auto & e : graph[i]) {
+            if (!testSafety(e, isSafe, graph)) ans = false;
+        }
+        return isSafe[i] = ans;
+    }
+};
