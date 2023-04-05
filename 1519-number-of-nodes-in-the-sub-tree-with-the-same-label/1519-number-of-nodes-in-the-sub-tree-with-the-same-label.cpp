a19ba592fa0ffee3d06e36bd287edf807bd7ddbf
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-05 23:33:53 +0900

    Time: 567 ms (79.34%), Space: 216.1 MB (61.64%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
         vector<vector<int>> adj(n);
+        vector<int> nodeCounts(26);
         for (auto & e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
         vector<int> ans(n, 0);
-        dfs(0, -1, adj, labels, ans);
+        dfs(0, -1, adj, labels, ans, nodeCounts);
         return ans;
     }
     
-    vector<int> dfs(int node, int parent, vector<vector<int>> & adj, string & labels, vector<int>& ans) {
-        vector<int> nodeCounts(26);
-        nodeCounts[labels[node]-'a'] = 1;
+    vector<int> dfs(int node, int parent, vector<vector<int>>& adj, string& labels, vector<int>& ans, vector<int>& nodeCounts) {
+        
+        int prev = nodeCounts[labels[node]-'a'];
+        ++nodeCounts[labels[node]-'a'];
         
         for (auto & child : adj[node]) {
-            if (child == parent) {
+            if (child == parent) 
                 continue;
-            }
-            vector<int> childCounts = dfs(child, node, adj, labels, ans);
-            for (int i=0; i<26; ++i) {
-                nodeCounts[i] += childCounts[i];
-            }
+            dfs(child, node, adj, labels, ans, nodeCounts);
         }
-        ans[node] = nodeCounts[labels[node]-'a'];
+        ans[node] = nodeCounts[labels[node]-'a'] - prev;
         return nodeCounts;
     }
 };
