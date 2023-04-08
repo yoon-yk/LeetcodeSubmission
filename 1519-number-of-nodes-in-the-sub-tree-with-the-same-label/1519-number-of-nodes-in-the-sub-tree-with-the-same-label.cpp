c8e5bba078be9022a9f22e0b094101ad1e276394
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-08 14:38:20 +0900

    Time: 420 ms (97.39%), Space: 156.9 MB (94.44%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -15,14 +15,16 @@ public:
     
     void dfs(int cur, int par, string & labels, vector<vector<int>>& adjList, vector<int>& freq, vector<int>& ans) {
         
-        int prev = freq[labels[cur]-'a'];
-        ++freq[labels[cur]-'a'];
+        int prev = freq[labels[cur]-'a']; // 지금 알파벳에 해당되는 애 frequency (그 위에 있는 애들)
         
         for (auto & nei : adjList[cur]) {
             if (nei == par) continue;
             dfs(nei, cur, labels, adjList, freq, ans);
         }
-        ans[cur] = freq[labels[cur]-'a'] - prev;
+        
+        freq[labels[cur]-'a'] += 1; // 지금 노드에 포함되는 애
+        ans[cur] = freq[labels[cur]-'a'] - prev; 
+        // 자신 포함 sub tree에서의 개수 - 그 위에 있던 애들
         
     }
 };
