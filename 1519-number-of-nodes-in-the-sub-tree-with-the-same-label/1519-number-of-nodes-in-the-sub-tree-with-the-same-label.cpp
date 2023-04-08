abad2a28d78d7c504bf5fb30036a35531eaea498
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-08 14:42:29 +0900

    Time: 610 ms (65.03%), Space: 219.7 MB (52.29%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-        vector<int> ans(n), freq(26, 0);
+        vector<int> ans(n);
         vector<vector<int>> adjList(n);
         
         for (auto & e : edges) {
@@ -9,22 +9,24 @@ public:
             adjList[e[1]].push_back(e[0]);
         }
         
-        dfs(0, -1, labels, adjList, freq, ans);
+        dfs(0, -1, labels, adjList, ans);
         return ans;
     }
     
-    void dfs(int cur, int par, string & labels, vector<vector<int>>& adjList, vector<int>& freq, vector<int>& ans) {
+    vector<int> dfs(int cur, int par, string & labels, vector<vector<int>>& adjList, vector<int>& ans) {
         
-        int prev = freq[labels[cur]-'a']; // 지금 알파벳에 해당되는 애 frequency (그 위에 있는 애들)
+        vector<int> ret(26, 0); // frequnecy of subtrees;
+        ++ret[labels[cur]-'a'];
         
         for (auto & nei : adjList[cur]) {
             if (nei == par) continue;
-            dfs(nei, cur, labels, adjList, freq, ans);
+            auto child = dfs(nei, cur, labels, adjList, ans);
+            for (int i=0; i<26; ++i) {
+                ret[i] += child[i];
+            }
         }
         
-        freq[labels[cur]-'a'] += 1; // 지금 노드에 포함되는 애
-        ans[cur] = freq[labels[cur]-'a'] - prev; 
-        // 자신 포함 sub tree에서의 개수 - 그 위에 있던 애들
-        
+        ans[cur] = ret[labels[cur]-'a'];
+        return ret;
     }
 };
