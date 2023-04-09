c1df4a146b1d44973ae9effe33f71de348f59b9d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-10 01:34:43 +0900

    Time: 4 ms (30.04%), Space: 7.1 MB (57.82%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.cpp
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -4,14 +4,12 @@ public:
         int prev = lower, cur = lower;
         vector<vector<int>> ans;
         for (int i=0; i<nums.size(); ++i) {
-            if (nums[i] == cur) {
-                prev = nums[i] + 1;
-                cur += 1;
-            } else if (cur < nums[i]) {
+            if (cur < nums[i]) {
                 ans.push_back({prev, nums[i]-1});
-                prev = nums[i] + 1;
-                cur = nums[i] + 1;
+                cur = nums[i];
             }
+            prev = nums[i] + 1;
+            ++cur;
         }
         if (nums.empty() || nums.back() < upper) {
             ans.push_back({prev, upper});
