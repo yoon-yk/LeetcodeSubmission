3e7ead222c3a40cee0316072afdcb3b089a4478d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-13 20:43:37 +0900

    Time: 9 ms (66.37%), Space: 15.5 MB (24.28%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        // [1, 2, 3 / 4 / 5]
-        // [1, 2 / 3, 4 / 5]
+        int N = pushed.size();
+        stack<int> st;
         
-        // [1, 2 / 3, 4, 5, 6 / 7, 8]
-        
-        // 한번 빠진 애의 왼쪽은 순서가 지켜져야 함 - freeze
-        int n = pushed.size();
-        unordered_map<int, int> idxMap;
-        for (int i=0; i<n; ++i) {
-            idxMap[pushed[i]] = i;
-        }
-        
-        int curIdx = n+1;
-        
-        vector<bool> used(n, false);
-        int freezeBound = -1;
-        for (int i=0; i<n; ++i) {
-            
-            int curNum = popped[i];
-            int curNumIdx = idxMap[curNum];
-            for (int j=curNumIdx+1; j<freezeBound; ++j) {
-                if (used[j] == false) {
-                    return false;
-                }
+        int j=0; 
+        for (int i=0; i<pushed.size(); ++i) {
+            st.push(pushed[i]);
+            while (!st.empty() && j < N && st.top() == popped[j]) {
+                st.pop();
+                ++j;
             }
-            
-            used[idxMap[popped[i]]] = true;
-            freezeBound = max(curNumIdx, freezeBound);
-        
         }
-        return true;
+        
+        return j == N;
     }
-    
 };
