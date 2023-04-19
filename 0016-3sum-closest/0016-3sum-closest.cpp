68e86216359568372ca5e908c86dead855df8f47
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-04-19 21:41:41 +0900

    Time: 67 ms (26.80%), Space: 10.2 MB (86.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        // -1, 2, 1, -4
-        // -1 + 2 + 1 = 2
-        
-        // -4 -1 1 2 
-        // a+b+c = target
-        // a+b ~ target-c
-        
-        sort(nums.begin(), nums.end());
-        
-        int minAbs = INT_MAX;
-        int sum = -1;
-
+    int threeSumClosest(vector<int>& nums, int target) {        
         int n = nums.size();
-        for (int ci=0; ci<n; ++ci) {
-            int newT = target - nums[ci];
-            int ai = ci+1, bi = n-1;
-            while (ai < bi) {
-                if (nums[ai] + nums[bi] == newT) {
-                    return target;
-                } 
-                
-                if (abs (newT - (nums[ai] + nums[bi])) < minAbs) {
-                    minAbs = abs (newT - (nums[ai] + nums[bi]));
-                    sum = nums[ai] + nums[bi] + nums[ci];
-                }   
-                
-                if (nums[ai] + nums[bi] < newT) {
-                    ++ai;
-                } else {
-                    --bi;
-                }
+        sort(nums.begin(), nums.end());
 
+        int ans = 100000, minDiff = 100000;
+        for (int a = 0; a < n-2; a++) {
+            twoSum(nums, a, target-nums[a], ans, minDiff);
+        }
+        return ans;
+    }
+    
+    void twoSum(vector<int>& nums, int idx, int targetSum, int& ans, int& minDiff) {
+        int begin = idx+1, end = nums.size()-1;
+        int curSum;
+        
+        while (begin < end) {
+            curSum = nums[begin] + nums[end];
+            if (ans > 10000 || 
+                abs(targetSum-curSum) < minDiff) {
+                ans = curSum + nums[idx];
+                minDiff = abs(targetSum-curSum);
+            }
+            if (nums[begin] + nums[end] < targetSum) {
+                begin ++;
+            } else {
+                end --;
             }
         }
-        return sum;
     }
 };
