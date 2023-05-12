198f5f726203ad46e32d37d0ea97757351c23f5e
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-05-12 14:53:48 +0900

    Time: 404 ms (20.22%), Space: 115.2 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -6,14 +6,14 @@ public:
         dp[n-1] = questions[n-1][0];
         
         for (int i=n-2; i>=0; --i) {
-            int p = questions[i][0], bp = questions[i][1];
+            int bp = questions[i][1];
 
             dp[i] = max(dp[i], dp[i+1]); // skip
 
             if (i+bp+1 < n){ // solve 
-                dp[i] = max(dp[i], dp[i+bp+1] + p); // 뒤에서 문제를 풀었을 때 
+                dp[i] = max(dp[i], dp[i+bp+1] + questions[i][0]); // 뒤에서 문제를 풀었을 때 
             } else {
-                dp[i] = max(dp[i], (long long) p); // 못풀었을 때
+                dp[i] = max(dp[i], (long long) questions[i][0]); // 못풀었을 때
             }
         }
         return dp[0];
