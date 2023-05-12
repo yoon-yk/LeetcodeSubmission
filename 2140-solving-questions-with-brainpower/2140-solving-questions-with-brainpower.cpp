43ae6833fa86dfef7909408283dab6ee7ac0f57b
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-05-12 14:50:07 +0900

    Time: 395 ms (29.21%), Space: 115.3 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long> dp(n);
+        vector<long long> dp(n); // dp[i] : 인덱스 i ~ (n-1) 까지 얻을 수 있는 포인트의 최대값
         dp[n-1] = questions[n-1][0];
         
         for (int i=n-2; i>=0; --i) {
-            dp[i] = questions[i][0];
-            int p = questions[i][0], bp = questions[i][1];
-            if (i+bp+1 < n){
+            int &p = questions[i][0], &bp = questions[i][1];
+            dp[i] = p;
+
+            if (i+bp+1 < n){ // solve 
                 dp[i] = max(dp[i], dp[i+bp+1] + p);
             }
-            dp[i] = max(dp[i], dp[i+1]);
+            dp[i] = max(dp[i], dp[i+1]); // skip
         }
         return dp[0];
     }
