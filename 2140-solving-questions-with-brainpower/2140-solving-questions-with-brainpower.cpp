c1fcd3e14abec317a43aca4b112115619daac9e0
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-05-12 14:43:41 +0900

    Time: 395 ms (29.21%), Space: 115.1 MB (93.26%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -2,27 +2,17 @@ class Solution {
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long> dp(n, -1);
-        return solution(0, questions, dp);
-    }
-    
-    long long solution(int idx, vector<vector<int>>& questions, vector<long long>& dp) {
-    
-        if (idx >= questions.size()) {
-            return 0;
+        vector<long long> dp(n);
+        dp[n-1] = questions[n-1][0];
+        
+        for (int i=n-2; i>=0; --i) {
+            dp[i] = questions[i][0];
+            int p = questions[i][0], bp = questions[i][1];
+            if (i+bp+1 < n){
+                dp[i] = max(dp[i], dp[i+bp+1] + p);
+            }
+            dp[i] = max(dp[i], dp[i+1]);
         }
-        
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-        
-        int p = questions[idx][0], bp = questions[idx][1];
-        // solve
-        long long solve = solution(idx + bp + 1, questions, dp) + p;
-        
-        // skip
-        long long skip = solution(idx+1, questions, dp);
-        
-        return dp[idx] = max(solve, skip);
+        return dp[0];
     }
 };
