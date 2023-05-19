cd98de8eca31bcec8dc3fdd95c8d9d96fb7723ad
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-05-19 09:25:22 +0900

    Time: 714 ms (5.06%), Space: 267.9 MB (5.84%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -3,14 +3,17 @@ public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
         long long ans = 0;
+        vector<vector<int>> min_dp(n, vector<int>(n)), max_dp(n, vector<int>(n));
         for (int i=0; i<n; ++i) {
             int maxN = nums[i], minN = nums[i];
-            for (int j=i; j>=0; --j) {
-                maxN = max(nums[j], maxN);
-                minN = min(nums[j], minN);
-                
-                ans = ans + (maxN - minN);     
+            min_dp[i][i] = max_dp[i][i] = nums[i];
+            
+            for (int j=i+1; j<n; ++j) {
+                max_dp[i][j] = max(nums[j], max_dp[i][j-1]);
+                min_dp[i][j] = min(nums[j], min_dp[i][j-1]);
+                ans += max_dp[i][j] - min_dp[i][j];                
             }
+            
         }
         return ans;
     }
