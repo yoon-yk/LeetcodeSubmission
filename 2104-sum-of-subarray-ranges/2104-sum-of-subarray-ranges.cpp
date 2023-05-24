d209869ada202777f4564c746e5d301801a9e99f
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-05-24 23:37:05 +0900

    Time: 19 ms (81.31%), Space: 11.6 MB (20.23%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,20 +1,71 @@
 class Solution {
 public:
+    
+    inline long long nH2(int n) { // H(n,2)=C(n+1,2) 
+        return (n+1)*n/2;
+    }
+    
     long long subArrayRanges(vector<int>& nums) {
+        
         int n = nums.size();
-        long long ans = 0;
-        vector<vector<int>> min_dp(n, vector<int>(n)), max_dp(n, vector<int>(n));
+        vector<int> min_left(n), min_right(n), max_left(n), max_right(n);
+        vector<int> minSt,maxSt;
+        
         for (int i=0; i<n; ++i) {
-            int maxN = nums[i], minN = nums[i];
-            min_dp[i][i] = max_dp[i][i] = nums[i];
-            
-            for (int j=i+1; j<n; ++j) {
-                max_dp[i][j] = max(nums[j], max_dp[i][j-1]);
-                min_dp[i][j] = min(nums[j], min_dp[i][j-1]);
-                ans += max_dp[i][j] - min_dp[i][j];                
+            min_left[i] = min_right[i] = max_left[i] = max_right[i] = i;
+        }
+        
+        
+        for (int i=0; i<n; ++i) {
+            while (!minSt.empty() && nums[minSt.back()] > nums[i]) {
+                min_left[i] = min_left[minSt.back()];
+                minSt.pop_back();
             }
             
+            while (!maxSt.empty() && nums[maxSt.back()] < nums[i]) {
+                max_left[i] = max_left[maxSt.back()];
+                maxSt.pop_back();
+            }
+            
+            minSt.push_back(i);
+            maxSt.push_back(i);
+
         }
+        
+        minSt.clear();  
+        maxSt.clear();
+        
+         for (int i=n-1; i>=0; --i) {
+            while (!minSt.empty() && nums[minSt.back()] >= nums[i]) {
+                min_right[i] = min_right[minSt.back()];
+                minSt.pop_back();
+            }
+            
+            while (!maxSt.empty() && nums[maxSt.back()] <= nums[i]) {
+                max_right[i] = max_right[maxSt.back()];
+                maxSt.pop_back();
+            }
+            minSt.push_back(i);
+            maxSt.push_back(i);
+        }
+        
+        long long ans = 0;
+        for (int i=0; i<n; ++i) {
+            long long totalMax = nH2(max_right[i]-max_left[i]+1);
+            long long rightDupMax = nH2(max_right[i]-i);
+            long long leftDupMax = nH2(i-max_left[i]);
+            
+            long long totalMin = nH2(min_right[i]-min_left[i]+1);
+            long long rightDupMin = nH2(min_right[i]-i);
+            long long leftDupMin = nH2(i-min_left[i]);
+
+            
+            ans += (totalMax-rightDupMax-leftDupMax) * nums[i];
+            ans -= (totalMin-rightDupMin-leftDupMin) * nums[i];
+
+        }
+
+        
         return ans;
     }
 };
