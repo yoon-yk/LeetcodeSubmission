945a4118d4e39facf9cc84b8a835f27bca88fd7d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-05-28 11:01:00 +0900

    Time: 19 ms (64.67%), Space: 11.4 MB (29.34%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int stoneGameII(vector<int>& piles) {
+        // a + a + x = sum(piles)
+        // 2a + x = sum(piles)
+        // 2a = sum(piles) - x
+        // a = (sum(piles)-x)/2
+        
         int n = piles.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2*n, vector<int>(2, -1)));
-        int rest = 0;
-        for (int & p : piles) rest += p;
-        return solve(piles, 0, 1, rest, true, dp);
+        int sum = accumulate(piles.begin(), piles.end(), 0);
+        vector<vector<int>> dp(n, vector<int> (2*n, -1));
+        return (sum+solve(piles, 0, 1, dp))/2;
     }
     
-    int solve(vector<int>& piles, int idx, int m, int rest, bool turn, vector<vector<vector<int>>>& dp) {
+    int solve(vector<int>& piles, int idx, int m, vector<vector<int>>& dp) {
         if (idx >= piles.size()) {
             return 0;
         }
         
-        if (dp[idx][m][turn] != -1) {
-            return dp[idx][m][turn];
+        if (dp[idx][m] != -1) {
+            return dp[idx][m];
         }
         
-        int ans = 0, sum = 0;
+        int ans = INT_MIN, sum = 0;
         
-        // 내가 여기서 얻을 수 있는 최대의 값
-        // 내가 이번에서 얻는 최대 sum + (나머지 - 상대가 얻을 수 있는 최대 값)
+        // 내가 여기서 얻을 수 있는 최대 선택
+        // max(내가 이번에서 얻는 sum - 상대가 얻을 수 있는 최대 값)
         for (int i=0; i<2*m; ++i) {
             if (idx+i >= piles.size()) break;
             sum += piles[idx+i];
-            rest -= piles[idx+i];
-            ans = max(ans, sum + rest - solve(piles, idx+i+1, max(i+1, m), rest, !turn, dp));
+            ans = max(ans, sum - solve(piles, idx+i+1, max(i+1, m), dp));
         }
-        return dp[idx][m][turn] = ans;
+        return dp[idx][m] = ans;
     }
 };
