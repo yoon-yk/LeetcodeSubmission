9f45de88b2e0ba621337c75c5f3a1732b92dcb3d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-05-28 21:05:55 +0900

    Time: 285 ms (44.09%), Space: 137.6 MB (40.08%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,38 +2,34 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        double sum = 0;
-        for (int & p : piles) sum += p;
-        double ret = solve(piles, 0, sum, true, dp);
+        vector<int> dp(n, -1);
+        double ret = solve(piles, 0, dp);
         // cout << ret << " " << (sum/2) << endl;
-        if (ret > (double)(sum/2)) {
+        if (ret > 0) {
             return "Alice";
-        } else if (ret == (double)(sum/2)) {
+        } else if (ret == 0) {
             return "Tie";
         } return "Bob";
     }
     
-    int solve(vector<int>& piles, int idx, int rest, bool turn, vector<vector<int>>& dp) {
+    int solve(vector<int>& piles, int idx, vector<int>& dp) {
         if (idx >= piles.size()) {
             return 0;
         }
         
-        if (dp[idx][turn] != -1) {
-            return dp[idx][turn];
+        if (dp[idx] != -1) {
+            return dp[idx];
         }
         
         int ans = INT_MIN, sum = 0;
         
-        // 내가 여기서 얻을 수 있는 최대의 값
-        // 내가 이번에서 얻는 최대 sum + (나머지 - 상대가 얻을 수 있는 최대 값)
+        // 내가 여기서 얻을 수 있는 최대 선택
+        // max(내가 이번에서 얻는 sum - 상대가 얻을 수 있는 최대 값)
         for (int i=0; i<3; ++i) {
             if (idx+i >= piles.size()) break;
             sum += piles[idx+i];
-            rest -= piles[idx+i];
-            ans = max(ans, sum + rest - solve(piles, idx+i+1, rest, !turn, dp));
+            ans = max(ans, sum - solve(piles, idx+i+1, dp));
         }
-        
-        return dp[idx][turn] = ans;
+        return dp[idx] = ans;
     }
 };
