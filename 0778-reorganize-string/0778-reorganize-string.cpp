a0563632fce437cf77b8efa7a19764ea3ea27f26
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-08-23 20:50:59 +0900

    Time: 0 ms (100%), Space: 6.4 MB (21.22%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -5,34 +5,31 @@ public:
         for (auto & c : s) {
             ++count[c-'a'];
         }
-        priority_queue<pair<int, int>> pq;
+        vector<pair<int, int>> v;
         for (int i=0; i<26; ++i) {
             if (count[i] == 0) continue;
-            pq.push({count[i], i});
+            v.push_back({count[i], i});
         }
 
-        if (pq.top().first > ((s.size()+1)/2)) return "";
+        sort(v.begin(), v.end(), greater());
+
+        if (v[0].first > ((s.size()+1)/2)) return "";
 
         queue<pair<int, int>> Q;
-        while (!pq.empty()) {
-            auto [cnt, ch] = pq.top(); pq.pop();
+        for (auto & [cnt, ch] : v) {
             Q.push({ch, cnt});
         }
 
-        string str(s.size(), '*');
         int idx = 0;
         while (!Q.empty()) {
           auto [ch, cnt] = Q.front(); Q.pop();
-        //   cout << (char)(ch + 'a') << ':' << cnt << endl;
-
           while (cnt--) {
-            str[idx] = ch + 'a';
+            s[idx] = ch + 'a';
             idx = (idx + 2);
-            if (idx >= str.size()) idx = 1;
+            if (idx >= s.size()) idx = 1;
           }
-        //   cout << str << endl;
         }
 
-        return str;
+        return s;
     }
 };
