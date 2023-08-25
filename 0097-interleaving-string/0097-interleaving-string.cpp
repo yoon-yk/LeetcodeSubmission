ecf1510f24779367dbd7536b895544fbe81f148d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-08-26 01:48:50 +0900

    Time: 5 ms (61.24%), Space: 7.1 MB (50.41%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int len1 = s1.size(), len2 = s2.size();
-        if (s3.size() != len1 + len2) return false;
-        if (len1 == 0 && len2 == 0 && s3.size() == 0) return true;
-        
-        vector<vector<int>> dp(len1+1, vector<int>(len2+1, false));
-        for (int i=len1; i>=0; --i) {
-            for (int j=len2; j>=0; --j) {
-                int l3 = i+j;
-                if (i == len1 && j == len2) 
-                    dp[i][j] = true;
-
-                if (j < len2 && s3[l3] == s2[j]) 
-                    dp[i][j] |= dp[i][j+1];
                 
-                if (i < len1 && s3[l3] == s1[i]) 
-                    dp[i][j] |= dp[i+1][j];
-            }
-        }        
+        int len1 = s1.size(), len2 = s2.size();
+        vector<vector<int>> dp(len1+1, vector<int>(len2+1, -1));
+        return backtrack(s1, s2, s3, 0, 0, dp);
+    }
+    
+    bool backtrack(string& s1, string& s2, string& s3, int idx1, int idx2, vector<vector<int>>& dp) {
         
-        return dp[0][0];
+        int idx3 = idx1 + idx2;
+        if (idx3 == s3.size()) {
+            return (idx1 == s1.size() && idx2 == s2.size());
+        }
+        
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+        bool ans = false;
+        // s1 끼우기 (하나씩!)
+        if (idx1 < s1.size() && s1[idx1] == s3[idx3]) {
+            ans |= backtrack(s1, s2, s3, idx1+1, idx2, dp);
+        }
+        
+        // s2 끼우기 (하나씩!)
+        if (idx2 < s2.size() && s2[idx2] == s3[idx3]) {
+            ans |= backtrack(s1, s2, s3, idx1, idx2+1, dp);
+        }
+        
+        return dp[idx1][idx2] = ans;
     }
 };
