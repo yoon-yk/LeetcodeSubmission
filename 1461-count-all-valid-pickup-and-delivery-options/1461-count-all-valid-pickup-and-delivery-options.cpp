e0583086771b3553c6a957a0ba739a367b246af6
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-09-10 23:40:17 +0900

    Time: 0 ms (100%), Space: 6 MB (71.3%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -5,11 +5,12 @@ public:
     int countOrders(int n) {
         long long ans = 1;
         /*
+        nPn / 2^n
         (2 * n)! / (2^n)
         */
         for (int i = 1; i <= n; ++i) {
             
-            ans = (ans * (i % MOD) /* ((2*i)/2)% MOD == i % MOD */ 
+            ans = ((ans * i) % MOD /* ((2*i)/2) == i */ 
                     * ((2 * i) - 1) % MOD) % MOD;
         }
         return (int) ans;
