c26dc6d3b8a9497d5591ef35207fdba61ff2ad00
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-09-17 15:47:03 +0900

    Time: 14 ms (84.42%), Space: 9.3 MB (82.74%) - LeetHub

---
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n = graph.size();
+        int target = (1 << n) - 1; // All nodes visited
+        queue<pair<int, int>> Q; // <node, visited state>
+        vector<vector<bool>> visited(n, vector<bool>(1 << n, false));
+        
+        for (int i = 0; i < n; ++i) {
+            Q.push({i, 1 << i});
+            visited[i][1 << i] = true;
+        }
+        
+        int steps = 0;
+        while (!Q.empty()) {
+            int qSize = Q.size();
+            
+            while (qSize--) {
+                auto [node, state] = Q.front();
+                Q.pop();
+                
+                // If we visited all nodes, return steps
+                if (state == target) return steps;
+                
+                for (const int& nei : graph[node]) {
+                    int newState = state | (1 << nei);
+                    
+                    if (!visited[nei][newState]) {
+                        visited[nei][newState] = true;
+                        Q.push({nei, newState});
+                    }
+                }
+            }
+            
+            ++steps;
+        }
+        
+        return -1; // Unreachable
+    }
+};
