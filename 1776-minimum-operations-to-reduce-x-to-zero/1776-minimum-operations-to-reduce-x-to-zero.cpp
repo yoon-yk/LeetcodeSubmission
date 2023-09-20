ee2c6a045304acef09e1dbd54178520ea3a7af64
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-09-20 09:43:03 +0900

    Time: 373 ms (14.55%), Space: 168.5 MB (8.36%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -14,19 +14,12 @@ public:
             leftSums[leftSum] = i;
         }
 
-        // for (auto & [n, id] : leftSums) {
-        //     cout << n << '_' << id << endl;
-        // }
-
         int rightSum = 0;
         for (int i=nums.size()-1; i>=0; --i) {
             rightSum += nums[i];
-            // cout << rightSum << endl;
             if (leftSums.count(x-rightSum)) {
                 if (leftSums[x-rightSum] >= i) continue;
-                // cout << "found " << x-rightSum << " at " << leftSums[x-rightSum] << endl;
                 ans = min(ans, (int)nums.size()-i + leftSums[x-rightSum] +  1);
-                // cout << "ans updated to " << ans << "(" << nums.size()-i << "+" << leftSums[x-rightSum]<< " + " << 1 << ")" << endl; 
             }
         }
 
