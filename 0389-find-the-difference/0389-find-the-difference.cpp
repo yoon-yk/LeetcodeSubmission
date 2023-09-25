4752b8240465b83dcd21f1ce8949590d825553df
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-09-25 09:42:17 +0900

    Time: 0 ms (100%), Space: 6.9 MB (25.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
+    char findTheDifference(string& s, string& t) {
         vector<int> mp(26, 0);
         for (int i=0; i<s.size(); ++i) {
             ++mp[s[i]-'a'];
