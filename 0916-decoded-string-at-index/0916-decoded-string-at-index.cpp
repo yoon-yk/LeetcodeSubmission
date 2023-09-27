7c0fa9150b6795441d70b2f08e7619a7b7d2ec79
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-09-27 18:17:44 +0900

    Time: 3 ms (9.11%), Space: 6.3 MB (16.05%) - LeetHub

---
+++ 0916-decoded-string-at-index/0916-decoded-string-at-index.cpp
@@ -0,0 +1,44 @@
+// not on my own
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long size = 0; // final size
+        int n = s.size();
+
+        /*
+        Forward Traversal:
+        
+        Traverse the string from start to end, updating the length based on whether you encounter a letter or a digit.
+
+        */
+        for (int i=0; i<n; ++i) {
+            if ('a' <= s[i] && s[i] <= 'z') {
+                ++size;
+            } else {
+                size *= s[i] - '0';
+            }
+        }
+
+        /*
+        Reverse Traversal:
+
+        Start from the end of the string. 
+        */
+
+        for (int i=n-1; i>=0; --i) {
+            char & ch = s[i];
+            if (isdigit(ch)) {
+                // If you encounter a digit, divide the length by the digit and update k using modulo operation. 
+                size /= ch-'0';
+            } else {
+                // If you encounter a letter and k equals 0 or length, return the letter.
+                if (k == 0 || k == size)
+                    return string(1, ch);
+                --size;
+            }
+                            k %= size;
+
+        }
+        return "";
+    }
+};
