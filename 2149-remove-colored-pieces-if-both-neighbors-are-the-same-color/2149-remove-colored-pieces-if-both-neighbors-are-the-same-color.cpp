0482a472c5b3e73e994cbdf854464783a735643d
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-10-03 04:52:32 +0900

    Time: 26 ms (96.45%), Space: 13.6 MB (17.87%) - LeetHub

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool winnerOfGame(string c) {
+        int a = 0, b = 0;
+        int n = c.size();
+        for (int i=1; i < n-1; ++i) {
+            if (c[i-1] != c[i] || c[i] != c[i+1]) continue;
+            if (c[i] == 'A') ++a;
+            else ++b;
+        }
+        return a-b > 0;
+    }
+};
