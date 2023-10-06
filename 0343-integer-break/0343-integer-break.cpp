835fd1aabcd114c639457d76dc9bedb8b4b4d4a3
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-10-07 07:22:20 +0900

    Time: 0 ms (100%), Space: 6.3 MB (54.22%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int f(int n, vector<int>& dp) {
+
+        if (dp[n] != -1) return dp[n];
+        // cout << n << endl;
+
+        int ans = n;
+        for (int i=1; i<=(n+1)/2; ++i) {
+            ans = max(ans, f(i, dp) * f(n-i, dp));
+        }
+        return dp[n] = ans; 
+    }
+
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        vector<int> dp(60, -1);
+        dp[0] = 0, dp[1] = 1;
+        return f(n, dp);
+    }
+};
