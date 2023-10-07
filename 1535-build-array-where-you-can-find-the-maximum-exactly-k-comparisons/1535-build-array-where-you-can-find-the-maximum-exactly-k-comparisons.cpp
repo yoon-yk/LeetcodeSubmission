827e11157bcf4cadbbb7e458713ada500cd678de
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-10-08 05:03:56 +0900

    Time: 216 ms (16.82%), Space: 10.6 MB (18.18%) - LeetHub

---
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int N, M, K;
+    const int MOD = 1e9 + 7;
+    int f(int idx, int localMax, int remaining, vector<vector<vector<int>>> &dp) {
+        if (idx == N) {
+            return !remaining;
+        }
+
+        if (remaining < 0) return 0;
+
+        if (dp[idx][localMax][remaining] != -1)
+            return dp[idx][localMax][remaining];
+
+        int ans = 0;
+
+        // no counting 
+        for (int num = 1; num <= localMax; ++num)
+            ans = (ans + f(idx+1, localMax, remaining, dp)) % MOD;
+
+        // counting since it's bigger than the local Max
+        for (int num = localMax+1; num <= M; ++num)
+            ans = (ans + f(idx+1, num, remaining-1, dp)) % MOD;
+
+        return dp[idx][localMax][remaining] = ans;
+    }
+
+    int numOfArrays(int n, int m, int k) {
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m+1, vector<int>(k+1, -1)));
+        N = n, M = m, K = k;
+        return f(0, 0, k, dp);
+    }
+};
