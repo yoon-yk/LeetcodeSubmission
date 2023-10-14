07222a18c9d01f4cec652aa70043f1f854858aff
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-10-15 02:50:02 +0900

    Time: 171 ms (42.44%), Space: 115.7 MB (45.71%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
+    // The longer the paid painter paints, 
+    // the more we can make use of the free painter. 
+
     int paintWalls(vector<int>& cost, vector<int>& t) {
         int n = cost.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1)); // idx, timeLefting t
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1)); // idx, remainingWalls
         return solve(0, n, cost, t, dp);
     }
     
-    int solve(int i, int timeLeft, vector<int>& cost, vector<int>& t, vector<vector<int>>& dp) { 
-        if (timeLeft <= 0) {
+    int solve(int i, int remainingWalls, vector<int>& cost, vector<int>& t, vector<vector<int>>& dp) { 
+        
+        // painted all 
+        if (remainingWalls <= 0) {
             return 0;
         }
         
+        // no painters left
         if (i == cost.size()) {
             return 1e9;
         }
         
-        if (dp[i][timeLeft] != -1) {
-            return dp[i][timeLeft];
+        if (dp[i][remainingWalls] != -1) {
+            return dp[i][remainingWalls];
         }
         
-        int paint = cost[i] + solve(i + 1, timeLeft - 1 - t[i], cost, t, dp);
-        int dontPaint = solve(i + 1, timeLeft, cost, t, dp);
+        // t[i] + 1 == # of walls (free painter paints + cur painter paints)
+        int paint = cost[i] + solve(i + 1, remainingWalls - 1 - t[i], cost, t, dp); 
+
+        // skip; let a free painter paint it 
+        int dontPaint = solve(i + 1, remainingWalls, cost, t, dp);
     
-        return dp[i][timeLeft] = min(paint, dontPaint);
+        return dp[i][remainingWalls] = min(paint, dontPaint);
     }
 };
