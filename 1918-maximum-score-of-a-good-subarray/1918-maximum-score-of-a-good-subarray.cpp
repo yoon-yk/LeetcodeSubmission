360f79569ec83a7141536b93b34a1dce78503877
Yoon <63157606+yoon-yk@users.noreply.github.com>
2023-10-23 01:01:31 +0900

    Time: 188 ms (45.39%), Space: 101 MB (25.44%) - LeetHub

--- 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -5,6 +5,14 @@ public:
 
         vector<int> left(n, -1), right(n, n), st;
 
+        /*
+                       *
+            [ 1, 4, 3, 7, 4, 5]
+        left[ X, 0, 0, 2, 2, 4]
+        righ[ X, 2, X, 4, X, X]
+        mulp[ 5, 4,15, 7,12, 5]
+        
+        */
         for (int i=n-1; i>=0; --i) {
             while (!st.empty() && nums[st.back()] > nums[i]) {
                 // idx of the smaller & 
@@ -31,7 +39,7 @@ public:
         int ans = 0;
         for (int i=0; i<n; ++i) {
             if (left[i] >= k || right[i] <= k) continue;
-            ans = max(ans, nums[i] * (right[i]-left[i]-1));
+            ans = max(ans, nums[i] * ((right[i]-1)-(left[i]+1)+1));
         }
         return ans;
     }
