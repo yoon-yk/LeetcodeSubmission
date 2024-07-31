79d449d4be1438f6c751acbe36f265e274a731ab
Yoon <63157606+yoon-yk@users.noreply.github.com>
2024-08-01 00:05:34 +0900

    Time: 10 ms (8.82%), Space: 12.1 MB (6.86%) - LeetHub

--- 0681-next-closest-time/0681-next-closest-time.cpp
+++ 0681-next-closest-time/0681-next-closest-time.cpp
@@ -1,46 +1,49 @@
 class Solution {
 public:
-    bool isValid(const string& time) const {
-        int hour = stoi(time.substr(0, 2));
-        int min = stoi(time.substr(2, 2));
-        return hour >= 0 && hour < 24 && min >= 0 && min < 60;
+    bool isValidTime(const std::string& time) const {
+        int hour = std::stoi(time.substr(0, 2));
+        int minute = std::stoi(time.substr(2, 2));
+        return hour >= 0 && hour < 24 && minute >= 0 && minute < 60;
     }
 
-    void produce(const string& digits, string& curTime, const string& target, string& ans) {
-        if (!ans.empty()) return;
-
-        if (curTime.size() == 4) {
-            if (isValid(curTime) && (stoi(target) < stoi(curTime))) {
-                ans = curTime;
+    std::string getNextTime(const std::string& digits, const std::string& currentTime) {
+        std::string nextTime = currentTime;
+        while (true) {
+            incrementTime(nextTime);
+            if (isValidTime(nextTime) && isTimeComposedOfDigits(nextTime, digits)) {
+                return nextTime;
             }
-            return;
-        }
-
-        for (int i=0; i<digits.size(); ++i) {
-            if (i > 0 && digits[i-1] == digits[i]) continue;
-            curTime.push_back(digits[i]);
-            produce(digits, curTime, target, ans);
-            curTime.pop_back();
         }
     }
 
-    string getForm(const string& ans) const {
-        return ans.substr(0, 2) + ":" + ans.substr(2, 2);
+    void incrementTime(std::string& time) {
+        int hour = std::stoi(time.substr(0, 2));
+        int minute = std::stoi(time.substr(2, 2));
+        minute++;
+        if (minute == 60) {
+            minute = 0;
+            hour++;
+            if (hour == 24) {
+                hour = 0;
+            }
+        }
+        time = (hour < 10 ? "0" : "") + std::to_string(hour) + (minute < 10 ? "0" : "") + std::to_string(minute);
     }
 
-    string getSmallest(char& a) const {
-        return getForm(string(4, a));
+    bool isTimeComposedOfDigits(const std::string& time, const std::string& digits) const {
+        for (char ch : time) {
+            if (digits.find(ch) == std::string::npos) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    string nextClosestTime(string time) {
-        string digits = time.substr(0, 2) + time.substr(3, 2);
-        time = digits;
-        sort(digits.begin(), digits.end());
-
-        string curTime, ans;
-        produce(digits, curTime, time, ans);
-
-        if (ans.empty()) return getSmallest(digits[0]);
-        return getForm(ans);
+    std::string nextClosestTime(std::string time) {
+        std::string digits = time.substr(0, 2) + time.substr(3, 2);
+        std::sort(digits.begin(), digits.end());
+        std::string currentTime = time.substr(0, 2) + time.substr(3, 2);
+        std::string nextTime = getNextTime(digits, currentTime);
+        return nextTime.substr(0, 2) + ":" + nextTime.substr(2, 2);
     }
 };
